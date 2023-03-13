4ad62c5e536b16527d5659d490f381588096ca04
scott-robertson1 <127202807+scott-robertson1@users.noreply.github.com>
2023-03-13 15:42:19 +0000

    DMP-26 run of the init.sh script
    initial run of the init.sh script and notification in the slack channel  darts-builds from the jenkinsfile_cnp.

--- src/integrationTest/java/uk/gov/hmcts/reform/darts-api/config/SwaggerPublisherTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/darts-api/config/SwaggerPublisherTest.java
@@ -1,4 +1,4 @@
-package uk.gov.hmcts.reform.demo.config;
+package uk.gov.hmcts.reform.darts-api.config;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
--- src/smokeTest/java/uk/gov/hmcts/reform/darts-api/controllers/SampleSmokeTest.java
+++ src/smokeTest/java/uk/gov/hmcts/reform/darts-api/controllers/SampleSmokeTest.java
@@ -1,4 +1,4 @@
-package uk.gov.hmcts.reform.demo.controllers;
+package uk.gov.hmcts.reform.darts-api.controllers;
 
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
