c0a9cf985e413f290bdc6c91d2e46d32303ab676
Jack Maloney <jack.maloney1@hmcts.net>
2023-03-15 10:04:47 +0000

    Remove deployment_namespace

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -10,13 +10,6 @@ variable "env" {}
 
 variable "subscription" {}
 
-variable "deployment_namespace" {}
-
 variable "common_tags" {
   type = map(string)
-}
-
-variable "appinsights_location" {
-  default     = "West Europe"
-  description = "Location for Application Insights"
 }
