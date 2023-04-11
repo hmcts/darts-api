99a5f26f70d19cb49be51d330f7fedc2b670bc1f
scott-robertson1 <127202807+scott-robertson1@users.noreply.github.com>
2023-04-11 15:48:31 +0100

    Update inputs-required.tf

--- infrastructure/inputs-required.tf
+++ infrastructure/inputs-required.tf
@@ -8,30 +8,30 @@
 #   type        = map(string)
 # }
 
-variable "pgsql_databases" {
-  description = "Databases for the pgsql instance."
-  type        = list(object({ name : string, collation : optional(string), charset : optional(string) }))
-}
+# variable "pgsql_databases" {
+#   description = "Databases for the pgsql instance."
+#   type        = list(object({ name : string, collation : optional(string), charset : optional(string) }))
+# }
 
-variable "pgsql_delegated_subnet_id" {
-  description = "PGSql delegated subnet id."
-  type        = string
-  default     = ""
-}
+# variable "pgsql_delegated_subnet_id" {
+#   description = "PGSql delegated subnet id."
+#   type        = string
+#   default     = ""
+# }
 
-variable "pgsql_version" {
-  description = "The PGSql flexible server instance version."
-  type        = string
-}
+# variable "pgsql_version" {
+#   description = "The PGSql flexible server instance version."
+#   type        = string
+# }
 
 # variable "product" {
 #   description = "https://hmcts.github.io/glossary/#product"
 #   type        = string
 # }
 
-variable "business_area" {
-  description = "business_area name - sds or cft."
-}
+# variable "business_area" {
+#   description = "business_area name - sds or cft."
+# }
 
 # variable "component" {
 #   description = "https://hmcts.github.io/glossary/#component"
