f59686d2be1320608fbd751a53ec349910352f92
scott-robertson1 <127202807+scott-robertson1@users.noreply.github.com>
2023-04-11 08:57:38 +0100

    Update main.tf

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,10 +1,31 @@
+provider "azurerm" {
+  features {}
+}
+
+module "db" {
+  source             = "git@github.com:hmcts/cnp-module-postgres?ref=master"
+  product            = var.product
+  component          = var.component
+  name               = "rpe-${var.product}"
+  location           = var.location
+  env                = var.env
+  database_name      = "darts-modernisation"
+  postgresql_user    = "darts-modernisation"
+  postgresql_version = "10"
+  sku_name           = "GP_Gen5_2"
+  sku_tier           = "GeneralPurpose"
+  common_tags        = var.common_tags
+  subscription       = var.subscription
+}
+
+
 module "postgresql" {
 
   providers = {
     azurerm.postgres_network = azurerm.postgres_network
   }
   
-  source = "git@github.com:hmcts/darts-api?ref=master"
+  source = "git@github.com:hmcts/terraform-module-postgresql-flexible?ref=master"
   env    = var.env
 
   product       = var.product
