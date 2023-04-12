69dd0af65507cb61d5f9a77abaf38e3cb7db2f56
scott-robertson1 <127202807+scott-robertson1@users.noreply.github.com>
2023-04-12 11:36:29 +0100

    Update output.tf

--- infrastructure/output.tf
+++ infrastructure/output.tf
@@ -1,23 +1,23 @@
-output "resource_group_name" {
-  value = local.postgresql_rg_name
-}
+# output "resource_group_name" {
+#   value = local.postgresql_rg_name
+# }
 
-output "resource_group_location" {
-  value = local.postgresql_rg_location
-}
+# output "resource_group_location" {
+#   value = local.postgresql_rg_location
+# }
 
-output "username" {
-  value = azurerm_postgresql_flexible_server.pgsql_server.administrator_login
-}
+# output "username" {
+#   value = azurerm_postgresql_flexible_server.pgsql_server.administrator_login
+# }
 
-output "password" {
-  value = azurerm_postgresql_flexible_server.pgsql_server.administrator_password
-}
+# output "password" {
+#   value = azurerm_postgresql_flexible_server.pgsql_server.administrator_password
+# }
 
-output "fqdn" {
-  value = azurerm_postgresql_flexible_server.pgsql_server.fqdn
-}
+# output "fqdn" {
+#   value = azurerm_postgresql_flexible_server.pgsql_server.fqdn
+# }
 
-output "instance_id" {
-  value = azurerm_postgresql_flexible_server.pgsql_server.id
-}
+# output "instance_id" {
+#   value = azurerm_postgresql_flexible_server.pgsql_server.id
+# }
