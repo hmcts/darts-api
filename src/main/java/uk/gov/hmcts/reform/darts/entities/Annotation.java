4f36efd9774c132b025c4f73aad828d80258b09f
Richard Bull <richard.bull@hmcts.net>
2023-04-18 15:42:12 +0100

    DMP-44 Data Modelling with JPA mappings for MVP (darts entities)

---
+++ src/main/java/uk/gov/hmcts/reform/darts/entities/Annotation.java
@@ -0,0 +1,71 @@
+package uk.gov.hmcts.reform.darts.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+import jakarta.persistence.Version;
+import lombok.Data;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "moj_annotation")
+@Data
+public class Annotation {
+
+    @Id
+    @Column(name = "moj_ann_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @OneToOne
+    @JoinColumn(name = "moj_cas_id")
+    private Case theCase;
+
+    @Column(name = "r_annotation_object_id", length = 16)
+    private String legacyObjectId;
+
+    @Column(name = "c_text", length = 2000)
+    private String text;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "c_time_stamp")
+    private Date timestamp;
+
+    @Temporal(TemporalType.DATE)
+    @Column(name = "c_start")
+    private Date start;
+
+    @Temporal(TemporalType.DATE)
+    @Column(name = "c_end")
+    private Date end;
+
+    @Column(name = "c_courthouse", length = 64)
+    private String courthouse;
+
+    @Column(name = "c_courtroom", length = 64)
+    private String courtroom;
+
+    @Column(name = "c_reporting_restrictions")
+    private Integer reportingRestrictions;
+
+    @Column(name = "r_case_object_id", length = 16)
+    private String legacyCaseObjectId;
+
+    @Column(name = "r_version_label", length = 32)
+    private String legacyVersionLabel;
+
+    @Column(name = "i_superseded")
+    private Boolean superseded;
+
+    @Version
+    @Column(name = "i_version_label")
+    private Short version;
+}
---
+++ src/main/java/uk/gov/hmcts/reform/darts/entities/Media.java
@@ -0,0 +1,65 @@
+package uk.gov.hmcts.reform.darts.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+import jakarta.persistence.Version;
+import lombok.Data;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "moj_media")
+@Data
+public class Media {
+
+    @Id
+    @Column(name = "moj_med_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "r_media_object_id", length = 16)
+    private String legacyObjectId;
+
+    @Column(name = "c_channel")
+    private Integer channel;
+
+    @Column(name = "c_total_channels")
+    private Integer totalChannels;
+
+    @Column(name = "c_reference_id", length = 32)
+    private String referenceId;
+
+    @Temporal(TemporalType.DATE)
+    @Column(name = "c_start")
+    private Date start;
+
+    @Temporal(TemporalType.DATE)
+    @Column(name = "c_end")
+    private Date end;
+
+    @Column(name = "c_courthouse", length = 64)
+    private String courthouse;
+
+    @Column(name = "c_courtroom", length = 64)
+    private String courtroom;
+
+    @Column(name = "c_reporting_restrictions")
+    private Integer reportingRestrictions;
+
+    @Column(name = "r_version_label", length = 32)
+    private String legacyVersionLabel;
+
+    @Column(name = "i_superseded")
+    private Boolean superseded;
+
+    @Version
+    @Column(name = "i_version_label")
+    private Short version;
+
+}
