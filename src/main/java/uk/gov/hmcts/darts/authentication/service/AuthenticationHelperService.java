7dc2c738a9e0d8099532190d263ac7222881694b
hemanta.sharma <Hemanta.Sharma@HMCTS.net>
2023-04-19 16:50:24 +0100

    dmp-125 : Restructure of Java Package

---
+++ src/main/java/uk/gov/hmcts/darts/authentication/service/AuthenticationHelperService.java
@@ -0,0 +1,10 @@
+package uk.gov.hmcts.darts.authentication.service;
+
+import groovy.util.logging.Slf4j;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class AuthenticationHelperService {
+
+}
--- src/main/java/uk/gov/hmcts/reform/darts/services/authentication/AuthService.java
+++
@@ -1,7 +0,0 @@
-package uk.gov.hmcts.reform.darts.services.authentication;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class AuthService {
-}
