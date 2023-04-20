cff8b30f6ca8251dea7c2ccc0f486ece7db54e77
scott-robertson1 <127202807+scott-robertson1@users.noreply.github.com>
2023-04-20 08:50:01 +0100

    commenting out resources

--- infrastructure/tf-db-secrets.tf
+++ infrastructure/tf-db-secrets.tf
@@ -28,68 +28,64 @@ locals {
 
 
 ## Loop secrets
-module "secret" {
-  source              = "git@github.com:https://github.com/hmcts/cnp-module-key-vault"
-  for_each     = { for secret in local.secrets : secret.name_suffix => secret }
-  key_vault_id = data.azurerm_key_vault.kv.id
-  name         = "${local.secret_prefix}-${each.value.name_suffix}"
-  value        = each.value.value
-  tags = merge(var.common_tags, {
-    "source" : "${var.component} PostgreSQL"
-  })
-  content_type    = ""
-  expiration_date = timeadd(timestamp(), "8760h")
+# resource "azurerm_key_vault_secret" "secret" {
+#   for_each     = { for secret in local.secrets : secret.name_suffix => secret }
+#   key_vault_id = data.azurerm_key_vault.kv.id
+#   name         = "${local.secret_prefix}-${each.value.name_suffix}"
+#   value        = each.value.value
+#   tags = merge(var.common_tags, {
+#     "source" : "${var.component} PostgreSQL"
+#   })
+#   content_type    = ""
+#   expiration_date = timeadd(timestamp(), "8760h")
 
-  depends_on = [
-    module.database
-  ]
-}
+#   depends_on = [
+#     module.database
+#   ]
+# }
 
-module  "sdp-host" {
-  source              = "git@github.com:https://github.com/hmcts/cnp-module-key-vault"
-  key_vault_id = data.azurerm_key_vault.sdp-kv.id
-  name         = "${local.secret_prefix}-HOST"
-  value        = module.database.host_name
-  tags = merge(var.common_tags, {
-    "source" : "${var.component} PostgreSQL"
-  })
-  content_type    = ""
-  expiration_date = timeadd(timestamp(), "8760h")
+# resource "azurerm_key_vault_secret" "sdp-host" {
+#   key_vault_id = data.azurerm_key_vault.sdp-kv.id
+#   name         = "${local.secret_prefix}-HOST"
+#   value        = module.database.host_name
+#   tags = merge(var.common_tags, {
+#     "source" : "${var.component} PostgreSQL"
+#   })
+#   content_type    = ""
+#   expiration_date = timeadd(timestamp(), "8760h")
 
-  depends_on = [
-    module.database
-  ]
-}
+#   depends_on = [
+#     module.database
+#   ]
+# }
 
-module "sdp-port" {
-  source              = "git@github.com:https://github.com/hmcts/cnp-module-key-vault"
-  key_vault_id = data.azurerm_key_vault.sdp-kv.id
-  name         = "${local.secret_prefix}-PORT"
-  value        = module.database.postgresql_listen_port
-  tags = merge(var.common_tags, {
-    "source" : "${var.component} PostgreSQL"
-  })
-  content_type    = ""
-  expiration_date = timeadd(timestamp(), "8760h")
+# resource "azurerm_key_vault_secret" "sdp-port" {
+#   key_vault_id = data.azurerm_key_vault.sdp-kv.id
+#   name         = "${local.secret_prefix}-PORT"
+#   value        = module.database.postgresql_listen_port
+#   tags = merge(var.common_tags, {
+#     "source" : "${var.component} PostgreSQL"
+#   })
+#   content_type    = ""
+#   expiration_date = timeadd(timestamp(), "8760h")
 
-  depends_on = [
-    module.database
-  ]
-}
+#   depends_on = [
+#     module.database
+#   ]
+# }
 
-module "sdp-database" {
-  source              = "git@github.com:https://github.com/hmcts/terraform-module-postgresql-flexible"
-  key_vault_id = data.azurerm_key_vault.sdp-kv.id
-  name         = "${local.secret_prefix}-DATABASE"
-  value        = module.database.postgresql_database
-  tags = merge(var.common_tags, {
-    "source" : "${var.component} PostgreSQL"
-  })
-  content_type    = ""
-  expiration_date = timeadd(timestamp(), "8760h")
+# resource "azurerm_key_vault_secret" "sdp-database" {
+#   key_vault_id = data.azurerm_key_vault.sdp-kv.id
+#   name         = "${local.secret_prefix}-DATABASE"
+#   value        = module.database.postgresql_database
+#   tags = merge(var.common_tags, {
+#     "source" : "${var.component} PostgreSQL"
+#   })
+#   content_type    = ""
+#   expiration_date = timeadd(timestamp(), "8760h")
 
-  depends_on = [
-    module.database
-  ]
-}
+#   depends_on = [
+#     module.database
+#   ]
+# }
 
