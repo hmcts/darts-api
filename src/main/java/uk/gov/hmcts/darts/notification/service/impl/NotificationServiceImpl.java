2d8398bf0be11dc818500b305cbf83c061a733f1
Chris Magowan <chris.magowan@hmcts.net>
2023-04-26 17:18:11 +0100

    DMP-54 Add Notification table

---
+++ src/main/java/uk/gov/hmcts/darts/notification/service/impl/NotificationServiceImpl.java
@@ -0,0 +1,41 @@
+package uk.gov.hmcts.darts.notification.service.impl;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import uk.gov.hmcts.darts.notification.dto.CreateNotificationReq;
+import uk.gov.hmcts.darts.notification.entity.Notification;
+import uk.gov.hmcts.darts.notification.enums.NotificationStatus;
+import uk.gov.hmcts.darts.notification.repository.NotificationRepository;
+import uk.gov.hmcts.darts.notification.service.NotificationService;
+
+import java.sql.Timestamp;
+
+
+@RequiredArgsConstructor
+@Service
+public class NotificationServiceImpl implements NotificationService {
+
+    @Autowired
+    private NotificationRepository notificationRepo;
+
+    @Override
+    public Notification sendNotification(CreateNotificationReq createNotificationReq) {
+        Notification dbNotification = new Notification();
+        dbNotification.setEventId(createNotificationReq.getEventId());
+        dbNotification.setCaseId(createNotificationReq.getCaseId());
+        dbNotification.setEmailAddress(createNotificationReq.getEmailAddress());
+        dbNotification.setStatus(NotificationStatus.OPEN.name());
+        dbNotification.setAttempts(0);
+        dbNotification.setTemplateValues(createNotificationReq.getTemplateValues());
+        Timestamp now = new Timestamp(System.currentTimeMillis());
+        dbNotification.setCreatedDatetime(now);
+        dbNotification.setLastUpdatedDatetime(now);
+
+        Notification result = notificationRepo.saveAndFlush(dbNotification);
+        return result;
+
+
+    }
+
+}
---
+++ src/test/java/uk/gov/hmcts/darts/notification/service/CreateNotificationReqServiceTest.java
@@ -0,0 +1,31 @@
+package uk.gov.hmcts.darts.notification.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import uk.gov.hmcts.darts.notification.dto.CreateNotificationReq;
+import uk.gov.hmcts.darts.notification.entity.Notification;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+class CreateNotificationReqServiceTest {
+
+    @Autowired
+    NotificationService service;
+
+    @Test
+    void createNotification() {
+        CreateNotificationReq createNotificationReq = new CreateNotificationReq();
+        createNotificationReq.setEventId("An eventId");
+        createNotificationReq.setCaseId("A caseId");
+        createNotificationReq.setEmailAddress("test@test.com");
+        createNotificationReq.setTemplateValues("a json string");
+        Notification result = service.sendNotification(createNotificationReq);
+        assertEquals(1, result.getId());
+        assertEquals("OPEN", result.getStatus());
+        assertEquals("A caseId", result.getCaseId());
+    }
+
+
+}
