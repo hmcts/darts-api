d5d8b76c38b85d5fc0a8d5cea1a09aaa241788f6
Hemanta Sharma <hemanta.sharma1129@gmail.com>
2023-04-26 10:14:26 +0100

    dmp-104 : Add Azure AD Config Properties (#46)
    Co-authored-by: hemanta.sharma <Hemanta.Sharma@HMCTS.net>

--- src/test/java/uk/gov/hmcts/darts/authentication/controller/AuthenticationExternalUserControllerTest.java
+++ src/test/java/uk/gov/hmcts/darts/authentication/controller/AuthenticationExternalUserControllerTest.java
@@ -1,27 +1,40 @@
 package uk.gov.hmcts.darts.authentication.controller;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.web.servlet.ModelAndView;
+import uk.gov.hmcts.darts.authentication.model.OAuthProviderRawResponse;
+import uk.gov.hmcts.darts.authentication.service.AuthenticationService;
 
 import java.util.Objects;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.when;
 
+@ExtendWith(MockitoExtension.class)
 class AuthenticationExternalUserControllerTest {
 
 
-    private final AuthenticationExternalUserController authExternalUserController =
-        new AuthenticationExternalUserController();
+    @InjectMocks
+    private AuthenticationExternalUserController authExternalUserController;
+
+    @Mock
+    private AuthenticationService authenticationService;
 
     @Test
     void loginAndRefreshTokenFromAzureWhenTokenDoesntExistsInSession() {
+        when(authenticationService.getAuthorizationUrl()).thenReturn("Test");
         ModelAndView mv = authExternalUserController.loginOrRefresh();
         assertTrue(Objects.nonNull(mv), "The Model View is not null");
     }
 
     @Test
     void handleOauthCodeFromAzureWhenCodeIsReturned() {
-        ModelAndView mv = authExternalUserController.handleOauthCode("code", "state");
+        when(authenticationService.fetchAccessToken("code")).thenReturn(new OAuthProviderRawResponse());
+        ModelAndView mv = authExternalUserController.handleOauthCode("code");
         assertTrue(Objects.nonNull(mv), "The Model View is not null");
     }
 
--- src/test/java/uk/gov/hmcts/darts/authentication/controller/AuthenticationInternalUserControllerTest.java
+++ src/test/java/uk/gov/hmcts/darts/authentication/controller/AuthenticationInternalUserControllerTest.java
@@ -1,16 +1,27 @@
 package uk.gov.hmcts.darts.authentication.controller;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.web.servlet.ModelAndView;
+import uk.gov.hmcts.darts.authentication.model.OAuthProviderRawResponse;
+import uk.gov.hmcts.darts.authentication.service.AuthenticationService;
 
 import java.util.Objects;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.when;
 
+@ExtendWith(MockitoExtension.class)
 class AuthenticationInternalUserControllerTest {
 
-    private final AuthenticationInternalUserController authInternalUserController =
-        new AuthenticationInternalUserController();
+    @InjectMocks
+    private AuthenticationInternalUserController authInternalUserController;
+
+    @Mock
+    private AuthenticationService authenticationService;
 
     @Test
     void loginAndRefreshTokenFromAzureWhenTokenDoesntExistsInSession() {
@@ -20,7 +31,8 @@ class AuthenticationInternalUserControllerTest {
 
     @Test
     void handleOauthCodeFromAzureWhenCodeIsReturned() {
-        ModelAndView mv = authInternalUserController.handleOauthCode("code", "state");
+        when(authenticationService.fetchAccessToken("code")).thenReturn(new OAuthProviderRawResponse());
+        ModelAndView mv = authInternalUserController.handleOauthCode("code");
         assertTrue(Objects.nonNull(mv), "The Model View is not null");
     }
 
