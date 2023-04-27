3cfcbb18be7a0551d9867c313e703ee82f379648
Alan Coleman <boomfelled@gmail.com>
2023-04-27 13:11:43 +0100

    2nd Attempt - Variables added back in to RG

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -17,7 +17,7 @@ locals {
 # Resource Group
 
 resource "azurerm_resource_group" "rg" {
-  name     = var.resource_group_name
+  name     = "${var.product}-${var.component}-${var.env}"
   location = var.resource_group_location
   #tags     = var.resource_group_tags
 }
