565a056e8f049010a7948a8b713d52b8d3e189f4
scott-robertson1 <127202807+scott-robertson1@users.noreply.github.com>
2023-04-28 15:26:34 +0100

    Update main.tf

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -15,10 +15,9 @@ resource "azurerm_resource_group" "rg" {
 
 module "key-vault" {
   source              = "git@github.com:hmcts/cnp-module-key-vault?ref=master"
-  name                = "darts-db-${var.env}"
+  name                = "darts-${var.env}"
   product             = var.product
   env                 = var.env
-  tenant_id           = var.tenant_id
   object_id           = var.jenkins_AAD_objectId
   resource_group_name = azurerm_resource_group.rg.name
   product_group_name  = "DTS Darts Modernisation"
