6917b7c6b5c60b2d8f557b9481cbb393d3113b78
scott-robertson1 <127202807+scott-robertson1@users.noreply.github.com>
2023-04-28 13:58:44 +0100

    updating product_group_name

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -20,7 +20,7 @@ module "key-vault" {
   tenant_id           = var.tenant_id
   object_id           = var.jenkins_AAD_objectId
   resource_group_name = azurerm_resource_group.rg.name
-  product_group_name  = "dcd_ccd"
+  product_group_name  = "DTS Darts Modernisation"
   common_tags         = var.common_tags
   create_managed_identity    = true
 }
