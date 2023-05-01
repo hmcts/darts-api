752f255ec9cb761fdf2c6e277b39ab2eebc79b36
scott-robertson1 <127202807+scott-robertson1@users.noreply.github.com>
2023-05-01 14:18:18 +0100

    removing subnet

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -33,13 +33,13 @@ locals {
 
 }
 
-data "azurerm_subnet" "pg_subnet" {
-  name                 = "postgresql"
-  resource_group_name  = local.vnet_rg_name
-  virtual_network_name = local.vnet_name
+# data "azurerm_subnet" "pg_subnet" {
+#   name                 = "postgresql"
+#   resource_group_name  = local.vnet_rg_name
+#   virtual_network_name = local.vnet_name
 
-  count = var.pgsql_delegated_subnet_id == "" ? 1 : 0
-}
+#   count = var.pgsql_delegated_subnet_id == "" ? 1 : 0
+# }
 
 data "azurerm_client_config" "current" {}
  
