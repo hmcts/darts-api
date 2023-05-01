372cf3532b5a78ef0ac6cf03808122dc3308c1a5
scott-robertson1 <127202807+scott-robertson1@users.noreply.github.com>
2023-05-01 15:40:34 +0100

    Update main.tf

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -33,20 +33,151 @@ locals {
 
 }
 
+data "azurerm_subnet" "pg_subnet" {
+  name                 = "postgresql"
+  resource_group_name  = local.vnet_rg_name
+  virtual_network_name = local.vnet_name
+
+  count = var.pgsql_delegated_subnet_id == "" ? 1 : 0
+}
+
 data "azurerm_client_config" "current" {}
- 
+
 data "azuread_group" "db_admin" {
-  display_name     = "DTS Platform Operations"
+  display_name     = local.admin_group
   security_enabled = true
 }
 
 data "azuread_service_principal" "mi_name" {
   count     = var.enable_read_only_group_access ? 1 : 0
-  # application_id = 
   object_id = var.admin_user_object_id
 }
 
+module "password" {
+  source = "git@github.com:hmcts/terraform-module-postgresql-flexible?ref=master"
+  length = 20
+  # safer set of special characters for pasting in the shell
+  override_special = "()-_"
+}
 
+module"pgsql_server" {
+  source = "git@github.com:hmcts/terraform-module-postgresql-flexible?ref=master"
+  name                = local.server_name
+  resource_group_name = local.postgresql_rg_name
+  location            = local.postgresql_rg_location
+  version             = var.pgsql_version
+
+  create_mode                       = var.create_mode
+  point_in_time_restore_time_in_utc = var.restore_time
+  source_server_id                  = var.source_server_id
+
+  delegated_subnet_id = var.pgsql_delegated_subnet_id == "" ? data.azurerm_subnet.pg_subnet[0].id : var.pgsql_delegated_subnet_id
+  private_dns_zone_id = local.private_dns_zone_id
+
+  administrator_login    = var.pgsql_admin_username
+  administrator_password = random_password.password.result
+
+  storage_mb = var.pgsql_storage_mb
+
+  sku_name = var.pgsql_sku
+
+  authentication {
+    active_directory_auth_enabled = true
+    tenant_id                     = data.azurerm_client_config.current.tenant_id
+    password_auth_enabled         = true
+  }
+
+  tags = var.common_tags
+
+  dynamic "high_availability" {
+    for_each = local.high_availability != false ? [1] : []
+    content {
+      mode = "ZoneRedundant"
+    }
+  }
+
+  maintenance_window {
+    day_of_week  = "0"
+    start_hour   = "03"
+    start_minute = "00"
+  }
+
+  lifecycle {
+    ignore_changes = [
+      zone,
+      high_availability.0.standby_availability_zone,
+    ]
+  }
+
+  backup_retention_days        = var.backup_retention_days
+  geo_redundant_backup_enabled = var.geo_redundant_backups
+
+}
+
+module "pgsql_server_config" {
+  source = "git@github.com:hmcts/terraform-module-postgresql-flexible?ref=master"
+  for_each = {
+    for index, config in var.pgsql_server_configuration :
+    config.name => config
+  }
+
+  name      = each.value.name
+  server_id = azurerm_postgresql_flexible_server.pgsql_server.id
+  value     = each.value.value
+}
+
+module"pgsql_adadmin" {
+  source = "git@github.com:hmcts/terraform-module-postgresql-flexible?ref=master"
+  server_name         = azurerm_postgresql_flexible_server.pgsql_server.name
+  resource_group_name = azurerm_postgresql_flexible_server.pgsql_server.resource_group_name
+  tenant_id           = data.azurerm_client_config.current.tenant_id
+  object_id           = data.azuread_group.db_admin.object_id
+  principal_name      = local.admin_group
+  principal_type      = "Group"
+  depends_on = [
+    azurerm_postgresql_flexible_server.pgsql_server
+  ]
+}
+
+module "pgsql_principal_admin" {
+  source = "git@github.com:hmcts/terraform-module-postgresql-flexible?ref=master"
+  count               = var.enable_read_only_group_access ? 1 : 0
+  server_name         = azurerm_postgresql_flexible_server.pgsql_server.name
+  resource_group_name = azurerm_postgresql_flexible_server.pgsql_server.resource_group_name
+  tenant_id           = data.azurerm_client_config.current.tenant_id
+  object_id           = var.admin_user_object_id
+  principal_name      = data.azuread_service_principal.mi_name[0].display_name
+  principal_type      = "ServicePrincipal"
+  depends_on = [
+    azurerm_postgresql_flexible_server_active_directory_administrator.pgsql_adadmin
+  ]
+}
+
+module"set-user-permissions-additionaldbs" {
+  source = "git@github.com:hmcts/terraform-module-postgresql-flexible?ref=master"
+  for_each = var.enable_read_only_group_access ? { for index, db in var.pgsql_databases : db.name => db } : {}
+
+  triggers = {
+    script_hash    = filesha256("${path.module}/set-postgres-permissions.bash")
+    name           = local.name
+    db_reader_user = local.db_reader_user
+  }
+
+  provisioner "local-exec" {
+    command = "${path.module}/set-postgres-permissions.bash"
+
+    environment = {
+      DB_HOST_NAME   = azurerm_postgresql_flexible_server.pgsql_server.fqdn
+      DB_USER        = data.azuread_service_principal.mi_name[0].display_name
+      DB_READER_USER = local.db_reader_user
+      DB_NAME        = each.value.name
+    }
+  }
+  depends_on = [
+    azurerm_postgresql_flexible_server_active_directory_administrator.pgsql_principal_admin,
+    azurerm_postgresql_flexible_server_database.pg_databases
+  ]
+}
 
 resource "azurerm_resource_group" "rg" {
   name     = "${var.product}-shared-${var.env}"
