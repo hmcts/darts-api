c58f88f85ebcbb8390581dba12bf974b87577528
scott-robertson1 <127202807+scott-robertson1@users.noreply.github.com>
2023-05-01 09:52:09 +0100

    Update state.tf

--- infrastructure/state.tf
+++ infrastructure/state.tf
@@ -1,14 +1,25 @@
-terraform {
-  backend "azurerm" {}
+# terraform {
+#   backend "azurerm" {}
 
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 3.45.0"
-    }
-    azuread = {
-      source  = "hashicorp/azuread"
-      version = "1.6.0"
-    }
-  }
+#   required_providers {
+#     azurerm = {
+#       source  = "hashicorp/azurerm"
+#       version = "~> 3.45.0"
+#     }
+#     azuread = {
+#       source  = "hashicorp/azuread"
+#       version = "1.6.0"
+#     }
+#   }
+# }
+
+provider "azurerm" {
+  features {}
+}
+
+provider "azurerm" {
+  features {}
+  skip_provider_registration = true
+  alias                      = "postgres_network"
+  subscription_id            = var.aks_subscription_id
 }
