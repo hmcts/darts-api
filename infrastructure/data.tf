4fcd5d47485880ff00173f3def71feaf25fda93b
Alan Coleman <boomfelled@gmail.com>
2023-05-02 16:32:24 +0100

    Changes made with Dave to allow pipeline to run with referencing keys that don't yet exist

--- infrastructure/data.tf
+++ infrastructure/data.tf
@@ -1,25 +1,12 @@
 locals {
-
-  auto_secret_prefix  = "auto-${var.product}-${local.env}"
   resource_group_name = "${var.product}-${var.env}-rg"
-  key_vault_name      = "${var.product}-${var.env}-kv"
-  sdp_key_vault_name  = "${local.prefix}-sdp-kv-${var.env}"
+  key_vault_name      = "${var.product}-${var.env}"
 }
-
-data "azurerm_client_config" "current" {}
-
 
 data "azurerm_subnet" "iaas" {
   name                 = "iaas"
   resource_group_name  = "ss-${var.env}-network-rg"
   virtual_network_name = "ss-${var.env}-vnet"
-}
-
-# Azure user assigned identity
-
-data "azurerm_user_assigned_identity" "rpe-shared-identity" {
-  name                = "pre-${var.env}-mi"
-  resource_group_name = "managed-identities-${var.env}-rg"
 }
 
 data "azurerm_key_vault" "key_vault" {
