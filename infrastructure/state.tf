a1f579601aeaff7fe2ecf63aa313dd299b288aac
scott-robertson1 <127202807+scott-robertson1@users.noreply.github.com>
2023-05-02 16:01:50 +0100

    removing providers file

--- infrastructure/state.tf
+++ infrastructure/state.tf
@@ -1,17 +1,17 @@
 terraform {
-  backend "azurerm" {}
+   backend "azurerm" {}
 
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "3.54"
-    }
-    random = {
-      source = "hashicorp/random"
-    }
-    azuread = {
-      source  = "hashicorp/azuread"
-      version = "2.38.0"
-    }
-  }
-}
+   required_providers {
+     azurerm = {
+       source  = "hashicorp/azurerm"
+       version = "3.54"
+     }
+     random = {
+       source = "hashicorp/random"
+     }
+     azuread = {
+       source  = "hashicorp/azuread"
+       version = "2.38.0"
+     }
+   }
+ }
