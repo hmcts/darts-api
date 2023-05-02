d42d20e8ab5b93d568ebd79867e6f622926e4565
Chris Magowan <chris.magowan@hmcts.net>
2023-05-02 11:06:19 +0100

    DMP-122 implement notification service

--- src/main/java/uk/gov/hmcts/darts/notification/repository/NotificationRepository.java
+++ src/main/java/uk/gov/hmcts/darts/notification/repository/NotificationRepository.java
@@ -4,7 +4,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import uk.gov.hmcts.darts.notification.entity.Notification;
 
+import java.util.List;
+
 @Repository
 public interface NotificationRepository extends JpaRepository<Notification, Integer> {
+    List<Notification> findByCaseId(String caseId);
+
+    List<Notification> findByStatusIn(List<String> status);
 
 }
---
+++ src/test/java/uk/gov/hmcts/darts/notification/helper/TemplateIdHelperTest.java
@@ -0,0 +1,48 @@
+package uk.gov.hmcts.darts.notification.helper;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import uk.gov.hmcts.darts.notification.exception.TemplateNotFoundException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+@ActiveProfiles({"dev","test"})
+class TemplateIdHelperTest {
+
+    @Autowired
+    TemplateIdHelper templateIdHelper;
+
+    @Test
+    void okCourtManagerApproveTranscript() throws TemplateNotFoundException {
+        String templateId = templateIdHelper.findTemplateId("court_manager_approve_transcript");
+        assertEquals("2f9e18b5-5046-44c2-8abd-e4ff15008a60", templateId);
+    }
+
+    @Test
+    void okRequestedAudioIsAvailable() throws TemplateNotFoundException {
+        String templateId = templateIdHelper.findTemplateId("requested_audio_is_available");
+        assertEquals("66a1864f-24a6-469a-ac55-66bc57c7e4f6", templateId);
+    }
+
+    @Test
+    void okRequestToTranscriber() throws TemplateNotFoundException {
+        String templateId = templateIdHelper.findTemplateId("request_to_transcriber");
+        assertEquals("976bf288-705d-4cbb-b24f-c5529abf14cf", templateId);
+    }
+
+    @Test
+    void notFound() throws TemplateNotFoundException {
+
+        TemplateNotFoundException thrown = Assertions.assertThrows(TemplateNotFoundException.class, () -> {
+            templateIdHelper.findTemplateId("test");
+        });
+
+
+        assertEquals("Unable to find template with name 'test'.", thrown.getMessage());
+    }
+
+}
