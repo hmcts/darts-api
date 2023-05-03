4017a170a495cc7768769a2b178b352564a64fcb
scott-robertson1 <127202807+scott-robertson1@users.noreply.github.com>
2023-05-03 09:54:34 +0100

    Update main.tf

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,21 +1,73 @@
-variable "product" {}
-
-variable "component" {}
-
-variable "location" {
-  default = "UK South"
+provider "azurerm" {
+  features {}
 }
 
-variable "env" {
+provider "azurerm" {
+  features {}
+  skip_provider_registration = true
+  alias                      = "postgres_network"
+  subscription_id            = var.aks_subscription_id
+}
+
+locals {
+  vault_name                = "${var.product}-${var.env}"
+  rg_name                   = "${var.product}-${var.env}-rg"
+}
+
+ data "azurerm_resource_group" "rg" {
+   name     = local.rg_name
  }
 
- variable aks_subscription_id {
+ data "azurerm_subnet" "postgres" {
+   name                 = "iaas"
+   resource_group_name  = "ss-${var.env}-network-rg"
+   virtual_network_name = "ss-${var.env}-vnet"
  }
 
- variable "jenkins_AAD_objectId" {
-   description = "(Required) The Azure AD object ID of a user, service principal or security group in the Azure Active Directory tenant for the vault. The object ID must be unique for the list of access policies."
+ data "azurerm_key_vault" "key_vault" {
+   name                = local.vault_name
+   resource_group_name = local.rg_name
  }
 
- variable "common_tags" {
-   type = map(string)
+
+ resource "azurerm_key_vault_secret" "POSTGRES-USER" {
+   name         = "darts-api-POSTGRES-USER"
+   value        = module.postgresql_flexible.username
+   key_vault_id = data.azurerm_key_vault.key_vault.id
+ }
+
+ resource "azurerm_key_vault_secret" "POSTGRES-PASS" {
+   name         = "darts-api-POSTGRES-PASS"
+   value        = module.postgresql_flexible.password
+   key_vault_id = data.azurerm_key_vault.key_vault.id
+ }
+
+ resource "azurerm_key_vault_secret" "POSTGRES_HOST" {
+   name         = "darts-api-POSTGRES-HOST"
+   value        = module.postgresql_flexible.fqdn
+   key_vault_id = data.azurerm_key_vault.key_vault.id
+ }
+
+ module "postgresql_flexible" {
+     providers = {
+     azurerm.postgres_network = azurerm.postgres_network
+   }
+
+   source        = "git@github.com:hmcts/terraform-module-postgresql-flexible?ref=master"
+   env           = var.env
+   product       = var.product
+   name          = "${var.product}-v14-flexible"
+   component     = var.component
+   business_area = "sds"
+   location      = var.location
+
+   common_tags = var.common_tags
+   admin_user_object_id = var.jenkins_AAD_objectId
+   pgsql_databases = [
+     {
+       name : "darts"
+     }
+   ]
+
+   pgsql_version = "14"
  }
