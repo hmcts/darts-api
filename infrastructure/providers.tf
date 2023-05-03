b0cac98a30bc43f3d3900f61c5c381859ff9bd6e
Alan Coleman <boomfelled@gmail.com>
2023-05-03 16:30:40 +0100

    Terraform format after fetch

--- infrastructure/providers.tf
+++ infrastructure/providers.tf
@@ -1,19 +1,19 @@
 terraform {
-   backend "azurerm" {}
-   required_providers {
-     azurerm = {
-       source  = "hashicorp/azurerm"
-       version = "3.54"
-     }
-     random = {
-       source = "hashicorp/random"
-     }
-     azuread = {
-       source  = "hashicorp/azuread"
-       version = "2.38.0"
-     }
-   }
- }
+  backend "azurerm" {}
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "3.54"
+    }
+    random = {
+      source = "hashicorp/random"
+    }
+    azuread = {
+      source  = "hashicorp/azuread"
+      version = "2.38.0"
+    }
+  }
+}
 
 provider "azurerm" {
   features {}
