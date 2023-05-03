7532db84dbaf8ddf2c6aefffdb4061d4f41a6a24
scott-robertson1 <127202807+scott-robertson1@users.noreply.github.com>
2023-05-03 09:34:02 +0100

    removing unused variables

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -12,32 +12,10 @@ variable "env" {
  variable aks_subscription_id {
  }
 
- variable "subscription" {
- }
-
- variable "tenant_id" {}
-
  variable "jenkins_AAD_objectId" {
    description = "(Required) The Azure AD object ID of a user, service principal or security group in the Azure Active Directory tenant for the vault. The object ID must be unique for the list of access policies."
  }
 
- variable "capacity" {
-   default = "1"
- }
-  # thumbprint of the SSL certificate for API gateway tests
- variable api_gateway_test_certificate_thumbprint {
-   # keeping this empty by default, so that no thumbprint will match
-   default = ""
- }
-
- variable "autoheal" {
-   description = "Enabling Proactive Auto Heal for Webapps"
-   default     = "True"
- }
-
- variable "private_dns_subscription_id" {
-   default = "1baf5470-1c3e-40d3-a6f7-74bfbce4b348"
- }
  variable "common_tags" {
    type = map(string)
  }
