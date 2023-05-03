5ea161badf161de89c75564d5b61db85fd4c5baa
Richard Bull <richard.bull@hmcts.net>
2023-05-03 11:56:15 +0100

    DMP-117 Concatenate audio (#60)

--- src/main/java/uk/gov/hmcts/darts/Application.java
+++ src/main/java/uk/gov/hmcts/darts/Application.java
@@ -4,10 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.ComponentScan;
+import uk.gov.hmcts.darts.audio.config.AudioTransformConfigurationProperties;
 
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class, UserDetailsServiceAutoConfiguration.class})
 @ComponentScan({"uk.gov.hmcts.darts"})
+@EnableConfigurationProperties(AudioTransformConfigurationProperties.class)
 @SuppressWarnings("HideUtilityClassConstructor") // Spring needs a constructor, its not a utility class
 public class Application {
 
---
+++ src/main/java/uk/gov/hmcts/darts/audio/config/AudioTransformConfigurationProperties.java
@@ -0,0 +1,18 @@
+package uk.gov.hmcts.darts.audio.config;
+
+import jakarta.validation.constraints.NotEmpty;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties("audiotransform")
+@Getter
+@Setter
+public class AudioTransformConfigurationProperties {
+
+    @NotEmpty
+    private String ffmpegExecutable;
+    @NotEmpty
+    private String concatWorkspace;
+
+}
