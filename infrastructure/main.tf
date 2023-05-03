a292b20d903c4bc7b982606d51bff0bcfdb3d12e
scott-robertson1 <127202807+scott-robertson1@users.noreply.github.com>
2023-05-03 11:12:06 +0100

    Update main.tf

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -56,7 +56,7 @@ locals {
    source        = "git@github.com:hmcts/terraform-module-postgresql-flexible?ref=master"
    env           = var.env
    product       = var.product
-   resource_group_name = key_vault.resource_group_name
+   resource_group_name = local.rg_name
    component     = var.component
    business_area = "sds"
    location      = var.location
