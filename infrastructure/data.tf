41518a5d7a4a310e1225835224f76ebc628e02b1
Alan Coleman <boomfelled@gmail.com>
2023-05-03 10:58:25 +0100

    Updates to accomodate merge from Scott R

--- infrastructure/data.tf
+++
@@ -1,9 +0,0 @@
-locals {
-  resource_group_name = "${var.product}-${var.env}-rg"
-  key_vault_name      = "${var.product}-${var.env}"
-}
-
-data "azurerm_key_vault" "key_vault" {
-  name                = local.key_vault_name
-  resource_group_name = local.resource_group_name
-}
--- infrastructure/providers.tf
+++ infrastructure/providers.tf
@@ -1,27 +1,16 @@
 terraform {
-  required_version = ">= 1.0.4"
-  required_providers {
-    azurerm = {
-      version = "3.41.0"
-    }
-  }
-}
-
-provider "azurerm" {
-  features {}
-}
-backend "azurerm" {}
-
-required_providers {
-  azurerm = {
-    source  = "hashicorp/azurerm"
-    version = "3.54"
-  }
-  random = {
-    source = "hashicorp/random"
-  }
-  azuread = {
-    source  = "hashicorp/azuread"
-    version = "2.38.0"
-  }
-}
+   backend "azurerm" {}
+   required_providers {
+     azurerm = {
+       source  = "hashicorp/azurerm"
+       version = "3.54"
+     }
+     random = {
+       source = "hashicorp/random"
+     }
+     azuread = {
+       source  = "hashicorp/azuread"
+       version = "2.38.0"
+     }
+   }
+ }
