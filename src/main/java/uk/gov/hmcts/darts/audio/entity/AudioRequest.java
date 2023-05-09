4f48684c2639822236fb8946186c8cca440b30d4
garethmorriscgi <gareth.morris1@hmcts.net>
2023-05-09 16:25:44 +0100

    DMP-142 - checkstyle changes

--- src/main/java/uk/gov/hmcts/darts/audio/entity/AudioRequest.java
+++ src/main/java/uk/gov/hmcts/darts/audio/entity/AudioRequest.java
@@ -1,6 +1,11 @@
 package uk.gov.hmcts.darts.audio.entity;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
