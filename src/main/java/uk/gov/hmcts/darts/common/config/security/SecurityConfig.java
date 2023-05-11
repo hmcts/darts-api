5b3e0d2cccdbff90072e9d251e56a4b1ce5b2afb
chrisbellingham-hmcts <chris.bellingham@hmcts.net>
2023-05-11 11:59:10 +0100

    DMP-145: Enable oauth security for non-whitelisted endpoints

--- src/main/java/uk/gov/hmcts/darts/common/config/security/SecurityConfig.java
+++ src/main/java/uk/gov/hmcts/darts/common/config/security/SecurityConfig.java
@@ -1,6 +1,6 @@
 package uk.gov.hmcts.darts.common.config.security;
 
-import org.springframework.boot.context.properties.ConfigurationProperties;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
@@ -8,17 +8,20 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import uk.gov.hmcts.darts.authentication.component.UriProvider;
 
 @Configuration
 @EnableWebSecurity
-@ConfigurationProperties(prefix = "spring.security")
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final UriProvider uriProvider;
 
     @Bean
     @Order(1)
     @SuppressWarnings("PMD.SignatureDeclareThrowsException")
     public SecurityFilterChain patternFilterChain(HttpSecurity http) throws Exception {
-        http
+        applyCommonConfig(http)
             .securityMatcher(
                 "/swagger-ui.html",
                 "/swagger-ui/**",
@@ -30,11 +33,11 @@ public class SecurityConfig {
                 "/info",
                 "/metrics",
                 "/metrics/**",
+                "/external-user/login-or-refresh",
+                "/external-user/handle-oauth-code",
                 "/"
             )
-            .authorizeHttpRequests(authorize -> authorize.anyRequest()
-                .permitAll()
-            );
+            .authorizeHttpRequests().anyRequest().permitAll();
 
         return http.build();
     }
@@ -42,16 +45,21 @@ public class SecurityConfig {
     @Bean
     @SuppressWarnings({"PMD.SignatureDeclareThrowsException", "squid:S4502"})
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
-            .csrf().disable()
+        applyCommonConfig(http)
+            .authorizeHttpRequests().anyRequest().authenticated()
+            .and()
+            .oauth2Login().loginPage(uriProvider.getAuthorizationUri().toString());
+        return http.build();
+    }
+
+    @SuppressWarnings("PMD.SignatureDeclareThrowsException")
+    private HttpSecurity applyCommonConfig(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
             .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
             .and()
-            .httpBasic()
-            .and()
+            .csrf().disable()
             .formLogin().disable()
-            .logout().disable()
-            .authorizeHttpRequests(authorize -> authorize.anyRequest().permitAll());
-        return http.build();
+            .logout().disable();
     }
 
 }
