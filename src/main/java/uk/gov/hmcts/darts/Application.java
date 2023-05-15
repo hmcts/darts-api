cc393d04678a3bec6b766c84ad80dcf663d04157
Richard Bull <richard.bull@hmcts.net>
2023-05-15 10:49:54 +0100

    Removed `ffmpegShowVersionCommand` because this will fail the integration tests in the build pipeline

--- src/main/java/uk/gov/hmcts/darts/Application.java
+++ src/main/java/uk/gov/hmcts/darts/Application.java
@@ -1,20 +1,13 @@
 package uk.gov.hmcts.darts;
 
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.exec.CommandLine;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
-import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.event.EventListener;
 import uk.gov.hmcts.darts.audio.config.AudioTransformConfigurationProperties;
-import uk.gov.hmcts.darts.audio.util.AudioUtil;
-
-import java.util.concurrent.ExecutionException;
 
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class, UserDetailsServiceAutoConfiguration.class})
 @ComponentScan({"uk.gov.hmcts.darts"})
@@ -23,20 +16,8 @@ import java.util.concurrent.ExecutionException;
 @SuppressWarnings("HideUtilityClassConstructor") // Spring needs a constructor, its not a utility class
 public class Application {
 
-    @Autowired
-    private AudioTransformConfigurationProperties audioTransformConfigurationProperties;
-    @Autowired
-    private AudioUtil audioUtil;
-
     public static void main(final String[] args) {
         SpringApplication.run(Application.class, args);
-    }
-
-    @EventListener(ApplicationReadyEvent.class)
-    void ffmpegShowVersionCommand() throws ExecutionException, InterruptedException {
-        CommandLine command = new CommandLine(audioTransformConfigurationProperties.getFfmpegExecutable());
-        command.addArgument("-version");
-        audioUtil.execute(command);
     }
 
 }
