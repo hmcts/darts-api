e0fd3573ac119ad9c9737e78102cb90be6004949
Richard Bull <richard.bull@hmcts.net>
2023-05-15 11:00:59 +0100

    DMP-119 Audio re-encode (#92)

--- src/main/java/uk/gov/hmcts/darts/Application.java
+++ src/main/java/uk/gov/hmcts/darts/Application.java
@@ -1,15 +1,12 @@
 package uk.gov.hmcts.darts;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
-import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.event.EventListener;
 import uk.gov.hmcts.darts.audio.config.AudioTransformConfigurationProperties;
 
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class, UserDetailsServiceAutoConfiguration.class})
@@ -19,22 +16,8 @@ import uk.gov.hmcts.darts.audio.config.AudioTransformConfigurationProperties;
 @SuppressWarnings("HideUtilityClassConstructor") // Spring needs a constructor, its not a utility class
 public class Application {
 
-    @Autowired
-    private AudioTransformConfigurationProperties audioTransformConfigurationProperties;
-
     public static void main(final String[] args) {
         SpringApplication.run(Application.class, args);
-    }
-
-    @EventListener(ApplicationReadyEvent.class)
-    public void runAfterStartup() {
-        log.info(String.format(
-            "ATS ffmpegExecutable: %s, concatWorkspace: %s, mergeWorkspace: %s, trimWorkspace: %s",
-            audioTransformConfigurationProperties.getFfmpegExecutable(),
-            audioTransformConfigurationProperties.getConcatWorkspace(),
-            audioTransformConfigurationProperties.getMergeWorkspace(),
-            audioTransformConfigurationProperties.getTrimWorkspace()
-        ));
     }
 
 }
