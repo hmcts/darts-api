d4be6bbf8b3465ad5dc03e8bf7e5febace8a2c22
Dave Thompson <davethompson21@gmail.com>
2023-05-17 15:51:40 +0100

    DMP-140 Changes to fix DB migration running as part of the pipeline (#99)
    * DMP-140 Changes to fix DB migration running as part of the pipeline
    
    * Bumping chart version/ fixing aliases
    
    ---------
    
    Co-authored-by: hmcts-jenkins-cnp <60659747+hmcts-jenkins-cnp[bot]@users.noreply.github.com>

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -10,64 +10,78 @@ provider "azurerm" {
 }
 
 locals {
-  vault_name                = "${var.product}-${var.env}"
-  rg_name                   = "${var.product}-${var.env}-rg"
+  vault_name = "${var.product}-${var.env}"
+  rg_name    = "${var.product}-${var.env}-rg"
+  db_name    = "darts"
+  db_port    = 5432
 }
 
- data "azurerm_resource_group" "rg" {
-   name     = local.rg_name
- }
+data "azurerm_resource_group" "rg" {
+  name = local.rg_name
+}
 
- data "azurerm_subnet" "postgres" {
-   name                 = "iaas"
-   resource_group_name  = "ss-${var.env}-network-rg"
-   virtual_network_name = "ss-${var.env}-vnet"
- }
+data "azurerm_subnet" "postgres" {
+  name                 = "iaas"
+  resource_group_name  = "ss-${var.env}-network-rg"
+  virtual_network_name = "ss-${var.env}-vnet"
+}
 
- data "azurerm_key_vault" "key_vault" {
-   name                = local.vault_name
-   resource_group_name = local.rg_name
- }
+data "azurerm_key_vault" "key_vault" {
+  name                = local.vault_name
+  resource_group_name = local.rg_name
+}
 
 
- resource "azurerm_key_vault_secret" "POSTGRES-USER" {
-   name         = "darts-api-POSTGRES-USER"
-   value        = module.postgresql_flexible.username
-   key_vault_id = data.azurerm_key_vault.key_vault.id
- }
+resource "azurerm_key_vault_secret" "POSTGRES-USER" {
+  name         = "api-POSTGRES-USER"
+  value        = module.postgresql_flexible.username
+  key_vault_id = data.azurerm_key_vault.key_vault.id
+}
 
- resource "azurerm_key_vault_secret" "POSTGRES-PASS" {
-   name         = "darts-api-POSTGRES-PASS"
-   value        = module.postgresql_flexible.password
-   key_vault_id = data.azurerm_key_vault.key_vault.id
- }
+resource "azurerm_key_vault_secret" "POSTGRES-PASS" {
+  name         = "api-POSTGRES-PASS"
+  value        = module.postgresql_flexible.password
+  key_vault_id = data.azurerm_key_vault.key_vault.id
+}
 
- resource "azurerm_key_vault_secret" "POSTGRES_HOST" {
-   name         = "darts-api-POSTGRES-HOST"
-   value        = module.postgresql_flexible.fqdn
-   key_vault_id = data.azurerm_key_vault.key_vault.id
- }
+resource "azurerm_key_vault_secret" "POSTGRES_HOST" {
+  name         = "api-POSTGRES-HOST"
+  value        = module.postgresql_flexible.fqdn
+  key_vault_id = data.azurerm_key_vault.key_vault.id
+}
 
- module "postgresql_flexible" {
-     providers = {
-     azurerm.postgres_network = azurerm.postgres_network
-   }
+resource "azurerm_key_vault_secret" "POSTGRES_PORT" {
+  name         = "api-POSTGRES-PORT"
+  value        = local.db_port
+  key_vault_id = data.azurerm_key_vault.key_vault.id
+}
 
-   source        = "git@github.com:hmcts/terraform-module-postgresql-flexible?ref=master"
-   env           = var.env
-   product       = var.product
-   resource_group_name = local.rg_name
-   component     = var.component
-   business_area = "sds"
-   location      = var.location
+resource "azurerm_key_vault_secret" "POSTGRES_DATABASE" {
+  name         = "api-POSTGRES-DATABASE"
+  value        = local.db_name
+  key_vault_id = data.azurerm_key_vault.key_vault.id
+}
 
-   common_tags = var.common_tags
-   admin_user_object_id = var.jenkins_AAD_objectId
-   pgsql_databases = [
-     {
-       name : "darts"
-     }
-   ]
+module "postgresql_flexible" {
+  providers = {
+    azurerm.postgres_network = azurerm.postgres_network
+  }
 
-   pgsql_version = "14"
- }
+  source              = "git@github.com:hmcts/terraform-module-postgresql-flexible?ref=master"
+  env                 = var.env
+  product             = var.product
+  resource_group_name = local.rg_name
+  component           = var.component
+  business_area       = "sds"
+  location            = var.location
+
+  common_tags          = var.common_tags
+  admin_user_object_id = var.jenkins_AAD_objectId
+  pgsql_databases = [
+    {
+      name : local.db_name
+    }
+  ]
+
+  pgsql_version = "14"
+}
