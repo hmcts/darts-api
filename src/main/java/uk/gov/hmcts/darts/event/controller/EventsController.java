1598a62ed013087818053a3cc5ce0920677b6b9f
marcmollins <130585648+marcmollins@users.noreply.github.com>
2023-05-30 09:53:08 +0100

    DMP-161 Create REST Endpoint to receive Events request (#107)

---
+++ src/main/java/uk/gov/hmcts/darts/event/controller/EventsController.java
@@ -0,0 +1,58 @@
+package uk.gov.hmcts.darts.event.controller;
+
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import uk.gov.hmcts.darts.event.api.EventApi;
+import uk.gov.hmcts.darts.event.model.AddDocumentResponse;
+
+import java.math.BigDecimal;
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+
+@Slf4j
+@RestController
+public class EventsController implements EventApi {
+
+    @Operation(
+        operationId = "eventAddDocumentPost",
+        summary = "An Endpoint which allows users to request and submit events",
+        description = "Event data received from XHIBIT and CPP through a custom web service to add context to the audio recordings it stores.",
+        tags = { "Event" },
+        responses = {
+            @ApiResponse(responseCode = "200", description = "OK", content = {
+                @Content(mediaType = "application/json", schema = @Schema(implementation = AddDocumentResponse.class))
+            }),
+            @ApiResponse(responseCode = "500", description = "Internal Server Error")
+        }
+    )
+    @RequestMapping(
+        method = RequestMethod.POST,
+        value = "/event/addDocument",
+        produces = { "application/json" },
+        consumes = { "application/json" }
+    )
+    @Override
+    public ResponseEntity<AddDocumentResponse> eventAddDocumentPost(
+        @NotNull @Parameter(name = "message_id", description = "ID of message", required = true, in = ParameterIn.QUERY) @Valid @RequestParam(value = "message_id", required = true) BigDecimal messageId,
+        @NotNull @Parameter(name = "type", description = "The type of Event", required = true, in = ParameterIn.QUERY) @Valid @RequestParam(value = "type", required = true) String type,
+        @NotNull @Parameter(name = "sub_type", description = "The subtype of Event", required = true, in = ParameterIn.QUERY) @Valid @RequestParam(value = "sub_type", required = true) String subType,
+        @Parameter(name = "body", description = "", required = true) @Valid @RequestBody String body
+    ) {
+        return new ResponseEntity<>(HttpStatus.NOT_IMPLEMENTED);
+
+    }
+
+}
