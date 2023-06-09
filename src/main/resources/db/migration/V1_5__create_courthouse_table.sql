7185636b53a9aaf07851d591849f5f8cbb169719
Saad Arif <saad.arif@hmcts.net>
2023-06-09 09:50:41 +0100

    changed schema based on barry's updates

--- src/main/resources/db/migration/V1_5__create_courthouse_table.sql
+++ src/main/resources/db/migration/V1_5__create_courthouse_table.sql
@@ -1,7 +1,7 @@
 CREATE TABLE moj_courthouse (
   moj_crt_id                INTEGER                         NOT NULL
-, courthouse_name           CHARACTER VARYING               NOT NULL
-, courthouse_code           SMALLINT                        UNIQUE
+, courthouse_name           CHARACTER VARYING               NOT NULL      UNIQUE
+, courthouse_code           SMALLINT                                      UNIQUE
 , created_ts         TIMESTAMP with TIME ZONE     NOT NULL
 , last_modified_ts   TIMESTAMP with TIME ZONE     NOT NULL
 , CONSTRAINT moj_courthouse_pkey PRIMARY KEY (moj_crt_id)
