5c22db4d7f93bf57bf312493d7f2d551446df64f
Marc Mollins <marc.mollins@hmcts.net>
2023-06-14 08:53:11 +0100

    Updated Entity class and renamed and updated sql script

--- src/main/java/uk/gov/hmcts/darts/common/entity/DailyList.java
+++ src/main/java/uk/gov/hmcts/darts/common/entity/DailyList.java
@@ -1,72 +1,99 @@
 package uk.gov.hmcts.darts.common.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.SequenceGenerator;
-import jakarta.persistence.Table;
-import jakarta.persistence.Version;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import java.time.OffsetDateTime;
 
 @Entity
-@Table(name = "moj_daily_list")
+@Table(name = DailyList.TABLE_NAME)
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class DailyList {
 
+    public static final String ID = "moj_dal_id";
+    public static final String COURTHOUSE_ID = "moj_crt_id";
+    public static final String DAILY_LIST_OBJECT_ID = "r_daily_list_object_id";
+    public static final String UNIQUE_ID = "c_unique_id";
+    public static final String JOB_STATUS = "c_job_status";
+    public static final String PUBLISHED_TIME = "c_timestamp";
+    public static final String DAILY_LIST_ID = "c_daily_list_id";
+    public static final String START_DATE = "c_start_date";
+    public static final String END_DATE = "c_end_date";
+    public static final String DAILY_LIST_ID_STRING = "c_daily_list_id_s";
+    public static final String DAILY_LIST_SOURCE = "c_daily_list_source";
+    public static final String CREATED_DATE_TIME = "created_ts";
+    public static final String LAST_UPDATED_DATE_TIME = "last_modified_ts";
+    public static final String LEGACY_COURTHOUSE_OBJECT_ID = "r_courthouse_object_id";
+    public static final String LEGACY_VERSION_LABEL = "r_version_label";
+    public static final String SUPERSEDED = "i_superseded";
+    public static final String VERSION_LABEL = "i_version_label";
+    public static final String TABLE_NAME = "moj_daily_list";
+
     @Id
-    @Column(name = "moj_dal_id")
+    @Column(name = ID)
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "moj_dal_gen")
     @SequenceGenerator(name = "moj_dal_gen", sequenceName = "moj_dal_seq", allocationSize = 1)
     private Integer id;
 
     @ManyToOne
-    @JoinColumn(name = "moj_crt_id")
-    private Courthouse theCourthouse;
+    @JoinColumn(name = COURTHOUSE_ID)
+    private Integer crtId;
 
-    @Column(name = "r_daily_list_object_id", length = 16)
-    private String legacyObjectId;
+    @Column(name = DAILY_LIST_OBJECT_ID)
+    private String dailyListObjectId;
 
-    @Column(name = "c_unique_id")
-    private String uniqueId;
+    @Column(name = UNIQUE_ID)
+    private String uniqueID;
 
-    @Column(name = "c_job_status")
+    @Column(name = JOB_STATUS)
     private String jobStatus;
 
-    @Column(name = "c_timestamp")
-    private OffsetDateTime timestamp;
+    @Column(name = PUBLISHED_TIME)
+    private OffsetDateTime publishedTime;
 
-    @Column(name = "c_daily_list_id")
-    private Integer dailyListId;
+    @Column(name = DAILY_LIST_ID)
+    private Integer dailylistId;
 
-    @Column(name = "c_start_date")
+    @Column(name = START_DATE)
     private OffsetDateTime startDate;
 
-    @Column(name = "c_end_date")
+    @Column(name = END_DATE)
     private OffsetDateTime endDate;
 
-    @Column(name = "c_daily_list_id_s", length = 100)
+    @CreationTimestamp
+    @Column(name = CREATED_DATE_TIME)
+    private OffsetDateTime createdDateTime;
+
+    @UpdateTimestamp
+    @Column(name = LAST_UPDATED_DATE_TIME)
+    private OffsetDateTime lastUpdatedDateTime;
+
+    @Column(name = DAILY_LIST_ID_STRING, length = 100)
     private String dailyListIdString;
 
-    @Column(name = "c_daily_list_source", length = 3)
+    @Column(name = DAILY_LIST_SOURCE, length = 3)
     private String dailyListSource;
 
-    @Column(name = "r_courthouse_object_id", length = 16)
+    @Column(name = LEGACY_COURTHOUSE_OBJECT_ID, length = 16)
     private String legacyCourthouseObjectId;
 
-    @Column(name = "r_version_label", length = 32)
+    @Column(name = LEGACY_VERSION_LABEL, length = 32)
     private String legacyVersionLabel;
 
-    @Column(name = "i_superseded")
+    @Column(name = SUPERSEDED)
     private Boolean superseded;
 
     @Version
-    @Column(name = "i_version")
+    @Column(name = VERSION_LABEL)
     private Short version;
 
 }
---
+++ src/main/resources/db/migration/V1_6__Create_daily_list_table.sql
@@ -0,0 +1,19 @@
+CREATE TABLE IF NOT EXISTS moj_daily_list (
+  moj_dal_id                     INTEGER                     NOT NULL
+, moj_crt_id                     INTEGER                     NOT NULL
+, r_daily_list_object_id         TEXT
+, c_unique_id                    TEXT
+, c_job_status                   TEXT
+, c_timestamp                    TIMESTAMP WITH TIME ZONE
+, c_daily_list_id                INTEGER
+, c_start_date                   TIMESTAMP WITH TIME ZONE
+, c_end_date                     TIMESTAMP WITH TIME ZONE
+, c_daily_list_id_s              TEXT
+, c_daily_list_source            TEXT
+, created_ts                     TIMESTAMP WITH TIME ZONE
+, last_modified_ts               TIMESTAMP WITH TIME ZONE
+, r_version_label                TEXT
+, i_superseded                   BOOLEAN
+, i_version                      SMALLINT
+, CONSTRAINT moj_daily_list_pkey PRIMARY KEY (moj_dal_id)
+)
