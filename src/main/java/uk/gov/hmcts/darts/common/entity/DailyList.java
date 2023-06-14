b0d61001d1581b3df45f697a687a2a6884d6f75d
Marc Mollins <marc.mollins@hmcts.net>
2023-06-14 09:07:10 +0100

    updated Dailylist courthouse id and changed name of script to stop conflicts

--- src/main/java/uk/gov/hmcts/darts/common/entity/DailyList.java
+++ src/main/java/uk/gov/hmcts/darts/common/entity/DailyList.java
@@ -1,10 +1,16 @@
 package uk.gov.hmcts.darts.common.entity;
 
-import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+import jakarta.persistence.Version;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
@@ -46,7 +52,7 @@ public class DailyList {
 
     @ManyToOne
     @JoinColumn(name = COURTHOUSE_ID)
-    private Integer crtId;
+    private Courthouse crtId;
 
     @Column(name = DAILY_LIST_OBJECT_ID)
     private String dailyListObjectId;
