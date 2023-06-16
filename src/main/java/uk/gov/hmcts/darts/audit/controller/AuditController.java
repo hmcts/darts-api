3e1a56c71eec349718665690dbb98e7e67722745
Saad Arif <saad.arif@hmcts.net>
2023-06-16 15:08:26 +0100

    review comments.

--- src/main/java/uk/gov/hmcts/darts/audit/controller/AuditController.java
+++ src/main/java/uk/gov/hmcts/darts/audit/controller/AuditController.java
@@ -8,7 +8,7 @@ import java.time.OffsetDateTime;
 
 public class AuditController implements AuditApi {
     @Override
-    public ResponseEntity<SearchResult> search(Object startDate, OffsetDateTime endDate, String caseId, String eventId) {
+    public ResponseEntity<SearchResult> search(OffsetDateTime startDate, OffsetDateTime endDate, String caseId, String eventId) {
         return AuditApi.super.search(startDate, endDate, caseId, eventId);
     }
 }
