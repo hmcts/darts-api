0767faecd0428a87c19e57f435110da0f1881bdf
Chris Magowan <131666106+cmagowan-hmcts@users.noreply.github.com>
2023-06-22 17:04:50 +0100

    DMP-123 enforce schema on all parts of application. (#166)

--- src/main/java/uk/gov/hmcts/darts/common/config/SchedLockConfig.java
+++ src/main/java/uk/gov/hmcts/darts/common/config/SchedLockConfig.java
@@ -3,6 +3,8 @@ package uk.gov.hmcts.darts.common.config;
 import net.javacrumbs.shedlock.core.LockProvider;
 import net.javacrumbs.shedlock.provider.jdbctemplate.JdbcTemplateLockProvider;
 import net.javacrumbs.shedlock.spring.annotation.EnableSchedulerLock;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -16,8 +18,16 @@ import javax.sql.DataSource;
 @Profile("!intTest")
 public class SchedLockConfig {
 
+    @Value("${spring.datasource.schema}")
+    private String schema;
+
+
     @Bean
     public LockProvider lockProvider(DataSource dataSource) {
-        return new JdbcTemplateLockProvider(dataSource);
+        String tablename = "shedlock";
+        if (StringUtils.isNotBlank(schema)) {
+            tablename = schema + "." + tablename;
+        }
+        return new JdbcTemplateLockProvider(dataSource, tablename);
     }
 }
