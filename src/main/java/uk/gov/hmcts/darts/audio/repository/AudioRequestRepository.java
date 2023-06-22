0223356e25a88ada70c9e0c274de2d2626326cac
Hemanta Sharma <hemanta.sharma1129@gmail.com>
2023-06-22 14:46:06 +0100

    dmp-506 : Hearing Media Association Table (#160)
    Co-authored-by: hemanta.sharma <Hemanta.Sharma@HMCTS.net>

--- src/main/java/uk/gov/hmcts/darts/audio/repository/AudioRequestRepository.java
+++ src/main/java/uk/gov/hmcts/darts/audio/repository/AudioRequestRepository.java
@@ -2,12 +2,12 @@ package uk.gov.hmcts.darts.audio.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import uk.gov.hmcts.darts.audio.entity.AudioRequest;
+import uk.gov.hmcts.darts.audio.entity.MediaRequest;
 
 import java.util.List;
 
 @Repository
-public interface AudioRequestRepository extends JpaRepository<AudioRequest, Integer> {
+public interface AudioRequestRepository extends JpaRepository<MediaRequest, Integer> {
 
-    List<AudioRequest> findByRequestId(Integer requestId);
+    List<MediaRequest> findByRequestId(Integer requestId);
 }
