9e5a8f3486084cd8d74ba79f95b46a197a1ef80b
Chris Magowan <chris.magowan@hmcts.net>
2023-06-22 15:21:51 +0100

    DMP-123 enforce schema on all parts of application. Hibernate, flyway, and schedlock.

--- src/main/java/uk/gov/hmcts/darts/common/config/SchedLockConfig.java
+++ src/main/java/uk/gov/hmcts/darts/common/config/SchedLockConfig.java
@@ -3,6 +3,7 @@ package uk.gov.hmcts.darts.common.config;
 import net.javacrumbs.shedlock.core.LockProvider;
 import net.javacrumbs.shedlock.provider.jdbctemplate.JdbcTemplateLockProvider;
 import net.javacrumbs.shedlock.spring.annotation.EnableSchedulerLock;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -16,8 +17,12 @@ import javax.sql.DataSource;
 @Profile("!intTest")
 public class SchedLockConfig {
 
+    @Value("${spring.datasource.schema}")
+    private String schema;
+
+
     @Bean
     public LockProvider lockProvider(DataSource dataSource) {
-        return new JdbcTemplateLockProvider(dataSource);
+        return new JdbcTemplateLockProvider(dataSource, schema + ".shedlock");
     }
 }
