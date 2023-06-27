a37a15618791ad47f167b8672d0d628b4f2782e0
Marc Mollins <marc.mollins@hmcts.net>
2023-06-27 16:34:15 +0100

    updated application.yaml and tests for adding an email template
    Added the email template and updated the tests for an error processing audio

--- src/functionalTest/java/uk/gov/hmcts/darts/notification/service/GovNotifyServiceTest.java
+++ src/functionalTest/java/uk/gov/hmcts/darts/notification/service/GovNotifyServiceTest.java
@@ -138,6 +138,16 @@ class GovNotifyServiceTest {
                          "DARTS", emailResponse.getBody());
     }
 
-
+    @Test
+    void errorProcessingAudio() throws NotificationClientException, TemplateNotFoundException {
+        SendEmailResponse emailResponse = createAndSend("error_processing_audio");
+        assertEquals("DARTS: Audio Request has Failed", emailResponse.getSubject());
+        assertEquals("Hello,\r\n" +
+                         "You audio request for case TheCaseId has failed.Please contact the helpdesk.\r\n" +
+                         "\r\n" +
+                         "Regards, \r\n" +
+                         "\r\n" +
+                         "DARTS", emailResponse.getBody());
+    }
 
 }
