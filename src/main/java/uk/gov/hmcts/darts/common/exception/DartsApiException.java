69b8b3c4237d78720830af46b4d6674026a5af2c
chris-bellingham <chris.bellingham@hmcts.net>
2023-06-27 14:58:58 +0100

    DMP-437: Make all fields final

--- src/main/java/uk/gov/hmcts/darts/common/exception/DartsApiException.java
+++ src/main/java/uk/gov/hmcts/darts/common/exception/DartsApiException.java
@@ -5,21 +5,24 @@ import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @Getter
+@SuppressWarnings("PMD.NullAssignment")
 public class DartsApiException extends RuntimeException {
 
     private final DartsApiError error;
-    private String detail;
+    private final String detail;
 
     public DartsApiException(DartsApiError error) {
         super(error.getTitle());
 
         this.error = error;
+        this.detail = null;
     }
 
     public DartsApiException(DartsApiError error, Throwable throwable) {
         super(error.getTitle(), throwable);
 
         this.error = error;
+        this.detail = null;
     }
 
     public DartsApiException(DartsApiError error, String detail) {
