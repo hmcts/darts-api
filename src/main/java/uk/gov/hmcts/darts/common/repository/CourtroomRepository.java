5ffd605f86c9231ea18e7975f3c07bb107be10d5
Chris Bellingham <131763968+chrisbellingham-hmcts@users.noreply.github.com>
2023-06-29 13:38:40 +0100

    Uplift Spring Boot to 3.1.1 and resolve resultant JPARepository @Quer… (#189)

--- src/main/java/uk/gov/hmcts/darts/common/repository/CourtroomRepository.java
+++ src/main/java/uk/gov/hmcts/darts/common/repository/CourtroomRepository.java
@@ -13,7 +13,7 @@ public interface CourtroomRepository extends JpaRepository<CourtroomEntity, Inte
     @Query("SELECT cr FROM CourthouseEntity ch, CourtroomEntity cr " +
         "WHERE upper(ch.courthouseName) = upper(:courthouse) " +
         "AND upper(cr.name) = upper(:courtroom) " +
-        "AND cr.courthouse = ch.id "
+        "AND cr.courthouse = ch"
     )
     CourtroomEntity findByNames(String courthouse, String courtroom);
 
