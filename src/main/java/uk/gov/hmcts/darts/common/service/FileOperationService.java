c4ea2f1dfb4687fa57a6dba2139cf3d7178c3df2
uche_nnamani <uche.nnamani@hmcts.net>
2023-07-03 13:04:59 +0100

    created the FileOperationService utility

--- src/main/java/uk/gov/hmcts/darts/common/service/FileOperationService.java
+++ src/main/java/uk/gov/hmcts/darts/common/service/FileOperationService.java
@@ -1,13 +1,7 @@
-package uk.gov.hmcts.darts.audio.service.impl;
+package uk.gov.hmcts.darts.common.service;
 
 import com.azure.core.util.BinaryData;
-import jakarta.transaction.Transactional;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
 import uk.gov.hmcts.darts.audio.config.AudioConfigurationProperties;
-import uk.gov.hmcts.darts.audio.entity.MediaRequestEntity;
-import uk.gov.hmcts.darts.audio.service.AudioTransformationService;
-import uk.gov.hmcts.darts.audio.service.MediaRequestService;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -15,23 +9,11 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardCopyOption;
 
-import static uk.gov.hmcts.darts.audio.enums.AudioRequestStatus.PROCESSING;
-
-@Service
-@RequiredArgsConstructor
-public class AudioTransformationServiceImpl implements AudioTransformationService {
-
-    private final MediaRequestService mediaRequestService;
+public interface FileOperationService {
 
 
-    @Transactional
-    @Override
-    public MediaRequestEntity processAudioRequest(Integer requestId) {
+    private final AudioConfigurationProperties audioConfigurationProperties;
 
-        return mediaRequestService.updateAudioRequestStatus(requestId, PROCESSING);
-    }
-
-    @Override
     public Path saveBlobDataToTempWorkspace(BinaryData mediaFile, String fileName) throws IOException {
         // get local logger or check with Hemanta on the project logging strategy
         // ca// file ops service
@@ -55,4 +37,6 @@ public class AudioTransformationServiceImpl implements AudioTransformationServic
 
         return targetTempFile;
     }
+
+
 }
