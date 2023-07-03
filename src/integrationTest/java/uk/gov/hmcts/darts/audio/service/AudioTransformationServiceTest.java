e40803b574d97cc86dd7290af7171bbbcc6f9faa
Richard Bull <richard.bull@hmcts.net>
2023-07-03 14:36:11 +0100

    DMP-519 Update AudioTransformationService: Add saveAudioBlobData / saveTransientDataLocation (#199)

--- src/integrationTest/java/uk/gov/hmcts/darts/audio/service/AudioTransformationServiceTest.java
+++ src/integrationTest/java/uk/gov/hmcts/darts/audio/service/AudioTransformationServiceTest.java
@@ -1,14 +1,19 @@
 package uk.gov.hmcts.darts.audio.service;
 
 import com.azure.core.util.BinaryData;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ActiveProfiles;
 import uk.gov.hmcts.darts.audio.entity.MediaRequestEntity;
 import uk.gov.hmcts.darts.audio.repository.MediaRequestRepository;
+import uk.gov.hmcts.darts.common.entity.TransientObjectDirectoryEntity;
+import uk.gov.hmcts.darts.common.service.TransientObjectDirectoryService;
 import uk.gov.hmcts.darts.datamanagement.config.DataManagementConfiguration;
 import uk.gov.hmcts.darts.datamanagement.service.DataManagementService;
 
@@ -17,6 +22,9 @@ import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
 import static org.mockito.Mockito.when;
 import static uk.gov.hmcts.darts.audio.enums.AudioRequestStatus.OPEN;
 import static uk.gov.hmcts.darts.audio.enums.AudioRequestStatus.PROCESSING;
@@ -24,6 +32,7 @@ import static uk.gov.hmcts.darts.audiorequest.model.AudioRequestType.DOWNLOAD;
 
 @SpringBootTest
 @ActiveProfiles({"intTest", "h2db"})
+@TestInstance(Lifecycle.PER_CLASS)
 class AudioTransformationServiceTest {
 
     @Autowired
@@ -33,16 +42,21 @@ class AudioTransformationServiceTest {
     @Autowired
     private DataManagementConfiguration dataManagementConfiguration;
     @MockBean
-    private DataManagementService dataManagementService;
+    private DataManagementService mockDataManagementService;
+    @MockBean
+    private TransientObjectDirectoryService mockTransientObjectDirectoryService;
 
     private static final String TEST_BINARY_STRING = "Test String to be converted to binary!";
     private static final BinaryData BINARY_DATA = BinaryData.fromBytes(TEST_BINARY_STRING.getBytes());
     private static final UUID BLOB_LOCATION = UUID.randomUUID();
 
-    private Integer requestId;
+    private String containerName;
+    private MediaRequestEntity savedMediaRequestEntity;
+    @Mock
+    private TransientObjectDirectoryEntity mockTransientObjectDirectoryEntity;
 
-    @BeforeEach
-    void setUp() {
+    @BeforeAll
+    void beforeAll() {
         MediaRequestEntity mediaRequestEntity = new MediaRequestEntity();
         mediaRequestEntity.setHearingId(-1);
         mediaRequestEntity.setRequestor(-2);
@@ -56,22 +70,71 @@ class AudioTransformationServiceTest {
         mediaRequestEntity.setOutputFilename(null);
         mediaRequestEntity.setLastAccessedDateTime(null);
 
-        MediaRequestEntity savedMediaRequestEntity = mediaRequestRepository.saveAndFlush(mediaRequestEntity);
+        savedMediaRequestEntity = mediaRequestRepository.saveAndFlush(mediaRequestEntity);
         assertNotNull(savedMediaRequestEntity);
-        requestId = savedMediaRequestEntity.getRequestId();
+
+        containerName = dataManagementConfiguration.getUnstructuredContainerName();
     }
 
     @Test
-    void processAudioRequest() {
-        MediaRequestEntity processingMediaRequestEntity = audioTransformationService.processAudioRequest(requestId);
+    void shouldProcessAudioRequest() {
+        MediaRequestEntity processingMediaRequestEntity = audioTransformationService.processAudioRequest(
+            savedMediaRequestEntity.getRequestId());
         assertEquals(PROCESSING, processingMediaRequestEntity.getStatus());
     }
 
     @Test
-    void testGetAudioBlobDataUsingLocation() {
-        when(dataManagementService.getBlobData(dataManagementConfiguration.getUnstructuredContainerName(), BLOB_LOCATION)).thenReturn(BINARY_DATA);
+    void shouldGetAudioBlobDataUsingLocation() {
+        when(mockDataManagementService.getBlobData(
+            containerName,
+            BLOB_LOCATION
+        )).thenReturn(BINARY_DATA);
+
         BinaryData binaryData = audioTransformationService.getAudioBlobData(BLOB_LOCATION);
+
         assertEquals(BINARY_DATA, binaryData);
+        verify(mockDataManagementService).getBlobData(
+            eq(containerName),
+            eq(BLOB_LOCATION)
+        );
+        verifyNoMoreInteractions(mockDataManagementService);
+    }
+
+    @Test
+    void shouldSaveAudioBlobData() {
+        when(mockDataManagementService.saveBlobData(
+            containerName,
+            BINARY_DATA
+        )).thenReturn(BLOB_LOCATION);
+
+        UUID externalLocation = audioTransformationService.saveAudioBlobData(BINARY_DATA);
+
+        assertEquals(BLOB_LOCATION, externalLocation);
+        verify(mockDataManagementService).saveBlobData(
+            eq(containerName),
+            eq(BINARY_DATA)
+        );
+        verifyNoMoreInteractions(mockDataManagementService);
+    }
+
+    @Test
+    void shouldSaveTransientDataLocation() {
+        when(mockTransientObjectDirectoryService.saveTransientDataLocation(
+            savedMediaRequestEntity,
+            BLOB_LOCATION
+        )).thenReturn(mockTransientObjectDirectoryEntity);
+
+        TransientObjectDirectoryEntity transientObjectDirectoryEntity = audioTransformationService.saveTransientDataLocation(
+            savedMediaRequestEntity,
+            BLOB_LOCATION
+        );
+
+        assertNotNull(transientObjectDirectoryEntity);
+        verify(mockTransientObjectDirectoryService).saveTransientDataLocation(
+            eq(savedMediaRequestEntity),
+            eq(BLOB_LOCATION)
+        );
+        verifyNoMoreInteractions(mockTransientObjectDirectoryService);
     }
 
 }
--- src/main/java/uk/gov/hmcts/darts/audio/service/AudioTransformationService.java
+++ src/main/java/uk/gov/hmcts/darts/audio/service/AudioTransformationService.java
@@ -2,6 +2,7 @@ package uk.gov.hmcts.darts.audio.service;
 
 import com.azure.core.util.BinaryData;
 import uk.gov.hmcts.darts.audio.entity.MediaRequestEntity;
+import uk.gov.hmcts.darts.common.entity.TransientObjectDirectoryEntity;
 
 import java.util.UUID;
 
@@ -10,5 +11,9 @@ public interface AudioTransformationService {
     MediaRequestEntity processAudioRequest(Integer requestId);
 
     BinaryData getAudioBlobData(UUID location);
+
+    UUID saveAudioBlobData(BinaryData binaryData);
+
+    TransientObjectDirectoryEntity saveTransientDataLocation(MediaRequestEntity mediaRequest, UUID externalLocation);
 
 }
