ec8194806b51c45bb7866f3f9b644ab19e8b04dc
Marc Mollins <marc.mollins@hmcts.net>
2023-07-11 14:37:24 +0100

    created a controller and test to show that its working
    controller throws back a 501 and test successfully checks that

---
+++ src/integrationTest/java/uk/gov/hmcts/darts/courtlogs/controller/CourtLogsControllerTest.java
@@ -0,0 +1,36 @@
+package uk.gov.hmcts.darts.courtlogs.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@ActiveProfiles({"intTest", "h2db"})
+@AutoConfigureMockMvc
+@SuppressWarnings({"PMD.JUnitTestsShouldIncludeAssert", "PMD.AvoidDuplicateLiterals", "PMD.TooManyMethods"})
+class CourtLogsControllerTest {
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Autowired
+    private transient MockMvc mockMvc;
+
+    @Test
+    void courtLogsGet() throws Exception {
+
+        MockHttpServletRequestBuilder requestBuilder = get("/courtlogs")
+            .contentType(MediaType.APPLICATION_JSON_VALUE);
+        mockMvc.perform(requestBuilder).andExpect(status().isNotImplemented());
+
+    }
+}
