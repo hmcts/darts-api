dd000bc43c99e3bf45b88a7777f080f8d1a6e0b4
tonym <incalabsltd@gmail.com>
2023-07-24 09:23:30 +0100

    Fixing imports

--- src/integrationTest/java/uk/gov/hmcts/darts/authentication/controller/impl/LogoutIntTest.java
+++ src/integrationTest/java/uk/gov/hmcts/darts/authentication/controller/impl/LogoutIntTest.java
@@ -3,16 +3,9 @@ package uk.gov.hmcts.darts.authentication.controller.impl;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpHeaders;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
-import uk.gov.hmcts.darts.audio.repository.MediaRequestRepository;
-import uk.gov.hmcts.darts.common.util.ReprovisionDatabaseBeforeEach;
-import uk.gov.hmcts.darts.courthouse.CourthouseRepository;
-import uk.gov.hmcts.darts.notification.repository.NotificationRepository;
 import uk.gov.hmcts.darts.testutils.IntegrationBase;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
