a70cd790b94f3f0d08780104960156f6f335c7fb
karen-hedges <133129444+karen-hedges@users.noreply.github.com>
2023-07-25 00:19:33 +0100

    Created playlist.xml

--- src/main/java/uk/gov/hmcts/darts/audio/service/ViqHeaderService.java
+++ src/main/java/uk/gov/hmcts/darts/audio/service/ViqHeaderService.java
@@ -1,10 +1,13 @@
 package uk.gov.hmcts.darts.audio.service;
 
+import uk.gov.hmcts.darts.audio.model.PlaylistInfo;
 import uk.gov.hmcts.darts.audio.model.ViqMetaData;
+
+import java.util.List;
 
 public interface ViqHeaderService {
 
-    String generatePlaylist(Integer hearingId, String startTime, String fileLocation);
+    String generatePlaylist(List<PlaylistInfo> playlistInfos, String outputFileLocation);
 
     String generateAnnotation(Integer hearingId, String startTime, String endTime);
 
