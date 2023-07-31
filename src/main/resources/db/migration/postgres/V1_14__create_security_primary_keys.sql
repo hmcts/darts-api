b17fa8fd8d1fdb83e95617b294ed1de753ef3406
Richard Bull <richard.bull@hmcts.net>
2023-07-31 15:55:33 +0100

    DMP-637 Fix version "1.14 - create security primary keys"

--- src/main/resources/db/migration/postgres/V1_14__create_security_primary_keys.sql
+++ src/main/resources/db/migration/postgres/V1_14__create_security_primary_keys.sql
@@ -1,17 +1,17 @@
-CREATE UNIQUE INDEX security_group_pk               ON security_group(grp_id) TABLESPACE darts_indexes;
+CREATE UNIQUE INDEX security_group_pk               ON security_group(grp_id);
 ALTER TABLE security_group                          ADD PRIMARY KEY USING INDEX security_group_pk;
 
-CREATE UNIQUE INDEX security_group_user_account_ae_pk ON security_group_user_account_ae(usr_id,grp_id) TABLESPACE darts_indexes;
+CREATE UNIQUE INDEX security_group_user_account_ae_pk ON security_group_user_account_ae(usr_id,grp_id);
 ALTER TABLE security_group_user_account_ae            ADD PRIMARY KEY USING INDEX security_group_user_account_ae_pk;
 
-CREATE UNIQUE INDEX security_role_pk                ON security_role(rol_id) TABLESPACE darts_indexes;
+CREATE UNIQUE INDEX security_role_pk                ON security_role(rol_id);
 ALTER TABLE security_role                           ADD PRIMARY KEY USING INDEX security_role_pk;
 
-CREATE UNIQUE INDEX security_permission_pk          ON security_permission(per_id) TABLESPACE darts_indexes;
+CREATE UNIQUE INDEX security_permission_pk          ON security_permission(per_id);
 ALTER TABLE security_permission                     ADD PRIMARY KEY USING INDEX security_permission_pk;
 
-CREATE UNIQUE INDEX security_role_permission_ae_pk  ON security_role_permission_ae(rol_id,per_id) TABLESPACE darts_indexes;
+CREATE UNIQUE INDEX security_role_permission_ae_pk  ON security_role_permission_ae(rol_id,per_id);
 ALTER TABLE security_role_permission_ae             ADD PRIMARY KEY USING INDEX security_role_permission_ae_pk;
 
-CREATE UNIQUE INDEX security_group_courthouse_ae_pk ON security_group_courthouse_ae(grp_id,cth_id) TABLESPACE darts_indexes;
+CREATE UNIQUE INDEX security_group_courthouse_ae_pk ON security_group_courthouse_ae(grp_id,cth_id);
 ALTER TABLE security_group_courthouse_ae            ADD PRIMARY KEY USING INDEX security_group_courthouse_ae_pk;
