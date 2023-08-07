58ddabb9e3de3dde2ffc5dae24ece2fc245f85c3
Richard Bull <richard.bull@hmcts.net>
2023-08-07 10:06:24 +0100

    DMP-527 Updated following chrisbellingham-hmcts review comment

--- src/main/java/uk/gov/hmcts/darts/common/repository/AutomatedTaskRepository.java
+++ src/main/java/uk/gov/hmcts/darts/common/repository/AutomatedTaskRepository.java
@@ -4,13 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import uk.gov.hmcts.darts.common.entity.AutomatedTaskEntity;
 
-import java.util.List;
-
 @Repository
 public interface AutomatedTaskRepository extends JpaRepository<AutomatedTaskEntity, Integer> {
-
-    AutomatedTaskEntity findByTaskName(String taskName);
-
-    List<AutomatedTaskEntity> findAllByTaskName(String taskName);
 
 }
