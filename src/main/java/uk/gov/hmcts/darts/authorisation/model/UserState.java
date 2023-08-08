02af713affd5fb34a81ed72bf95f31e993d8f26f
Hemanta Sharma <hemanta.sharma1129@gmail.com>
2023-08-08 13:22:25 +0100

    dmp-727 : Fix Authorisation error on Stg (#317)
    Co-authored-by: hemanta.sharma <Hemanta.Sharma@HMCTS.net>

--- src/main/java/uk/gov/hmcts/darts/authorisation/model/UserState.java
+++ src/main/java/uk/gov/hmcts/darts/authorisation/model/UserState.java
@@ -1,7 +1,6 @@
 package uk.gov.hmcts.darts.authorisation.model;
 
 import lombok.Builder;
-import lombok.NonNull;
 import lombok.Value;
 
 import java.util.Set;
@@ -10,11 +9,8 @@ import java.util.Set;
 @Value
 public class UserState {
 
-    @NonNull
     private Integer userId;
-    @NonNull
     private String userName;
-    @NonNull
     private Set<Role> roles;
 
 }
