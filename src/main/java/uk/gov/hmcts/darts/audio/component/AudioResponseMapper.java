e78ccbc17fd5bd7697b965317551b108e3aab06d
chris-bellingham <chris.bellingham@hmcts.net>
2023-08-17 14:43:52 +0100

    DMP-755: Implement get audio metadata API endpoint

---
+++ src/main/java/uk/gov/hmcts/darts/audio/component/AudioResponseMapper.java
@@ -0,0 +1,12 @@
+package uk.gov.hmcts.darts.audio.component;
+
+import uk.gov.hmcts.darts.audio.model.AudioMetadata;
+import uk.gov.hmcts.darts.common.entity.MediaEntity;
+
+import java.util.List;
+
+public interface AudioResponseMapper {
+
+    List<AudioMetadata> mapToAudioMetadata(List<MediaEntity> mediaEntities);
+
+}
