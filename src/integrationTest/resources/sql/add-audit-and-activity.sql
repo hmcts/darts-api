7a9823c2c0c27f4d3c353e5f4ab1ccf87d695721
Chris Magowan <chris.magowan@hmcts.net>
2023-08-24 16:59:27 +0100

    fix build

--- src/integrationTest/resources/sql/add-audit-and-activity.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO darts.audit_activity(
-	aua_id, activity_name, activity_description, created_ts, created_by, last_modified_ts, last_modified_by)
-	VALUES (998, 'test name', 'test description', '2023-05-06T00:00:00+00', 0, '2023-05-06T00:00:00+00', 0);
-INSERT INTO darts.audit(
-	aud_id, cas_id, aua_id, usr_id, created_ts, application_server, additional_data, created_by, last_modified_ts, last_modified_by)
-	VALUES (999, 3, 998, -1, '2023-06-13T08:13:09.688537759Z', 'application_server', 'additional_data', 0, '2023-05-06T00:00:00+00', 0);
