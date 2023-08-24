d1cee15ccc6cf79e3688952fde1e6a4bcf206c4b
Saad Arif <saad.arif@hmcts.net>
2023-08-24 10:21:06 +0100

    DMP-233
    Added bad request error for missing courthouse and tests.

--- src/integrationTest/java/uk/gov/hmcts/darts/dailylist/controller/DailyListProcessEndpointTest.java
+++ src/integrationTest/java/uk/gov/hmcts/darts/dailylist/controller/DailyListProcessEndpointTest.java
@@ -6,11 +6,17 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import uk.gov.hmcts.darts.common.entity.CourthouseEntity;
+import uk.gov.hmcts.darts.common.exception.DartsApiException;
+import uk.gov.hmcts.darts.courthouse.CourthouseRepository;
 import uk.gov.hmcts.darts.dailylist.service.DailyListProcessor;
 
 import java.time.LocalDate;
+import java.util.Optional;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyInt;
 
 @ExtendWith(MockitoExtension.class)
 class DailyListProcessEndpointTest {
@@ -20,16 +26,26 @@ class DailyListProcessEndpointTest {
     @Mock
     private DailyListProcessor processor;
 
+    @Mock
+    private CourthouseRepository courthouseRepository;
+
 
     @Test
     void dailyListRun() {
         controller.dailylistsRunPost(null);
-        Mockito.verify(processor).processAllDailyLists(LocalDate.now());
+        Mockito.verify(processor, Mockito.timeout(100).times(1)).processAllDailyLists(LocalDate.now());
     }
 
     @Test
     void dailyListRunWithCourthouse() {
+        CourthouseEntity courthouse = Mockito.mock(CourthouseEntity.class);
+        Mockito.when(courthouseRepository.findById(anyInt())).thenReturn(Optional.of(courthouse));
         controller.dailylistsRunPost(1);
-        Mockito.verify(processor).processAllDailyListForCourthouse(any());
+        Mockito.verify(processor, Mockito.timeout(100).times(1)).processAllDailyListForCourthouse(any());
+    }
+
+    @Test
+    void dailyListRunWithCourthouseDoesNotExist() {
+        assertThrows(DartsApiException.class, () -> controller.dailylistsRunPost(1));
     }
 }
