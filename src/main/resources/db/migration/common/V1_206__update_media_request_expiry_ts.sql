0945ec13305aaa866055110c1b11cf2ff7fb6245
Richard Bull <richard.bull@hmcts.net>
2023-08-25 14:51:46 +0100

    BUGFIX media_request created_by / last_modified_by columns

--- src/main/resources/db/migration/common/V1_206__update_media_request_expiry_ts.sql
+++ src/main/resources/db/migration/common/V1_206__update_media_request_expiry_ts.sql
@@ -4,9 +4,7 @@ ALTER TABLE media_request ALTER COLUMN request_type SET NOT NULL;
 ALTER TABLE media_request ALTER COLUMN start_ts SET NOT NULL;
 ALTER TABLE media_request ALTER COLUMN end_ts SET NOT NULL;
 ALTER TABLE media_request ALTER COLUMN created_ts SET NOT NULL;
-ALTER TABLE media_request ALTER COLUMN created_by SET NOT NULL;
 ALTER TABLE media_request ALTER COLUMN last_modified_ts SET NOT NULL;
-ALTER TABLE media_request ALTER COLUMN last_modified_by SET NOT NULL;
 
 ALTER TABLE media_request DROP CONSTRAINT media_hearing_fk;
 
