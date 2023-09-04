754c5e50b1a72a30f0f4489456836012222b33d5
Saad Arif <saad.arif@hmcts.net>
2023-09-04 10:47:57 +0100

    DMP-783 - added tests for coverage.

--- src/test/java/uk/gov/hmcts/darts/task/service/impl/AutomatedTaskServiceImplTest.java
+++ src/test/java/uk/gov/hmcts/darts/task/service/impl/AutomatedTaskServiceImplTest.java
@@ -8,7 +8,12 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.scheduling.TaskScheduler;
+import org.springframework.scheduling.Trigger;
+import org.springframework.scheduling.config.ScheduledTask;
+import org.springframework.scheduling.config.ScheduledTaskHolder;
 import org.springframework.scheduling.config.ScheduledTaskRegistrar;
+import org.springframework.scheduling.config.TriggerTask;
 import uk.gov.hmcts.darts.common.entity.AutomatedTaskEntity;
 import uk.gov.hmcts.darts.common.exception.DartsApiException;
 import uk.gov.hmcts.darts.common.repository.AutomatedTaskRepository;
@@ -19,10 +24,14 @@ import uk.gov.hmcts.darts.task.status.AutomatedTaskStatus;
 
 import java.time.Duration;
 import java.time.Instant;
+import java.util.HashSet;
 import java.util.Optional;
+import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 @Slf4j
@@ -37,6 +46,12 @@ class AutomatedTaskServiceImplTest {
 
     @Mock
     private LockProvider mockLockProvider;
+
+    @Mock
+    private TaskScheduler taskScheduler;
+
+    @Mock
+    private ScheduledTaskHolder scheduledTaskHolder;
 
     @Test
     void getAutomatedTaskUsingProcessDailyListAutomatedTask() {
@@ -82,49 +97,43 @@ class AutomatedTaskServiceImplTest {
     }
 
     @Test
-    void updateNonExistingAutomatedTaskCronExpressionThrowsException() {
+    void reloadByTaskName() {
 
-        AutomatedTask automatedTask = new AutomatedTask() {
-            private String lastCronExpression = "*/10 * * * * *";
+        ScheduledTask scheduledTask = mock(ScheduledTask.class);
+        Set<ScheduledTask> scheduledTaskList = new HashSet<>();
+        scheduledTaskList.add(scheduledTask);
 
-            private static Duration getLockAtLeastFor() {
-                return Duration.ofSeconds(10);
+        AbstractLockableAutomatedTask automatedTask = new AbstractLockableAutomatedTask(
+            mockAutomatedTaskRepository,
+            mockLockProvider
+        ) {
+            @Override
+            protected void runTask() {
+            }
+
+            @Override
+            protected void handleException(Exception exception) {
             }
 
             @Override
             public String getTaskName() {
-                return "Test";
-            }
-
-            @Override
-            public AutomatedTaskStatus getAutomatedTaskStatus() {
-                return AutomatedTaskStatus.NOT_STARTED;
-            }
-
-            @Override
-            public LockConfiguration getLockConfiguration() {
-                return new LockConfiguration(Instant.now(), getTaskName(), getLockAtMostFor(), getLockAtLeastFor());
-            }
-
-            @Override
-            public String getLastCronExpression() {
-                return lastCronExpression;
-            }
-
-            @Override
-            public void setLastCronExpression(String cronExpression) {
-                this.lastCronExpression = cronExpression;
-            }
-
-            private Duration getLockAtMostFor() {
-                return Duration.ofSeconds(20);
-            }
-
-            @Override
-            public void run() {
-                log.debug("Running test automated task");
+                return "ProcessDailyList";
             }
         };
+        Trigger trigger = triggerContext -> null;
+        TriggerTask task = new TriggerTask(automatedTask, trigger);
+        when(scheduledTaskHolder.getScheduledTasks()).thenReturn(scheduledTaskList);
+        when(scheduledTask.getTask()).thenReturn(task);
+
+        automatedTaskService.reloadTaskByName("ProcessDailyList");
+
+        verify(taskScheduler).schedule(automatedTask, trigger);
+    }
+
+    @Test
+    void updateNonExistingAutomatedTaskCronExpressionThrowsException() {
+
+        AutomatedTask automatedTask = createAutomatedTask("Test");
         assertThrows(DartsApiException.class, () ->
             automatedTaskService.updateAutomatedTaskCronExpression(automatedTask.getTaskName(), "*/8 * * * * *"));
     }
@@ -173,4 +182,53 @@ class AutomatedTaskServiceImplTest {
         automatedTaskEntity1.setTaskDescription("Test Automated Task");
         return automatedTaskEntity1;
     }
+
+
+    private AutomatedTask createAutomatedTask(String taskName) {
+        return new AutomatedTask() {
+            private String lastCronExpression = "*/10 * * * * *";
+
+            private static Duration getLockAtLeastFor() {
+                return Duration.ofSeconds(10);
+            }
+
+            @Override
+            public String getTaskName() {
+                return taskName;
+            }
+
+            @Override
+            public AutomatedTaskStatus getAutomatedTaskStatus() {
+                return AutomatedTaskStatus.NOT_STARTED;
+            }
+
+            @Override
+            public LockConfiguration getLockConfiguration() {
+                return new LockConfiguration(Instant.now(), getTaskName(), getLockAtMostFor(), getLockAtLeastFor());
+            }
+
+            @Override
+            public String getLastCronExpression() {
+                return lastCronExpression;
+            }
+
+            @Override
+            public void setLastCronExpression(String cronExpression) {
+                this.lastCronExpression = cronExpression;
+            }
+
+            private Duration getLockAtMostFor() {
+                return Duration.ofSeconds(20);
+            }
+
+            @Override
+            public void run() {
+                log.debug("Running test automated task");
+            }
+
+
+        };
+    }
+
+
 }
