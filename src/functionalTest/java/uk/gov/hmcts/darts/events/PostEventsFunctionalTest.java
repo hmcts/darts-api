56edd8deda1de167c59156a2ba28272525082615
Chris Magowan <131666106+cmagowan-hmcts@users.noreply.github.com>
2023-09-06 10:55:49 +0100

    DMP-813 - add functional tests for /events (#413)

---
+++ src/functionalTest/java/uk/gov/hmcts/darts/events/PostEventsFunctionalTest.java
@@ -0,0 +1,70 @@
+package uk.gov.hmcts.darts.events;
+
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+import uk.gov.hmcts.darts.FunctionalTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class PostEventsFunctionalTest extends FunctionalTest {
+
+
+    public static final String ENDPOINT_URL = "/events";
+
+    @Test
+    void success() {
+        Response response = buildRequestWithAuth()
+            .contentType(ContentType.JSON)
+            .body("""
+                      {
+                        "message_id": "100",
+                        "type": "1000",
+                        "sub_type": "1002",
+                        "event_id": "12345",
+                        "courthouse": "swansea",
+                        "courtroom": "1",
+                        "case_numbers": [
+                          "Swansea_case_1"
+                        ],
+                        "event_text": "some text for the event",
+                        "date_time": "2023-08-08T14:01:06.085Z"
+                      }""")
+            .when()
+            .baseUri(getUri(ENDPOINT_URL))
+            .redirects().follow(false)
+            .post()
+            .then()
+            .extract().response();
+
+        assertEquals(201, response.statusCode());
+    }
+
+    @Test
+    void fail() {
+        Response response = buildRequestWithAuth()
+            .contentType(ContentType.JSON)
+            .body("""
+                      {
+                        "message_id": "100",
+                        "type": "1000",
+                        "sub_type": "1002",
+                        "event_id": "12345",
+                        "courthouse": "",
+                        "courtroom": "1",
+                        "case_numbers": [
+                          "Swansea_case_1"
+                        ],
+                        "event_text": "some text for the event",
+                        "date_time": "2023-08-08T14:01:06.085Z"
+                      }""")
+            .when()
+            .baseUri(getUri(ENDPOINT_URL))
+            .redirects().follow(false)
+            .post()
+            .then()
+            .extract().response();
+
+        assertEquals(400, response.statusCode());
+    }
+}
