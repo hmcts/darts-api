8b418ff4f48dcfe5a684641a07ce80606ce5a7ec
mmmalik1 <mohammed.malik1@hmcts.net>
2023-09-08 17:43:48 +0100

    Update CasesFunctionalTest.java
    getCaseId and getCaseNum added

--- src/functionalTest/java/uk/gov/hmcts/darts/cases/CasesFunctionalTest.java
+++ src/functionalTest/java/uk/gov/hmcts/darts/cases/CasesFunctionalTest.java
@@ -6,6 +6,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import uk.gov.hmcts.darts.FunctionalTest;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @Slf4j
@@ -17,9 +19,9 @@ class CasesFunctionalTest  extends FunctionalTest {
     public static final String COURTHOUSE = "courthouse";
     public static final String COURTROOM = "courtroom";
     public static final String CASE_DATE = "date";
-    public static final String COURTHOUSE1 = "LEEDS";
-    public static final String COURTHOUSE_ROOM = "ROOM";
-    public static final String DATE1 = "2023-09-07";
+    public static final String COURTHOUSE1 = "SWANSEA";
+    public static final String COURTHOUSE_ROOM = "1";
+    public static final String DATE1 = "2020-06-20"; //Hearing
     public static final int NOT_FOUND = 404;
     public static final int OK = 200;
     public static final int CREATED = 201;
@@ -83,12 +85,35 @@ class CasesFunctionalTest  extends FunctionalTest {
         assertEquals(OK, response.statusCode());
     }
 
+    /**
+     * TBD: Review
+     */
     @Test
-    void getExistingCase() {
+    void getAllCases2() {
         Response response = buildRequestWithAuth()
             .contentType(ContentType.JSON)
             .when()
-            .baseUri(getUri(CASES_URI + CASE_ID))
+            .baseUri(getUri(CASES_URI))
+            .param(COURTHOUSE, "SWANSEA")
+            .param(COURTROOM, "ROOM")
+            .param(CASE_DATE, DATE1)
+            .get()
+            .then()
+            .extract().response();
+
+        printDebug(response);
+
+        assertEquals(OK, response.statusCode());
+    }
+
+    @Test
+    void getExistingCase() {
+//        String caseNum = getCaseNumber();
+        int caseId = getCaseId();
+        Response response = buildRequestWithAuth()
+            .contentType(ContentType.JSON)
+            .when()
+            .baseUri(getUri(CASES_URI + "/" + caseId))
             .get()
             .then()
             .extract().response();
@@ -138,7 +163,7 @@ class CasesFunctionalTest  extends FunctionalTest {
             .contentType(ContentType.JSON)
             .when()
             .baseUri(getUri(CASES_URI + SEARCH))
-            .param("case_id", "41")
+//            .param("case_id", "41")
             .param("case_number","42GD2391421")
             .param("courthouse","SWANSEA")
             .param("courtroom","1")
@@ -155,6 +180,8 @@ class CasesFunctionalTest  extends FunctionalTest {
 
         assertEquals(OK, response.statusCode());
     }
+
+
 
     @Test
     void getCaseHearing() {
@@ -194,6 +221,45 @@ class CasesFunctionalTest  extends FunctionalTest {
         log.debug("<=========================COURTHOUSES-BODY=====================================>");
         log.debug("BODY: " + response.getBody().prettyPrint());
         log.debug("<==============================CASES-BODY======================================>");
+    }
+
+
+    public int getCaseId() {
+        List<Integer> ids = buildRequestWithAuth()
+            .contentType(ContentType.JSON)
+            .when()
+            .baseUri(getUri(CASES_URI + SEARCH))
+            .param("case_number","CA")
+            .get()
+            .then()
+            .extract()
+            .response()
+            .getBody()
+            .jsonPath().get("case_id");
+
+        log.debug("*************> CASE_ID: " + ids.get(ids.size()-1) + "<*************" );
+
+        return ids.get(ids.size()-1);
+    }
+
+    private String getCaseNumber() {
+        List<String> ids = buildRequestWithAuth()
+            .contentType(ContentType.JSON)
+            .when()
+            .baseUri(getUri(CASES_URI))
+            .param(COURTHOUSE, COURTHOUSE1)
+            .param(COURTROOM, COURTHOUSE_ROOM)
+            .param(CASE_DATE, DATE1)
+            .get()
+            .then()
+            .extract()
+            .response()
+            .getBody()
+            .jsonPath().get("case_number");
+
+        log.debug("*************> CASE_NUM: " + ids.get(ids.size()-1) + "<*************" );
+
+        return ids.get(ids.size()-1);
     }
 
 
