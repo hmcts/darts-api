daeb135c1b2e09a526c06bf8ac814d00ea7c4013
Chris Davidson <christopher.davidson@btinternet.com>
2023-09-15 16:38:02 +0100

    DMP 881 import

--- src/test/java/uk/gov/hmcts/darts/cases/service/impl/CaseServiceImplTest.java
+++ src/test/java/uk/gov/hmcts/darts/cases/service/impl/CaseServiceImplTest.java
@@ -47,7 +47,6 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.Mockito.mock;
 import static uk.gov.hmcts.darts.common.util.TestUtils.getContentsFromFile;
 
 @ExtendWith(MockitoExtension.class)
