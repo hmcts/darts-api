95bf8e84cb760d3bf6c0b8f6fd2522c12bec83f0
Chris Davidson <christopher.davidson@btinternet.com>
2023-09-15 16:22:34 +0100

    DMP 881 another import

--- src/main/java/uk/gov/hmcts/darts/cases/mapper/CasesMapper.java
+++ src/main/java/uk/gov/hmcts/darts/cases/mapper/CasesMapper.java
@@ -3,14 +3,12 @@ package uk.gov.hmcts.darts.cases.mapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.darts.cases.model.AddCaseRequest;
-import uk.gov.hmcts.darts.cases.model.EventResponse;
 import uk.gov.hmcts.darts.cases.model.PostCaseResponse;
 import uk.gov.hmcts.darts.cases.model.ScheduledCase;
 import uk.gov.hmcts.darts.cases.model.SingleCase;
 import uk.gov.hmcts.darts.common.entity.CourtCaseEntity;
 import uk.gov.hmcts.darts.common.entity.DefenceEntity;
 import uk.gov.hmcts.darts.common.entity.DefendantEntity;
-import uk.gov.hmcts.darts.common.entity.EventEntity;
 import uk.gov.hmcts.darts.common.entity.HearingEntity;
 import uk.gov.hmcts.darts.common.entity.ProsecutorEntity;
 import uk.gov.hmcts.darts.common.service.RetrieveCoreObjectService;
@@ -18,7 +16,6 @@ import uk.gov.hmcts.darts.common.service.RetrieveCoreObjectService;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Objects;
-import java.util.stream.Collectors;
 
 import static org.apache.commons.collections4.ListUtils.emptyIfNull;
 
