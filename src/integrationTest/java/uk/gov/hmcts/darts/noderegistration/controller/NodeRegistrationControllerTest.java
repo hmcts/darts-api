6605e3b86e757d3c00d95b81103481c570074f14
Chris Davidson <christopher.davidson@btinternet.com>
2023-09-28 10:52:42 +0100

    DMP 705 reviewed integration test

--- src/integrationTest/java/uk/gov/hmcts/darts/noderegistration/controller/NodeRegistrationControllerTest.java
+++ src/integrationTest/java/uk/gov/hmcts/darts/noderegistration/controller/NodeRegistrationControllerTest.java
@@ -5,36 +5,42 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import uk.gov.hmcts.darts.common.entity.CourtroomEntity;
+import uk.gov.hmcts.darts.testutils.IntegrationBase;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @Slf4j
-@SpringBootTest
-@ActiveProfiles("intTest")
 @AutoConfigureMockMvc
-public class NodeRegistrationControllerTest {
+class NodeRegistrationControllerTest  extends IntegrationBase {
 
     @Autowired
     private transient MockMvc mockMvc;
 
     @Test
-    void checkPostEndpoint() throws Exception {
+    void testPostRegisterDevices() throws Exception {
+        dartsDatabase.createCourthouseWithTwoCourtrooms();
+
+        CourtroomEntity courtroomEntity = dartsDatabase.findCourtroomBy("SWANSEA", "1");
+
+        @SuppressWarnings("PMD.AvoidUsingHardCodedIP")
+        String ipAddress = "192.0.0.1";
         MockHttpServletRequestBuilder requestBuilder = post("/register-devices")
             .contentType(MediaType.APPLICATION_JSON_VALUE)
             .param("node_type", "DAR")
-            .param("courthouse", "SWANSEA")
-            .param("court_room", "1")
+            .param("courthouse", courtroomEntity.getCourthouse().getCourthouseName())
+            .param("court_room", courtroomEntity.getName())
             .param("host_name", "XXXXX.MMM.net")
-            .param("ip_address", "192.0.0.1")
+            .param("ip_address", ipAddress)
             .param("mac_address", "6A-5F-90-A4-2C-12");
         MvcResult mvcResult = mockMvc.perform(requestBuilder).andExpect(status().is2xxSuccessful()).andReturn();
         Assertions.assertTrue(mvcResult.getResponse().getContentAsString().contains("node_id"));
+
+
     }
 }
--- src/main/java/uk/gov/hmcts/darts/noderegistration/controller/NodeRegistrationController.java
+++ src/main/java/uk/gov/hmcts/darts/noderegistration/controller/NodeRegistrationController.java
@@ -1,96 +1,44 @@
 package uk.gov.hmcts.darts.noderegistration.controller;
 
-import io.swagger.v3.oas.annotations.Operation;
+
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.enums.ParameterIn;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-import uk.gov.hmcts.darts.dailylist.model.DailyListJsonObject;
-import uk.gov.hmcts.darts.dailylist.model.DailyListPostRequest;
-import uk.gov.hmcts.darts.dailylist.model.PostDailyListResponse;
-import uk.gov.hmcts.darts.dailylist.model.Problem;
-import uk.gov.hmcts.darts.dailylist.validation.DailyListPostValidator;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import uk.gov.hmcts.darts.noderegistration.api.DevicesApi;
 import uk.gov.hmcts.darts.noderegistration.model.PostNodeRegistrationResponse;
-import uk.gov.hmcts.darts.noderegistration.model.PostRegisterDeviceResponse;
+import uk.gov.hmcts.darts.noderegistration.service.NodeRegistrationService;
 
 import javax.validation.Valid;
-import javax.validation.constraints.NotNull;
-import java.time.LocalDate;
-import java.time.OffsetDateTime;
 
 @RestController
 public class NodeRegistrationController implements DevicesApi {
 
+    @Autowired
+    NodeRegistrationService nodeRegistrationService;
 
     public ResponseEntity<PostNodeRegistrationResponse> registerDevicesPost(
-        @Parameter(name = "node_type", description = "???", in = ParameterIn.QUERY) @Valid @RequestParam(value = "node_type", required = false) String nodeType,
-        @Parameter(name = "courthouse", description = "???", in = ParameterIn.QUERY) @Valid @RequestParam(value = "courthouse", required = false) String courtHouse,
-        @Parameter(name = "court_room", description = "???", in = ParameterIn.QUERY) @Valid @RequestParam(value = "court_room", required = false) String courtRoom,
-        @Parameter(name = "host_name", description = "", in = ParameterIn.QUERY) @Valid @RequestParam(value = "host_name", required = false) String hostName,
-        @Parameter(name = "ip_address", description = "", in = ParameterIn.QUERY) @Valid @RequestParam(value = "ip_address", required = false) String ipAddress,
-        @Parameter(name = "mac_address", description = "", in = ParameterIn.QUERY) @Valid @RequestParam(value = "mac_address", required = false) String macAddress
+        @Parameter(name = "node_type", description = "The type of device being registered, might just be DAR", in = ParameterIn.QUERY)
+        @Valid @RequestParam(value = "node_type") String nodeType,
+        @Parameter(name = "courthouse", description = "The name of the location of the courtroom containing the device", in = ParameterIn.QUERY)
+        @Valid @RequestParam(value = "courthouse") String courtHouse,
+        @Parameter(name = "court_room", description = "The name of the courtroom containing the device", in = ParameterIn.QUERY)
+        @Valid @RequestParam(value = "court_room") String courtRoom,
+        @Parameter(name = "host_name", description = "The host name of the device on the network", in = ParameterIn.QUERY)
+        @Valid @RequestParam(value = "host_name") String hostName,
+        @Parameter(name = "ip_address", description = "The IP address of the device on the network", in = ParameterIn.QUERY)
+        @Valid @RequestParam(value = "ip_address") String ipAddress,
+        @Parameter(name = "mac_address", description = "Tha MAC address of the device", in = ParameterIn.QUERY)
+        @Valid @RequestParam(value = "mac_address") String macAddress
     ) {
+        Integer nodeId = nodeRegistrationService.registerDevices(nodeType, courtHouse, courtRoom,
+              hostName, ipAddress, macAddress);
+
         PostNodeRegistrationResponse postRegisterDeviceResponse = new PostNodeRegistrationResponse();
-        postRegisterDeviceResponse.setNodeId(101);
+        postRegisterDeviceResponse.setNodeId(nodeId);
         return new ResponseEntity<>(postRegisterDeviceResponse, HttpStatus.OK);
     }
-
-    /*
-    @Override
-    @Operation(
-        operationId = "registerDevicesPost",
-        summary = "?????",
-        description = "description",
-        tags = {"Devices"},
-        responses = {
-            @ApiResponse(responseCode = "200", description = "Created", content = {
-                @Content(mediaType = "application/json", schema = @Schema(implementation = PostRegisterDeviceResponse.class)),
-                @Content(mediaType = "application/json+problem", schema = @Schema(implementation = PostRegisterDeviceResponse.class))
-            }),
-            @ApiResponse(responseCode = "500", description = "Internal Server Error", content = {
-                @Content(mediaType = "application/json", schema = @Schema(implementation = Problem.class)),
-                @Content(mediaType = "application/json+problem", schema = @Schema(implementation = Problem.class))
-            })
-        }
-    )
-    @RequestMapping(
-        method = RequestMethod.POST,
-        value = "/register-devices",
-        produces = {"application/json", "application/json+problem"}
-    )
-    public ResponseEntity<PostDailyListResponse> registerDevicesPost(
-        //@NotNull @Parameter(name = "source_system", description = "The source system that has sent the message", required = true, in = ParameterIn.QUERY) @Valid @RequestParam(value = "source_system", required = true) String sourceSystem,
-        @Parameter(name = "node_type", description = "???", in = ParameterIn.QUERY) @Valid @RequestParam(value = "node_type", required = false) String nodeType,
-        @Parameter(name = "court_house", description = "???", in = ParameterIn.QUERY) @Valid @RequestParam(value = "court_house", required = false) String courtHouse,
-        @Parameter(name = "court_room", description = "???", in = ParameterIn.QUERY) @Valid @RequestParam(value = "court_room", required = false) String courtRoom,
-        @Parameter(name = "host_name", description = "", in = ParameterIn.QUERY) @Valid @RequestParam(value = "host_name", required = false) String hostName,
-        @Parameter(name = "ip_address", description = "", in = ParameterIn.QUERY) @Valid @RequestParam(value = "ip_address", required = false) String ipAddress,
-        @Parameter(name = "mac_address", description = "", in = ParameterIn.QUERY) @Valid @RequestParam(value = "mac_address", required = false) String macAddress
-
-    ) {
-
-        DailyListPostRequest postRequest = new DailyListPostRequest();
-        postRequest.setSourceSystem(sourceSystem);
-        postRequest.setCourthouse(courthouse);
-        postRequest.setDailyListXml(xmlDocument);
-        postRequest.setDailyListJson(jsonDocument);
-        postRequest.setHearingDate(hearingDate);
-        postRequest.setUniqueId(uniqueId);
-        postRequest.setPublishedDateTime(publishedTs);
-
-        DailyListPostValidator.validate(postRequest);
-        PostDailyListResponse postDailyListResponse = dailyListService.saveDailyListToDatabase(postRequest);
-        return new ResponseEntity<>(postDailyListResponse, HttpStatus.OK);
-
-
-        return new ResponseEntity<>(null, HttpStatus.OK);
-    }
-
-     */
 }
