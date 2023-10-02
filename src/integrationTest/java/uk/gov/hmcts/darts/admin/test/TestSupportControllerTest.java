e78b7676d03943dbed79d7a7541dd71e5786c028
tonym <incalabsltd@gmail.com>
2023-10-02 15:53:40 +0100

    Adding test coverage db clean

--- src/integrationTest/java/uk/gov/hmcts/darts/admin/test/TestSupportControllerTest.java
+++ src/integrationTest/java/uk/gov/hmcts/darts/admin/test/TestSupportControllerTest.java
@@ -7,6 +7,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import uk.gov.hmcts.darts.testutils.IntegrationBase;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -43,6 +44,17 @@ class TestSupportControllerTest extends IntegrationBase {
 
         assertThat(dartsDatabase.findCourtroomBy("func-swansea", "cr1")).isNotNull();
         assertThat(dartsDatabase.findCourtroomBy("func-swansea", "cr2")).isNotNull();
+    }
+
+    @Test
+    void cleansData() throws Exception {
+        mockMvc.perform(post(ENDPOINT_URL + "/courthouse/func-swansea/courtroom/cr1"))
+            .andExpect(status().isCreated());
+
+        mockMvc.perform(delete(ENDPOINT_URL + "/clean"))
+            .andExpect(status().is2xxSuccessful());
+
+        assertThat(dartsDatabase.findCourtroomBy("func-swansea", "cr1")).isNull();
     }
 
 }
