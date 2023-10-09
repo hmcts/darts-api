ea459681b7e64fefc836aef44b0b42adc78f9b6b
Chris Magowan <131666106+cmagowan-hmcts@users.noreply.github.com>
2023-10-09 09:59:18 +0100

    DMP-154 - Transcription Request handler (#533)

---
+++ src/main/resources/db/migration/common/V1_222__DeleteSecurityGroups.sql
@@ -0,0 +1,5 @@
+delete from darts.security_group_courthouse_ae where grp_id>0;
+delete from darts.security_group_user_account_ae where grp_id>0;
+delete from darts.security_group where grp_id>0;
+
+
