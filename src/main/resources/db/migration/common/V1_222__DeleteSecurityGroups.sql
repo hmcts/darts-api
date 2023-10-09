e19144b3dc3586b4b7f0f0b48267a0fbf6d35b29
Chris Magowan <chris.magowan@hmcts.net>
2023-10-09 09:25:54 +0100

    fix typo

--- src/main/resources/db/migration/common/V1_222__DeleteSecurityGroups.sql
+++ src/main/resources/db/migration/common/V1_222__DeleteSecurityGroups.sql
@@ -1,4 +1,4 @@
-delete from darts.security_group_courthouse_aev where grp_id>0;
+delete from darts.security_group_courthouse_ae where grp_id>0;
 delete from darts.security_group_user_account_ae where grp_id>0;
 delete from darts.security_group where grp_id>0;
 
