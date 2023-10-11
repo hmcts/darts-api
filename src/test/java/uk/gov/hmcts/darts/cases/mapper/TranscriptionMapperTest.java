3b68b28f290f536374aa1a4e9c0cf1dbab8dbc4d
Chris Magowan <chris.magowan@hmcts.net>
2023-10-11 11:17:21 +0100

    add coverage

--- src/test/java/uk/gov/hmcts/darts/cases/mapper/TranscriptionMapperTest.java
+++ src/test/java/uk/gov/hmcts/darts/cases/mapper/TranscriptionMapperTest.java
@@ -4,10 +4,16 @@ import org.junit.jupiter.api.Test;
 import uk.gov.hmcts.darts.cases.model.Transcript;
 import uk.gov.hmcts.darts.common.entity.HearingEntity;
 import uk.gov.hmcts.darts.common.entity.TranscriptionEntity;
+import uk.gov.hmcts.darts.common.entity.TranscriptionStatusEntity;
+import uk.gov.hmcts.darts.common.entity.TranscriptionTypeEntity;
 import uk.gov.hmcts.darts.common.util.CommonTestDataUtil;
+import uk.gov.hmcts.darts.transcriptions.enums.TranscriptionStatusEnum;
+import uk.gov.hmcts.darts.transcriptions.enums.TranscriptionTypeEnum;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.time.OffsetDateTime;
+import java.time.ZoneOffset;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -27,6 +33,34 @@ class TranscriptionMapperTest {
         assertEquals("SENTENCING_REMARKS", transcript.getType());
         assertEquals(LocalDate.of(2020, 6, 20), transcript.getRequestedOn());
         assertEquals("testUsername", transcript.getRequestedByName());
+        assertEquals("APPROVED", transcript.getStatus());
+    }
+
+    @Test
+    void happyPathLegacy() {
+        TranscriptionEntity transcription = new TranscriptionEntity();
+        transcription.setCourtCase(CommonTestDataUtil.createCase("theCase"));
+        TranscriptionTypeEntity transcriptionType = new TranscriptionTypeEntity();
+        transcriptionType.setId(1);
+        transcriptionType.setDescription(TranscriptionTypeEnum.SENTENCING_REMARKS.name());
+        transcription.setTranscriptionType(transcriptionType);
+        transcription.setCreatedDateTime(OffsetDateTime.of(2020, 6, 20, 10, 10, 0, 0, ZoneOffset.UTC));
+        transcription.setId(1);
+        transcription.setHearingDate(OffsetDateTime.of(2023, 6, 20, 12, 0, 0, 0, ZoneOffset.UTC));
+        transcription.setRequestor("someLegacyRequestor");
+
+        TranscriptionStatusEntity transcriptionStatus = new TranscriptionStatusEntity();
+        transcriptionStatus.setId(TranscriptionStatusEnum.APPROVED.getId());
+        transcriptionStatus.setStatusType(TranscriptionStatusEnum.APPROVED.name());
+        transcription.setTranscriptionStatus(transcriptionStatus);
+
+        List<Transcript> transcripts = TranscriptionMapper.mapResponse(List.of(transcription));
+        Transcript transcript = transcripts.get(0);
+        assertEquals(1, transcript.getTraId());
+        assertEquals(LocalDate.of(2023, 6, 20), transcript.getHearingDate());
+        assertEquals("SENTENCING_REMARKS", transcript.getType());
+        assertEquals(LocalDate.of(2020, 6, 20), transcript.getRequestedOn());
+        assertEquals("someLegacyRequestor", transcript.getRequestedByName());
         assertEquals("APPROVED", transcript.getStatus());
     }
 }
