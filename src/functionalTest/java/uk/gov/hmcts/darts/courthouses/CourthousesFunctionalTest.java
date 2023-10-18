8b8edee6c7bc172215f8b222495df0954e863c04
mmmalik1 <mohammed.malik1@hmcts.net>
2023-10-18 14:31:10 +0100

    Test data updated
    Test data updated and clean up added

--- src/functionalTest/java/uk/gov/hmcts/darts/courthouses/CourthousesFunctionalTest.java
+++ src/functionalTest/java/uk/gov/hmcts/darts/courthouses/CourthousesFunctionalTest.java
@@ -3,12 +3,11 @@ package uk.gov.hmcts.darts.courthouses;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import lombok.extern.slf4j.Slf4j;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import uk.gov.hmcts.darts.FunctionalTest;
 
-import java.util.List;
+import java.util.Random;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -16,11 +15,21 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class CourthousesFunctionalTest extends FunctionalTest {
     public static final String COURTHOUSES_URI = "/courthouses";
     public static final String COURTHOUSE_BODY = """
-        {"courthouse_name": "BIRMINGHAM","code": 5705}""";
+        {"courthouse_name": """ +
+        getRandomCourtnameAsTimestamp() +
+        """
+            ,"code": """ +
+        getCode() +
+        """
+            }""";
+
     public static final String COURTHOUSE_UPDATEBODY = """
         {"courthouse_name": "MANCHESTER","code": 2112}""";
     public static final String COURTHOUSE_INVALIDBODY = """
         {"courthouse_name": "READING", code: "1234"}""";
+    public static final String CLEAN_UP_DELETE_COURTHOUSE = "Clean Up: Delete courthouse: ";
+
+    private int createdCourthouseId;
     public static final String COURTHOUSE_BAD_ID = "/99999";
     public static final int OK = 200;
     public static final int CREATED = 201;
@@ -29,12 +38,11 @@ class CourthousesFunctionalTest extends FunctionalTest {
     public static final int NOT_FOUND = 404;
     public static final int RESOURCE_ALREADY_EXISTS = 409;
 
-    public static final int INTERNAL_SERVER_ERROR = 500;
-
 
     @Test
     @Order(1)
-    void createCourthouse() {
+    void createAndDeleteCourthouse() {
+        /*
         Response response = buildRequestWithExternalAuth()
             .contentType(ContentType.JSON)
             .when()
@@ -43,81 +51,84 @@ class CourthousesFunctionalTest extends FunctionalTest {
             .post()
             .then()
             .extract().response();
+*/
+        Response response = createCourthouse(COURTHOUSE_BODY);
+        createdCourthouseId = getCreatedCourthouseId(response);
+
+        log.info("Created id: " + createdCourthouseId);
 
         assertEquals(CREATED, response.statusCode());
+
+        cleanUpCourthouse(createdCourthouseId);
     }
 
     @Test
-    @Disabled
     @Order(2)
     void createSameCourthouse() {
+        String courthouseBody = COURTHOUSE_BODY;
+        createCourthouse(courthouseBody);
+        int courthouseId = createdCourthouseId;
+
+        log.info("Courthouse created id: " + courthouseId);
+        log.info("Create same courthouse with the same body.");
+
         Response response = buildRequestWithExternalAuth()
             .contentType(ContentType.JSON)
             .when()
             .baseUri(getUri(COURTHOUSES_URI))
-            .body(COURTHOUSE_BODY)
+            .body(courthouseBody)
             .post()
             .then()
             .extract().response();
-
         assertEquals(RESOURCE_ALREADY_EXISTS, response.statusCode());
+
+        cleanUpCourthouse(courthouseId);
     }
 
     @Test
-    @Disabled
     @Order(3)
     void updateCourthouse() {
-        int courthouseID = getLatestCourthouseID();
+        createCourthouse(COURTHOUSE_BODY);
+        int courthouseId = createdCourthouseId;
+
+        log.info("Courthouse created id: " + courthouseId);
+        log.info("Update same courthouse with the same body.");
+
         Response response = buildRequestWithExternalAuth()
             .contentType(ContentType.JSON)
             .when()
-            .baseUri(getUri(COURTHOUSES_URI + "/" + courthouseID))
+            .baseUri(getUri(COURTHOUSES_URI + "/" + courthouseId))
             .body(COURTHOUSE_UPDATEBODY)
             .put()
             .then()
             .extract().response();
 
         assertEquals(NO_CONTENT, response.statusCode());
+
+        cleanUpCourthouse(courthouseId);
     }
 
     @Test
     @Order(4)
     void updateCourthouseWithInvalidBody() {
-        int courthouseID = getLatestCourthouseID();
+        createCourthouse(COURTHOUSE_BODY);
+        int courthouseId = createdCourthouseId;
         Response response = buildRequestWithExternalAuth()
             .contentType(ContentType.JSON)
             .when()
-            .baseUri(getUri(COURTHOUSES_URI + "/" + courthouseID))
+            .baseUri(getUri(COURTHOUSES_URI + "/" + courthouseId))
             .body(COURTHOUSE_INVALIDBODY)
             .put()
             .then()
             .extract().response();
 
         assertEquals(BAD_REQUEST, response.statusCode());
+
+        cleanUpCourthouse(courthouseId);
     }
 
     @Test
-    @Disabled
     @Order(5)
-    void deleteCourthouse() {
-        int courthouseID = getLatestCourthouseID();
-        Response response = buildRequestWithExternalAuth()
-            .contentType(ContentType.JSON)
-            .when()
-            .baseUri(getUri(COURTHOUSES_URI  + "/" + courthouseID))
-            .delete()
-            .then()
-            .extract().response();
-
-        int respCode = response.statusCode();
-        int output = INTERNAL_SERVER_ERROR;
-
-        assertEquals(output, respCode);
-
-    }
-
-    @Test
-    @Order(6)
     void getAllCourthouses() {
         Response response = buildRequestWithExternalAuth()
             .contentType(ContentType.JSON)
@@ -131,9 +142,11 @@ class CourthousesFunctionalTest extends FunctionalTest {
     }
 
     @Test
-    @Order(7)
+    @Order(6)
     void getExistingCourthouse() {
-        int courthouseID = getLatestCourthouseID();
+        createCourthouse(COURTHOUSE_BODY);
+        int courthouseID = createdCourthouseId;
+
         Response response = buildRequestWithExternalAuth()
             .contentType(ContentType.JSON)
             .when()
@@ -143,11 +156,12 @@ class CourthousesFunctionalTest extends FunctionalTest {
             .extract().response();
 
         assertEquals(OK, response.statusCode());
+
+        cleanUpCourthouse(courthouseID);
     }
 
-
     @Test
-    @Order(8)
+    @Order(7)
     void getCourthouseIdDoesNotExist() {
         Response response = buildRequestWithExternalAuth()
             .contentType(ContentType.JSON)
@@ -160,20 +174,58 @@ class CourthousesFunctionalTest extends FunctionalTest {
         assertEquals(NOT_FOUND, response.statusCode());
     }
 
-    //----------------------------------
-    private int getLatestCourthouseID() {
-        List<Integer> ids = buildRequestWithExternalAuth()
+    //================
+
+    Response createCourthouse(String courthouseBody) {
+        Response response = buildRequestWithExternalAuth()
             .contentType(ContentType.JSON)
             .when()
             .baseUri(getUri(COURTHOUSES_URI))
-            .get()
+            .body(courthouseBody)
+            .post()
             .then()
-            .extract()
-            .response()
-            .getBody()
-            .jsonPath().get("id");
+            .extract().response();
 
-        int idLen = ids.size();
-        return ids.get(--idLen);
+        createdCourthouseId = getCreatedCourthouseId(response);
+
+        log.info("Created id: " + createdCourthouseId);
+
+        assertEquals(CREATED, response.statusCode());
+
+        return response;
+    }
+
+    private void cleanUpCourthouse(int courthouseID) {
+        log.info(CLEAN_UP_DELETE_COURTHOUSE + courthouseID);
+        deleteCourthouse(courthouseID);
+    }
+
+    void deleteCourthouse(int courthouseId) {
+        log.info("Delete id: " + courthouseId);
+
+        Response response = buildRequestWithExternalAuth()
+            .contentType(ContentType.JSON)
+            .when()
+            .baseUri(getUri(COURTHOUSES_URI  + "/" + courthouseId))
+            .delete()
+            .then()
+            .extract().response();
+
+        assertEquals(NO_CONTENT, response.statusCode());
+    }
+
+    //----------------------------------
+
+    private static long getRandomCourtnameAsTimestamp() {
+        return System.currentTimeMillis();
+    }
+
+    private static int getCode() {
+        Random random = new Random(System.currentTimeMillis());
+        return 10_000 + random.nextInt(20_000);
+    }
+
+    private static int getCreatedCourthouseId(Response response) {
+        return response.getBody().jsonPath().get("id");
     }
 }
