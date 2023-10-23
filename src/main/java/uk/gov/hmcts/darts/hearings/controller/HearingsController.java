9537e7048be82d44ad1100da051666a2777473f3
Chris Davidson <christopher.davidson@btinternet.com>
2023-10-23 15:19:06 +0100

    dmp 1327 fix merge

--- src/main/java/uk/gov/hmcts/darts/hearings/controller/HearingsController.java
+++ src/main/java/uk/gov/hmcts/darts/hearings/controller/HearingsController.java
@@ -1,9 +1,11 @@
 package uk.gov.hmcts.darts.hearings.controller;
 
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
+import uk.gov.hmcts.darts.authorisation.annotation.Authorisation;
 import uk.gov.hmcts.darts.hearings.api.HearingsApi;
 import uk.gov.hmcts.darts.hearings.model.EventResponse;
 import uk.gov.hmcts.darts.hearings.model.GetHearingResponse;
