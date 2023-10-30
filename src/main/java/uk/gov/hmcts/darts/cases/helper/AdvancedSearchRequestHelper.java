4a13143ccb3d15f1257c4b8297d74b3d3914cec2
john.race@hmcts.net <john.race@hmcts.net>
2023-10-30 09:57:30 +0000

    DMP-626 Added toLower()

--- src/main/java/uk/gov/hmcts/darts/cases/helper/AdvancedSearchRequestHelper.java
+++ src/main/java/uk/gov/hmcts/darts/cases/helper/AdvancedSearchRequestHelper.java
@@ -160,7 +160,7 @@ public class AdvancedSearchRequestHelper {
         Join<CourtCaseEntity, UserAccountEntity> userJoin = joinUser(caseRoot);
         predicateList.add(criteriaBuilder.equal(
                               criteriaBuilder.lower(userJoin.get(UserAccountEntity_.EMAIL_ADDRESS)),
-                              userIdentity.getEmailAddress()
+                              userIdentity.getEmailAddress().toLowerCase()
                           )
         );
         return predicateList;
