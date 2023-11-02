3f5ce2e5bbdf42ac04c82e8191ce21a44701717b
Saad Arif <saad.arif@hmcts.net>
2023-11-02 09:44:23 +0000

    DMP-593 - review comments.

--- src/main/java/uk/gov/hmcts/darts/audio/config/AudioConfiguration.java
+++ src/main/java/uk/gov/hmcts/darts/audio/config/AudioConfiguration.java
@@ -1,6 +1,5 @@
 package uk.gov.hmcts.darts.audio.config;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,9 +9,6 @@ import java.time.Clock;
 @Configuration
 @EnableConfigurationProperties(AudioConfigurationProperties.class)
 public class AudioConfiguration {
-
-    @Value("${darts.audio.outbounddeleter.last-accessed-deletion-day:2}")
-    private long lastAccessedDeletionDays;
 
     @Bean
     public Clock clock() {
