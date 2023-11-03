fff7d398baac620dbe4896b25a3d0f6d90751bfc
tonym <incalabsltd@gmail.com>
2023-11-03 11:06:42 +0000

    Tidying

--- src/main/resources/db/migration/common/V1_232__security_tables_add_columns.sql
+++ src/main/resources/db/migration/common/V1_232__security_tables_add_columns.sql
@@ -1,8 +1,5 @@
-ALTER TABLE security_group
-  ADD COLUMN global_access boolean NOT NULL DEFAULT FALSE;
-
-ALTER TABLE security_group
-  ADD COLUMN display_state boolean NOT NULL DEFAULT TRUE;
+ALTER TABLE security_group ADD COLUMN global_access boolean NOT NULL DEFAULT FALSE;
+ALTER TABLE security_group ADD COLUMN display_state boolean NOT NULL DEFAULT TRUE;
 
 ALTER TABLE security_role ADD COLUMN display_name CHARACTER VARYING;
 UPDATE security_role SET display_name = '';
