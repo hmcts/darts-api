2fa262043ffb0f6123d7aad86d991890b38f38ff
Dave Thompson <david.thompson@hmcts.net>
2023-11-06 15:11:40 +0000

    Adding `/preview2` audio preview endpoint (#644)
    Temporary endpoint for testing audio skipping and streaming

--- src/main/java/uk/gov/hmcts/darts/audio/controller/AudioController.java
+++ src/main/java/uk/gov/hmcts/darts/audio/controller/AudioController.java
@@ -2,11 +2,14 @@ package uk.gov.hmcts.darts.audio.controller;
 
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.io.IOUtils;
 import org.springframework.core.io.InputStreamResource;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 import uk.gov.hmcts.darts.audio.api.AudioApi;
 import uk.gov.hmcts.darts.audio.component.AudioResponseMapper;
@@ -77,12 +80,62 @@ public class AudioController implements AudioApi {
         return new ResponseEntity<>(new InputStreamResource(audioMediaFile), HttpStatus.OK);
     }
 
+    @SneakyThrows
+    @Override
+    @SecurityRequirement(name = SECURITY_SCHEMES_BEARER_AUTH)
+    @Authorisation(contextId = MEDIA_ID,
+        securityRoles = {JUDGE, REQUESTER, APPROVER, TRANSCRIBER, LANGUAGE_SHOP_USER, RCJ_APPEALS})
+    public ResponseEntity<byte[]> preview2(Integer mediaId, @RequestHeader(value = "Range", required = false) String httpRangeList) {
+        InputStream audioMediaFile = audioService.preview(mediaId);
+
+        if (httpRangeList == null) {
+            byte[] bytes = IOUtils.toByteArray(audioMediaFile);
+            return ResponseEntity.status(HttpStatus.OK)
+                .header("Content-Type", "audio/mpeg")
+                .header("Content-Length", String.valueOf(bytes.length))
+                .body(bytes);
+        } else {
+            byte[] bytes = IOUtils.toByteArray(audioMediaFile);
+            long fileSize = bytes.length;
+            String[] ranges = httpRangeList.split("-");
+            long rangeStart = Long.parseLong(ranges[0].substring(6));
+            long rangeEnd;
+            if (ranges.length > 1) {
+                rangeEnd = Long.parseLong(ranges[1]);
+            } else {
+                rangeEnd = fileSize - 1;
+            }
+            if (fileSize < rangeEnd) {
+                rangeEnd = fileSize - 1;
+            }
+            String contentLength = String.valueOf((rangeEnd - rangeStart) + 1);
+            String contentRange = "bytes" + " " + rangeStart + "-" + rangeEnd + "/" + fileSize;
+            return ResponseEntity.status(HttpStatus.OK)
+                .header("Content-Type", "audio/mpeg")
+                .header("Content-Length", contentLength)
+                .header("Content-Range", contentRange)
+                .body(readByteRange(bytes, rangeStart, rangeEnd));
+        }
+    }
+
+    public byte[] readByteRange(byte[] wholeFile, long start, long end) {
+        int srcPos;
+        if (start > Integer.MIN_VALUE && start < Integer.MAX_VALUE) {
+            srcPos = (int) start;
+        } else {
+            throw new IllegalArgumentException("Invalid input: start bytes truncated");
+        }
+
+        byte[] result = new byte[(int) (end - start) + 1];
+        System.arraycopy(wholeFile, srcPos, result, 0, result.length);
+        return result;
+    }
+
     @Override
     @SecurityRequirement(name = SECURITY_SCHEMES_BEARER_AUTH)
     public ResponseEntity<Void> addAudioMetaData(AddAudioMetadataRequest addAudioMetadataRequest) {
         audioService.addAudio(addAudioMetadataRequest);
         return new ResponseEntity<>(HttpStatus.OK);
-
     }
 
 }
