baf35eff087bbec34b5c16346f2016a49312ab0a
Dave Thompson <david.thompson@hmcts.net>
2023-11-07 12:17:44 +0000

    Amending audio/preview2 to use different headers (#651)

--- src/main/java/uk/gov/hmcts/darts/audio/controller/AudioController.java
+++ src/main/java/uk/gov/hmcts/darts/audio/controller/AudioController.java
@@ -88,14 +88,14 @@ public class AudioController implements AudioApi {
     public ResponseEntity<byte[]> preview2(Integer mediaId, @RequestHeader(value = "Range", required = false) String httpRangeList) {
         InputStream audioMediaFile = audioService.preview(mediaId);
 
+        byte[] bytes = IOUtils.toByteArray(audioMediaFile);
         if (httpRangeList == null) {
-            byte[] bytes = IOUtils.toByteArray(audioMediaFile);
             return ResponseEntity.status(HttpStatus.OK)
-                .header("Content-Type", "audio/mpeg")
+                .header("Content-Type", "application/octet-stream")
+                .header("Content-Disposition", "attachment; filename=\"" + mediaId.toString() + ".mp3\"")
                 .header("Content-Length", String.valueOf(bytes.length))
                 .body(bytes);
         } else {
-            byte[] bytes = IOUtils.toByteArray(audioMediaFile);
             long fileSize = bytes.length;
             String[] ranges = httpRangeList.split("-");
             long rangeStart = Long.parseLong(ranges[0].substring(6));
