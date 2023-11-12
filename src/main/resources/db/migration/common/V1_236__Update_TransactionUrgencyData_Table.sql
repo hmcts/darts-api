b918431ab492f01ee836744e9a07129aef0d161e
karen-hedges <133129444+karen-hedges@users.noreply.github.com>
2023-11-12 15:25:30 +0000

    DMP-1420-TranscriptionsUrgenciesFix
    Fix to remove the Up to from the urgency descriptions

---
+++ src/main/resources/db/migration/common/V1_236__Update_TransactionUrgencyData_Table.sql
@@ -0,0 +1,12 @@
+UPDATE darts.transcription_urgency
+SET description = '3 working days'
+WHERE tru_id = 4;
+
+UPDATE darts.transcription_urgency
+SET description = '7 working days'
+WHERE tru_id = 5;
+
+UPDATE darts.transcription_urgency
+SET description = '12 working days'
+WHERE tru_id = 6;
+
