0814662d02c68d17f9935e5035620141e1dd6a01
Saad Arif <saad.arif@hmcts.net>
2023-11-15 13:05:12 +0000

    DMP-594 - fixed rebase flyway conflict.

--- src/main/resources/db/migration/common/V1_236__insert_external_datastore_deleter.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO darts.automated_task (aut_id,task_name,task_description,cron_expression,cron_editable)
-VALUES (4,'ExternalDataStoreDeleter','Deletes data marked for deletion in inbound, unstructured, outbound datastores','00 22 * * * *',true);
-
-ALTER SEQUENCE aut_seq RESTART WITH 5;
