464a48c4c330dfe62ddd47aa9ffdd0d6a92fa555
Chris Davidson <christopher.davidson@btinternet.com>
2023-11-15 11:34:43 +0000

    checkstyle

--- src/main/java/uk/gov/hmcts/darts/common/entity/CaseManagementRetentionEntity.java
+++ src/main/java/uk/gov/hmcts/darts/common/entity/CaseManagementRetentionEntity.java
@@ -1,6 +1,15 @@
 package uk.gov.hmcts.darts.common.entity;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
 
