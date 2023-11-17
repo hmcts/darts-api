6db690795e7679bc34bbfa5173acd0938d263920
Saad Arif <saad.arif@hmcts.net>
2023-11-17 12:39:28 +0000

    DMP-594 - changed job to run at 8 pm everyday

--- src/main/resources/db/migration/common/V1_239__insert_external_datastore_deleter.sql
+++ src/main/resources/db/migration/common/V1_239__insert_external_datastore_deleter.sql
@@ -1,4 +1,4 @@
 INSERT INTO darts.automated_task (aut_id,task_name,task_description,cron_expression,cron_editable)
-VALUES (5,'ExternalDataStoreDeleter','Deletes data marked for deletion in inbound, unstructured, outbound datastores','00 22 * * * *',true);
+VALUES (5,'ExternalDataStoreDeleter','Deletes data marked for deletion in inbound, unstructured, outbound datastores','0 0 20 * * *',true);
 
 ALTER SEQUENCE aut_seq RESTART WITH 6;
