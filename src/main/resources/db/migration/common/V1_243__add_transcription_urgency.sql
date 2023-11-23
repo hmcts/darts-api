75f36d74fb4dba0e0fd4bb69c8212fd188816051
Chris Davidson <christopher.davidson@btinternet.com>
2023-11-23 12:16:12 +0000

    dmp 1582 added urgencies

---
+++ src/main/resources/db/migration/common/V1_243__add_transcription_urgency.sql
@@ -0,0 +1,6 @@
+INSERT INTO transcription_urgency
+(tru_id, description, display_state)
+VALUES(7, 'Up to 2 working days', true);
+UPDATE transcription_urgency
+set display_state=true
+WHERE tru_id = 3;
