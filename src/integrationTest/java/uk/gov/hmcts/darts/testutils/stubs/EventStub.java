8757f9a3802a6ee9fad8f567006c5d497adb1784
Chris Davidson <christopher.davidson@btinternet.com>
2023-12-13 10:36:42 +0000

    DMP-1599 added view, entity and test (#815)

--- src/integrationTest/java/uk/gov/hmcts/darts/testutils/stubs/EventStub.java
+++ src/integrationTest/java/uk/gov/hmcts/darts/testutils/stubs/EventStub.java
@@ -3,6 +3,7 @@ package uk.gov.hmcts.darts.testutils.stubs;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.darts.common.entity.EventEntity;
+import uk.gov.hmcts.darts.common.entity.EventHandlerEntity;
 import uk.gov.hmcts.darts.common.entity.HearingEntity;
 import uk.gov.hmcts.darts.common.repository.EventHandlerRepository;
 import uk.gov.hmcts.darts.common.repository.EventRepository;
@@ -18,15 +19,21 @@ public class EventStub {
     private final EventHandlerRepository eventHandlerRepository;
     private final UserAccountStub userAccountStub;
 
-
     public EventEntity createEvent(HearingEntity hearing) {
+        return createEvent(hearing, 10);
+    }
+
+    public EventEntity createEvent(HearingEntity hearing, int eventHandlerId) {
         EventEntity eventEntity = new EventEntity();
         eventEntity.setEventText("testEventText");
         eventEntity.setEventName("testEventName");
-        eventEntity.setEventType(eventHandlerRepository.findById(10).get());
+        EventHandlerEntity eventHandlerEntity = eventHandlerRepository.findById(eventHandlerId).get();
+        eventEntity.setEventType(eventHandlerEntity);
         eventEntity.setTimestamp(OffsetDateTime.of(2020, 6, 20, 10, 0, 0, 0, ZoneOffset.UTC));
         eventEntity.setCreatedBy(userAccountStub.getIntegrationTestUserAccountEntity());
         eventEntity.addHearing(hearing);
+        eventEntity.setLastModifiedBy(userAccountStub.getIntegrationTestUserAccountEntity());
+        eventEntity.setLastModifiedDateTime(OffsetDateTime.of(2020, 6, 20, 10, 0, 0, 0, ZoneOffset.UTC));
         eventRepository.saveAndFlush(eventEntity);
         return eventEntity;
     }
