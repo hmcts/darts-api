67895e3df01be1a9c4fc9d421cd1ea67cd67b756
Jack Maloney <jack.maloney1@hmcts.net>
2023-12-15 14:18:30 +0000

    Checkstyle fixes

--- src/integrationTest/java/uk/gov/hmcts/darts/transcriptions/controller/TranscriptionControllerGetTranscriptionTranscriberCountsIntTest.java
+++ src/integrationTest/java/uk/gov/hmcts/darts/transcriptions/controller/TranscriptionControllerGetTranscriptionTranscriberCountsIntTest.java
@@ -39,28 +39,37 @@ class TranscriptionControllerGetTranscriptionTranscriberCountsIntTest extends In
     @SuppressWarnings({"checkstyle.LineLengthCheck"})
     void beforeAll() {
         jdbcTemplate.update("""
-                                INSERT INTO darts.courthouse (cth_id, courthouse_code, courthouse_name, created_ts, last_modified_ts, created_by, last_modified_by, display_name)
+                                INSERT INTO darts.courthouse (cth_id, courthouse_code, courthouse_name, created_ts, last_modified_ts, created_by,
+                                last_modified_by, display_name)
                                 VALUES (-1, NULL, 'Bristol', '2023-11-17 15:06:15.859244+00', '2023-11-17 15:06:15.859244+00', NULL, NULL, 'Bristol');
                                 INSERT INTO darts.courtroom (ctr_id, cth_id, courtroom_name, created_ts, created_by)
                                 VALUES (-1, -1, 'Court 1', NULL, NULL);
-                                INSERT INTO darts.court_case (cas_id, cth_id, evh_id, case_object_id, case_number, case_closed, interpreter_used, case_closed_ts, version_label, created_ts, created_by, last_modified_ts, last_modified_by, retention_applies_from_ts, end_of_sentence_ts)
+                                INSERT INTO darts.court_case (cas_id, cth_id, evh_id, case_object_id, case_number, case_closed, interpreter_used,
+                                case_closed_ts, version_label, created_ts, created_by, last_modified_ts, last_modified_by, retention_applies_from_ts,
+                                end_of_sentence_ts)
                                 VALUES (-1, -1, NULL, NULL, 'T20231009-1', false, false, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
-                                INSERT INTO darts.hearing (hea_id, cas_id, ctr_id, hearing_date, scheduled_start_time, hearing_is_actual, judge_hearing_date, created_ts, created_by, last_modified_ts, last_modified_by)
+                                INSERT INTO darts.hearing (hea_id, cas_id, ctr_id, hearing_date, scheduled_start_time, hearing_is_actual, judge_hearing_date,
+                                created_ts, created_by, last_modified_ts, last_modified_by)
                                 VALUES (-1, -1, -1, '2023-11-17', NULL, true, NULL, NULL, NULL, NULL, NULL);
 
-                                INSERT INTO darts.user_account (usr_id, dm_user_s_object_id, user_name, user_email_address, description, is_active, created_ts, last_modified_ts, last_login_ts, last_modified_by, created_by, account_guid, is_system_user)
+                                INSERT INTO darts.user_account (usr_id, dm_user_s_object_id, user_name, user_email_address, description, is_active, created_ts,
+                                last_modified_ts, last_login_ts, last_modified_by, created_by, account_guid, is_system_user)
                                 VALUES (-10, NULL, 'John R', 'John.R@example.com', NULL, true, NULL, NULL, NULL, NULL, NULL, NULL, false);
                                 INSERT INTO darts.security_group_user_account_ae (usr_id, grp_id)
                                 VALUES (-10, -4);
                                 INSERT INTO darts.security_group_courthouse_ae (grp_id, cth_id)
                                 VALUES (-4, -1);
 
-                                INSERT INTO darts.user_account (usr_id, dm_user_s_object_id, user_name, user_email_address, description, is_active, created_ts, last_modified_ts, last_login_ts, last_modified_by, created_by, account_guid, is_system_user)
+                                INSERT INTO darts.user_account (usr_id, dm_user_s_object_id, user_name, user_email_address, description, is_active, created_ts,
+                                last_modified_ts, last_login_ts, last_modified_by, created_by, account_guid, is_system_user)
                                 VALUES (-20, NULL, 'John R', 'John.R@example.com', NULL, true, NULL, NULL, NULL, NULL, NULL, NULL, false);
 
                                 -- Transcript Requests: Approved
-                                INSERT INTO darts.transcription (tra_id, cas_id, ctr_id, trt_id, hea_id, transcription_object_id, requestor, start_ts, end_ts, created_ts, last_modified_ts, last_modified_by, version_label, created_by, tru_id, trs_id, hearing_date, is_manual_transcription, hide_request_from_requestor)
-                                VALUES (41, -1, -1, 9, -1, NULL, NULL, '2023-11-23 09:00:00+00', '2023-11-23 09:30:00+00', '2023-11-23 16:25:55.297666+00', '2023-11-23 16:26:20.451054+00', -10, NULL, -10, 1, 3, NULL, true, false);
+                                INSERT INTO darts.transcription (tra_id, cas_id, ctr_id, trt_id, hea_id, transcription_object_id, requestor, start_ts, end_ts,
+                                created_ts, last_modified_ts, last_modified_by, version_label, created_by, tru_id, trs_id, hearing_date,
+                                is_manual_transcription, hide_request_from_requestor)
+                                VALUES (41, -1, -1, 9, -1, NULL, NULL, '2023-11-23 09:00:00+00', '2023-11-23 09:30:00+00', '2023-11-23 16:25:55.297666+00',
+                                '2023-11-23 16:26:20.451054+00', -10, NULL, -10, 1, 3, NULL, true, false);
                                 INSERT INTO darts.transcription_workflow (trw_id, tra_id, trs_id, workflow_actor, workflow_ts)
                                 VALUES (41, 41, 1, -10, '2023-11-23 16:25:55.304517+00');
                                 INSERT INTO darts.transcription_workflow (trw_id, tra_id, trs_id, workflow_actor, workflow_ts)
@@ -69,8 +78,11 @@ class TranscriptionControllerGetTranscriptionTranscriberCountsIntTest extends In
                                 VALUES (43, 41, 3, -10, '2023-11-23 16:26:20.441633+00');
 
                                 -- Transcript Requests: With Transcriber
-                                INSERT INTO darts.transcription (tra_id, cas_id, ctr_id, trt_id, hea_id, transcription_object_id, requestor, start_ts, end_ts, created_ts, last_modified_ts, last_modified_by, version_label, created_by, tru_id, trs_id, hearing_date, is_manual_transcription, hide_request_from_requestor)
-                                VALUES (81, -1, NULL, 9, -1, NULL, NULL, '2023-11-23 09:20:00+00', '2023-11-23 09:30:00+00', '2023-11-23 17:45:14.938855+00', '2023-11-23 17:45:51.1549+00', -10, NULL, -10, 1, 5, NULL, true, false);
+                                INSERT INTO darts.transcription (tra_id, cas_id, ctr_id, trt_id, hea_id, transcription_object_id, requestor, start_ts, end_ts,
+                                created_ts, last_modified_ts, last_modified_by, version_label, created_by, tru_id, trs_id, hearing_date,
+                                is_manual_transcription, hide_request_from_requestor)
+                                VALUES (81, -1, NULL, 9, -1, NULL, NULL, '2023-11-23 09:20:00+00', '2023-11-23 09:30:00+00', '2023-11-23 17:45:14.938855+00',
+                                '2023-11-23 17:45:51.1549+00', -10, NULL, -10, 1, 5, NULL, true, false);
                                 INSERT INTO darts.transcription_workflow (trw_id, tra_id, trs_id, workflow_actor, workflow_ts)
                                 VALUES (81, 81, 1, -10, '2023-11-23 17:45:14.940936+00');
                                 INSERT INTO darts.transcription_workflow (trw_id, tra_id, trs_id, workflow_actor, workflow_ts)
@@ -81,8 +93,11 @@ class TranscriptionControllerGetTranscriptionTranscriberCountsIntTest extends In
                                 VALUES (102, 81, 5, -10, '2023-11-23 17:45:51.151621+00');
 
                                 -- Transcript Requests: Approved
-                                INSERT INTO darts.transcription (tra_id, cas_id, ctr_id, trt_id, hea_id, transcription_object_id, requestor, start_ts, end_ts, created_ts, last_modified_ts, last_modified_by, version_label, created_by, tru_id, trs_id, hearing_date, is_manual_transcription, hide_request_from_requestor)
-                                VALUES (101, -1, NULL, 9, -1, NULL, NULL, '2023-11-24 09:00:00+00', '2023-11-24 09:30:00+00', '2023-11-24 12:37:00.782036+00', '2023-11-24 12:53:42.870475+00', -10, NULL, -10, 1, 3, NULL, true, false);
+                                INSERT INTO darts.transcription (tra_id, cas_id, ctr_id, trt_id, hea_id, transcription_object_id, requestor, start_ts, end_ts,
+                                created_ts, last_modified_ts, last_modified_by, version_label, created_by, tru_id, trs_id, hearing_date,
+                                is_manual_transcription, hide_request_from_requestor)
+                                VALUES (101, -1, NULL, 9, -1, NULL, NULL, '2023-11-24 09:00:00+00', '2023-11-24 09:30:00+00', '2023-11-24 12:37:00.782036+00',
+                                '2023-11-24 12:53:42.870475+00', -10, NULL, -10, 1, 3, NULL, true, false);
                                 INSERT INTO darts.transcription_workflow (trw_id, tra_id, trs_id, workflow_actor, workflow_ts)
                                 VALUES (121, 101, 1, -10, '2023-11-24 12:37:00.812692+00');
                                 INSERT INTO darts.transcription_workflow (trw_id, tra_id, trs_id, workflow_actor, workflow_ts)
