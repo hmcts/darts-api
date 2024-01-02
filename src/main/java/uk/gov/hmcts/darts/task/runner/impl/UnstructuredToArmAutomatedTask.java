30287a9c8b92864a5d161339e8a289361e704207
karen-hedges <133129444+karen-hedges@users.noreply.github.com>
2024-01-02 16:22:39 +0000

    DMP-1625-ARM-CreateMediaManifestFile
    Updated the way null pointer is handled in arm task

--- src/main/java/uk/gov/hmcts/darts/task/runner/impl/UnstructuredToArmAutomatedTask.java
+++ src/main/java/uk/gov/hmcts/darts/task/runner/impl/UnstructuredToArmAutomatedTask.java
@@ -34,6 +34,10 @@ public class UnstructuredToArmAutomatedTask extends AbstractLockableAutomatedTas
 
     @Override
     protected void handleException(Exception exception) {
+        if (exception instanceof NullPointerException npe) {
+            log.error("Exception with null: {}", npe.getMessage());
+            npe.printStackTrace();
+        }
         log.error("Exception: {}", exception.getMessage());
     }
 }
