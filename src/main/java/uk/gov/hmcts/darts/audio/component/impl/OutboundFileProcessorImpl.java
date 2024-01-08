c622dc05df152a7b9d0e8968b9baad52a124f7ae
john.race@hmcts.net <john.race@hmcts.net>
2024-01-08 16:31:24 +0000

    DMP-1873 Fixed merge error

--- src/main/java/uk/gov/hmcts/darts/audio/component/impl/OutboundFileProcessorImpl.java
+++ src/main/java/uk/gov/hmcts/darts/audio/component/impl/OutboundFileProcessorImpl.java
@@ -14,6 +14,7 @@ import uk.gov.hmcts.darts.common.entity.MediaEntity;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.time.Duration;
+import java.time.Instant;
 import java.time.OffsetDateTime;
 import java.time.ZoneOffset;
 import java.util.ArrayList;
