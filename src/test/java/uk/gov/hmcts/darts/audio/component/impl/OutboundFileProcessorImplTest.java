00774dab19716da1c1a395df2af4eb04150ecbf7
john.race@hmcts.net <john.race@hmcts.net>
2024-01-12 14:53:58 +0000

    DMP-2033 Removed unused import

--- src/test/java/uk/gov/hmcts/darts/audio/component/impl/OutboundFileProcessorImplTest.java
+++ src/test/java/uk/gov/hmcts/darts/audio/component/impl/OutboundFileProcessorImplTest.java
@@ -1,6 +1,5 @@
 package uk.gov.hmcts.darts.audio.component.impl;
 
-import org.apache.commons.lang3.StringUtils;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
