501bb970d269b1525f0fd628970a7999f8229c9c
Chris Magowan <chris.magowan@hmcts.net>
2024-01-16 11:45:37 +0000

    DMP-2063 - removed unused alias

--- src/main/resources/db/migration/common/V1_255_3_1_v59_v60_updates_user_account.sql
+++ src/main/resources/db/migration/common/V1_255_3_1_v59_v60_updates_user_account.sql
@@ -1,5 +1,5 @@
 ALTER TABLE user_account ADD COLUMN user_full_name  CHARACTER VARYING;
-update darts.user_account dl
+update darts.user_account
 set user_full_name = user_name;
 
 ALTER TABLE user_account ALTER COLUMN user_full_name SET NOT NULL;
