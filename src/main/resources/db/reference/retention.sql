c07d7883a1e43ce07e48e8925b0c732b37f13db5
barry-jones50410 <139450268+barry-jones50410@users.noreply.github.com>
2024-01-16 11:57:04 +0000

    Add files via upload
    correcting file retention.sql for v7, previous had missed a change documented in the change header and making end_date on retention_policy_type nullable

--- src/main/resources/db/reference/retention.sql
+++ src/main/resources/db/reference/retention.sql
@@ -11,15 +11,16 @@
 --    add event_ts to case_management_retention
 --    amend eve_id on case_managment_retention to be nullable
 -- v6 change submitted_ts to submitted_by on case_retention and FK for same, and missing one on created_by
---    add last_modified* to case_retention and FK 
+--    add last_modified* to case_retention and FK
 -- v7 change retention_policy_type.duration from integer to character varying, to support nYnMnD format used elsewhere
+--    amend retention_policy_type.policy_end_ts to nullable
 
 SET ROLE DARTS_OWNER;
 SET SEARCH_PATH TO darts;
 
 --List of Table Aliases
 -- case_management_retention         CMR
--- case_retention                    CAR   
+-- case_retention                    CAR
 -- retention_policy_type             RPT
 
 
@@ -27,8 +28,8 @@ CREATE TABLE case_management_retention
 (cmr_id                      INTEGER                       NOT NULL
 ,cas_id                      INTEGER                       NOT NULL
 ,rpt_id                      INTEGER                       NOT NULL
-,eve_id                      INTEGER                       
-,event_ts                    TIMESTAMP WITH TIME ZONE      
+,eve_id                      INTEGER
+,event_ts                    TIMESTAMP WITH TIME ZONE
 ,total_sentence              CHARACTER VARYING             NOT NULL  -- < is this integer or the nYnMnD >
 ) TABLESPACE darts_tables;
 
@@ -36,13 +37,13 @@ CREATE TABLE case_retention
 (car_id                      INTEGER                       NOT NULL
 ,cas_id                      INTEGER                       NOT NULL
 ,rpt_id                      INTEGER                       NOT NULL
-,cmr_id                      INTEGER            
+,cmr_id                      INTEGER
 ,total_sentence              CHARACTER VARYING             NOT NULL -- < is this integer or the nYnMnD >
-,retain_until_ts             TIMESTAMP WITH TIME ZONE      NOT NULL 
-,retain_until_applied_on_ts  TIMESTAMP WITH TIME ZONE      NOT NULL 
+,retain_until_ts             TIMESTAMP WITH TIME ZONE      NOT NULL
+,retain_until_applied_on_ts  TIMESTAMP WITH TIME ZONE      NOT NULL
 ,current_state               CHARACTER VARYING             NOT NULL  -- can we agree on single chars, eg P-pending, E-expired, A-active
-,comments                    CHARACTER VARYING 
-,retention_object_id         CHARACTER VARYING                       -- PK of legacy source migration table 
+,comments                    CHARACTER VARYING
+,retention_object_id         CHARACTER VARYING                       -- PK of legacy source migration table
 ,submitted_by                INTEGER                       NOT NULL
 ,is_manual_override          BOOLEAN                       NOT NULL
 ,created_ts                  TIMESTAMP WITH TIME ZONE      NOT NULL
@@ -53,11 +54,11 @@ CREATE TABLE case_retention
 
 CREATE TABLE retention_policy_type
 (rpt_id                      INTEGER                       NOT NULL
-,fixed_policy_key            INTEGER                       NOT NULL 
+,fixed_policy_key            INTEGER                       NOT NULL
 ,policy_name                 CHARACTER VARYING             NOT NULL
-,duration                    INTEGER                       NOT NULL -- <is this integer or decimal > NOT NULL
+,duration                    CHARACTER VARYING             NOT NULL -- changed to accommodate nYnMnD
 ,policy_start_ts             TIMESTAMP WITH TIME ZONE      NOT NULL
-,policy_end_ts               TIMESTAMP WITH TIME ZONE      NOT NULL
+,policy_end_ts               TIMESTAMP WITH TIME ZONE
 ,retention_policy_object_id  CHARACTER VARYING                      -- PK of legacy source migration table
 ,created_ts                  TIMESTAMP WITH TIME ZONE      NOT NULL
 ,created_by                  INTEGER                       NOT NULL
@@ -68,57 +69,57 @@ CREATE TABLE retention_policy_type
 CREATE UNIQUE INDEX case_management_retention_pk ON case_management_retention(cmr_id) TABLESPACE darts_indexes;
 ALTER TABLE case_management_retention ADD PRIMARY KEY USING INDEX case_management_retention_pk;
 
-CREATE UNIQUE INDEX case_retention_pk ON case_retention(car_id) TABLESPACE darts_indexes; 
+CREATE UNIQUE INDEX case_retention_pk ON case_retention(car_id) TABLESPACE darts_indexes;
 ALTER TABLE case_retention            ADD PRIMARY KEY USING INDEX case_retention_pk;
 
-CREATE UNIQUE INDEX retention_policy_type_pk ON retention_policy_type(rpt_id) TABLESPACE darts_indexes; 
+CREATE UNIQUE INDEX retention_policy_type_pk ON retention_policy_type(rpt_id) TABLESPACE darts_indexes;
 ALTER TABLE retention_policy_type     ADD PRIMARY KEY USING INDEX retention_policy_type_pk;
 
 CREATE SEQUENCE cmr_seq CACHE 20;
 CREATE SEQUENCE car_seq CACHE 20;
 CREATE SEQUENCE rpt_seq CACHE 20;
 
-ALTER TABLE case_retention            
+ALTER TABLE case_retention
 ADD CONSTRAINT case_retention_court_case_fk
 FOREIGN KEY (cas_id) REFERENCES court_case(cas_id);
 
-ALTER TABLE case_retention           
+ALTER TABLE case_retention
 ADD CONSTRAINT case_retention_retention_policy_type_fk
 FOREIGN KEY (rpt_id) REFERENCES retention_policy_type(rpt_id);
 
-ALTER TABLE case_retention             
+ALTER TABLE case_retention
 ADD CONSTRAINT case_retention_created_by_fk
 FOREIGN KEY (created_by) REFERENCES user_account(usr_id);
 
-ALTER TABLE case_retention             
+ALTER TABLE case_retention
 ADD CONSTRAINT case_retention_last_modified_by_fk
 FOREIGN KEY (last_modified_by) REFERENCES user_account(usr_id);
 
-ALTER TABLE case_retention             
+ALTER TABLE case_retention
 ADD CONSTRAINT case_retention_submitted_by_fk
 FOREIGN KEY (submitted_by) REFERENCES user_account(usr_id);
 
-ALTER TABLE case_retention            
+ALTER TABLE case_retention
 ADD CONSTRAINT case_retention_case_management_retention_fk
 FOREIGN KEY (cmr_id) REFERENCES case_management_retention(cmr_id);
 
-ALTER TABLE case_management_retention            
+ALTER TABLE case_management_retention
 ADD CONSTRAINT case_management_retention_court_case_fk
 FOREIGN KEY (cas_id) REFERENCES court_case(cas_id);
 
-ALTER TABLE case_management_retention            
+ALTER TABLE case_management_retention
 ADD CONSTRAINT case_management_retention_retention_policy_type_fk
 FOREIGN KEY (rpt_id) REFERENCES retention_policy_type(rpt_id);
 
-ALTER TABLE case_management_retention            
+ALTER TABLE case_management_retention
 ADD CONSTRAINT case_management_retention_event_fk
 FOREIGN KEY (eve_id) REFERENCES event(eve_id);
 
-ALTER TABLE retention_policy_type             
+ALTER TABLE retention_policy_type
 ADD CONSTRAINT retention_policy_type_created_by_fk
 FOREIGN KEY (created_by) REFERENCES user_account(usr_id);
 
-ALTER TABLE retention_policy_type             
+ALTER TABLE retention_policy_type
 ADD CONSTRAINT retention_policy_type_last_modified_by_fk
 FOREIGN KEY (last_modified_by) REFERENCES user_account(usr_id);
 
