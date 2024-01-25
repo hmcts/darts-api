8255de1565f1fa05c00fd0e9daebd93acf4e6a70
karen-hedges <133129444+karen-hedges@users.noreply.github.com>
2024-01-25 16:13:05 +0000

    DMP-1737-ARM-PullResponseFiles
    Added extra logging

--- src/main/java/uk/gov/hmcts/darts/arm/service/impl/UnstructuredToArmProcessorImpl.java
+++ src/main/java/uk/gov/hmcts/darts/arm/service/impl/UnstructuredToArmProcessorImpl.java
@@ -157,6 +157,7 @@ public class UnstructuredToArmProcessorImpl implements UnstructuredToArmProcesso
     }
 
     private void updateExternalObjectDirctoryStatus(ExternalObjectDirectoryEntity armExternalObjectDirectory, ObjectRecordStatusEnum armStatus) {
+        log.debug("Updating ARM status from {} to {}", armExternalObjectDirectory.getStatus().getDescription(), armStatus.name());
         armExternalObjectDirectory.setStatus(armStatuses.get(armStatus));
         armExternalObjectDirectory.setLastModifiedBy(userIdentity.getUserAccount());
         externalObjectDirectoryRepository.saveAndFlush(armExternalObjectDirectory);
@@ -261,6 +262,7 @@ public class UnstructuredToArmProcessorImpl implements UnstructuredToArmProcesso
 
     private void updateExternalObjectDirectoryStatusToFailed(ExternalObjectDirectoryEntity armExternalObjectDirectory,
                                                              ObjectRecordStatusEnum objectRecordStatusEnum) {
+        log.debug("Updating ARM status from {} to {}", armExternalObjectDirectory.getStatus().getDescription(), objectRecordStatusEnum.name());
         armExternalObjectDirectory.setStatus(armStatuses.get(objectRecordStatusEnum));
         updateTransferAttempts(armExternalObjectDirectory);
         armExternalObjectDirectory.setLastModifiedBy(userIdentity.getUserAccount());
