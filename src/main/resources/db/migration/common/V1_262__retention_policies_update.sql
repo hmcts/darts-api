655fdd0a6901c7403d6a2e4f36ac04c9b59240fe
Chris Davidson <christopher.davidson@btinternet.com>
2024-01-25 15:27:17 +0000

    dmp 2032 correct values for retention

---
+++ src/main/resources/db/migration/common/V1_262__retention_policies_update.sql
@@ -0,0 +1,6 @@
+update darts.retention_policy_type set duration='30Y0m0D' where rpt_id=1;
+update darts.retention_policy_type set duration='7Y0m0D' where rpt_id=2;
+update darts.retention_policy_type set duration='1Y0m0D' where rpt_id=3;
+update darts.retention_policy_type set duration='7Y0m0D' where rpt_id=4;
+update darts.retention_policy_type set duration='7Y0m0D' where rpt_id=5;
+update darts.retention_policy_type set duration='99Y0m0D' where rpt_id=6;
