9b030923af60d6e1cdd461db6166ce1ca9fd2147
karen-hedges <133129444+karen-hedges@users.noreply.github.com>
2024-02-08 11:37:01 +0000

    Dmp 1737.transactional fix (#1016)
    Co-authored-by: Chris Magowan <chris.magowan@hmcts.net>

---
+++ src/test/java/uk/gov/hmcts/darts/arm/service/ArmResponseFilesProcessorImplTest.java
@@ -0,0 +1,107 @@
+package uk.gov.hmcts.darts.arm.service;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import uk.gov.hmcts.darts.arm.component.ArmResponseFilesProcessSingleElement;
+import uk.gov.hmcts.darts.arm.service.impl.ArmResponseFilesProcessorImpl;
+import uk.gov.hmcts.darts.authorisation.component.UserIdentity;
+import uk.gov.hmcts.darts.common.entity.ExternalLocationTypeEntity;
+import uk.gov.hmcts.darts.common.entity.ExternalObjectDirectoryEntity;
+import uk.gov.hmcts.darts.common.entity.MediaEntity;
+import uk.gov.hmcts.darts.common.entity.ObjectRecordStatusEntity;
+import uk.gov.hmcts.darts.common.repository.ExternalLocationTypeRepository;
+import uk.gov.hmcts.darts.common.repository.ExternalObjectDirectoryRepository;
+import uk.gov.hmcts.darts.common.repository.ObjectRecordStatusRepository;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class ArmResponseFilesProcessorImplTest {
+
+    private ArmResponseFilesProcessor armResponseFilesProcessor;
+
+    @Mock
+    private ExternalObjectDirectoryRepository externalObjectDirectoryRepository;
+    @Mock
+    private ObjectRecordStatusRepository objectRecordStatusRepository;
+    @Mock
+    private ExternalLocationTypeRepository externalLocationTypeRepository;
+    @Mock
+    private UserIdentity userIdentity;
+
+    @Mock
+    private ExternalLocationTypeEntity externalLocationTypeArm;
+    @Mock
+    private ObjectRecordStatusEntity objectRecordStatusStored;
+    @Mock
+    private ObjectRecordStatusEntity objectRecordStatusArmDropZone;
+    @Mock
+    private ObjectRecordStatusEntity objectRecordStatusArmResponseProcessingFailed;
+    @Mock
+    private ObjectRecordStatusEntity objectRecordStatusArmProcessingFiles;
+    @Mock
+    private ObjectRecordStatusEntity objectRecordStatusArmChecksumFailed;
+    @Mock
+    private ExternalObjectDirectoryEntity externalObjectDirectoryArmDropZone;
+    @Mock
+    private ExternalObjectDirectoryEntity externalObjectDirectoryArmResponseProcessing;
+
+    @Mock
+    private ArmResponseFilesProcessSingleElement armResponseFilesProcessSingleElement;
+
+    @Mock
+    private MediaEntity mediaEntity;
+
+
+    @Captor
+    private ArgumentCaptor<ExternalObjectDirectoryEntity> externalObjectDirectoryEntityCaptor;
+
+
+    @BeforeEach
+    void setupData() {
+
+        armResponseFilesProcessor = new ArmResponseFilesProcessorImpl(
+                externalObjectDirectoryRepository,
+                objectRecordStatusRepository,
+                externalLocationTypeRepository,
+                userIdentity,
+                armResponseFilesProcessSingleElement
+        );
+    }
+
+    @Test
+    void processResponseFilesUnableToFindInputUploadFile() {
+
+        when(externalLocationTypeRepository.getReferenceById(3)).thenReturn(externalLocationTypeArm);
+
+        when(objectRecordStatusRepository.findById(13)).thenReturn(Optional.of(objectRecordStatusArmDropZone));
+        when(objectRecordStatusRepository.findById(17)).thenReturn(Optional.of(objectRecordStatusArmResponseProcessingFailed));
+        when(objectRecordStatusRepository.findById(16)).thenReturn(Optional.of(objectRecordStatusArmProcessingFiles));
+
+        when(externalObjectDirectoryArmDropZone.getId()).thenReturn(1);
+        when(externalObjectDirectoryArmDropZone.getStatus()).thenReturn(objectRecordStatusArmDropZone);
+
+        List<ExternalObjectDirectoryEntity> inboundList = new ArrayList<>(Collections.singletonList(externalObjectDirectoryArmDropZone));
+        when(externalObjectDirectoryRepository.findByExternalLocationTypeAndObjectStatus(externalLocationTypeArm, objectRecordStatusArmDropZone))
+                .thenReturn(inboundList);
+
+
+        armResponseFilesProcessor.processResponseFiles();
+
+        verify(externalObjectDirectoryRepository, times(1)).saveAndFlush(externalObjectDirectoryEntityCaptor.capture());
+
+    }
+
+}
