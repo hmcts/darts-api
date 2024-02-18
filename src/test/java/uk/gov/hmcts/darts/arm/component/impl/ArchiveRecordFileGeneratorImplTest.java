d1b6b8f88768d504a5a8195fbd9dc26e1419d799
karen-hedges <133129444+karen-hedges@users.noreply.github.com>
2024-02-18 13:50:23 +0000

    DMP-2181 Update ARM Manifest file fields - Transcription, Annotation & Case
    Added more tests

--- src/test/java/uk/gov/hmcts/darts/arm/component/impl/ArchiveRecordFileGeneratorImplTest.java
+++ src/test/java/uk/gov/hmcts/darts/arm/component/impl/ArchiveRecordFileGeneratorImplTest.java
@@ -10,11 +10,17 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.skyscreamer.jsonassert.JSONCompareMode;
 import uk.gov.hmcts.darts.arm.enums.ArchiveRecordType;
 import uk.gov.hmcts.darts.arm.model.ArchiveRecord;
+import uk.gov.hmcts.darts.arm.model.record.AnnotationArchiveRecord;
+import uk.gov.hmcts.darts.arm.model.record.CaseArchiveRecord;
 import uk.gov.hmcts.darts.arm.model.record.MediaArchiveRecord;
+import uk.gov.hmcts.darts.arm.model.record.TranscriptionArchiveRecord;
 import uk.gov.hmcts.darts.arm.model.record.UploadNewFileRecord;
 import uk.gov.hmcts.darts.arm.model.record.metadata.RecordMetadata;
 import uk.gov.hmcts.darts.arm.model.record.metadata.UploadNewFileRecordMetadata;
+import uk.gov.hmcts.darts.arm.model.record.operation.AnnotationCreateArchiveRecordOperation;
+import uk.gov.hmcts.darts.arm.model.record.operation.CaseCreateArchiveRecordOperation;
 import uk.gov.hmcts.darts.arm.model.record.operation.MediaCreateArchiveRecordOperation;
+import uk.gov.hmcts.darts.arm.model.record.operation.TranscriptionCreateArchiveRecordOperation;
 import uk.gov.hmcts.darts.common.config.ObjectMapperConfig;
 
 import java.io.BufferedReader;
@@ -58,6 +64,55 @@ class ArchiveRecordFileGeneratorImplTest {
 
         String actualResponse = getFileContents(archiveFile);
         String expectedResponse = getContentsFromFile("Tests/arm/component/ArchiveMediaMetadata/expectedResponse.a360");
+        log.info("actual Response {}", actualResponse);
+        log.info("expect Response {}", expectedResponse);
+        assertEquals(expectedResponse, actualResponse, JSONCompareMode.NON_EXTENSIBLE);
+    }
+
+    @Test
+    void generateArchiveRecordWithTranscription() throws IOException {
+        String fileLocation = tempDirectory.getAbsolutePath();
+        String relationId = "1234";
+        File archiveFile = new File(fileLocation, "1234-1-1.a360");
+        archiveRecordFileGenerator.generateArchiveRecord(createTranscriptionArchiveRecord(relationId), archiveFile,
+                                                         ArchiveRecordType.TRANSCRIPTION_ARCHIVE_TYPE);
+
+        log.info("Reading file {}", archiveFile.getAbsolutePath());
+
+        String actualResponse = getFileContents(archiveFile);
+        String expectedResponse = getContentsFromFile("Tests/arm/component/ArchiveTranscriptionMetadata/expectedResponse.a360");
+        log.info("actual Response {}", actualResponse);
+        log.info("expect Response {}", expectedResponse);
+        assertEquals(expectedResponse, actualResponse, JSONCompareMode.NON_EXTENSIBLE);
+    }
+
+    @Test
+    void generateArchiveRecordWithAnnotation() throws IOException {
+        String fileLocation = tempDirectory.getAbsolutePath();
+        String relationId = "1234";
+        File archiveFile = new File(fileLocation, "1234-1-1.a360");
+        archiveRecordFileGenerator.generateArchiveRecord(createAnnotationArchiveRecord(relationId), archiveFile, ArchiveRecordType.ANNOTATION_ARCHIVE_TYPE);
+
+        log.info("Reading file {}", archiveFile.getAbsolutePath());
+
+        String actualResponse = getFileContents(archiveFile);
+        String expectedResponse = getContentsFromFile("Tests/arm/component/ArchiveAnnotationMetadata/expectedResponse.a360");
+        log.info("actual Response {}", actualResponse);
+        log.info("expect Response {}", expectedResponse);
+        assertEquals(expectedResponse, actualResponse, JSONCompareMode.NON_EXTENSIBLE);
+    }
+
+    @Test
+    void generateArchiveRecordWithCase() throws IOException {
+        String fileLocation = tempDirectory.getAbsolutePath();
+        String relationId = "1234";
+        File archiveFile = new File(fileLocation, "1234-1-1.a360");
+        archiveRecordFileGenerator.generateArchiveRecord(createCaseArchiveRecord(relationId), archiveFile, ArchiveRecordType.CASE_ARCHIVE_TYPE);
+
+        log.info("Reading file {}", archiveFile.getAbsolutePath());
+
+        String actualResponse = getFileContents(archiveFile);
+        String expectedResponse = getContentsFromFile("Tests/arm/component/ArchiveCaseMetadata/expectedResponse.a360");
         log.info("actual Response {}", actualResponse);
         log.info("expect Response {}", expectedResponse);
         assertEquals(expectedResponse, actualResponse, JSONCompareMode.NON_EXTENSIBLE);
@@ -138,9 +193,181 @@ class ArchiveRecordFileGeneratorImplTest {
     private UploadNewFileRecordMetadata createMediaUploadNewFileRecordMetadata() {
         UploadNewFileRecordMetadata uploadNewFileRecordMetadata = new UploadNewFileRecordMetadata();
         uploadNewFileRecordMetadata.setPublisher("DARTS");
-        uploadNewFileRecordMetadata.setDzFilename("123_456_1.mp2");
+        uploadNewFileRecordMetadata.setDzFilename("123_456_1");
         uploadNewFileRecordMetadata.setFileTag("mp2");
         return uploadNewFileRecordMetadata;
+    }
 
+
+    private TranscriptionArchiveRecord createTranscriptionArchiveRecord(String relationId) {
+        return TranscriptionArchiveRecord.builder()
+            .transcriptionCreateArchiveRecordOperation(createTranscriptionArchiveRecordOperation(relationId))
+            .uploadNewFileRecord(createTranscriptionUploadNewFileRecord(relationId))
+            .build();
+    }
+
+    private UploadNewFileRecord createTranscriptionUploadNewFileRecord(String relationId) {
+        UploadNewFileRecord uploadNewFileRecord = new UploadNewFileRecord();
+        uploadNewFileRecord.setOperation(UPLOAD_NEW_FILE);
+        uploadNewFileRecord.setRelationId(relationId);
+        uploadNewFileRecord.setFileMetadata(createTranscriptionUploadNewFileRecordMetadata());
+        return uploadNewFileRecord;
+    }
+
+    private UploadNewFileRecordMetadata createTranscriptionUploadNewFileRecordMetadata() {
+        UploadNewFileRecordMetadata uploadNewFileRecordMetadata = new UploadNewFileRecordMetadata();
+        uploadNewFileRecordMetadata.setPublisher("DARTS");
+        uploadNewFileRecordMetadata.setDzFilename("123_456_1");
+        uploadNewFileRecordMetadata.setFileTag("docx");
+        return uploadNewFileRecordMetadata;
+    }
+
+
+    private TranscriptionCreateArchiveRecordOperation createTranscriptionArchiveRecordOperation(String relationId) {
+        return TranscriptionCreateArchiveRecordOperation.builder()
+            .relationId(relationId)
+            .recordMetadata(createTranscriptionArchiveRecordMetadata())
+            .build();
+    }
+
+    private RecordMetadata createTranscriptionArchiveRecordMetadata() {
+        OffsetDateTime recordTime = OffsetDateTime.of(2024, 1, 23, 10, 0, 0, 0, ZoneOffset.UTC);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss");
+        return RecordMetadata.builder()
+            .recordClass("DARTSMedia")
+            .publisher("DARTS")
+            .region("GBR")
+            .recordDate(recordTime.format(formatter))
+            .eventDate("2024-01-23T11:40:00Z")
+            .title("Filename")
+            .clientId("1234")
+            .contributor("Swansea & Courtroom 1")
+            .bf001("Transcription")
+            .bf002("Case_1|Case_2|Case_3")
+            .bf003("mp2")
+            .bf004("2024-01-23")
+            .bf005("xi/XkzD2HuqTUzDafW8Cgw==")
+            .bf011("2024-01-23T11:39:30Z")
+            .bf012(1)
+            .bf013(1)
+            .bf017("2024-01-23T11:40:00Z")
+            .bf018("2024-01-23T13:40:00Z")
+            .bf019("Swansea")
+            .bf020("Courtroom 1")
+            .build();
+    }
+
+
+    private AnnotationArchiveRecord createAnnotationArchiveRecord(String relationId) {
+        return AnnotationArchiveRecord.builder()
+            .annotationCreateArchiveRecordOperation(createAnnotationArchiveRecordOperation(relationId))
+            .uploadNewFileRecord(createAnnotationUploadNewFileRecord(relationId))
+            .build();
+    }
+
+    private UploadNewFileRecord createAnnotationUploadNewFileRecord(String relationId) {
+        UploadNewFileRecord uploadNewFileRecord = new UploadNewFileRecord();
+        uploadNewFileRecord.setOperation(UPLOAD_NEW_FILE);
+        uploadNewFileRecord.setRelationId(relationId);
+        uploadNewFileRecord.setFileMetadata(createAnnotationUploadNewFileRecordMetadata());
+        return uploadNewFileRecord;
+    }
+
+    private UploadNewFileRecordMetadata createAnnotationUploadNewFileRecordMetadata() {
+        UploadNewFileRecordMetadata uploadNewFileRecordMetadata = new UploadNewFileRecordMetadata();
+        uploadNewFileRecordMetadata.setPublisher("DARTS");
+        uploadNewFileRecordMetadata.setDzFilename("123_456_1");
+        uploadNewFileRecordMetadata.setFileTag("txt");
+        return uploadNewFileRecordMetadata;
+    }
+
+    private AnnotationCreateArchiveRecordOperation createAnnotationArchiveRecordOperation(String relationId) {
+        return AnnotationCreateArchiveRecordOperation.builder()
+            .relationId(relationId)
+            .recordMetadata(createAnnotationArchiveRecordMetadata())
+            .build();
+    }
+
+    private RecordMetadata createAnnotationArchiveRecordMetadata() {
+        OffsetDateTime recordTime = OffsetDateTime.of(2024, 1, 23, 10, 0, 0, 0, ZoneOffset.UTC);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss");
+        return RecordMetadata.builder()
+            .recordClass("DARTSMedia")
+            .publisher("DARTS")
+            .region("GBR")
+            .recordDate(recordTime.format(formatter))
+            .eventDate("2024-01-23T11:40:00Z")
+            .title("Filename")
+            .clientId("1234")
+            .contributor("Swansea & Courtroom 1")
+            .bf001("Annotation")
+            .bf002("Case_1|Case_2|Case_3")
+            .bf003("txt")
+            .bf004("2024-01-23")
+            .bf005("xi/XkzD2HuqTUzDafW8Cgw==")
+            .bf011("2024-01-23T11:39:30Z")
+            .bf012(1)
+            .bf013(1)
+            .bf017("2024-01-23T11:40:00Z")
+            .bf018("2024-01-23T13:40:00Z")
+            .bf019("Swansea")
+            .bf020("Courtroom 1")
+            .build();
+    }
+
+    private CaseArchiveRecord createCaseArchiveRecord(String relationId) {
+        return CaseArchiveRecord.builder()
+            .caseCreateArchiveRecordOperation(createCaseArchiveRecordOperation(relationId))
+            .uploadNewFileRecord(createCaseUploadNewFileRecord(relationId))
+            .build();
+    }
+
+    private UploadNewFileRecord createCaseUploadNewFileRecord(String relationId) {
+        UploadNewFileRecord uploadNewFileRecord = new UploadNewFileRecord();
+        uploadNewFileRecord.setOperation(UPLOAD_NEW_FILE);
+        uploadNewFileRecord.setRelationId(relationId);
+        uploadNewFileRecord.setFileMetadata(createCaseUploadNewFileRecordMetadata());
+        return uploadNewFileRecord;
+    }
+
+    private UploadNewFileRecordMetadata createCaseUploadNewFileRecordMetadata() {
+        UploadNewFileRecordMetadata uploadNewFileRecordMetadata = new UploadNewFileRecordMetadata();
+        uploadNewFileRecordMetadata.setPublisher("DARTS");
+        uploadNewFileRecordMetadata.setDzFilename("123_456_1");
+        uploadNewFileRecordMetadata.setFileTag("txt");
+        return uploadNewFileRecordMetadata;
+    }
+
+    private CaseCreateArchiveRecordOperation createCaseArchiveRecordOperation(String relationId) {
+        return CaseCreateArchiveRecordOperation.builder()
+            .relationId(relationId)
+            .recordMetadata(createCaseArchiveRecordMetadata())
+            .build();
+    }
+
+    private RecordMetadata createCaseArchiveRecordMetadata() {
+        OffsetDateTime recordTime = OffsetDateTime.of(2024, 1, 23, 10, 0, 0, 0, ZoneOffset.UTC);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss");
+        return RecordMetadata.builder()
+            .recordClass("DARTSMedia")
+            .publisher("DARTS")
+            .region("GBR")
+            .recordDate(recordTime.format(formatter))
+            .eventDate("2024-01-23T11:40:00Z")
+            .title("Filename")
+            .clientId("1234")
+            .contributor("Swansea")
+            .bf001("Case")
+            .bf002("Case_1|Case_2|Case_3")
+            .bf003("txt")
+            .bf004("2024-01-23")
+            .bf005("xi/XkzD2HuqTUzDafW8Cgw==")
+            .bf011("2024-01-23T11:39:30Z")
+            .bf012(1)
+            .bf013(1)
+            .bf017("2024-01-23T11:40:00Z")
+            .bf018("2024-01-23T13:40:00Z")
+            .bf019("Swansea")
+            .build();
     }
 }
