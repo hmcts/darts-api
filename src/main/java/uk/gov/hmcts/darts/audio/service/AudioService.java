9e53b8b9fc3af970c2d849676cb024a330a4ca3e
Dave Thompson <davethompson21@gmail.com>
2024-03-05 11:52:07 +0000

    Adding caching for audio previews

--- src/main/java/uk/gov/hmcts/darts/audio/service/AudioService.java
+++ src/main/java/uk/gov/hmcts/darts/audio/service/AudioService.java
@@ -1,12 +1,11 @@
 package uk.gov.hmcts.darts.audio.service;
 
+import com.azure.core.util.BinaryData;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 import uk.gov.hmcts.darts.audio.model.AddAudioMetadataRequest;
 import uk.gov.hmcts.darts.audio.model.AudioMetadata;
 import uk.gov.hmcts.darts.common.entity.MediaEntity;
 
-import java.io.InputStream;
 import java.util.List;
 
 
@@ -14,15 +13,13 @@ public interface AudioService {
 
     List<MediaEntity> getAudioMetadata(Integer hearingId, Integer channel);
 
-    InputStream preview(Integer mediaId);
+    BinaryData encode(Integer mediaId);
 
     void addAudio(MultipartFile audioFile, AddAudioMetadataRequest addAudioMetadata);
 
     void linkAudioToHearingInMetadata(AddAudioMetadataRequest addAudioMetadataRequest, MediaEntity savedMedia);
 
     void linkAudioToHearingByEvent(AddAudioMetadataRequest addAudioMetadataRequest, MediaEntity savedMedia);
-
-    SseEmitter startStreamingPreview(Integer mediaId, String range, SseEmitter emitter);
 
     void setIsArchived(List<AudioMetadata> audioMetadata, Integer hearingId);
 
