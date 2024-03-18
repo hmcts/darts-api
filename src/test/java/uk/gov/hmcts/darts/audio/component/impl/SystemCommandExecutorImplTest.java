294a3db9715bc30f1f0d3a7040ba4af9f8dc9ab3
karen-hedges <133129444+karen-hedges@users.noreply.github.com>
2024-03-18 14:30:55 +0000

    DMP-1661-IntegrationTestsATSDownload
    Added the facility to create an ATS test

--- src/test/java/uk/gov/hmcts/darts/audio/component/impl/SystemCommandExecutorImplTest.java
+++ src/test/java/uk/gov/hmcts/darts/audio/component/impl/SystemCommandExecutorImplTest.java
@@ -6,8 +6,6 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.util.concurrent.ExecutionException;
-
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
@@ -26,14 +24,4 @@ class SystemCommandExecutorImplTest {
     void shouldThrowExceptionWhenCommandIsInValid() {
         assertThrows(Exception.class, () -> systemCommandExecutor.execute(new CommandLine("Dummy Command")));
     }
-
-    @Test
-    void executeWithFfmpegHelpCommand() throws ExecutionException, InterruptedException {
-
-        String command = "ffmpeg -h";
-        CommandLine commandLine = CommandLine.parse(command);
-        boolean result = systemCommandExecutor.execute(commandLine);
-        assertTrue(result);
-    }
-
 }
