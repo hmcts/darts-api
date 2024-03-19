694e75831c25677839aeb2c49203d25f92949e85
Ben Neill <benjaminrneill@gmail.com>
2024-03-19 10:03:17 +0000

    Update main.tf

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -14,6 +14,8 @@ locals {
   rg_name    = "${var.product}-${var.env}-rg"
   db_name    = "darts"
   db_port    = 5432
+  private_endpoint_rg_name   = var.businessArea == "sds" ? "ss-${var.env}-network-rg" : "${var.businessArea}-${var.env}-network-rg"
+  private_endpoint_vnet_name = var.businessArea == "sds" ? "ss-${var.env}-vnet" : "${var.businessArea}-${var.env}-vnet"
 }
 
 data "azurerm_resource_group" "rg" {
@@ -96,11 +98,6 @@ module "postgresql_flexible" {
   pgsql_version = "15"
 }
 
-locals {
-  private_endpoint_rg_name   = var.businessArea == "sds" ? "ss-${var.env}-network-rg" : "${var.businessArea}-${var.env}-network-rg"
-  private_endpoint_vnet_name = var.businessArea == "sds" ? "ss-${var.env}-vnet" : "${var.businessArea}-${var.env}-vnet"
-}
-
 data "azurerm_subnet" "private_endpoints" {
   resource_group_name  = local.private_endpoint_rg_name
   virtual_network_name = local.private_endpoint_vnet_name
@@ -110,7 +107,7 @@ data "azurerm_subnet" "private_endpoints" {
 module "armsa" {
   source                   = "git@github.com:hmcts/cnp-module-storage-account?ref=master"
   env                      = var.env
-  storage_account_name     = var.storage_account_name
+  storage_account_name     = "${var.product}-arm-{var.env}"
   resource_group_name      = var.resource_group_name
   location                 = var.location
   account_kind             = var.account_kind
