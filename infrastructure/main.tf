ae19b14b724804a476028aad5d3afb47342f46ea
Ben Neill <benjaminrneill@gmail.com>
2024-03-19 09:52:45 +0000

    add in arm sa

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -95,3 +95,26 @@ module "postgresql_flexible" {
   ]
   pgsql_version = "15"
 }
+
+locals {
+  private_endpoint_rg_name   = var.businessArea == "sds" ? "ss-${var.env}-network-rg" : "${var.businessArea}-${var.env}-network-rg"
+  private_endpoint_vnet_name = var.businessArea == "sds" ? "ss-${var.env}-vnet" : "${var.businessArea}-${var.env}-vnet"
+}
+
+data "azurerm_subnet" "private_endpoints" {
+  resource_group_name  = local.private_endpoint_rg_name
+  virtual_network_name = local.private_endpoint_vnet_name
+  name                 = "private-endpoints"
+}
+
+module "armsa" {
+  source                   = "git@github.com:hmcts/cnp-module-storage-account?ref=master"
+  env                      = var.env
+  storage_account_name     = var.storage_account_name
+  resource_group_name      = var.resource_group_name
+  location                 = var.location
+  account_kind             = var.account_kind
+  account_replication_type = var.account_replication_type
+
+  private_endpoint_subnet_id = data.azurerm_subnet.private_endpoints.id
+}
