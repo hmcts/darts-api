8ee41d2d1177552fc7537bfca92eb0b22eea394d
mmmalik1 <mohammed.malik1@hmcts.net>
2024-03-26 12:30:04 +0000

    audit search endpoint removed
    audit search test removed
    
    tests corrected

--- src/main/java/uk/gov/hmcts/darts/common/repository/AuditRepository.java
+++ src/main/java/uk/gov/hmcts/darts/common/repository/AuditRepository.java
@@ -2,10 +2,24 @@ package uk.gov.hmcts.darts.common.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import uk.gov.hmcts.darts.common.entity.AuditEntity;
 
+import java.time.OffsetDateTime;
+import java.util.List;
+
 @Repository
 public interface AuditRepository extends JpaRepository<AuditEntity, Integer>, JpaSpecificationExecutor<AuditEntity> {
+    @Query("""
+        SELECT audit FROM AuditEntity audit
+        Where audit.courtCase.id = :caseId
+        AND audit.auditActivity.id = :activityId
+        AND audit.createdDateTime between :fromDate AND :toDate
+        """)
+    public List<AuditEntity> getAuditEntitiesByCaseAndActivityForDateRange(Integer caseId,
+                                                            Integer activityId,
+                                                            OffsetDateTime fromDate,
+                                                            OffsetDateTime toDate);
 
 }
