4cebb8fd0b0414bb0376979e16885cb6f8cacf2d
mariopaniccia <mario.paniccia@hmcts.net>
2024-03-27 10:36:35 +0000

    DMP-2244 improved EodHelper

--- src/main/java/uk/gov/hmcts/darts/common/util/EodHelper.java
+++ src/main/java/uk/gov/hmcts/darts/common/util/EodHelper.java
@@ -20,6 +20,7 @@ import static uk.gov.hmcts.darts.common.enums.ObjectRecordStatusEnum.ARM_INGESTI
 import static uk.gov.hmcts.darts.common.enums.ObjectRecordStatusEnum.ARM_MANIFEST_FAILED;
 import static uk.gov.hmcts.darts.common.enums.ObjectRecordStatusEnum.ARM_RAW_DATA_FAILED;
 import static uk.gov.hmcts.darts.common.enums.ObjectRecordStatusEnum.ARM_RESPONSE_MANIFEST_FAILED;
+import static uk.gov.hmcts.darts.common.enums.ObjectRecordStatusEnum.MARKED_FOR_DELETION;
 import static uk.gov.hmcts.darts.common.enums.ObjectRecordStatusEnum.STORED;
 
 @Component
@@ -32,33 +33,27 @@ public class EodHelper {
     private final ExternalLocationTypeRepository eltRepository;
     private final ObjectRecordStatusRepository orsRepository;
 
-    @Getter
-    private static ExternalLocationTypeEntity unstructuredLocation;
-    @Getter
-    private static ExternalLocationTypeEntity armLocation;
-    @Getter
-    private static ExternalLocationTypeEntity detsLocation;
+    @Getter private static ExternalLocationTypeEntity unstructuredLocation;
+    @Getter private static ExternalLocationTypeEntity armLocation;
+    @Getter private static ExternalLocationTypeEntity detsLocation;
+    @Getter private static ExternalLocationTypeEntity inboundLocation;
 
-    @Getter
-    private static ObjectRecordStatusEntity failedArmRawDataStatus;
-    @Getter
-    private static ObjectRecordStatusEntity failedArmManifestFileStatus;
-    @Getter
-    private static ObjectRecordStatusEntity storedStatus;
-    @Getter
-    private static ObjectRecordStatusEntity armIngestionStatus;
-    @Getter
-    private static ObjectRecordStatusEntity failedArmResponseManifestFileStatus;
-    @Getter
-    private static ObjectRecordStatusEntity armDropZoneStatus;
-    @Getter
-    private static List<ObjectRecordStatusEntity> failedArmStatuses;
+    @Getter private static ObjectRecordStatusEntity failedArmRawDataStatus;
+    @Getter private static ObjectRecordStatusEntity failedArmManifestFileStatus;
+    @Getter private static ObjectRecordStatusEntity storedStatus;
+    @Getter private static ObjectRecordStatusEntity armIngestionStatus;
+    @Getter private static ObjectRecordStatusEntity failedArmResponseManifestFileStatus;
+    @Getter private static ObjectRecordStatusEntity armDropZoneStatus;
+    @Getter private static ObjectRecordStatusEntity markForDeletionStatus;
+
+    @Getter private static List<ObjectRecordStatusEntity> failedArmStatuses;
 
     @PostConstruct
     public void init() {
         EodHelper.unstructuredLocation = eltRepository.findById(ExternalLocationTypeEnum.UNSTRUCTURED.getId()).orElseThrow();
         EodHelper.armLocation = eltRepository.findById(ExternalLocationTypeEnum.ARM.getId()).orElseThrow();
         EodHelper.detsLocation = eltRepository.findById(ExternalLocationTypeEnum.DETS.getId()).orElseThrow();
+        EodHelper.inboundLocation = eltRepository.findById(ExternalLocationTypeEnum.INBOUND.getId()).orElseThrow();
 
         EodHelper.storedStatus = orsRepository.findById(STORED.getId()).orElseThrow();
         EodHelper.failedArmRawDataStatus = orsRepository.findById(ARM_RAW_DATA_FAILED.getId()).orElseThrow();
@@ -66,6 +61,7 @@ public class EodHelper {
         EodHelper.failedArmResponseManifestFileStatus = orsRepository.findById(ARM_RESPONSE_MANIFEST_FAILED.getId()).orElseThrow();
         EodHelper.armIngestionStatus = orsRepository.findById(ARM_INGESTION.getId()).orElseThrow();
         EodHelper.armDropZoneStatus = orsRepository.findById(ARM_DROP_ZONE.getId()).orElseThrow();
+        EodHelper.markForDeletionStatus = orsRepository.findById(MARKED_FOR_DELETION.getId()).orElseThrow();
 
         EodHelper.failedArmStatuses  = List.of(failedArmRawDataStatus, failedArmManifestFileStatus, failedArmResponseManifestFileStatus);
     }
