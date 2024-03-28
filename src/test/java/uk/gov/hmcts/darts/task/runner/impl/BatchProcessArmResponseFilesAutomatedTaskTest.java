308c5c3c45c5e543c9274ccd5f00d22972e03a19
karen-hedges <133129444+karen-hedges@users.noreply.github.com>
2024-03-28 15:19:31 +0000

    DMP-2247 ARM BAU Batch - Pull Response files
    Added some tests

--- src/test/java/uk/gov/hmcts/darts/task/runner/impl/BatchProcessArmResponseFilesAutomatedTaskTest.java
+++ src/test/java/uk/gov/hmcts/darts/task/runner/impl/BatchProcessArmResponseFilesAutomatedTaskTest.java
@@ -2,11 +2,14 @@ package uk.gov.hmcts.darts.task.runner.impl;
 
 import net.javacrumbs.shedlock.core.LockProvider;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import uk.gov.hmcts.darts.arm.service.ArmBatchProcessResponseFiles;
 
 import static org.mockito.Mockito.verify;
 
+@ExtendWith(MockitoExtension.class)
 class BatchProcessArmResponseFilesAutomatedTaskTest {
 
     @Mock
