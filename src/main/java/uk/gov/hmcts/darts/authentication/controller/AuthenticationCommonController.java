475040693545f8c9bd7eef9e70838cef96128358
Chris Magowan <chris.magowan@hmcts.net>
2024-04-03 15:39:53 +0100

    add "Authentication" group to generated swagger

--- src/main/java/uk/gov/hmcts/darts/authentication/controller/AuthenticationCommonController.java
+++ src/main/java/uk/gov/hmcts/darts/authentication/controller/AuthenticationCommonController.java
@@ -1,10 +1,14 @@
 package uk.gov.hmcts.darts.authentication.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.web.bind.annotation.GetMapping;
 import uk.gov.hmcts.darts.authorisation.model.UserState;
 
 public interface AuthenticationCommonController {
 
+    @Operation(
+        tags = {"Authentication"}
+    )
     @GetMapping("/userstate")
     UserState getUserState();
 
