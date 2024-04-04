0f933c9219646f98fa4bedbf79c9d08bf3a333a0
karen-hedges <133129444+karen-hedges@users.noreply.github.com>
2024-04-04 16:39:13 +0100

    DMP-1661 (Dev Only) Integration tests for ATS - Download
    Made simple change to force a rebuild

--- src/integrationTest/java/uk/gov/hmcts/darts/audio/component/impl/SystemCommandExecutorIntTest.java
+++ src/integrationTest/java/uk/gov/hmcts/darts/audio/component/impl/SystemCommandExecutorIntTest.java
@@ -30,11 +30,18 @@ class SystemCommandExecutorIntTest extends IntegrationBase {
     }
 
     @Test
+    void executeWithFfmpegVersionCommand() throws ExecutionException, InterruptedException {
+        String command = "/usr/bin/ffmpeg -version";
+        CommandLine commandLine = CommandLine.parse(command);
+        boolean result = systemCommandExecutor.execute(commandLine);
+        assertTrue(result);
+    }
+
+    @Test
     void executeWithFfmpegAudioCommand() {
-        String command = "/usr/bin/ffmpeg -i /path/to/audio/original0.mp2 -i /path/to/audio/original1.mp2"
-            + " -filter_complex [0:a][1:a]concat=n=2:v=0:a=1 /path/to/output/audio.mp2";
+        String command = "/usr/bin/ffmpeg -i original0.mp2 -i original1.mp2"
+            + " -filter_complex [0:a][1:a]concat=n=2:v=0:a=1 audio.mp2";
         CommandLine commandLine = CommandLine.parse(command);
         assertThrows(ExecutionException.class, () -> systemCommandExecutor.execute(commandLine));
-
     }
 }
--- src/test/java/uk/gov/hmcts/darts/audio/component/impl/SystemCommandExecutorImplFfmpegTest.java
+++ src/test/java/uk/gov/hmcts/darts/audio/component/impl/SystemCommandExecutorImplFfmpegTest.java
@@ -25,6 +25,33 @@ class SystemCommandExecutorImplFfmpegTest {
     @Test
     void executeWithFfmpegHelpCommand() throws ExecutionException, InterruptedException {
 
+        String command = "ffmpeg -h";
+        CommandLine commandLine = CommandLine.parse(command);
+        boolean result = systemCommandExecutor.execute(commandLine);
+        assertTrue(result);
+    }
+
+    @Test
+    void executeWithFfmpegVersionCommand() throws ExecutionException, InterruptedException {
+        String command = "ffmpeg -version";
+        CommandLine commandLine = CommandLine.parse(command);
+        boolean result = systemCommandExecutor.execute(commandLine);
+        assertTrue(result);
+    }
+
+
+    @Test
+    void executeWithFfmpegAudioCommand() {
+        String command = "ffmpeg -i original0.mp2 -i original1.mp2"
+            + " -filter_complex [0:a][1:a]concat=n=2:v=0:a=1 audio.mp2";
+        CommandLine commandLine = CommandLine.parse(command);
+        assertThrows(ExecutionException.class, () -> systemCommandExecutor.execute(commandLine));
+
+    }
+
+    @Test
+    void executeWithFfmpegHelpCommandWithFullPath() throws ExecutionException, InterruptedException {
+
         String command = "/usr/bin/ffmpeg -h";
         CommandLine commandLine = CommandLine.parse(command);
         boolean result = systemCommandExecutor.execute(commandLine);
@@ -32,7 +59,16 @@ class SystemCommandExecutorImplFfmpegTest {
     }
 
     @Test
-    void executeWithFfmpegAudioCommand() {
+    void executeWithFfmpegVersionCommandWithFullPath() throws ExecutionException, InterruptedException {
+        String command = "/usr/bin/ffmpeg -version";
+        CommandLine commandLine = CommandLine.parse(command);
+        boolean result = systemCommandExecutor.execute(commandLine);
+        assertTrue(result);
+    }
+
+
+    @Test
+    void executeWithFfmpegAudioCommandWithFullPath() {
         String command = "/usr/bin/ffmpeg -i /path/to/audio/original0.mp2 -i /path/to/audio/original1.mp2"
             + " -filter_complex [0:a][1:a]concat=n=2:v=0:a=1 /path/to/output/audio.mp2";
         CommandLine commandLine = CommandLine.parse(command);
