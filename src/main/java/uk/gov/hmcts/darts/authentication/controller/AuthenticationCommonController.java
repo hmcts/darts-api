554e5b3c7b54854f73f74de50cdc9ece7876c3bf
Chris Magowan <chris.magowan@hmcts.net>
2024-04-04 09:04:40 +0100

    add summary and description swagger elements to new endpoint

--- src/main/java/uk/gov/hmcts/darts/authentication/controller/AuthenticationCommonController.java
+++ src/main/java/uk/gov/hmcts/darts/authentication/controller/AuthenticationCommonController.java
@@ -7,6 +7,9 @@ import uk.gov.hmcts.darts.authorisation.model.UserState;
 public interface AuthenticationCommonController {
 
     @Operation(
+        summary = "Gets the User State for the currently logged in user",
+        description = "Gets the User State for the currently logged in user. The User State details the Users roles and permissions. " +
+            "This allows the screen to refresh the roles and permissions of a user easily.",
         tags = {"Authentication"}
     )
     @GetMapping("/userstate")
