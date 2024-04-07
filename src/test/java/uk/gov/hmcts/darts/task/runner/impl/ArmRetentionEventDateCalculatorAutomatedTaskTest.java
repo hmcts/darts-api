94931659afe7157761df3a54e37c840815a98f50
karen-hedges <133129444+karen-hedges@users.noreply.github.com>
2024-04-07 23:30:14 +0100

    DMP-2057 ARM event date calculation
    Added facility to update the ARM metadata with the correct event date

--- src/test/java/uk/gov/hmcts/darts/task/runner/impl/ArmRetentionEventDateCalculatorAutomatedTaskTest.java
+++ src/test/java/uk/gov/hmcts/darts/task/runner/impl/ArmRetentionEventDateCalculatorAutomatedTaskTest.java
@@ -2,10 +2,13 @@ package uk.gov.hmcts.darts.task.runner.impl;
 
 import net.javacrumbs.shedlock.core.LockProvider;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 import uk.gov.hmcts.darts.arm.service.ArmRetentionEventDateProcessor;
 
+@ExtendWith(MockitoExtension.class)
 class ArmRetentionEventDateCalculatorAutomatedTaskTest {
 
     @Mock
