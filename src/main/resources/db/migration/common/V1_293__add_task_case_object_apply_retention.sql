5d9d80310c76a9e231899c9f3ca8c97a20907c25
mariopaniccia <mario.paniccia@hmcts.net>
2024-04-15 11:44:44 +0100

    DMP-2627 fixed conflicts resolution mistakes

--- src/main/resources/db/migration/common/V1_293__add_task_case_object_apply_retention.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO darts.automated_task (aut_id,task_name,task_description,cron_expression,cron_editable)
-VALUES (14,'ApplyRetentionCaseAssociatedObjects','Apply retention to case associated objects','0 0 20 * * *',true);
-
-ALTER SEQUENCE aut_seq RESTART WITH 15;
