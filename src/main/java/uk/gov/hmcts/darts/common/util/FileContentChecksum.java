5d004e646cb1de06c0c24e06039d561370ee65cb
karen-hedges <133129444+karen-hedges@users.noreply.github.com>
2024-04-19 14:20:51 +0100

    DMP-2915 MD5 checksum issue (#1357)

--- src/main/java/uk/gov/hmcts/darts/common/util/FileContentChecksum.java
+++ src/main/java/uk/gov/hmcts/darts/common/util/FileContentChecksum.java
@@ -1,19 +1,24 @@
 package uk.gov.hmcts.darts.common.util;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
+import java.io.IOException;
 import java.security.DigestInputStream;
 
-import static org.apache.commons.codec.binary.Base64.encodeBase64;
 import static org.apache.commons.codec.digest.DigestUtils.md5;
 
 /**
- * Calculates the file checksum equivalent to the Azure Blob Storage CONTENT-MD5 tag value as a base64 encoded
+ * Calculates the file checksum equivalent to "md5 filename" on the command line.
+ * <p>
+ * This is not the same as the Azure Blob Storage CONTENT-MD5 tag value as a base64 encoded
  * representation of the binary MD5 hash value. This could be done in Bash e.g. openssl md5 -binary "Test
  * Document.doc" | base64
+ * </p>
  */
 @Component
 @SuppressWarnings({"java:S4790", "checkstyle:SummaryJavadoc"})
+@Slf4j
 public class FileContentChecksum {
 
     /**
@@ -24,12 +29,17 @@ public class FileContentChecksum {
         return encodeToString(md5(bytes));
     }
 
-    public String calculate(DigestInputStream digestInputStream) {
+    public String calculate(DigestInputStream digestInputStream) throws IOException {
         return encodeToString(digestInputStream.getMessageDigest().digest());
     }
 
     private String encodeToString(byte[] bytes) {
-        return new String(encodeBase64(bytes));
+        StringBuilder result = new StringBuilder();
+        for (byte b : bytes) {
+            result.append(String.format("%02x", b));
+        }
+
+        return result.toString();
     }
 
 }
