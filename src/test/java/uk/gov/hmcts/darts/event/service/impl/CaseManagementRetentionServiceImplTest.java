7cca8bdbc7647d9f733919a854b129a0bfe7ef69
JasonPittockCGI <jason.pittock@hmcts.net>
2024-04-25 17:05:24 +0100

    DMP-2748 Move store retention details
    add a failing test for sonar

--- src/test/java/uk/gov/hmcts/darts/event/service/impl/CaseManagementRetentionServiceImplTest.java
+++ src/test/java/uk/gov/hmcts/darts/event/service/impl/CaseManagementRetentionServiceImplTest.java
@@ -11,16 +11,20 @@ import uk.gov.hmcts.darts.common.entity.CourtCaseEntity;
 import uk.gov.hmcts.darts.common.entity.CourthouseEntity;
 import uk.gov.hmcts.darts.common.entity.EventEntity;
 import uk.gov.hmcts.darts.common.entity.RetentionPolicyTypeEntity;
+import uk.gov.hmcts.darts.common.exception.DartsApiException;
 import uk.gov.hmcts.darts.common.helper.CurrentTimeHelper;
 import uk.gov.hmcts.darts.common.repository.CaseManagementRetentionRepository;
 import uk.gov.hmcts.darts.common.repository.RetentionPolicyTypeRepository;
 import uk.gov.hmcts.darts.common.util.CommonTestDataUtil;
+import uk.gov.hmcts.darts.event.exception.EventError;
 import uk.gov.hmcts.darts.event.model.DartsEventRetentionPolicy;
 
 import java.time.LocalTime;
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.times;
@@ -33,6 +37,9 @@ import static uk.gov.hmcts.darts.event.service.impl.CourtLogsServiceImplTest.CAS
 class CaseManagementRetentionServiceImplTest {
 
     static final String SWANSEA = "SWANSEA";
+
+    static final String ERROR_MESSAGE = """
+    Data on the event could not be reconciled with Darts records. Could not find a retention policy for fixedPolicyKey '1000'""";
 
     @InjectMocks
     CaseManagementRetentionServiceImpl caseManagementRetentionService;
@@ -82,6 +89,35 @@ class CaseManagementRetentionServiceImplTest {
         final CaseManagementRetentionEntity compare = caseManagementRetentionRepository.save(any());
 
         assertEquals(compare, caseManagementRetentionEntity);
+
+
+    }
+
+    @Test
+    void shouldFailAndThrowInvalidRetentionPolicy() {
+
+        var hearingEntity = CommonTestDataUtil.createHearing(CASE_0000001, LocalTime.of(10, 0));
+        EventEntity event = CommonTestDataUtil.createEventWith("LOG", "Test", hearingEntity);
+
+        RetentionPolicyTypeEntity retentionPolicyType = new RetentionPolicyTypeEntity();
+        retentionPolicyType.setDuration("99Y0M0D");
+
+        CourthouseEntity courthouseEntity = CommonTestDataUtil.createCourthouse(SWANSEA);
+        CourtCaseEntity existingCaseEntity = CommonTestDataUtil.createCase("case1", courthouseEntity);
+        existingCaseEntity.setId(1);
+
+        when(retentionPolicyTypeRepository.findCurrentWithFixedPolicyKey(anyString(), any())).thenReturn(new ArrayList<>());
+
+        DartsEventRetentionPolicy retentionPolicy = new DartsEventRetentionPolicy();
+        retentionPolicy.caseRetentionFixedPolicy("1000");
+        retentionPolicy.setCaseTotalSentence("20Y3M4D");
+
+        DartsApiException dartsApiException = assertThrows(DartsApiException.class,
+                                                           () -> caseManagementRetentionService.createCaseManagementRetention(event, existingCaseEntity,
+                                                                                                                              retentionPolicy));
+
+        assertEquals(EventError.EVENT_DATA_NOT_FOUND, dartsApiException.getError());
+        assertEquals(ERROR_MESSAGE, dartsApiException.getMessage());
 
 
     }
