5e70e9041d0d077d5e535d37e29e44fda786454b
JasonPittockCGI <jason.pittock@hmcts.net>
2024-04-25 09:01:05 +0100

    DMP-2696-populate-last-modifiedBy
    populate existing lastModifiedBy

--- src/main/resources/db/migration/common/V1_295__alter_tables_for_lastModifiedBy_nonNull.sql
+++ src/main/resources/db/migration/common/V1_295__alter_tables_for_lastModifiedBy_nonNull.sql
@@ -1,3 +1,7 @@
+UPDATE transcription_document SET last_modified_by=0 WHERE last_modified_by IS NULL;
+UPDATE case_document SET last_modified_by=0 WHERE last_modified_by IS NULL;
+UPDATE annotation_document SET last_modified_by=0 WHERE last_modified_by IS NULL;
+
 ALTER TABLE transcription_document ALTER COLUMN last_modified_by SET NOT NULL;
 ALTER TABLE case_document ALTER COLUMN last_modified_by SET NOT NULL;
 ALTER TABLE annotation_document ALTER COLUMN last_modified_by SET NOT NULL;
