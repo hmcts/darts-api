d4fd590f439dd4cbee6bdb693b21541dc47a9842
JasonPittockCGI <jason.pittock@hmcts.net>
2024-04-25 11:32:28 +0100

    DMP-2696-populate-last-modifiedBy
    set lastmodifiedBy to uloadedBy When not null

--- src/main/resources/db/migration/common/V1_295__alter_tables_for_lastModifiedBy_nonNull.sql
+++ src/main/resources/db/migration/common/V1_295__alter_tables_for_lastModifiedBy_nonNull.sql
@@ -1,6 +1,9 @@
-UPDATE transcription_document SET last_modified_by=0 WHERE last_modified_by IS NULL;
+UPDATE transcription_document SET last_modified_by=0 WHERE last_modified_by IS NULL AND uploaded_by IS NULL;
 UPDATE case_document SET last_modified_by=0 WHERE last_modified_by IS NULL;
-UPDATE annotation_document SET last_modified_by=0 WHERE last_modified_by IS NULL;
+UPDATE annotation_document SET last_modified_by=0 WHERE last_modified_by IS NULL AND uploaded_by IS NULL;
+
+UPDATE transcription_document SET last_modified_by=uploaded_by WHERE last_modified_by IS NULL AND uploaded_by IS NOT NULL;
+UPDATE annotation_document SET last_modified_by=uploaded_by WHERE last_modified_by IS NULL AND uploaded_by IS NOT NULL;
 
 ALTER TABLE transcription_document ALTER COLUMN last_modified_by SET NOT NULL;
 ALTER TABLE case_document ALTER COLUMN last_modified_by SET NOT NULL;
