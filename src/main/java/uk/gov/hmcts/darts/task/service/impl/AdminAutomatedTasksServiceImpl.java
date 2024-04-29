4ec3ef06764dc1100a4868f1535910e5a6255bb4
tonym <incalabsltd@gmail.com>
2024-04-29 14:51:00 +0100

    Returning automated task by id

--- src/main/java/uk/gov/hmcts/darts/task/service/impl/AdminAutomatedTasksServiceImpl.java
+++ src/main/java/uk/gov/hmcts/darts/task/service/impl/AdminAutomatedTasksServiceImpl.java
@@ -3,9 +3,12 @@ package uk.gov.hmcts.darts.task.service.impl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import uk.gov.hmcts.darts.common.entity.AutomatedTaskEntity;
+import uk.gov.hmcts.darts.common.exception.DartsApiException;
 import uk.gov.hmcts.darts.common.repository.AutomatedTaskRepository;
+import uk.gov.hmcts.darts.task.exception.AutomatedTaskApiError;
 import uk.gov.hmcts.darts.task.service.AdminAutomatedTaskService;
-import uk.gov.hmcts.darts.tasks.model.AutomatedTask;
+import uk.gov.hmcts.darts.tasks.model.AutomatedTaskSummary;
+import uk.gov.hmcts.darts.tasks.model.DetailedAutomatedTask;
 
 import java.util.List;
 
@@ -16,19 +19,43 @@ public class AdminAutomatedTasksServiceImpl implements AdminAutomatedTaskService
     private final AutomatedTaskRepository automatedTaskRepository;
 
     @Override
-    public List<AutomatedTask> getAllAutomatedTasks() {
+    public List<AutomatedTaskSummary> getAllAutomatedTasks() {
         var automatedTask = automatedTaskRepository.findAll();
         return mapEntitiesToModel(automatedTask);
     }
 
-    private List<AutomatedTask> mapEntitiesToModel(List<AutomatedTaskEntity> automatedTaskEntities) {
+    @Override
+    public DetailedAutomatedTask getAutomatedTaskById(Integer taskId) {
+        var maybeAutomatedTask = automatedTaskRepository.findById(taskId);
+        if (maybeAutomatedTask.isEmpty()) {
+            throw new DartsApiException(AutomatedTaskApiError.AUTOMATED_TASK_NOT_FOUND);
+        }
+
+        return mapEntityToDetailedAutomatedTask(maybeAutomatedTask.get());
+    }
+
+    private DetailedAutomatedTask mapEntityToDetailedAutomatedTask(AutomatedTaskEntity automatedTaskEntity) {
+        return new DetailedAutomatedTask()
+            .id(automatedTaskEntity.getId())
+            .name(automatedTaskEntity.getTaskName())
+            .isActive(automatedTaskEntity.getTaskEnabled())
+            .description(automatedTaskEntity.getTaskDescription())
+            .cronExpression(automatedTaskEntity.getCronExpression())
+            .createdAt(automatedTaskEntity.getCreatedDateTime())
+            .createdBy(automatedTaskEntity.getCreatedBy().getId())
+            .lastModifiedAt(automatedTaskEntity.getLastModifiedDateTime())
+            .lastModifiedBy(automatedTaskEntity.getLastModifiedBy().getId())
+            .isCronEditable(automatedTaskEntity.getCronEditable());
+    }
+
+    private List<AutomatedTaskSummary> mapEntitiesToModel(List<AutomatedTaskEntity> automatedTaskEntities) {
         return automatedTaskEntities.stream()
-            .map(this::mapEntityToModel)
+            .map(this::mapEntityToTaskSummary)
             .toList();
     }
 
-    private AutomatedTask mapEntityToModel(AutomatedTaskEntity automatedTaskEntity) {
-        return new AutomatedTask()
+    private AutomatedTaskSummary mapEntityToTaskSummary(AutomatedTaskEntity automatedTaskEntity) {
+        return new AutomatedTaskSummary()
             .id(automatedTaskEntity.getId())
             .name(automatedTaskEntity.getTaskName())
             .isActive(automatedTaskEntity.getTaskEnabled())
---
+++ src/main/resources/db/migration/common/V1_295__add_users_and_timestamps_to_automated_tasks.sql
@@ -0,0 +1,3 @@
+UPDATE darts.automated_task
+SET created_ts = now(), last_modified_ts = now(), created_by = 0, last_modified_by = 0
+WHERE true;
