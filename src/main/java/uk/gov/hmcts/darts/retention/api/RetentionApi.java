e3ecbdb466b2232f8aeddc31505541efbe5fd8ce
Chris Magowan <chris.magowan@hmcts.net>
2024-05-01 16:02:38 +0100

    DMP-2205 - add pending retention to auto closed cases.

--- src/main/java/uk/gov/hmcts/darts/retention/api/RetentionApi.java
+++ src/main/java/uk/gov/hmcts/darts/retention/api/RetentionApi.java
@@ -1,9 +1,17 @@
 package uk.gov.hmcts.darts.retention.api;
 
+import uk.gov.hmcts.darts.common.entity.CaseRetentionEntity;
+import uk.gov.hmcts.darts.common.entity.CourtCaseEntity;
 import uk.gov.hmcts.darts.common.entity.RetentionPolicyTypeEntity;
+import uk.gov.hmcts.darts.common.entity.UserAccountEntity;
+import uk.gov.hmcts.darts.retention.enums.CaseRetentionStatus;
+import uk.gov.hmcts.darts.retentions.model.PostRetentionRequest;
 
 import java.time.LocalDate;
 
 public interface RetentionApi {
     LocalDate applyPolicyStringToDate(LocalDate dateToAppend, String policyString, RetentionPolicyTypeEntity retentionPolicyType);
+
+    CaseRetentionEntity createRetention(PostRetentionRequest postRetentionRequest, CourtCaseEntity courtCase, LocalDate newRetentionDate,
+                                        UserAccountEntity userAccount, CaseRetentionStatus caseRetentionStatus);
 }
--- src/main/java/uk/gov/hmcts/darts/retention/service/impl/RetentionPostServiceImpl.java
+++ src/main/java/uk/gov/hmcts/darts/retention/service/impl/RetentionPostServiceImpl.java
@@ -165,15 +165,22 @@ public class RetentionPostServiceImpl implements RetentionPostService {
 
     private CaseRetentionEntity createNewCaseRetention(PostRetentionRequest postRetentionRequest, CourtCaseEntity courtCase,
                                                        LocalDate newRetentionDate) {
+        UserAccountEntity currentUser = authorisationApi.getCurrentUser();
+        return createNewCaseRetention(postRetentionRequest, courtCase,
+                                      newRetentionDate, currentUser, CaseRetentionStatus.COMPLETE);
+    }
+
+    @Override
+    public CaseRetentionEntity createNewCaseRetention(PostRetentionRequest postRetentionRequest, CourtCaseEntity courtCase,
+                                                      LocalDate newRetentionDate, UserAccountEntity userAccount, CaseRetentionStatus caseRetentionStatus) {
         CaseRetentionEntity caseRetention = new CaseRetentionEntity();
         caseRetention.setCourtCase(courtCase);
-        UserAccountEntity currentUser = authorisationApi.getCurrentUser();
-        caseRetention.setLastModifiedBy(currentUser);
-        caseRetention.setCreatedBy(currentUser);
-        caseRetention.setSubmittedBy(currentUser);
+        caseRetention.setLastModifiedBy(userAccount);
+        caseRetention.setCreatedBy(userAccount);
+        caseRetention.setSubmittedBy(userAccount);
         caseRetention.setComments(postRetentionRequest.getComments());
         caseRetention.setRetainUntil(newRetentionDate.atTime(OffsetTime.of(0, 0, 0, 0, ZoneOffset.UTC)));
-        caseRetention.setCurrentState(String.valueOf(CaseRetentionStatus.COMPLETE));
+        caseRetention.setCurrentState(String.valueOf(caseRetentionStatus));
         caseRetention.setRetainUntilAppliedOn(currentTimeHelper.currentOffsetDateTime());
 
         caseRetention.setRetentionPolicyType(getRetentionPolicy(postRetentionRequest.getIsPermanentRetention()));
@@ -181,7 +188,7 @@ public class RetentionPostServiceImpl implements RetentionPostService {
         caseRetentionRepository.saveAndFlush(caseRetention);
         auditApi.recordAudit(
             AuditActivity.APPLY_RETENTION,
-            currentUser,
+            userAccount,
             courtCase
         );
         return caseRetention;
