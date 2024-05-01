bf966c1d3281f3d2f35f1f19bcd5031efa01a9db
tonym <incalabsltd@gmail.com>
2024-05-01 15:01:00 +0100

    WIP

--- src/main/java/uk/gov/hmcts/darts/task/service/impl/AdminAutomatedTasksServiceImpl.java
+++ src/main/java/uk/gov/hmcts/darts/task/service/impl/AdminAutomatedTasksServiceImpl.java
@@ -2,7 +2,6 @@ package uk.gov.hmcts.darts.task.service.impl;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import uk.gov.hmcts.darts.common.entity.AutomatedTaskEntity;
 import uk.gov.hmcts.darts.common.exception.DartsApiException;
 import uk.gov.hmcts.darts.common.repository.AutomatedTaskRepository;
 import uk.gov.hmcts.darts.task.exception.AutomatedTaskApiError;
@@ -17,11 +16,12 @@ import java.util.List;
 public class AdminAutomatedTasksServiceImpl implements AdminAutomatedTaskService {
 
     private final AutomatedTaskRepository automatedTaskRepository;
+    private final AutomatedTasksMapper mapper;
 
     @Override
     public List<AutomatedTaskSummary> getAllAutomatedTasks() {
         var automatedTask = automatedTaskRepository.findAll();
-        return mapEntitiesToModel(automatedTask);
+        return mapper.mapEntitiesToModel(automatedTask);
     }
 
     @Override
@@ -31,35 +31,6 @@ public class AdminAutomatedTasksServiceImpl implements AdminAutomatedTaskService
             throw new DartsApiException(AutomatedTaskApiError.AUTOMATED_TASK_NOT_FOUND);
         }
 
-        return mapEntityToDetailedAutomatedTask(maybeAutomatedTask.get());
-    }
-
-    private DetailedAutomatedTask mapEntityToDetailedAutomatedTask(AutomatedTaskEntity automatedTaskEntity) {
-        return new DetailedAutomatedTask()
-            .id(automatedTaskEntity.getId())
-            .name(automatedTaskEntity.getTaskName())
-            .isActive(automatedTaskEntity.getTaskEnabled())
-            .description(automatedTaskEntity.getTaskDescription())
-            .cronExpression(automatedTaskEntity.getCronExpression())
-            .createdAt(automatedTaskEntity.getCreatedDateTime())
-            .createdBy(automatedTaskEntity.getCreatedBy().getId())
-            .lastModifiedAt(automatedTaskEntity.getLastModifiedDateTime())
-            .lastModifiedBy(automatedTaskEntity.getLastModifiedBy().getId())
-            .isCronEditable(automatedTaskEntity.getCronEditable());
-    }
-
-    private List<AutomatedTaskSummary> mapEntitiesToModel(List<AutomatedTaskEntity> automatedTaskEntities) {
-        return automatedTaskEntities.stream()
-            .map(this::mapEntityToTaskSummary)
-            .toList();
-    }
-
-    private AutomatedTaskSummary mapEntityToTaskSummary(AutomatedTaskEntity automatedTaskEntity) {
-        return new AutomatedTaskSummary()
-            .id(automatedTaskEntity.getId())
-            .name(automatedTaskEntity.getTaskName())
-            .isActive(automatedTaskEntity.getTaskEnabled())
-            .description(automatedTaskEntity.getTaskDescription())
-            .cronExpression(automatedTaskEntity.getCronExpression());
+        return mapper.mapEntityToDetailedAutomatedTask(maybeAutomatedTask.get());
     }
 }
