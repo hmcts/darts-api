d575020a49b59279ce35e787252979d565c0d4cb
tonym <129744569+tonymort@users.noreply.github.com>
2024-05-01 16:50:24 +0100

    DMP-2812: Returning automated task by id (#1397)

--- src/main/java/uk/gov/hmcts/darts/task/service/AdminAutomatedTaskService.java
+++ src/main/java/uk/gov/hmcts/darts/task/service/AdminAutomatedTaskService.java
@@ -1,11 +1,13 @@
 package uk.gov.hmcts.darts.task.service;
 
-import uk.gov.hmcts.darts.tasks.model.AutomatedTask;
+import uk.gov.hmcts.darts.tasks.model.AutomatedTaskSummary;
+import uk.gov.hmcts.darts.tasks.model.DetailedAutomatedTask;
 
 import java.util.List;
 
 public interface AdminAutomatedTaskService {
 
-    List<AutomatedTask> getAllAutomatedTasks();
+    List<AutomatedTaskSummary> getAllAutomatedTasks();
 
+    DetailedAutomatedTask getAutomatedTaskById(Integer taskId);
 }
