d9deadfa019ef454b4e106c20b8d63a62e359d5e
Chris Magowan <131666106+cmagowan-hmcts@users.noreply.github.com>
2024-05-02 09:17:21 +0000

    DMP-2205 - add pending retention to auto closed cases. (#1403)

--- src/main/java/uk/gov/hmcts/darts/retention/api/impl/RetentionApiImpl.java
+++ src/main/java/uk/gov/hmcts/darts/retention/api/impl/RetentionApiImpl.java
@@ -2,9 +2,15 @@ package uk.gov.hmcts.darts.retention.api.impl;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import uk.gov.hmcts.darts.common.entity.CaseRetentionEntity;
+import uk.gov.hmcts.darts.common.entity.CourtCaseEntity;
 import uk.gov.hmcts.darts.common.entity.RetentionPolicyTypeEntity;
+import uk.gov.hmcts.darts.common.entity.UserAccountEntity;
 import uk.gov.hmcts.darts.retention.api.RetentionApi;
+import uk.gov.hmcts.darts.retention.enums.CaseRetentionStatus;
 import uk.gov.hmcts.darts.retention.helper.RetentionDateHelper;
+import uk.gov.hmcts.darts.retention.service.RetentionPostService;
+import uk.gov.hmcts.darts.retentions.model.PostRetentionRequest;
 
 import java.time.LocalDate;
 
@@ -13,9 +19,17 @@ import java.time.LocalDate;
 public class RetentionApiImpl implements RetentionApi {
 
     private final RetentionDateHelper retentionDateHelper;
+    private final RetentionPostService retentionPostService;
 
     @Override
     public LocalDate applyPolicyStringToDate(LocalDate dateToAppend, String policyString, RetentionPolicyTypeEntity retentionPolicyType) {
         return retentionDateHelper.applyPolicyString(dateToAppend, policyString, retentionPolicyType);
+    }
+
+    @Override
+    public CaseRetentionEntity createRetention(PostRetentionRequest postRetentionRequest, CourtCaseEntity courtCase, LocalDate newRetentionDate,
+                                               UserAccountEntity userAccount, CaseRetentionStatus caseRetentionStatus) {
+        return retentionPostService.createNewCaseRetention(postRetentionRequest, courtCase,
+                                                           newRetentionDate, userAccount, caseRetentionStatus);
     }
 }
