f25a0a2eec971f8db285edf3bd8ca9a81279496b
garethmorriscgi <127204755+garethmorriscgi@users.noreply.github.com>
2024-05-07 10:52:56 +0100

    DMP-2694 refinements based on sonar suggestions

--- src/main/java/uk/gov/hmcts/darts/task/runner/impl/ProcessArmResponseFilesAutomatedTask.java
+++ src/main/java/uk/gov/hmcts/darts/task/runner/impl/ProcessArmResponseFilesAutomatedTask.java
@@ -4,14 +4,10 @@ import lombok.extern.slf4j.Slf4j;
 import net.javacrumbs.shedlock.core.LockProvider;
 import uk.gov.hmcts.darts.arm.component.AutomatedTaskProcessorFactory;
 import uk.gov.hmcts.darts.arm.service.ArmResponseFilesProcessor;
-import uk.gov.hmcts.darts.common.entity.AutomatedTaskEntity;
 import uk.gov.hmcts.darts.common.repository.AutomatedTaskRepository;
 import uk.gov.hmcts.darts.log.api.LogApi;
 import uk.gov.hmcts.darts.task.config.AutomatedTaskConfigurationProperties;
 
-import java.util.Optional;
-
-import static java.util.Objects.nonNull;
 import static uk.gov.hmcts.darts.task.runner.AutomatedTaskName.PROCESS_ARM_RESPONSE_FILES_TASK_NAME;
 
 @Slf4j
@@ -35,17 +31,9 @@ public class ProcessArmResponseFilesAutomatedTask extends AbstractLockableAutoma
 
     @Override
     protected void runTask() {
-        Optional<AutomatedTaskEntity> automatedTaskEntity = getAutomatedTaskDetails(taskName);
+        boolean inBatchMode = isAutomatedTaskInBatchMode(taskName);
 
-        if (automatedTaskEntity.isEmpty()) {
-            handleException(new Exception("Unable to find automated task details"));
-            return;
-        }
-
-        AutomatedTaskEntity automatedTask = automatedTaskEntity.get();
-        boolean isBatchMode = nonNull(automatedTask.getBatchSize()) && automatedTask.getBatchSize() > 0;
-
-        ArmResponseFilesProcessor armResponseFilesProcessor = automatedTaskProcessorFactory.createArmResponseFilesProcessor(isBatchMode);
+        ArmResponseFilesProcessor armResponseFilesProcessor = automatedTaskProcessorFactory.createArmResponseFilesProcessor(inBatchMode);
         armResponseFilesProcessor.processResponseFiles();
     }
 
