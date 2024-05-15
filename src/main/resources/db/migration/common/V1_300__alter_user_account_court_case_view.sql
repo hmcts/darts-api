07dc62dceddba5846f0d5237d1fdd491df0de45d
karen-hedges <133129444+karen-hedges@users.noreply.github.com>
2024-05-15 11:38:33 +0100

    DMP-2994 Add TRANSLATION_QA Role wherever use_interpreter flag of security_group table is used
    Added translation qa role check to the case search view

--- src/main/resources/db/migration/common/V1_300__alter_user_account_court_case_view.sql
+++ src/main/resources/db/migration/common/V1_300__alter_user_account_court_case_view.sql
@@ -22,10 +22,11 @@ select distinct cth_id, usr_id, rol_id, cas_id from (SELECT ccase.cth_id,
      JOIN security_group_courthouse_ae grc ON grc.grp_id = grp.grp_id
      JOIN courthouse cth ON grc.cth_id = cth.cth_id
 	 JOIN court_case ccase ON cth.cth_id = ccase.cth_id
+	 JOIN security_role rol ON grp.rol_id = rol.rol_id
   WHERE grp.global_access = false
   and grp.use_interpreter = true
   and ccase.interpreter_used = true
-  and grp.rol_id in (SELECT DISTINCT rol_id FROM security_role WHERE role_name = 'TRANSLATION_QA')
+  and rol.role_name = 'TRANSLATION_QA'
 UNION
  SELECT ccase.cth_id,
     usr.usr_id,
@@ -35,7 +36,7 @@ UNION
      JOIN security_group_user_account_ae gua ON usr.usr_id = gua.usr_id
      JOIN security_group grp ON grp.grp_id = gua.grp_id
      CROSS JOIN court_case ccase
-  WHERE grp.global_access = true							  
+  WHERE grp.global_access = true
   and grp.use_interpreter = false
 UNION
  SELECT ccase.cth_id,
@@ -45,9 +46,10 @@ UNION
    FROM user_account usr
      JOIN security_group_user_account_ae gua ON usr.usr_id = gua.usr_id
      JOIN security_group grp ON grp.grp_id = gua.grp_id
+     JOIN security_role rol ON grp.rol_id = rol.rol_id
      CROSS JOIN court_case ccase
-  WHERE grp.global_access = true							  
+  WHERE grp.global_access = true
   and grp.use_interpreter = true
   and ccase.interpreter_used = true
-  and grp.rol_id in (SELECT DISTINCT rol_id FROM security_role WHERE role_name = 'TRANSLATION_QA')
+  and rol.role_name = 'TRANSLATION_QA'
   ) sub_query
