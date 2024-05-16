aae8d9c8c05b0eb64d2f09754f361e052403ec8a
karen-hedges <133129444+karen-hedges@users.noreply.github.com>
2024-05-16 10:33:51 +0100

    DMP-2994 Add TRANSLATION_QA Role wherever use_interpreter flag of security_group table is used
    Added translation qa role check to the case search view

--- src/integrationTest/java/uk/gov/hmcts/darts/cases/service/CaseServiceAdvancedSearchUseInterpreterTest.java
+++ src/integrationTest/java/uk/gov/hmcts/darts/cases/service/CaseServiceAdvancedSearchUseInterpreterTest.java
@@ -23,7 +23,6 @@ import uk.gov.hmcts.darts.testutils.data.SecurityGroupTestData;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static uk.gov.hmcts.darts.common.enums.SecurityRoleEnum.APPROVER;
 import static uk.gov.hmcts.darts.common.enums.SecurityRoleEnum.TRANSLATION_QA;
 import static uk.gov.hmcts.darts.testutils.data.CaseTestData.createCaseAt;
 import static uk.gov.hmcts.darts.testutils.data.CourthouseTestData.createCourthouse;
@@ -248,30 +247,6 @@ class CaseServiceAdvancedSearchUseInterpreterTest extends IntegrationBase {
         // then
         var caseNumbers = resultList.stream().map(AdvancedSearchResult::getCaseNumber).toList();
         assertThat(caseNumbers).containsExactlyInAnyOrder("Case2");
-    }
-
-    @Test
-    void testSearchCasesWithPermissionsGlobalTrueOnCourthouselessUserGroupUseInterpreterFalseOnCourthouse() {
-        // given
-        var securityGroup = SecurityGroupTestData.buildGroupForRole(APPROVER);
-        securityGroup.setGlobalAccess(true);
-        securityGroup.setUseInterpreter(false);
-        assignSecurityGroupToUser(user, securityGroup);
-
-        var securityGroupSwansea = SecurityGroupTestData.buildGroupForRoleAndCourthouse(TRANSLATION_QA, swanseaCourthouse);
-        securityGroupSwansea.setGlobalAccess(false);
-        securityGroupSwansea.setUseInterpreter(true);
-        assignSecurityGroupToUser(user, securityGroupSwansea);
-
-        userAccountRepository.save(user);
-
-        // when
-        GetCasesSearchRequest allCasesRequest = GetCasesSearchRequest.builder().build();
-        List<AdvancedSearchResult> resultList = service.advancedSearch(allCasesRequest);
-
-        // then
-        var caseNumbers = resultList.stream().map(AdvancedSearchResult::getCaseNumber).toList();
-        assertThat(caseNumbers).containsExactlyInAnyOrder("Case1", "Case2", "Case3", "Case4", "Case5", "Case6");
     }
 
     @Test
