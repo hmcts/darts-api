9e6643227301ae48f281bd6dc5ba851d62904956
Mario Paniccia <mario.paniccia@hmcts.net>
2024-05-17 09:14:21 +0100

    DMP-3051 Updated /admin/transcription-workflows endpoint to return migrated comments (#1460)

--- src/main/java/uk/gov/hmcts/darts/common/repository/TranscriptionCommentRepository.java
+++ src/main/java/uk/gov/hmcts/darts/common/repository/TranscriptionCommentRepository.java
@@ -4,7 +4,12 @@ package uk.gov.hmcts.darts.common.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import uk.gov.hmcts.darts.common.entity.TranscriptionCommentEntity;
+import uk.gov.hmcts.darts.common.entity.TranscriptionEntity;
+
+import java.util.List;
 
 @Repository
 public interface TranscriptionCommentRepository extends JpaRepository<TranscriptionCommentEntity, Integer> {
+
+    List<TranscriptionCommentEntity> getByTranscriptionAndTranscriptionWorkflowIsNull(TranscriptionEntity transcription);
 }
