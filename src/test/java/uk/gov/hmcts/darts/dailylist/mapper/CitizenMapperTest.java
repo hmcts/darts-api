cefaa11c4025e6bc8dcf1b046a1504c91235989c
Mr Michael Estebanez <63051024+mestebanez@users.noreply.github.com>
2024-05-23 17:18:26 +0100

    DMP-3002 - Proposal for preventing duplicate daily list records (#1454)
    Co-authored-by: Jack Maloney <jack.maloney1@hmcts.net>

---
+++ src/test/java/uk/gov/hmcts/darts/dailylist/mapper/CitizenMapperTest.java
@@ -0,0 +1,41 @@
+package uk.gov.hmcts.darts.dailylist.mapper;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import uk.gov.hmcts.darts.dailylist.model.CitizenName;
+
+class CitizenMapperTest {
+    @Test
+    void testCreateCitizenMapperFromStringWithNoBreaks() {
+        String name = "DMP-723-AC1-D001";
+        CitizenNameMapper mapper = new CitizenNameMapper();
+        CitizenName citizenName = mapper.getCitizenName(name);
+        Assertions.assertEquals(name, citizenName.getCitizenNameForename());
+        Assertions.assertEquals("", citizenName.getCitizenNameSurname());
+    }
+
+    @Test
+    void testCreateCitizenMapperFromStringWithBreaks() {
+        String name = "Jow Bloggs Test";
+        CitizenNameMapper mapper = new CitizenNameMapper();
+        CitizenName citizenName = mapper.getCitizenName(name);
+        Assertions.assertEquals("Jow", citizenName.getCitizenNameForename());
+        Assertions.assertEquals("Bloggs Test", citizenName.getCitizenNameSurname());
+    }
+
+    @Test
+    void testGetStringFromCitizenMapperWithSurnameAndForename() {
+        String name = "Jow Bloggs Test";
+        CitizenNameMapper mapper = new CitizenNameMapper();
+        CitizenName citizenName = mapper.getCitizenName(name);
+        Assertions.assertEquals(name, mapper.getCitizenName(citizenName));
+    }
+
+    @Test
+    void testGetStringFromCitizenMapperWithForename() {
+        String name = "DMP-723-AC1-D001";
+        CitizenNameMapper mapper = new CitizenNameMapper();
+        CitizenName citizenName = mapper.getCitizenName(name);
+        Assertions.assertEquals(name, mapper.getCitizenName(citizenName));
+    }
+}
