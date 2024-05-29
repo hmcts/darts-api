7ed7257b30b40fe7ea3fc6e4771553ea7c48caaf
tonym <incalabsltd@gmail.com>
2024-05-29 17:10:26 +0100

    WIP

--- src/main/resources/db/migration/common/V1_304__update_updcase_handler.sql
+++ src/main/resources/db/migration/common/V1_304__update_updcase_handler.sql
@@ -1,3 +1,19 @@
 update event_handler
 set handler = 'DartsEventNullHandler'
 where (event_type = 'UPDCASE' and event_sub_type is null);
+
+update event_handler
+set event_name = 'Equipment / accommodation'
+where event_type = '20198' and event_sub_type = '13937';
+
+update event_handler
+set event_name = 'Appellant Read'
+where event_type = '20935' and event_sub_type = '10633';
+
+update event_handler
+set event_name = 'Appellant Read'
+where event_type = '20936' and event_sub_type = '10633';
+
+update event_handler
+set event_name = 'Defendant subject to an electronically monitored curfew'
+where event_type = '20936' and event_sub_type = '10633';
