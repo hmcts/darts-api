01f19bf4f7f5209be657972e712dfae92fe75807
mfesteba@yahoo.co.uk <63051024+mestebanez@users.noreply.github.com>
2024-06-18 12:17:26 +0100

    Hard code primary keys

--- src/main/resources/db/migration/common/V1_317__insert_automated_task_activity.sql
+++ src/main/resources/db/migration/common/V1_317__insert_automated_task_activity.sql
@@ -1,4 +1,4 @@
 INSERT INTO audit_activity (aua_id, activity_name, activity_description, created_ts, created_by, last_modified_ts, last_modified_by)
-VALUES (nextval('aua_seq'), 'Run Job Manually', 'Run Job Manually', CURRENT_TIMESTAMP, 0, CURRENT_TIMESTAMP, 0);
+VALUES (28, 'Run Job Manually', 'Run Job Manually', CURRENT_TIMESTAMP, 0, CURRENT_TIMESTAMP, 0);
 INSERT INTO audit_activity (aua_id, activity_name, activity_description, created_ts, created_by, last_modified_ts, last_modified_by)
-VALUES (nextval('aua_seq'), 'Enable/Disable Job', 'Enable/Disable Job', CURRENT_TIMESTAMP, 0, CURRENT_TIMESTAMP, 0);
+VALUES (29, 'Enable/Disable Job', 'Enable/Disable Job', CURRENT_TIMESTAMP, 0, CURRENT_TIMESTAMP, 0);
