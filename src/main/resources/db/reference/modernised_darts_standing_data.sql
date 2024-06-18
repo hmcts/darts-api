af7e92ce3eb759a4b390dbb152c13fa1c7153613
mfesteba@yahoo.co.uk <63051024+mestebanez@users.noreply.github.com>
2024-06-18 10:27:04 +0100

    Update the standing data as per PR comment

--- src/main/resources/db/reference/modernised_darts_standing_data.sql
+++ src/main/resources/db/reference/modernised_darts_standing_data.sql
@@ -33,6 +33,8 @@ INSERT INTO audit_activity (aua_id, activity_name, activity_description, created
 INSERT INTO audit_activity (aua_id, activity_name, activity_description, created_ts, created_by, last_modified_ts, last_modified_by) VALUES (14, 'Create Courthouse', 'Create Courthouse', CURRENT_TIMESTAMP, 0, CURRENT_TIMESTAMP, 0);
 INSERT INTO audit_activity (aua_id, activity_name, activity_description, created_ts, created_by, last_modified_ts, last_modified_by) VALUES (15, 'Update Courthouse Group', 'Update Courthouse Group', CURRENT_TIMESTAMP, 0, CURRENT_TIMESTAMP, 0);
 INSERT INTO audit_activity (aua_id, activity_name, activity_description, created_ts, created_by, last_modified_ts, last_modified_by) VALUES (16, 'Update Courthouse', 'Update Courthouse', CURRENT_TIMESTAMP, 0, CURRENT_TIMESTAMP, 0);
+INSERT INTO audit_activity (aua_id, activity_name, activity_description, created_ts, created_by, last_modified_ts, last_modified_by) VALUES (25, 'Run Job Manually', 'Run Job Manually', CURRENT_TIMESTAMP, 0, CURRENT_TIMESTAMP, 0);
+INSERT INTO audit_activity (aua_id, activity_name, activity_description, created_ts, created_by, last_modified_ts, last_modified_by) VALUES (26, 'Enable/Disable Job', 'Enable/Disable Job', CURRENT_TIMESTAMP, 0, CURRENT_TIMESTAMP, 0);
 
 INSERT INTO event_handler (evh_id, event_type, event_sub_type, event_name, handler, active,is_reporting_restriction, created_by, created_ts) VALUES (1, '1000', '1002', 'Proceedings in chambers', 'StandardEventHandler', true,false,0,current_timestamp);
 INSERT INTO event_handler (evh_id, event_type, event_sub_type, event_name, handler, active,is_reporting_restriction, created_by, created_ts) VALUES (2, '1000', '1001', 'Offences put to defendant', 'StandardEventHandler', true,false,0,current_timestamp);
