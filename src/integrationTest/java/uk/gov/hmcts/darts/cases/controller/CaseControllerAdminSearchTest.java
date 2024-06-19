97c93551ac5f1488b8831e1c963cfac3f3287e6c
Chris Magowan <131666106+cmagowan-hmcts@users.noreply.github.com>
2024-06-19 16:35:08 +0000

    DMP-3266 implement POST /admin/cases/search (#1597)

---
+++ src/integrationTest/java/uk/gov/hmcts/darts/cases/controller/CaseControllerAdminSearchTest.java
@@ -0,0 +1,211 @@
+package uk.gov.hmcts.darts.cases.controller;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.skyscreamer.jsonassert.JSONCompareMode;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import uk.gov.hmcts.darts.common.entity.CourtCaseEntity;
+import uk.gov.hmcts.darts.common.entity.CourthouseEntity;
+import uk.gov.hmcts.darts.common.entity.CourtroomEntity;
+import uk.gov.hmcts.darts.common.entity.EventEntity;
+import uk.gov.hmcts.darts.common.entity.HearingEntity;
+import uk.gov.hmcts.darts.common.entity.JudgeEntity;
+import uk.gov.hmcts.darts.common.entity.SecurityGroupEntity;
+import uk.gov.hmcts.darts.common.entity.UserAccountEntity;
+import uk.gov.hmcts.darts.common.repository.SecurityGroupRepository;
+import uk.gov.hmcts.darts.common.repository.UserAccountRepository;
+import uk.gov.hmcts.darts.test.common.TestUtils;
+import uk.gov.hmcts.darts.test.common.data.SecurityGroupTestData;
+import uk.gov.hmcts.darts.testutils.IntegrationBase;
+
+import java.time.LocalDate;
+import java.time.OffsetDateTime;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.skyscreamer.jsonassert.JSONAssert.assertEquals;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static uk.gov.hmcts.darts.common.enums.SecurityRoleEnum.SUPER_ADMIN;
+import static uk.gov.hmcts.darts.test.common.TestUtils.getContentsFromFile;
+import static uk.gov.hmcts.darts.test.common.data.CaseTestData.createCaseAt;
+import static uk.gov.hmcts.darts.test.common.data.CourthouseTestData.someMinimalCourthouse;
+import static uk.gov.hmcts.darts.test.common.data.CourtroomTestData.createCourtRoomWithNameAtCourthouse;
+import static uk.gov.hmcts.darts.test.common.data.DefendantTestData.createDefendantForCaseWithName;
+import static uk.gov.hmcts.darts.test.common.data.EventTestData.createEventWith;
+import static uk.gov.hmcts.darts.test.common.data.HearingTestData.createHearingWithDefaults;
+import static uk.gov.hmcts.darts.test.common.data.JudgeTestData.createJudgeWithName;
+import static uk.gov.hmcts.darts.testutils.stubs.UserAccountStub.INTEGRATION_TEST_USER_EMAIL;
+
+
+@AutoConfigureMockMvc
+
+@SuppressWarnings({"PMD.VariableDeclarationUsageDistance", "PMD.NcssCount", "PMD.ExcessiveImports"})
+class CaseControllerAdminSearchTest extends IntegrationBase {
+
+    private static final String ENDPOINT_URL = "/admin/cases/search";
+
+    @Autowired
+    SecurityGroupRepository securityGroupRepository;
+    @Autowired
+    UserAccountRepository userAccountRepository;
+    @Autowired
+    private transient MockMvc mockMvc;
+    CourthouseEntity swanseaCourthouse;
+    UserAccountEntity user;
+
+    @BeforeEach
+    void setupData() {
+        swanseaCourthouse = someMinimalCourthouse();
+        swanseaCourthouse.setCourthouseName("SWANSEA");
+        swanseaCourthouse.setDisplayName("SWANSEA");
+
+        CourtCaseEntity case1 = createCaseAt(swanseaCourthouse);
+        case1.setCaseNumber("Case1");
+
+        CourtCaseEntity case2 = createCaseAt(swanseaCourthouse);
+        case2.setCaseNumber("Case2");
+        case2.setDefendantList(Arrays.asList(createDefendantForCaseWithName(case2, "Defendant2")));
+
+        CourtCaseEntity case3 = createCaseAt(swanseaCourthouse);
+        case3.setCaseNumber("Case3");
+
+        CourtCaseEntity case4 = createCaseAt(swanseaCourthouse);
+        case4.setCaseNumber("Case4");
+
+        CourtCaseEntity case5 = createCaseAt(swanseaCourthouse);
+        case5.setCaseNumber("case5");
+
+        CourtCaseEntity case6 = createCaseAt(swanseaCourthouse);
+        case6.setCaseNumber("case6");
+
+        JudgeEntity judge = createJudgeWithName("aJudge");
+        CourtroomEntity courtroom1 = createCourtRoomWithNameAtCourthouse(swanseaCourthouse, "courtroom1");
+
+        HearingEntity hearing1a = createHearingWithDefaults(case1, courtroom1, LocalDate.of(2023, 5, 20), judge);
+
+        HearingEntity hearing1b = createHearingWithDefaults(case1, courtroom1, LocalDate.of(2023, 5, 21), judge);
+
+        HearingEntity hearing1c = createHearingWithDefaults(case1, courtroom1, LocalDate.of(2023, 5, 22), judge);
+
+        HearingEntity hearing2a = createHearingWithDefaults(case2, courtroom1, LocalDate.of(2023, 6, 20), judge);
+
+        HearingEntity hearing2b = createHearingWithDefaults(case2, courtroom1, LocalDate.of(2023, 6, 21), judge);
+
+        HearingEntity hearing2c = createHearingWithDefaults(case2, courtroom1, LocalDate.of(2023, 6, 22), judge);
+
+        HearingEntity hearing3a = createHearingWithDefaults(case3, courtroom1, LocalDate.of(2023, 7, 20), judge);
+        JudgeEntity judge3a = createJudgeWithName("Judge3a");
+        hearing3a.addJudge(judge3a);
+
+        HearingEntity hearing3b = createHearingWithDefaults(case3, courtroom1, LocalDate.of(2023, 7, 21), judge);
+
+        HearingEntity hearing3c = createHearingWithDefaults(case3, courtroom1, LocalDate.of(2023, 7, 22), judge);
+
+        CourtroomEntity courtroom2 = createCourtRoomWithNameAtCourthouse(swanseaCourthouse, "courtroom2");
+
+        HearingEntity hearing4a = createHearingWithDefaults(case4, courtroom2, LocalDate.of(2023, 8, 20), judge);
+
+        HearingEntity hearing4b = createHearingWithDefaults(case4, courtroom1, LocalDate.of(2023, 8, 21), judge);
+
+        HearingEntity hearing4c = createHearingWithDefaults(case4, courtroom1, LocalDate.of(2023, 8, 22), judge);
+
+        HearingEntity hearing5a = createHearingWithDefaults(case5, courtroom2, LocalDate.of(2023, 9, 20), judge);
+
+        HearingEntity hearing5b = createHearingWithDefaults(case5, courtroom1, LocalDate.of(2023, 9, 21), judge);
+
+        CourtroomEntity courtroom3 = createCourtRoomWithNameAtCourthouse(swanseaCourthouse, "courtroom3");
+        HearingEntity hearing5c = createHearingWithDefaults(case5, courtroom3, LocalDate.of(2023, 9, 22), judge);
+
+        HearingEntity hearing6a = createHearingWithDefaults(case6, courtroom2, LocalDate.of(2023, 9, 20), judge);
+
+        HearingEntity hearing6b = createHearingWithDefaults(case6, courtroom3, LocalDate.of(2023, 9, 21), judge);
+        hearing6b.addJudge(createJudgeWithName("Judge6b"));
+
+        HearingEntity hearing6c = createHearingWithDefaults(case6, courtroom1, LocalDate.of(2023, 9, 22), judge);
+
+        dartsDatabase.saveAll(hearing1a, hearing1b, hearing1c,
+                              hearing2a, hearing2b, hearing2c,
+                              hearing3a, hearing3b, hearing3c,
+                              hearing4a, hearing4b, hearing4c,
+                              hearing5a, hearing5b, hearing5c,
+                              hearing6a, hearing6b, hearing6c
+
+        );
+
+        EventEntity event4a = createEventWith("eventName", "event4a", hearing4a, OffsetDateTime.now());
+        EventEntity event5b = createEventWith("eventName", "event5b", hearing5b, OffsetDateTime.now());
+        dartsDatabase.saveAll(event4a, event5b);
+
+        givenBearerTokenExists(INTEGRATION_TEST_USER_EMAIL);
+        user = dartsDatabase.getUserAccountStub().getIntegrationTestUserAccountEntity();
+        setupUserAccountAndSecurityGroup();
+
+
+    }
+
+    @AfterEach
+    void deleteUser() {
+        dartsDatabase.addToUserAccountTrash(INTEGRATION_TEST_USER_EMAIL);
+    }
+
+    @Test
+    void testOk() throws Exception {
+
+        String requestBody = """
+            {
+              "courthouse_ids": [
+                <<courthouseId>>
+              ],
+              "case_number": "Case1",
+              "courtroom_name": "courtroom1",
+              "hearing_start_at": "2020-06-18",
+              "hearing_end_at": "2024-06-18"
+            }""";
+
+        requestBody = requestBody.replace("<<courthouseId>>", swanseaCourthouse.getId().toString());
+        MockHttpServletRequestBuilder requestBuilder = post(ENDPOINT_URL)
+            .contentType(MediaType.APPLICATION_JSON_VALUE)
+            .content(requestBody);
+        MvcResult response = mockMvc.perform(requestBuilder).andExpect(status().isOk()).andReturn();
+
+        String actualResponse = TestUtils.removeIds(response.getResponse().getContentAsString());
+
+        String expectedResponse = getContentsFromFile(
+            "tests/cases/CaseControllerAdminSearchTest/testOk/expectedResponse.json");
+        assertEquals(expectedResponse, actualResponse, JSONCompareMode.NON_EXTENSIBLE);
+    }
+
+
+    private static void givenBearerTokenExists(String email) {
+        Jwt jwt = Jwt.withTokenValue("test")
+            .header("alg", "RS256")
+            .claim("emails", List.of(email))
+            .build();
+        SecurityContextHolder.getContext().setAuthentication(new JwtAuthenticationToken(jwt));
+    }
+
+    private void setupUserAccountAndSecurityGroup() {
+        var securityGroup = SecurityGroupTestData.buildGroupForRole(SUPER_ADMIN);
+        securityGroup.setGlobalAccess(true);
+        securityGroup.setUseInterpreter(false);
+        assignSecurityGroupToUser(user, securityGroup);
+    }
+
+    private void assignSecurityGroupToUser(UserAccountEntity user, SecurityGroupEntity securityGroup) {
+        securityGroup.getUsers().add(user);
+        user.getSecurityGroupEntities().add(securityGroup);
+        securityGroupRepository.save(securityGroup);
+        userAccountRepository.save(user);
+    }
+
+}
