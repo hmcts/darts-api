2c99b58d0f52264393815507570202cf5471f4fe
Mr Michael Estebanez <63051024+mestebanez@users.noreply.github.com>
2024-06-20 12:33:42 +0100

    Dmp 2858 audit admin job activities (#1598)

--- src/main/java/uk/gov/hmcts/darts/common/repository/AutomatedTaskRepository.java
+++ src/main/java/uk/gov/hmcts/darts/common/repository/AutomatedTaskRepository.java
@@ -2,6 +2,7 @@ package uk.gov.hmcts.darts.common.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.history.RevisionRepository;
 import org.springframework.stereotype.Repository;
 import uk.gov.hmcts.darts.common.entity.AutomatedTaskEntity;
 
@@ -10,8 +11,7 @@ import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface AutomatedTaskRepository extends JpaRepository<AutomatedTaskEntity, Integer> {
-
+public interface AutomatedTaskRepository extends RevisionRepository<AutomatedTaskEntity, Integer, Long>, JpaRepository<AutomatedTaskEntity, Integer> {
     Optional<AutomatedTaskEntity> findByTaskName(String taskName);
 
     @Query(value = """
--- src/test/java/uk/gov/hmcts/darts/task/service/impl/AdminAutomatedTasksServiceImplTest.java
+++ src/test/java/uk/gov/hmcts/darts/task/service/impl/AdminAutomatedTasksServiceImplTest.java
@@ -1,16 +1,21 @@
 package uk.gov.hmcts.darts.task.service.impl;
 
-
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import uk.gov.hmcts.darts.audit.api.AuditActivity;
+import uk.gov.hmcts.darts.audit.api.AuditApi;
+import uk.gov.hmcts.darts.authorisation.api.AuthorisationApi;
 import uk.gov.hmcts.darts.common.entity.AutomatedTaskEntity;
+import uk.gov.hmcts.darts.common.entity.UserAccountEntity;
 import uk.gov.hmcts.darts.common.helper.CurrentTimeHelper;
 import uk.gov.hmcts.darts.common.repository.AutomatedTaskRepository;
 import uk.gov.hmcts.darts.task.runner.impl.AbstractLockableAutomatedTask;
 import uk.gov.hmcts.darts.task.service.AdminAutomatedTaskService;
+import uk.gov.hmcts.darts.tasks.model.AutomatedTaskPatch;
+import uk.gov.hmcts.darts.tasks.model.DetailedAutomatedTask;
 
 import java.sql.Timestamp;
 import java.time.LocalDateTime;
@@ -19,6 +24,8 @@ import java.time.ZoneOffset;
 import java.util.List;
 import java.util.Optional;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -39,6 +46,12 @@ class AdminAutomatedTasksServiceImplTest {
     @Mock
     private AbstractLockableAutomatedTask someAutomatedTask;
 
+    @Mock
+    private AuditApi auditApi;
+
+    @Mock
+    private AuthorisationApi authorisationApi;
+
     private AdminAutomatedTaskService adminAutomatedTaskService;
 
     @BeforeEach
@@ -48,15 +61,17 @@ class AdminAutomatedTasksServiceImplTest {
             mapper,
             manualTaskService,
             automatedTaskRunner,
-            currentTimeHelper
+            currentTimeHelper,
+            auditApi,
+            authorisationApi
         );
-
-        when(someAutomatedTask.getTaskName()).thenReturn("some-task-name");
-        when(currentTimeHelper.currentOffsetDateTime()).thenReturn(OffsetDateTime.now());
     }
 
     @Test
     void invokesTaskWhenTaskIsNotLocked() {
+        when(someAutomatedTask.getTaskName()).thenReturn("some-task-name");
+        when(currentTimeHelper.currentOffsetDateTime()).thenReturn(OffsetDateTime.now());
+
         when(automatedTaskRepository.findById(1)).thenReturn(anAutomatedTaskEntityWithName("some-task-name"));
         when(automatedTaskRepository.findLockedUntilForTask("some-task-name")).thenReturn(anSqlDateInThePast());
         when(manualTaskService.getAutomatedTasks()).thenReturn(List.of(someAutomatedTask));
@@ -64,6 +79,27 @@ class AdminAutomatedTasksServiceImplTest {
         adminAutomatedTaskService.runAutomatedTask(1);
 
         verify(automatedTaskRunner, times(1)).run(someAutomatedTask);
+        verify(auditApi, times(1)).record(AuditActivity.RUN_JOB_MANUALLY);
+    }
+
+    @Test
+    void updateAutomatedTask() {
+        Optional<AutomatedTaskEntity> automatedTaskEntity = anAutomatedTaskEntityWithName("some-task-name");
+        when(automatedTaskRepository.findById(1)).thenReturn(automatedTaskEntity);
+
+        AutomatedTaskPatch automatedTaskPatch = new AutomatedTaskPatch();
+        automatedTaskPatch.setIsActive(false);
+
+        when(automatedTaskRepository.save(automatedTaskEntity.get())).thenReturn(automatedTaskEntity.get());
+
+        DetailedAutomatedTask expectedReturnTask = new DetailedAutomatedTask();
+        when(mapper.mapEntityToDetailedAutomatedTask(automatedTaskEntity.get())).thenReturn(expectedReturnTask);
+
+        DetailedAutomatedTask task = adminAutomatedTaskService.updateAutomatedTask(1, automatedTaskPatch);
+
+        assertFalse(automatedTaskEntity.get().getTaskEnabled());
+        assertEquals(expectedReturnTask, task);
+        verify(auditApi, times(1)).record(AuditActivity.ENABLE_DISABLE_JOB);
     }
 
     private List<Timestamp> anSqlDateInThePast() {
@@ -75,6 +111,20 @@ class AdminAutomatedTasksServiceImplTest {
     private Optional<AutomatedTaskEntity> anAutomatedTaskEntityWithName(String taskName) {
         var automatedTaskEntity = new AutomatedTaskEntity();
         automatedTaskEntity.setTaskName(taskName);
+        automatedTaskEntity.setId(1234);
+        automatedTaskEntity.setTaskEnabled(true);
+        automatedTaskEntity.setCronEditable(true);
+        automatedTaskEntity.setTaskDescription("task description");
+        automatedTaskEntity.setTaskName(taskName != null ? taskName : "task name");
+        automatedTaskEntity.setCronExpression("");
+        UserAccountEntity accountEntity = new UserAccountEntity();
+        accountEntity.setId(999);
+
+        automatedTaskEntity.setLastModifiedBy(accountEntity);
+        automatedTaskEntity.setCreatedDateTime(OffsetDateTime.now());
+        automatedTaskEntity.setCreatedBy(accountEntity);
+        automatedTaskEntity.setLastModifiedDateTime(OffsetDateTime.now());
+
         return Optional.of(automatedTaskEntity);
     }
 }
