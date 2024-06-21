ff01746edf565b871ca3c700e00bdb6dfdfca1e5
mariopaniccia <mario.paniccia@hmcts.net>
2024-06-21 18:53:59 +0100

    DMP-2623 added tests

--- src/integrationTest/java/uk/gov/hmcts/darts/task/service/GenerateCaseDocumentProcessorIntTest.java
+++ src/integrationTest/java/uk/gov/hmcts/darts/task/service/GenerateCaseDocumentProcessorIntTest.java
@@ -7,7 +7,9 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 import uk.gov.hmcts.darts.casedocument.service.GenerateCaseDocumentSingleCaseProcessor;
+import uk.gov.hmcts.darts.common.entity.CaseDocumentEntity;
 import uk.gov.hmcts.darts.common.entity.CourtCaseEntity;
+import uk.gov.hmcts.darts.common.entity.ExternalObjectDirectoryEntity;
 import uk.gov.hmcts.darts.common.entity.MediaEntity;
 import uk.gov.hmcts.darts.common.entity.UserAccountEntity;
 import uk.gov.hmcts.darts.common.repository.AnnotationRepository;
@@ -22,6 +24,7 @@ import java.time.OffsetDateTime;
 import java.util.List;
 
 import static java.time.ZoneOffset.UTC;
+import static org.assertj.core.api.Assertions.assertThat;
 import static uk.gov.hmcts.darts.common.enums.ExternalLocationTypeEnum.ARM;
 import static uk.gov.hmcts.darts.common.enums.ObjectRecordStatusEnum.ARM_DROP_ZONE;
 
@@ -46,10 +49,10 @@ public class GenerateCaseDocumentProcessorIntTest extends IntegrationBase {
 
     @Test
     public void testGenerateCaseDocument() {
+        // given
         givenBearerTokenExists("darts.global.user@hmcts.net");
         UserAccountEntity testUser = dartsDatabase.getUserAccountStub().getIntegrationTestUserAccountEntity();
 
-        // given
         CourtCaseEntity courtCase = dartsDatabase.getCourtCaseStub().createAndSaveCourtCaseWithHearings(createdCourtCase -> {
             createdCourtCase.setRetentionUpdated(true);
             createdCourtCase.setRetentionRetries(1);
@@ -57,21 +60,28 @@ public class GenerateCaseDocumentProcessorIntTest extends IntegrationBase {
         });
 
         List<MediaEntity> medias = dartsDatabase.getMediaStub().createAndSaveSomeMedias();
-        var hearA1 = courtCase.getHearings().get(0);
-        hearA1.addMedia(medias.get(0));
+        var hearing = courtCase.getHearings().get(0);
+        hearing.addMedia(medias.get(0));
 
-        hearingRepository.save(hearA1);
+        hearingRepository.save(hearing);
         dartsDatabase.getCaseRetentionStub().createCaseRetentionObject(courtCase, DT_2025);
 
         dartsDatabase.getExternalObjectDirectoryStub().createAndSaveEod(medias.get(0), ARM_DROP_ZONE, ARM, eod -> {});
 
-        var annotation1A = dartsDatabase.getAnnotationStub().createAndSaveAnnotationEntityWith(testUser, "TestAnnotation", hearA1);
-        annotationRepository.save(annotation1A);
+        var annotation = dartsDatabase.getAnnotationStub().createAndSaveAnnotationEntityWith(testUser, "TestAnnotation", hearing);
+        annotationRepository.save(annotation);
 
         // when
         processor.processGenerateCaseDocument(courtCase.getId());
 
         // then
+        List<CaseDocumentEntity> caseDocumentEntities = caseDocumentRepository.findByCourtCase(courtCase);
+        assertThat(caseDocumentEntities.size()).isEqualTo(1);
+        CaseDocumentEntity caseDocument = caseDocumentEntities.get(0);
+        assertThat(caseDocument.getCourtCase().getId()).isEqualTo(courtCase.getId());
+
+        List<ExternalObjectDirectoryEntity> eodCaseDocument = eodRepository.findByCaseDocument(caseDocument);
+        assertThat(eodCaseDocument.size()).isEqualTo(1);
     }
 
     private static void givenBearerTokenExists(String email) {
