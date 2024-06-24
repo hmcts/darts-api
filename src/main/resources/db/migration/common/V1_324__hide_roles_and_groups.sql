1ff0b1dce2d92b58f8c5f5f17a972362f2bf31e1
chris-bellingham <chris.bellingham@hmcts.net>
2024-06-24 13:57:33 +0100

    DMP-3305: Implement review comments

--- src/main/resources/db/migration/common/V1_324__hide_roles_and_groups.sql
+++ src/main/resources/db/migration/common/V1_324__hide_roles_and_groups.sql
@@ -1,13 +1,7 @@
 UPDATE security_role
 SET display_state = false
-WHERE role_name = 'XHIBIT'
-   OR role_name = 'CPP'
-   OR role_name = 'DAR_PC'
-   OR role_name = 'MID_TIER';
+WHERE role_name IN ('XHIBIT', 'CPP', 'DAR_PC', 'MID_TIER');
 
 UPDATE security_group
 SET display_state = false
-WHERE group_name = 'Xhibit Group'
-   OR group_name = 'Cpp Group'
-   OR group_name = 'Dar Pc Group'
-   OR group_name = 'Mid Tier Group';
+WHERE group_name IN ('Xhibit Group', 'Cpp Group', 'Dar Pc Group', 'Mid Tier Group')
