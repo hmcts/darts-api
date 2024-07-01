e1c8a1e28dc8c9e0e5b995bb89cfae60b640c287
tonym <incalabsltd@gmail.com>
2024-07-01 15:51:12 +0100

    WIP

--- src/main/java/uk/gov/hmcts/darts/common/config/AsyncEventsConfig.java
+++ src/main/java/uk/gov/hmcts/darts/common/config/AsyncEventsConfig.java
@@ -4,7 +4,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.event.ApplicationEventMulticaster;
 import org.springframework.context.event.SimpleApplicationEventMulticaster;
-import org.springframework.core.task.SimpleAsyncTaskExecutor;
 import org.springframework.core.task.TaskExecutor;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
