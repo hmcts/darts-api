aa5dd1a9f81f53161e59c1e5a9b2eae19bf950eb
Chris Bellingham <131763968+chrisbellingham-hmcts@users.noreply.github.com>
2024-07-18 10:33:30 +0100

    DMP-3330: Add validation for PATCH /admin/retention-policy-types to p… (#1724)

--- src/integrationTest/java/uk/gov/hmcts/darts/testutils/PostgresIntegrationBase.java
+++ src/integrationTest/java/uk/gov/hmcts/darts/testutils/PostgresIntegrationBase.java
@@ -24,19 +24,26 @@ public class PostgresIntegrationBase {
 
     protected MemoryLogAppender logAppender = LogUtil.getMemoryLogger();
 
+    /**
+     * We shouldn't need to change this value. If we need to increase the limit as more tests use PostgresIntegrationBase, then it suggests we have a
+     * connection leak somewhere and that should be investigated.
+     */
+    private static final int SERVER_MAX_CONNECTIONS = 50;
+
     private static final PostgreSQLContainer<?> POSTGRES = new PostgreSQLContainer<>(
         "postgres:15-alpine"
     ).withDatabaseName("darts");
 
     @DynamicPropertySource
     static void configureProperties(DynamicPropertyRegistry registry) {
-
         registry.add("spring.datasource.url", POSTGRES::getJdbcUrl);
         registry.add("spring.datasource.username", POSTGRES::getUsername);
         registry.add("spring.datasource.password", POSTGRES::getPassword);
     }
 
     static {
+        POSTGRES.setCommand("postgres", "-c", String.format("max_connections=%d", SERVER_MAX_CONNECTIONS));
+
         // container will be automatically stopped
         POSTGRES.start();
     }
