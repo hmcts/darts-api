e448e1523330389491718d7f1d377ca0b1f43643
chris-bellingham <chris.bellingham@hmcts.net>
2024-07-22 16:05:42 +0100

    DMP-3614: Utilise io streams for file upload to outbound storage for ATS

---
+++ src/test/java/uk/gov/hmcts/darts/datamanagement/model/BlobClientUploadResponseImplTest.java
@@ -0,0 +1,93 @@
+package uk.gov.hmcts.darts.datamanagement.model;
+
+import com.azure.storage.blob.BlobClient;
+import com.azure.storage.blob.models.BlobProperties;
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Map;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+@ExtendWith(MockitoExtension.class)
+class BlobClientUploadResponseImplTest {
+
+    private BlobClientUploadResponseImpl blobClientUploadResponse;
+
+    @Mock
+    private BlobClient blobClient;
+    @Mock
+    private BlobProperties blobProperties;
+
+    @BeforeEach
+    void setUp() {
+        blobClientUploadResponse = new BlobClientUploadResponseImpl(blobClient);
+    }
+
+    @Test
+    void shouldReturnExpectedBlobName() {
+        // Given
+        final UUID someName = UUID.randomUUID();
+        Mockito.when(blobClient.getBlobName())
+            .thenReturn(someName.toString());
+
+        // When
+        UUID blobName = blobClientUploadResponse.getBlobName();
+
+        // Then
+        assertEquals(someName, blobName);
+    }
+
+    @Test
+    void shouldReturnExpectedBlobSizeAndOnlyInvokeClientOnce() {
+        // Given
+        Mockito.when(blobClient.getProperties())
+            .thenReturn(blobProperties);
+
+        final long someSize = 1000L;
+        Mockito.when(blobProperties.getBlobSize())
+            .thenReturn(someSize);
+
+        // When invoked multiple times
+        for (int i = 0; i < 2; i++) {
+            Long blobSize = blobClientUploadResponse.getBlobSize();
+
+            // Then
+            assertEquals(someSize, blobSize);
+        }
+
+        // And
+        Mockito.verify(blobClient, Mockito.times(1))
+            .getProperties();
+    }
+
+    @Test
+    void shouldAppendMetadataToExistingMetadataAndInvokeClient() {
+        // Given
+        Mockito.when(blobClient.getProperties())
+            .thenReturn(blobProperties);
+
+        Mockito.when(blobProperties.getMetadata())
+            .thenReturn(Map.of("someExistingKey", "someExistingValue"));
+
+        // When
+        Map<String, String> metadata = blobClientUploadResponse.addMetadata(Map.of("someAdditionalKey", "someAdditionalValue"));
+
+        // Then
+        Mockito.verify(blobClient, Mockito.times(1))
+            .setMetadata(metadata);
+
+        assertEquals(2, metadata.size());
+        MatcherAssert.assertThat(metadata, Matchers.hasEntry("someExistingKey", "someExistingValue"));
+        MatcherAssert.assertThat(metadata, Matchers.hasEntry("someAdditionalKey", "someAdditionalValue"));
+    }
+
+}
