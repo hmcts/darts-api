b743ba819b042b45b002b29f8f04bc4041221adc
mfesteba@yahoo.co.uk <63051024+mestebanez@users.noreply.github.com>
2024-07-23 12:15:44 +0100

    Updated related to PR comments

--- src/main/resources/db/migration/common/V1_334__add_task_inbound_transcription_annotation_deletion.sql
+++ src/main/resources/db/migration/common/V1_334__add_task_inbound_transcription_annotation_deletion.sql
@@ -1,2 +1,2 @@
 INSERT INTO darts.automated_task (aut_id,task_name,task_description,cron_expression,cron_editable, batch_size)
-VALUES (nextval('aut_seq'),'InboundTranscriptionAnnotationDeleter','Marks for deletion the transcription and annotation that is stored in inbound for the longer than a specific date.','0 0 22 * * *',true,50);
+VALUES (nextval('aut_seq'),'InboundTranscriptionAnnotationDeleter','Marks for deletion the transcription and annotation that is stored in inbound for the longer than a specific time.','0 0 22 * * *',true,50);
