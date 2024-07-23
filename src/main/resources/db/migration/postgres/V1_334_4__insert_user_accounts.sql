04685049db01f0907d0a594e129c52980edb327d
Chris Magowan <chris.magowan@hmcts.net>
2024-07-23 16:39:20 +0100

    remove security group constraints

--- src/main/resources/db/migration/postgres/V1_334_4__insert_user_accounts.sql
+++ src/main/resources/db/migration/postgres/V1_334_4__insert_user_accounts.sql
@@ -10,19 +10,3 @@ ALTER TABLE user_account ALTER COLUMN last_modified_ts SET NOT NULL;
 update user_account set last_modified_by = 0 where last_modified_by is null;
 ALTER TABLE user_account ALTER COLUMN last_modified_by SET NOT NULL;
 
-
-
-update security_group set created_ts = current_timestamp where created_ts is null;
-ALTER TABLE security_group ALTER COLUMN created_ts SET NOT NULL;
-
-update security_group set created_by = 0 where created_by is null;
-ALTER TABLE security_group ALTER COLUMN created_by SET NOT NULL;
-
-update security_group set last_modified_ts = current_timestamp where last_modified_ts is null;
-ALTER TABLE security_group ALTER COLUMN last_modified_ts SET NOT NULL;
-
-update security_group set last_modified_by = 0 where last_modified_by is null;
-ALTER TABLE security_group ALTER COLUMN last_modified_by SET NOT NULL;
-
-update security_group set display_name = group_name where display_name is null;
-ALTER TABLE security_group ALTER COLUMN display_name SET NOT NULL;
