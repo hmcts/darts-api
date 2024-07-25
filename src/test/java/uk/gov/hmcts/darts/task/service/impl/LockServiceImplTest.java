b9a693fbb1076666668927555415977381b7de3d
Dave Thompson <davethompson21@gmail.com>
2024-07-25 16:04:12 +0100

    Extra tests

---
+++ src/test/java/uk/gov/hmcts/darts/task/service/impl/LockServiceImplTest.java
@@ -0,0 +1,87 @@
+package uk.gov.hmcts.darts.task.service.impl;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import uk.gov.hmcts.darts.common.entity.AutomatedTaskEntity;
+import uk.gov.hmcts.darts.common.helper.CurrentTimeHelper;
+import uk.gov.hmcts.darts.common.repository.AutomatedTaskRepository;
+
+import java.sql.Timestamp;
+import java.time.Duration;
+import java.time.OffsetDateTime;
+import java.util.Collections;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class LockServiceImplTest {
+
+    @InjectMocks
+    private LockServiceImpl lockService;
+
+    @Mock
+    private AutomatedTaskRepository automatedTaskRepository;
+    @Mock
+    private CurrentTimeHelper currentTimeHelper;
+
+    @Test
+    void getLockingTaskExecutor() {
+    }
+
+    @Test
+    void getLockAtMostFor() {
+        assertEquals(Duration.ofSeconds(600), lockService.getLockAtMostFor());
+    }
+
+    @Test
+    void getLockAtLeastFor() {
+        assertEquals(Duration.ofSeconds(20), lockService.getLockAtLeastFor());
+    }
+
+    @Test
+    void isLockedTrue() {
+        OffsetDateTime someHoursAgo = OffsetDateTime.now().minusHours(5);
+        when(currentTimeHelper.currentOffsetDateTime()).thenReturn(someHoursAgo);
+
+        Timestamp timestamp = new Timestamp(System.currentTimeMillis());
+        when(automatedTaskRepository.findLockedUntilForTask(anyString())).thenReturn(List.of(timestamp));
+
+        var taskName = "TestTask";
+        var automatedTaskEntity = new AutomatedTaskEntity();
+        automatedTaskEntity.setTaskName(taskName);
+
+        assertTrue(lockService.isLocked(automatedTaskEntity));
+    }
+
+    @Test
+    void isLockedFalse() {
+        OffsetDateTime someHoursAhead = OffsetDateTime.now().plusHours(5);
+        when(currentTimeHelper.currentOffsetDateTime()).thenReturn(someHoursAhead);
+
+        Timestamp timestamp = new Timestamp(System.currentTimeMillis());
+        when(automatedTaskRepository.findLockedUntilForTask(anyString())).thenReturn(List.of(timestamp));
+
+        var taskName = "TestTask";
+        var automatedTaskEntity = new AutomatedTaskEntity();
+        automatedTaskEntity.setTaskName(taskName);
+
+        assertFalse(lockService.isLocked(automatedTaskEntity));
+    }
+
+    @Test
+    void isLockedWhenNotSet() {
+        var taskName = "TestTask";
+        var automatedTaskEntity = new AutomatedTaskEntity();
+        automatedTaskEntity.setTaskName(taskName);
+        when(automatedTaskRepository.findLockedUntilForTask(anyString())).thenReturn(Collections.emptyList());
+        assertFalse(lockService.isLocked(automatedTaskEntity));
+    }
+}
