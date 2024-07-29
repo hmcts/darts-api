eb35950cbd3a2dd850a21607ce85148c742ebe04
Chris Magowan <chris.magowan@hmcts.net>
2024-07-29 13:53:00 +0100

    update script to move test data

--- src/main/resources/db/migration/postgres/V1_337_2__add_4_new_courthouses.sql
+++ src/main/resources/db/migration/postgres/V1_337_2__add_4_new_courthouses.sql
@@ -1,24 +1,22 @@
-ALTER SEQUENCE cth_seq CACHE 1;
-
 DO $$
 DECLARE
 	cHouseId integer;
 	securityGroupId integer;
 BEGIN
     --WOLVERHAMPTON SITTING AT TELFORD
-    SELECT nextval('cth_seq') into cHouseId;
+    SELECT 152 into cHouseId;
     INSERT INTO courthouse (cth_id,courthouse_code,courthouse_name,display_name,created_ts,created_by,last_modified_ts,last_modified_by) VALUES
 	 (cHouseId,NULL,'WOLVERHAMPTON SITTING AT TELFORD','Wolverhampton sitting at Telford','2024-01-01',0,'2024-01-01',0);
 
     INSERT INTO courthouse_region_ae (cth_id, reg_id) VALUES (cHouseId,4);
 
-    SELECT nextval('grp_seq') into securityGroupId;
+    SELECT 352 into securityGroupId;
     INSERT INTO security_group(
     grp_id, rol_id, group_name, is_private, description, group_global_unique_id, global_access, display_state, use_interpreter, display_name, dm_group_s_object_id, created_ts, created_by, last_modified_ts, last_modified_by)
     VALUES (securityGroupId, 3, 'moj_ch_wolverhampton_telford_appr', null, null, null, FALSE, TRUE, FALSE, 'Wolverhampton sitting at Telford Approver',null, '2024-01-01', 0, '2024-01-01', 0);
     INSERT INTO darts.security_group_courthouse_ae (grp_id, cth_id) VALUES (securityGroupId,cHouseId);
 
-    SELECT nextval('grp_seq') into securityGroupId;
+    SELECT 353 into securityGroupId;
     INSERT INTO security_group(
     grp_id, rol_id, group_name, is_private, description, group_global_unique_id, global_access, display_state, use_interpreter, display_name, dm_group_s_object_id, created_ts, created_by, last_modified_ts, last_modified_by)
     VALUES (securityGroupId, 2, 'moj_ch_wolverhampton_telford_staff', null, null, null, FALSE, TRUE, FALSE, 'Wolverhampton sitting at Telford Requester',null, '2024-01-01', 0, '2024-01-01', 0);
@@ -29,19 +27,19 @@ BEGIN
 
 
     --WORCESTER SITTING AT REDDITCH JC
-    SELECT nextval('cth_seq') into cHouseId;
+    SELECT 153 into cHouseId;
     INSERT INTO courthouse (cth_id,courthouse_code,courthouse_name,display_name,created_ts,created_by,last_modified_ts,last_modified_by) VALUES
 	 (cHouseId,NULL,'WORCESTER SITTING AT REDDITCH JC','Worcester Sitting At Redditch JC','2024-01-01',0,'2024-01-01',0);
 
     INSERT INTO courthouse_region_ae (cth_id, reg_id) VALUES (cHouseId,4);
 
-    SELECT nextval('grp_seq') into securityGroupId;
+    SELECT 354 into securityGroupId;
     INSERT INTO security_group(
     grp_id, rol_id, group_name, is_private, description, group_global_unique_id, global_access, display_state, use_interpreter, display_name, dm_group_s_object_id, created_ts, created_by, last_modified_ts, last_modified_by)
     VALUES (securityGroupId, 3, 'moj_ch_worcester_redditch_appr', null, null, null, FALSE, TRUE, FALSE, 'Worcester Sitting At Redditch JC Approver',null, '2024-01-01', 0, '2024-01-01', 0);
     INSERT INTO darts.security_group_courthouse_ae (grp_id, cth_id) VALUES (securityGroupId,cHouseId);
 
-    SELECT nextval('grp_seq') into securityGroupId;
+    SELECT 355 into securityGroupId;
     INSERT INTO security_group(
     grp_id, rol_id, group_name, is_private, description, group_global_unique_id, global_access, display_state, use_interpreter, display_name, dm_group_s_object_id, created_ts, created_by, last_modified_ts, last_modified_by)
     VALUES (securityGroupId, 2, 'moj_ch_worcester_redditch_staff', null, null, null, FALSE, TRUE, FALSE, 'Worcester Sitting At Redditch JC Requester',null, '2024-01-01', 0, '2024-01-01', 0);
@@ -52,19 +50,19 @@ BEGIN
 
 
     --CHESTERFIELD JUSTICE CENTRE
-    SELECT nextval('cth_seq') into cHouseId;
+    SELECT 154 into cHouseId;
     INSERT INTO courthouse (cth_id,courthouse_code,courthouse_name,display_name,created_ts,created_by,last_modified_ts,last_modified_by) VALUES
 	 (cHouseId,NULL,'CHESTERFIELD JUSTICE CENTRE','Chesterfield Justice Centre','2024-01-01',0,'2024-01-01',0);
 
     INSERT INTO courthouse_region_ae (cth_id, reg_id) VALUES (cHouseId,4);
 
-    SELECT nextval('grp_seq') into securityGroupId;
+    SELECT 356 into securityGroupId;
     INSERT INTO security_group(
     grp_id, rol_id, group_name, is_private, description, group_global_unique_id, global_access, display_state, use_interpreter, display_name, dm_group_s_object_id, created_ts, created_by, last_modified_ts, last_modified_by)
     VALUES (securityGroupId, 3, 'moj_ch_chesterfield_appr', null, null, null, FALSE, TRUE, FALSE, 'Chesterfield Justice Centre Approver',null, '2024-01-01', 0, '2024-01-01', 0);
     INSERT INTO darts.security_group_courthouse_ae (grp_id, cth_id) VALUES (securityGroupId,cHouseId);
 
-    SELECT nextval('grp_seq') into securityGroupId;
+    SELECT 357 into securityGroupId;
     INSERT INTO security_group(
     grp_id, rol_id, group_name, is_private, description, group_global_unique_id, global_access, display_state, use_interpreter, display_name, dm_group_s_object_id, created_ts, created_by, last_modified_ts, last_modified_by)
     VALUES (securityGroupId, 2, 'moj_ch_chesterfield_staff', null, null, null, FALSE, TRUE, FALSE, 'Chesterfield Justice Centre Requester',null, '2024-01-01', 0, '2024-01-01', 0);
@@ -75,19 +73,19 @@ BEGIN
 
 
     --MANSFIELD JUSTICE CENTRE
-    SELECT nextval('cth_seq') into cHouseId;
+    SELECT 155 into cHouseId;
     INSERT INTO courthouse (cth_id,courthouse_code,courthouse_name,display_name,created_ts,created_by,last_modified_ts,last_modified_by) VALUES
 	 (cHouseId,NULL,'MANSFIELD JUSTICE CENTRE','Mansfield Justice Centre','2024-01-01',0,'2024-01-01',0);
 
     INSERT INTO courthouse_region_ae (cth_id, reg_id) VALUES (cHouseId,4);
 
-    SELECT nextval('grp_seq') into securityGroupId;
+    SELECT 358 into securityGroupId;
     INSERT INTO security_group(
     grp_id, rol_id, group_name, is_private, description, group_global_unique_id, global_access, display_state, use_interpreter, display_name, dm_group_s_object_id, created_ts, created_by, last_modified_ts, last_modified_by)
     VALUES (securityGroupId, 3, 'moj_ch_mansfield_appr', null, null, null, FALSE, TRUE, FALSE, 'Mansfield Justice Centre Approver',null, '2024-01-01', 0, '2024-01-01', 0);
     INSERT INTO darts.security_group_courthouse_ae (grp_id, cth_id) VALUES (securityGroupId,cHouseId);
 
-    SELECT nextval('grp_seq') into securityGroupId;
+    SELECT 359 into securityGroupId;
     INSERT INTO security_group(
     grp_id, rol_id, group_name, is_private, description, group_global_unique_id, global_access, display_state, use_interpreter, display_name, dm_group_s_object_id, created_ts, created_by, last_modified_ts, last_modified_by)
     VALUES (securityGroupId, 2, 'moj_ch_mansfield_staff', null, null, null, FALSE, TRUE, FALSE, 'Mansfield Justice Centre Requester',null, '2024-01-01', 0, '2024-01-01', 0);
---
+++ src/main/resources/db/migration/postgres/V1_337_3__add_4_new_courthouses.sql
@@ -0,0 +1,40 @@
+DO $$
+DECLARE
+	recordToBeMigrated record;
+	newId integer;
+	oldId integer;
+	--Prod data will have an id below this value, a deliberate gap will be left between prod data and test data.
+	sequenceRestartValue integer = 500;
+BEGIN
+perform setval('cth_seq', sequenceRestartValue, false);
+RAISE NOTICE 'migrating duplicate records';
+FOR recordToBeMigrated IN
+    SELECT cth_id from courthouse where cth_id > sequenceRestartValue
+    order by 1
+	LOOP
+	select nextval('cth_seq') into newId;
+	select recordToBeMigrated.cth_id into oldId;
+	RAISE NOTICE 'migrating % to %', oldId, newId;
+
+	--duplicate record
+	INSERT INTO courthouse(
+	cth_id, courthouse_code, courthouse_name, created_ts, last_modified_ts, created_by, last_modified_by, display_name)
+	select newId, courthouse_code, courthouse_name, created_ts, last_modified_ts, created_by, last_modified_by, display_name from courthouse where cth_id = oldId;
+
+	--move foreign references
+	--"court_case"
+	update court_case set cth_id = newId where cth_id = oldId;
+	--"courthouse_region_ae"
+	delete from courthouse_region_ae; --repopulating in next script
+	--"courtroom"
+	update courtroom set cth_id = newId where cth_id = oldId;
+	--"security_group_courthouse_ae"
+	update security_group_courthouse_ae set cth_id = newId where cth_id = oldId;
+
+	--delete old record
+	delete from courthouse where cth_id = oldId;
+
+    END LOOP;
+END $$;
+
+CREATE UNIQUE INDEX courthouse_name_unique_idx on courthouse (UPPER(courthouse_name));
