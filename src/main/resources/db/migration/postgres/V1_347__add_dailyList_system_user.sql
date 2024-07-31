7d326fd763a537c6daa1302c8a4b2358a21ca7bf
Chris Magowan <chris.magowan@hmcts.net>
2024-07-31 13:51:39 +0100

    fix DailyList processing

--- src/main/resources/db/migration/postgres/V1_347__add_dailyList_system_user.sql
+++ src/main/resources/db/migration/postgres/V1_347__add_dailyList_system_user.sql
@@ -1,1 +1,2 @@
-INSERT INTO darts.user_account VALUES (-2, NULL, 'system - DailyList Processor', NULL, 'DailyList Processor', NULL, NULL, NULL, NULL, NULL, 'f6f71122-ff85-4ebe-93d9-1706460dbea5', true, true, 'system');
+delete from user_account where account_guid = 'f6f71122-ff85-4ebe-93d9-1706460dbea5';
+INSERT INTO user_account VALUES (-2, NULL, 'system - DailyList Processor', NULL, 'DailyList Processor', NULL, NULL, NULL, NULL, NULL, 'f6f71122-ff85-4ebe-93d9-1706460dbea5', true, true, 'system');
