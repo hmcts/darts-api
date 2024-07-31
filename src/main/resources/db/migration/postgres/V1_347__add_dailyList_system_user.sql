0d3cd762d74265a39725352a785389ed57ffd31a
Chris Magowan <chris.magowan@hmcts.net>
2024-07-31 13:57:19 +0100

    fix DailyList processing

--- src/main/resources/db/migration/postgres/V1_347__add_dailyList_system_user.sql
+++ src/main/resources/db/migration/postgres/V1_347__add_dailyList_system_user.sql
@@ -1,2 +1,2 @@
 delete from user_account where account_guid = 'f6f71122-ff85-4ebe-93d9-1706460dbea5';
-INSERT INTO user_account VALUES (-2, NULL, 'system - DailyList Processor', NULL, 'DailyList Processor', NULL, NULL, NULL, NULL, NULL, 'f6f71122-ff85-4ebe-93d9-1706460dbea5', true, true, 'system');
+INSERT INTO user_account VALUES (-2, NULL, 'system - DailyList Processor', NULL, 'DailyList Processor', '2024-01-01', '2024-01-01', NULL, 0, 0, 'f6f71122-ff85-4ebe-93d9-1706460dbea5', true, true, 'system');
