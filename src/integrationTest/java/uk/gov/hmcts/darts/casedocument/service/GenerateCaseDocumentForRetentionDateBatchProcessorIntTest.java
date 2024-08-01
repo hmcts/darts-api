2761abab889eb165b474dcb07b13c9d6761a9835
karen-hedges <133129444+karen-hedges@users.noreply.github.com>
2024-08-01 14:34:56 +0100

    DMP-3158 Generate Case Document - Before a Case is getting Expired dquote> dquote> Added extra tests

---
+++ src/integrationTest/java/uk/gov/hmcts/darts/casedocument/service/GenerateCaseDocumentForRetentionDateBatchProcessorIntTest.java
@@ -0,0 +1,176 @@
+package uk.gov.hmcts.darts.casedocument.service;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.transaction.annotation.Transactional;
+import uk.gov.hmcts.darts.authorisation.component.UserIdentity;
+import uk.gov.hmcts.darts.common.entity.CaseDocumentEntity;
+import uk.gov.hmcts.darts.common.entity.CaseRetentionEntity;
+import uk.gov.hmcts.darts.common.entity.CourtCaseEntity;
+import uk.gov.hmcts.darts.common.entity.UserAccountEntity;
+import uk.gov.hmcts.darts.common.repository.CaseRepository;
+import uk.gov.hmcts.darts.common.util.DateConverterUtil;
+import uk.gov.hmcts.darts.retention.enums.CaseRetentionStatus;
+import uk.gov.hmcts.darts.testutils.IntegrationBase;
+
+import java.time.OffsetDateTime;
+import java.time.ZoneOffset;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.when;
+
+@Transactional
+class GenerateCaseDocumentForRetentionDateBatchProcessorIntTest extends IntegrationBase {
+    protected static final String SOME_COURTHOUSE = "some-courthouse";
+    protected static final String SOME_ROOM = "some-room";
+    protected static final String SOME_CASE_NUMBER_1 = "CASE1";
+    protected static final String SOME_CASE_NUMBER_2 = "CASE2";
+
+    @MockBean
+    private UserIdentity mockUserIdentity;
+
+    private final OffsetDateTime testTime = OffsetDateTime.of(2020, 10, 10, 10, 0, 0, 0, ZoneOffset.UTC);
+
+    @Autowired
+    private CaseRepository caseRepository;
+
+    @Autowired
+    private GenerateCaseDocumentForRetentionDateProcessor generateCaseDocumentForRetentionDateBatchProcessor;
+
+    @BeforeEach
+    void setupData() {
+        UserAccountEntity testUser = dartsDatabase.getUserAccountStub().getIntegrationTestUserAccountEntity();
+        when(mockUserIdentity.getUserAccount()).thenReturn(testUser);
+    }
+
+    @Test
+    void testProcessGenerateCaseDocumentForRetentionDateSuccess() {
+        // given
+        CourtCaseEntity courtCaseEntityWithNoCaseDocuments = dartsDatabase.createCase(SOME_COURTHOUSE, SOME_CASE_NUMBER_1);
+
+        dartsDatabase.getHearingStub().createHearing(SOME_COURTHOUSE, SOME_ROOM, SOME_CASE_NUMBER_1,
+                                                     DateConverterUtil.toLocalDateTime(testTime));
+
+        CaseRetentionEntity caseRetentionObject1 = dartsDatabase.createCaseRetentionObject(
+            courtCaseEntityWithNoCaseDocuments, CaseRetentionStatus.COMPLETE, OffsetDateTime.now().plusDays(30), false);
+        dartsDatabase.save(caseRetentionObject1);
+
+        CaseRetentionEntity caseRetentionObject2 = dartsDatabase.createCaseRetentionObject(
+            courtCaseEntityWithNoCaseDocuments, CaseRetentionStatus.COMPLETE, OffsetDateTime.now().plusDays(20), false);
+        dartsDatabase.save(caseRetentionObject2);
+
+        CourtCaseEntity courtCaseEntityWithCaseDocument = dartsDatabase.createCase(SOME_COURTHOUSE, SOME_CASE_NUMBER_2);
+
+        dartsDatabase.getHearingStub().createHearing(SOME_COURTHOUSE, SOME_ROOM, SOME_CASE_NUMBER_2,
+                                                     DateConverterUtil.toLocalDateTime(testTime));
+
+        CaseRetentionEntity caseRetentionObject3 = dartsDatabase.createCaseRetentionObject(
+            courtCaseEntityWithCaseDocument, CaseRetentionStatus.COMPLETE, OffsetDateTime.now().plusDays(10), false);
+        dartsDatabase.save(caseRetentionObject3);
+
+        UserAccountEntity uploadedBy = dartsDatabase.getUserAccountStub().getIntegrationTestUserAccountEntity();
+        CaseDocumentEntity caseDocument = dartsDatabase.getCaseDocumentStub().createAndSaveCaseDocumentEntity(
+            courtCaseEntityWithCaseDocument, uploadedBy);
+        caseDocument.setCreatedDateTime(OffsetDateTime.now().minusDays(30));
+        dartsDatabase.save(caseDocument);
+
+        // when
+        generateCaseDocumentForRetentionDateBatchProcessor.processGenerateCaseDocumentForRetentionDate(2);
+
+        // then
+        CourtCaseEntity courtCaseEntity1 = dartsDatabase.getCaseRepository().getReferenceById(courtCaseEntityWithNoCaseDocuments.getId());
+        assertTrue(courtCaseEntity1.isRetentionUpdated());
+
+        CourtCaseEntity courtCaseEntity2 = dartsDatabase.getCaseRepository().getReferenceById(courtCaseEntityWithCaseDocument.getId());
+        assertTrue(courtCaseEntity2.isRetentionUpdated());
+
+    }
+
+    @Test
+    void testProcessGenerateCaseDocumentForRetentionDateRetentionDateTooFarInTheFuture() {
+        // given
+        CourtCaseEntity courtCaseEntityWithNoCaseDocuments = dartsDatabase.createCase(SOME_COURTHOUSE, SOME_CASE_NUMBER_1);
+
+        dartsDatabase.getHearingStub().createHearing(SOME_COURTHOUSE, SOME_ROOM, SOME_CASE_NUMBER_1,
+                                                     DateConverterUtil.toLocalDateTime(testTime));
+
+        CaseRetentionEntity caseRetentionObject1 = dartsDatabase.createCaseRetentionObject(
+            courtCaseEntityWithNoCaseDocuments, CaseRetentionStatus.COMPLETE, OffsetDateTime.now().plusDays(30), false);
+        dartsDatabase.save(caseRetentionObject1);
+
+        CourtCaseEntity courtCaseEntityWithCaseDocument = dartsDatabase.createCase(SOME_COURTHOUSE, SOME_CASE_NUMBER_2);
+
+        dartsDatabase.getHearingStub().createHearing(SOME_COURTHOUSE, SOME_ROOM, SOME_CASE_NUMBER_2,
+                                                     DateConverterUtil.toLocalDateTime(testTime));
+
+        CaseRetentionEntity caseRetentionObject3 = dartsDatabase.createCaseRetentionObject(
+            courtCaseEntityWithCaseDocument, CaseRetentionStatus.COMPLETE, OffsetDateTime.now().plusDays(40), false);
+        dartsDatabase.save(caseRetentionObject3);
+
+        UserAccountEntity uploadedBy = dartsDatabase.getUserAccountStub().getIntegrationTestUserAccountEntity();
+        CaseDocumentEntity caseDocument = dartsDatabase.getCaseDocumentStub().createAndSaveCaseDocumentEntity(courtCaseEntityWithCaseDocument, uploadedBy);
+        caseDocument.setCreatedDateTime(OffsetDateTime.now().minusDays(30));
+        dartsDatabase.save(caseDocument);
+
+        // when
+        generateCaseDocumentForRetentionDateBatchProcessor.processGenerateCaseDocumentForRetentionDate(2);
+
+        // then
+        CourtCaseEntity courtCaseEntity1 = dartsDatabase.getCaseRepository().getReferenceById(courtCaseEntityWithNoCaseDocuments.getId());
+        assertFalse(courtCaseEntity1.isRetentionUpdated());
+
+        CourtCaseEntity courtCaseEntity2 = dartsDatabase.getCaseRepository().getReferenceById(courtCaseEntityWithCaseDocument.getId());
+        assertFalse(courtCaseEntity2.isRetentionUpdated());
+
+    }
+
+    @Test
+    void testProcessGenerateCaseDocumentForRetentionDateWithRecentDocumentsNotProcessed() {
+        // given
+        CourtCaseEntity courtCaseEntityWithCaseDocuments1 = dartsDatabase.createCase(SOME_COURTHOUSE, SOME_CASE_NUMBER_1);
+
+        dartsDatabase.getHearingStub().createHearing(SOME_COURTHOUSE, SOME_ROOM, SOME_CASE_NUMBER_1,
+                                                     DateConverterUtil.toLocalDateTime(testTime));
+
+        CaseRetentionEntity caseRetentionObject1 = dartsDatabase.createCaseRetentionObject(
+            courtCaseEntityWithCaseDocuments1, CaseRetentionStatus.COMPLETE, OffsetDateTime.now().plusDays(30), false);
+        dartsDatabase.save(caseRetentionObject1);
+
+        CaseRetentionEntity caseRetentionObject2 = dartsDatabase.createCaseRetentionObject(
+            courtCaseEntityWithCaseDocuments1, CaseRetentionStatus.COMPLETE, OffsetDateTime.now().plusDays(20), false);
+        dartsDatabase.save(caseRetentionObject2);
+
+        UserAccountEntity uploadedBy = dartsDatabase.getUserAccountStub().getIntegrationTestUserAccountEntity();
+        CaseDocumentEntity caseDocument1 = dartsDatabase.getCaseDocumentStub().createAndSaveCaseDocumentEntity(
+            courtCaseEntityWithCaseDocuments1, uploadedBy);
+        caseDocument1.setCreatedDateTime(OffsetDateTime.now().minusDays(27));
+        dartsDatabase.save(caseDocument1);
+
+        CourtCaseEntity courtCaseEntityWithCaseDocument2 = dartsDatabase.createCase(SOME_COURTHOUSE, SOME_CASE_NUMBER_2);
+
+        dartsDatabase.getHearingStub().createHearing(SOME_COURTHOUSE, SOME_ROOM, SOME_CASE_NUMBER_2,
+                                                     DateConverterUtil.toLocalDateTime(testTime));
+
+        CaseRetentionEntity caseRetentionObject3 = dartsDatabase.createCaseRetentionObject(
+            courtCaseEntityWithCaseDocument2, CaseRetentionStatus.COMPLETE, OffsetDateTime.now().plusDays(10), false);
+        dartsDatabase.save(caseRetentionObject3);
+
+        CaseDocumentEntity caseDocument = dartsDatabase.getCaseDocumentStub().createAndSaveCaseDocumentEntity(
+            courtCaseEntityWithCaseDocument2, uploadedBy);
+        dartsDatabase.save(caseDocument);
+
+        // when
+        generateCaseDocumentForRetentionDateBatchProcessor.processGenerateCaseDocumentForRetentionDate(2);
+
+        // then
+        CourtCaseEntity courtCaseEntity1 = dartsDatabase.getCaseRepository().getReferenceById(courtCaseEntityWithCaseDocuments1.getId());
+        assertFalse(courtCaseEntity1.isRetentionUpdated());
+
+        CourtCaseEntity courtCaseEntity2 = dartsDatabase.getCaseRepository().getReferenceById(courtCaseEntityWithCaseDocument2.getId());
+        assertFalse(courtCaseEntity2.isRetentionUpdated());
+
+    }
+}
