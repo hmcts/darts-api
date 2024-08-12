1e321cfbd20afc55050e40bc48411c5b4ab9e35d
scott-robertson1 <127202807+scott-robertson1@users.noreply.github.com>
2024-08-12 09:23:59 +0100

    Update main.tf

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -82,7 +82,7 @@ module "postgresql_flexible" {
   business_area        = "sds"
   location             = var.location
   pgsql_storage_mb     = "524288"
-  pgsql_sku            = "GP_Standard_D4s_v3"
+  pgsql_sku            = var.pgsqlSku
   auto_grow_enabled    = true
   common_tags          = var.common_tags
   admin_user_object_id = var.jenkins_AAD_objectId
