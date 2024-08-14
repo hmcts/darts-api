898ac0be4716241e52bdafa6ec3790767d1a18a2
karen-hedges <133129444+karen-hedges@users.noreply.github.com>
2024-08-14 18:27:09 +0100

    Added script to create arm response files in single mode to test DMP-3292

--- bin/generateSingleArmResponses.sh
+++ bin/generateSingleArmResponses.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+echo "This script generates the success path ARM pull response files. These can be used for testing where there is a need for an object such as media, transcription document, annotation document or case document to be stored in the ARM storage"
 
 uuid1=$(uuidgen)
 uuid2=$(uuidgen)
@@ -13,7 +14,7 @@ then
   exit 1
 fi
 
-read -p 'enter object ID: ' objectid
+read -p 'enter object ID (media, transcription document, annotation document or case document): ' objectid
 if [ -z "$objectid" ]
 then
   exit 1
@@ -47,7 +48,6 @@ ufFilename="UUID1_UUID3_1_uf.rsp"
 ufFilename=${ufFilename//UUID1/$uuid1}
 ufFilename=${ufFilename//UUID3/$uuid3}
 echo "UF filename: $ufFilename"
-
 
 #${parameter//pattern/string}
 uiFileContentsParam="{\"operation\": \"upload_file\", \"transaction_id\": \"2d1c7f6f-224e-768e-a274-41af570e6502\", \"relation_id\": \"EODID\", \"a360_record_id\": \"1cf976c7-cedd-703f-ab70-01588bd56d50\", \"process_time\": \"2023-07-11T11:39:26.790000\", \"status\": 1, \"input\": \"{\\\"operation\\\": \\\"create_record\\\",\\\"relation_id\\\": \\\"EODID\",\\\"record_metadata\\\": {\\\"record_class\\\": \\\"A360TEST\",\\\"publisher\\\": \\\"A360\\\",\\\"recordDate\\\": \\\"2016-11-22T11:39:30Z\\\",\\\"region\\\": \\\"GBR\",\\\"title\\\": \\\"A360230711_TestIngestion_2\\\"}}\", \"exception_description\": null, \"error_status\": null}"
