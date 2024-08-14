633d9a4410661cf16de63ad6907a47cfd2f60df2
karen-hedges <133129444+karen-hedges@users.noreply.github.com>
2024-08-14 10:22:16 +0100

    DMP-3765 create unique index on user_account.user_email_address
    Added unique index on user_email_address where is_active is true

--- src/main/resources/db/migration/postgres/V1_357__add_unique_email_index_tables.sql
+++ src/main/resources/db/migration/postgres/V1_357__add_unique_email_index_tables.sql
@@ -1,8 +1,9 @@
-update darts.user_account ua1 set is_active = false where ua1.usr_id not in
-(select ua2.usr_id
-from darts.user_account ua2
-where ua2.user_email_address = ua1.user_email_address
-and ua2.is_active = true
-order by last_modified_ts desc limit 1);
+update darts.user_account ua1 set is_active = false where exists (
+    select 1
+    from darts.user_account ua2
+    where ua2.user_email_address = ua1.user_email_address
+    and ua2.is_active = true
+    and ua1.is_active = true
+    and ua1.created_ts < ua2.created_ts);
 
 CREATE UNIQUE INDEX user_account_user_email_address_unq ON darts.user_account (user_email_address) where is_active;
