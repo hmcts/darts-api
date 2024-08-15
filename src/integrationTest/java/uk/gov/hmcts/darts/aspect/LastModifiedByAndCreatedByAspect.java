85ce59eff62ca4e530c12a7df25bbe59fd085d1f
mfesteba@yahoo.co.uk <63051024+mestebanez@users.noreply.github.com>
2024-08-15 14:14:37 +0100

    Checkin aspect

---
+++ src/integrationTest/java/uk/gov/hmcts/darts/aspect/LastModifiedByAndCreatedByAspect.java
@@ -0,0 +1,111 @@
+package uk.gov.hmcts.darts.aspect;
+
+import lombok.RequiredArgsConstructor;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+import uk.gov.hmcts.darts.common.entity.UserAccountEntity;
+import uk.gov.hmcts.darts.common.entity.base.CreatedBaseEntity;
+import uk.gov.hmcts.darts.common.entity.base.CreatedModifiedBaseEntity;
+import uk.gov.hmcts.darts.common.entity.base.MandatoryCreatedBaseEntity;
+import uk.gov.hmcts.darts.common.entity.base.MandatoryCreatedModifiedBaseEntity;
+import uk.gov.hmcts.darts.common.entity.base.ModifiedBaseEntity;
+import uk.gov.hmcts.darts.common.repository.UserAccountRepository;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+import javax.annotation.PostConstruct;
+
+@Aspect
+@Component
+@Configuration
+@RequiredArgsConstructor
+public class LastModifiedByAndCreatedByAspect {
+
+    @Autowired
+    private UserAccountRepository userAccountRepository;
+
+    private static final int SYSTEM_USER_ID = 0;
+
+    @PostConstruct
+    public void test () {
+        System.out.println("");
+    }
+    @Around("execution(* org.springframework.data.jpa.repository.JpaRepository+.save(..))")
+    public Object handleUserCreatedByAndLastModifiedByForSave(ProceedingJoinPoint joinPoint)
+        throws Throwable {
+
+        Object[] args = joinPoint.getArgs();
+        Object body = args[0];
+
+        processEntities(body);
+
+
+        return joinPoint.proceed();
+    }
+
+    @Around("execution(* org.springframework.data.jpa.repository.JpaRepository+.saveAndFlush(..))")
+    public Object handleUserCreatedByAndLastModifiedByForSaveAndFlush(ProceedingJoinPoint joinPoint) throws Throwable {
+        Object[] objects = joinPoint.getArgs();
+
+        if (objects[0] instanceof Iterable == false) {
+            processEntities(objects[0]);
+        } else {
+            for (Object object : (Iterable)objects[0]) {
+                processEntities(object);
+            }
+        }
+
+        return joinPoint.proceed();
+    }
+
+
+    private void processEntities(Object body){
+        if (body instanceof ModifiedBaseEntity) {
+            ModifiedBaseEntity entity = (ModifiedBaseEntity) body;
+
+            Optional<UserAccountEntity> userAccount = userAccountRepository.findById(SYSTEM_USER_ID);
+
+            if (entity.getLastModifiedBy() == null) {
+                entity.setLastModifiedBy(userAccount.get());
+            }
+        }
+
+        if (body instanceof CreatedModifiedBaseEntity) {
+            CreatedModifiedBaseEntity entity = (CreatedModifiedBaseEntity) body;
+            Optional<UserAccountEntity> userAccount = userAccountRepository.findById(SYSTEM_USER_ID);
+
+            if (entity.getLastModifiedBy() == null) {
+                entity.setLastModifiedBy(userAccount.get());
+            }
+
+            if (entity.getCreatedBy() == null) {
+                entity.setCreatedBy(userAccount.get());
+            }
+        }
+
+        if (body instanceof MandatoryCreatedBaseEntity) {
+            MandatoryCreatedBaseEntity entity = (MandatoryCreatedBaseEntity) body;
+            Optional<UserAccountEntity> userAccount = userAccountRepository.findById(SYSTEM_USER_ID);
+
+            if (entity.getCreatedBy() == null) {
+                entity.setCreatedBy(userAccount.get());
+            }
+        }
+
+        if (body instanceof MandatoryCreatedModifiedBaseEntity) {
+            MandatoryCreatedModifiedBaseEntity entity = (MandatoryCreatedModifiedBaseEntity) body;
+            Optional<UserAccountEntity> userAccount = userAccountRepository.findById(SYSTEM_USER_ID);
+
+            if (entity.getCreatedBy() == null) {
+                entity.setCreatedBy(userAccount.get());
+            }
+        }
+    }
+
+}
--- src/main/resources/db/migration/common/V1_359__constraint_transcription_user_part6.sql
+++ src/main/resources/db/migration/common/V1_359__constraint_transcription_user_part6.sql
@@ -15,19 +15,14 @@ ALTER TABLE transformed_media ALTER COLUMN end_ts SET NOT NULL;
 
 ALTER TABLE transient_object_directory ALTER COLUMN created_by SET NOT NULL;
 
-
 alter table user_account alter column created_by drop not null;
 alter table user_account alter column last_modified_by drop not null;
 
 UPDATE user_account SET created_by = NULL WHERE usr_id=0;
 UPDATE user_account SET last_modified_by = NULL WHERE usr_id=0;
 
-UPDATE user_account SET created_by = 0 WHERE usr_id<>0 AND (created_by IS NULL OR NOT EXISTS (SELECT 1 FROM user_account ua WHERE ua.usr_id = user_account.created_by));
-
+update user_account theTable set created_by = 0 where usr_id<>0 AND (created_by is null or not exists (select 1 from user_account ua where ua.usr_id = theTable.created_by));
 ALTER TABLE user_account ADD CONSTRAINT user_account_created_by_fk FOREIGN KEY (created_by) REFERENCES user_account(usr_id);
 
 UPDATE user_account SET last_modified_by = 0 WHERE usr_id<>0 AND  (last_modified_by IS NULL OR NOT EXISTS (SELECT 1 FROM user_account ua WHERE ua.usr_id = user_account.last_modified_by));
 ALTER TABLE user_account ADD CONSTRAINT user_account_modified_by_fk FOREIGN KEY (last_modified_by) REFERENCES user_account(usr_id);
-
-ALTER TABLE user_account
-ADD CONSTRAINT checksystemuser CHECK (usr_id != 0 AND created_by!=null AND last_modified_by!=null OR usr_id=0 AND created_by=null AND last_modified_by=null);
