dd34b2cb0a017d8fe8229fbb05b9e589a71a1ead
Chris Magowan <chris.magowan@hmcts.net>
2024-08-16 08:55:55 +0100

    flyway fix

--- src/main/resources/db/migration/postgres/V1_360__add_unique_email_index_tables.sql
+++ src/main/resources/db/migration/postgres/V1_360__add_unique_email_index_tables.sql
@@ -4,6 +4,6 @@ update darts.user_account ua1 set is_active = false where exists (
     where ua2.user_email_address = ua1.user_email_address
     and ua2.is_active = true
     and ua1.is_active = true
-    and ua1.created_ts < ua2.created_ts);
+    and (ua1.created_ts < ua2.created_ts or (ua1.created_ts = ua2.created_ts and ua1.usr_id < ua2.usr_id)));
 
 CREATE UNIQUE INDEX user_account_user_email_address_unq ON darts.user_account (user_email_address) where is_active;
