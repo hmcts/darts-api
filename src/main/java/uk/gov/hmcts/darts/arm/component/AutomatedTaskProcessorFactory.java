dab3909003518bd25a84e5868ee1fc5fd2cb58b8
Chris Magowan <131666106+cmagowan-hmcts@users.noreply.github.com>
2024-08-28 10:12:00 +0000

    DMP-3755 - improve performance and allow batching (#1919)

--- src/main/java/uk/gov/hmcts/darts/arm/component/AutomatedTaskProcessorFactory.java
+++ src/main/java/uk/gov/hmcts/darts/arm/component/AutomatedTaskProcessorFactory.java
@@ -1,7 +1,6 @@
 package uk.gov.hmcts.darts.arm.component;
 
 import uk.gov.hmcts.darts.arm.service.ArmResponseFilesProcessor;
-import uk.gov.hmcts.darts.arm.service.UnstructuredToArmProcessor;
 import uk.gov.hmcts.darts.casedocument.service.GenerateCaseDocumentForRetentionDateProcessor;
 import uk.gov.hmcts.darts.casedocument.service.GenerateCaseDocumentProcessor;
 import uk.gov.hmcts.darts.event.service.CleanupCurrentFlagEventProcessor;
@@ -9,8 +8,6 @@ import uk.gov.hmcts.darts.event.service.CleanupCurrentFlagEventProcessor;
 public interface AutomatedTaskProcessorFactory {
 
     ArmResponseFilesProcessor createArmResponseFilesProcessor(int batchSize);
-
-    UnstructuredToArmProcessor createUnstructuredToArmProcessor(int batchSize);
 
     GenerateCaseDocumentProcessor createGenerateCaseDocumentProcessor(int batchSize);
 
