6f5749e7ce053cfc2cb367ed043f189b6467fca0
Sabah u Din Irfan <sabah.irfan@gmail.com>
2024-08-30 10:37:20 +0100

    Fix checkstyle issues

--- src/main/java/uk/gov/hmcts/darts/common/service/impl/HearingCommonServiceImpl.java
+++ src/main/java/uk/gov/hmcts/darts/common/service/impl/HearingCommonServiceImpl.java
@@ -2,7 +2,6 @@ package uk.gov.hmcts.darts.common.service.impl;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 import uk.gov.hmcts.darts.common.entity.CourtCaseEntity;
 import uk.gov.hmcts.darts.common.entity.CourtroomEntity;
