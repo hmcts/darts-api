e83632ef9ed7eafd2176fd4c2918a843c97d6105
mfesteba@yahoo.co.uk <63051024+mestebanez@users.noreply.github.com>
2024-09-06 19:53:24 +0100

    Finish test code

--- src/main/java/uk/gov/hmcts/darts/common/datamanagement/component/impl/FileBasedDownloadResponseMetaData.java
+++ src/main/java/uk/gov/hmcts/darts/common/datamanagement/component/impl/FileBasedDownloadResponseMetaData.java
@@ -1,7 +1,6 @@
 package uk.gov.hmcts.darts.common.datamanagement.component.impl;
 
 import lombok.Getter;
-import lombok.Setter;
 import org.apache.commons.io.FileUtils;
 import org.springframework.core.io.FileUrlResource;
 import org.springframework.core.io.Resource;
