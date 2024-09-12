0076b39676faf75557f436dd7e67de3314630206
Ben Edwards <147524406+Ben-Edwards-cgi@users.noreply.github.com>
2024-09-12 11:14:08 +0100

    DMP-3932 - (Dev Only) -> Refactor Manual Task Service to use spring dependency injection (#1984)

--- src/main/java/uk/gov/hmcts/darts/task/runner/impl/ApplyRetentionAutomatedTask.java
+++ src/main/java/uk/gov/hmcts/darts/task/runner/impl/ApplyRetentionAutomatedTask.java
@@ -1,16 +1,20 @@
 package uk.gov.hmcts.darts.task.runner.impl;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
 import uk.gov.hmcts.darts.common.repository.AutomatedTaskRepository;
 import uk.gov.hmcts.darts.log.api.LogApi;
 import uk.gov.hmcts.darts.retention.service.ApplyRetentionProcessor;
 import uk.gov.hmcts.darts.task.config.AutomatedTaskConfigurationProperties;
+import uk.gov.hmcts.darts.task.runner.AutoloadingManualTask;
 import uk.gov.hmcts.darts.task.service.LockService;
 
 import static uk.gov.hmcts.darts.task.api.AutomatedTaskName.APPLY_RETENTION_TASK_NAME;
 
 @Slf4j
-public class ApplyRetentionAutomatedTask extends AbstractLockableAutomatedTask {
+@Component
+public class ApplyRetentionAutomatedTask extends AbstractLockableAutomatedTask
+    implements AutoloadingManualTask {
 
     protected String taskName = APPLY_RETENTION_TASK_NAME.getTaskName();
     ApplyRetentionProcessor applyRetentionProcessor;
