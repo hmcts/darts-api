21a0271ffdf3f825a49eade6eaed7821cda828d9
benedwards <ben.edwards@cgi.com>
2024-09-18 12:58:31 +0100

    Added 2 new Archive Retention Policies

---
+++ src/main/resources/db/migration/common/V1_379__added_additional_retention_policy_type.sql
@@ -0,0 +1,3 @@
+INSERT INTO darts.retention_policy_type (rpt_id,policy_name,display_name,fixed_policy_key,duration,policy_start_ts,policy_end_ts,retention_policy_object_id,created_ts,created_by,last_modified_ts,last_modified_by,description) VALUES
+(nextval('rpt_seq'),'DARTS Archive Permanent Retention','Archive Permanent',-3,'30Y0M0D',current_timestamp,null,null,current_timestamp,0,current_timestamp,0,'DARTS Archive Permanent Retention'),
+(nextval('rpt_seq'),'DARTS Archive Standard Retention','Archive Standard',-4,'7Y0M0D',current_timestamp,null,null,current_timestamp,0,current_timestamp,0,'DARTS Archive Standard Retention');
