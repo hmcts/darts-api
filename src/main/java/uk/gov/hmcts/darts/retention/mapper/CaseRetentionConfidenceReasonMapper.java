6f8485aebd2fe664fce417437a45263766c3c032
karen-hedges <133129444+karen-hedges@users.noreply.github.com>
2024-09-18 09:13:14 +0100

    Dmp 3536 calculate confidence for associated object (#1997)

---
+++ src/main/java/uk/gov/hmcts/darts/retention/mapper/CaseRetentionConfidenceReasonMapper.java
@@ -0,0 +1,68 @@
+package uk.gov.hmcts.darts.retention.mapper;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import uk.gov.hmcts.darts.arm.config.ArmDataManagementConfiguration;
+import uk.gov.hmcts.darts.common.entity.CourtCaseEntity;
+import uk.gov.hmcts.darts.retention.model.CaseRetentionConfidenceReason;
+
+import java.time.OffsetDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+
+import static java.util.Objects.nonNull;
+
+@Component
+@RequiredArgsConstructor
+public class CaseRetentionConfidenceReasonMapper {
+
+    private final ArmDataManagementConfiguration armDataManagementConfiguration;
+
+    private DateTimeFormatter dateTimeFormatter;
+
+    public CaseRetentionConfidenceReason mapToCaseRetentionConfidenceReason(OffsetDateTime offsetDateTime, List<CourtCaseEntity> courtCases) {
+        return CaseRetentionConfidenceReason.builder()
+            .retentionConfidenceAppliedTimestamp(formatDateTime(offsetDateTime))
+            .retentionCases(buildRetentionCases(courtCases))
+            .build();
+    }
+
+    private List<CaseRetentionConfidenceReason.RetentionCase> buildRetentionCases(List<CourtCaseEntity> courtCases) {
+        List<CaseRetentionConfidenceReason.RetentionCase> retentionCases = new ArrayList<>();
+        for (CourtCaseEntity courtCase : courtCases) {
+            retentionCases.add(buildRetentionCase(courtCase));
+        }
+        if (retentionCases.isEmpty()) {
+            retentionCases = null;
+        }
+        return retentionCases;
+    }
+
+    private CaseRetentionConfidenceReason.RetentionCase buildRetentionCase(CourtCaseEntity courtCase) {
+        var retentionCase = CaseRetentionConfidenceReason.RetentionCase.builder()
+            .courthouse(courtCase.getCourthouse().getCourthouseName())
+            .caseNumber(courtCase.getCaseNumber())
+            .retentionConfidenceUpdatedTimestamp(formatDateTime(courtCase.getRetConfUpdatedTs()))
+            .retentionConfidenceReason(getCaseRetentionConfidenceReason(courtCase))
+            .build();
+        return retentionCase;
+    }
+
+    private static String getCaseRetentionConfidenceReason(CourtCaseEntity courtCase) {
+        String caseRetentionConfidenceReason = null;
+        if (nonNull(courtCase.getRetConfReason())) {
+            caseRetentionConfidenceReason = courtCase.getRetConfReason().name();
+        }
+        return caseRetentionConfidenceReason;
+    }
+
+    private String formatDateTime(OffsetDateTime offsetDateTime) {
+        dateTimeFormatter = DateTimeFormatter.ofPattern(armDataManagementConfiguration.getDateTimeFormat());
+        String dateTime = null;
+        if (nonNull(offsetDateTime)) {
+            dateTime = offsetDateTime.format(dateTimeFormatter);
+        }
+        return dateTime;
+    }
+}
