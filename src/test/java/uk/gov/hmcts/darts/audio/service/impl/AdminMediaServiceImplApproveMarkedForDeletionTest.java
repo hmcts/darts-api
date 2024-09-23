1fc93b7ccfb52e9449927635a9ec7165a47239fe
karen-hedges <133129444+karen-hedges@users.noreply.github.com>
2024-09-23 16:37:32 +0100

    DMP-2893 Implement POST /admin/medias/<media_id>/approve-deletion
    Added more tests

---
+++ src/test/java/uk/gov/hmcts/darts/audio/service/impl/AdminMediaServiceImplApproveMarkedForDeletionTest.java
@@ -0,0 +1,120 @@
+package uk.gov.hmcts.darts.audio.service.impl;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import uk.gov.hmcts.darts.audio.exception.AudioApiError;
+import uk.gov.hmcts.darts.audio.model.MediaApproveMarkedForDeletionResponse;
+import uk.gov.hmcts.darts.audio.validation.MediaApproveMarkForDeletionValidator;
+import uk.gov.hmcts.darts.authorisation.component.UserIdentity;
+import uk.gov.hmcts.darts.common.entity.MediaEntity;
+import uk.gov.hmcts.darts.common.entity.UserAccountEntity;
+import uk.gov.hmcts.darts.common.exception.DartsApiException;
+import uk.gov.hmcts.darts.common.helper.CurrentTimeHelper;
+import uk.gov.hmcts.darts.common.repository.MediaRepository;
+import uk.gov.hmcts.darts.common.repository.ObjectAdminActionRepository;
+import uk.gov.hmcts.darts.common.repository.ObjectHiddenReasonRepository;
+import uk.gov.hmcts.darts.test.common.data.ObjectAdminActionTestData;
+import uk.gov.hmcts.darts.test.common.data.ObjectHiddenReasonTestData;
+import uk.gov.hmcts.darts.test.common.data.UserAccountTestData;
+
+import java.time.OffsetDateTime;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.lenient;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class AdminMediaServiceImplApproveMarkedForDeletionTest {
+    @Mock
+    private MediaRepository mediaRepository;
+
+    @Mock
+    private ObjectAdminActionRepository objectAdminActionRepository;
+
+    @Mock
+    private ObjectHiddenReasonRepository objectHiddenReasonRepository;
+
+    @Mock
+    private MediaApproveMarkForDeletionValidator mediaApproveMarkForDeletionValidator;
+
+    @Mock
+    private UserIdentity userIdentity;
+
+    @Mock
+    private CurrentTimeHelper currentTimeHelper;
+
+    @InjectMocks
+    private AdminMediaServiceImpl adminMediaService;
+    @Mock
+    private UserAccountEntity userAccount;
+
+    @BeforeEach
+    void setUp() {
+        lenient().when(currentTimeHelper.currentOffsetDateTime()).thenReturn(OffsetDateTime.now());
+    }
+
+    @Test
+    void testAdminApproveMediaMarkedForDeletionSuccess() {
+        // given
+        Integer mediaId = 1;
+        MediaEntity mediaEntity = new MediaEntity();
+        mediaEntity.setId(mediaId);
+
+        var hiddenByUserAccount = UserAccountTestData.minimalUserAccount();
+        hiddenByUserAccount.setId(123);
+        var objectAdminAction = ObjectAdminActionTestData.minimalObjectAdminAction();
+        objectAdminAction.setMedia(mediaEntity);
+        objectAdminAction.setMarkedForManualDeletion(false);
+        objectAdminAction.setHiddenBy(hiddenByUserAccount);
+
+        var hiddenReason = ObjectHiddenReasonTestData.otherDelete();
+        objectAdminAction.setObjectHiddenReason(hiddenReason);
+
+        var authorisedByUserAccount = UserAccountTestData.minimalUserAccount();
+        authorisedByUserAccount.setId(345);
+
+        when(mediaRepository.findById(mediaId)).thenReturn(Optional.of(mediaEntity));
+        when(userIdentity.getUserAccount()).thenReturn(userAccount);
+
+        when(objectAdminActionRepository.findByMedia_Id(mediaId)).thenReturn(List.of(objectAdminAction));
+        //when(objectHiddenReasonRepository.findById(hiddenReason.getId())).thenReturn(Optional.of(hiddenReason));
+        when(userIdentity.getUserAccount()).thenReturn(authorisedByUserAccount);
+
+        MediaApproveMarkedForDeletionResponse response = adminMediaService.adminApproveMediaMarkedForDeletion(mediaId);
+
+        assertNotNull(response);
+        verify(mediaApproveMarkForDeletionValidator, times(1)).validate(mediaId);
+        verify(objectAdminActionRepository, times(1)).save(objectAdminAction);
+    }
+
+    @Test
+    void testAdminApproveMediaMarkedForDeletionMediaNotFound() {
+        // given
+        Integer mediaId = 1;
+
+        when(mediaRepository.findById(mediaId)).thenReturn(Optional.empty());
+
+        // when
+        DartsApiException exception = assertThrows(DartsApiException.class, () -> {
+            adminMediaService.adminApproveMediaMarkedForDeletion(mediaId);
+        });
+
+        //then
+        assertEquals(AudioApiError.MEDIA_NOT_FOUND, exception.getError());
+        verify(mediaApproveMarkForDeletionValidator, times(1)).validate(mediaId);
+        verify(objectAdminActionRepository, never()).save(any());
+    }
+
+}
