bc64ae8a9af77e60d4a42e38af7818de2cd4fd26
Chris Magowan <131666106+cmagowan-hmcts@users.noreply.github.com>
2024-09-24 11:53:53 +0000

    DMP-3966 - allow ATS to go into a loop if multiple requests to process. (#2039)

--- src/integrationTest/java/uk/gov/hmcts/darts/audio/repository/MediaRequestRepositoryIntTest.java
+++ src/integrationTest/java/uk/gov/hmcts/darts/audio/repository/MediaRequestRepositoryIntTest.java
@@ -7,6 +7,8 @@ import uk.gov.hmcts.darts.common.repository.MediaRequestRepository;
 import uk.gov.hmcts.darts.testutils.PostgresIntegrationBase;
 import uk.gov.hmcts.darts.testutils.stubs.MediaRequestStub;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static uk.gov.hmcts.darts.audio.enums.MediaRequestStatus.COMPLETED;
 import static uk.gov.hmcts.darts.audio.enums.MediaRequestStatus.OPEN;
@@ -27,7 +29,8 @@ class MediaRequestRepositoryIntTest extends PostgresIntegrationBase {
         mediaRequestStub.createAndSaveMediaRequestEntity(OPEN);
         mediaRequestStub.createAndSaveMediaRequestEntity(OPEN);
 
-        var updatedMediaRequest = mediaRequestRepository.updateAndRetrieveMediaRequestToProcessing(mediaRequestEntity.getLastModifiedBy().getId());
+        var updatedMediaRequest = mediaRequestRepository.updateAndRetrieveMediaRequestToProcessing(mediaRequestEntity.getLastModifiedBy().getId(),
+                                                                                                   List.of(0));
 
         assertThat(updatedMediaRequest.getId()).isEqualTo(mediaRequest1.getId());
         assertThat(updatedMediaRequest.getStatus()).isEqualTo(PROCESSING);
@@ -40,7 +43,8 @@ class MediaRequestRepositoryIntTest extends PostgresIntegrationBase {
         mediaRequestStub.createAndSaveMediaRequestEntity(COMPLETED);
         mediaRequestStub.createAndSaveMediaRequestEntity(COMPLETED);
 
-        var updatedMediaRequest = mediaRequestRepository.updateAndRetrieveMediaRequestToProcessing(mediaRequestEntity.getLastModifiedBy().getId());
+        var updatedMediaRequest = mediaRequestRepository.updateAndRetrieveMediaRequestToProcessing(mediaRequestEntity.getLastModifiedBy().getId(),
+                                                                                                   List.of(0));
 
         assertThat(updatedMediaRequest).isNull();
     }
