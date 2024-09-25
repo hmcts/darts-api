8c143a6c8d6b5b85a5750597284a0880d8fd6d86
benedwards <ben.edwards@cgi.com>
2024-09-25 14:02:03 +0100

    Removed created_at from test

--- src/integrationTest/java/uk/gov/hmcts/darts/event/controller/EventSearchControllerTest.java
+++ src/integrationTest/java/uk/gov/hmcts/darts/event/controller/EventSearchControllerTest.java
@@ -102,7 +102,6 @@ class EventSearchControllerTest extends IntegrationBase {
 
         var response = json.from(mvcResult.getResponse().getContentAsString());
         assertThat(response).hasJsonPathNumberValue("[0].id");
-        assertThat(response).hasJsonPathStringValue("[0].created_at");
         assertThat(response).hasJsonPathStringValue("[0].event_ts");
         assertThat(response).hasJsonPathNumberValue("[0].courthouse.id");
         assertThat(response).hasJsonPathStringValue("[0].courthouse.display_name");
