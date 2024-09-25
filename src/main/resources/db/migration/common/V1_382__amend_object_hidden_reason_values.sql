837fd75aed764540da8034641f1e90c691a3de83
Sabah u Din Irfan <sabah.irfan@gmail.com>
2024-09-25 15:32:17 +0100

    Amend vlues in object hidden table

--- src/main/resources/db/migration/common/V1_382__amend_object_hidden_reason_values.sql
+++ src/main/resources/db/migration/common/V1_382__amend_object_hidden_reason_values.sql
@@ -3,16 +3,3 @@
 UPDATE darts.object_hidden_reason
 SET display_state = true
 WHERE ohr_id IN (1, 2, 3);
-
--- Optionally insert new values if they don't already exist
-INSERT INTO darts.object_hidden_reason (ohr_id, ohr_reason, display_name, display_state, display_order, marked_for_deletion)
-SELECT 1, 'PUBLIC_INTEREST_IMMUNITY', 'Public interest immunity', true, 1, true
-WHERE NOT EXISTS (SELECT 1 FROM darts.object_hidden_reason WHERE ohr_reason = 'PUBLIC_INTEREST_IMMUNITY');
-
-INSERT INTO darts.object_hidden_reason (ohr_id, ohr_reason, display_name, display_state, display_order, marked_for_deletion)
-SELECT 2, 'CLASSIFIED', 'Classified above official', true, 2, true
-WHERE NOT EXISTS (SELECT 1 FROM darts.object_hidden_reason WHERE ohr_reason = 'CLASSIFIED');
-
-INSERT INTO darts.object_hidden_reason (ohr_id, ohr_reason, display_name, display_state, display_order, marked_for_deletion)
-SELECT 3, 'OTHER_DELETE', 'Other reason to delete', true, 3, true
-WHERE NOT EXISTS (SELECT 1 FROM darts.object_hidden_reason WHERE ohr_reason = 'OTHER_DELETE');
