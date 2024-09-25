ec43f84735dca9b5d64d2ef7f3d55bac170dbbdd
Sabah u Din Irfan <sabah.irfan@gmail.com>
2024-09-25 14:44:13 +0100

    Amend vlues in object hidden table

--- src/main/resources/db/migration/common/V1_382__amend_object_hidden_reason_values.sql
+++ src/main/resources/db/migration/common/V1_382__amend_object_hidden_reason_values.sql
@@ -16,7 +16,3 @@ WHERE NOT EXISTS (SELECT 1 FROM darts.object_hidden_reason WHERE ohr_reason = 'C
 INSERT INTO darts.object_hidden_reason (ohr_id, ohr_reason, display_name, display_state, display_order, marked_for_deletion)
 SELECT 3, 'OTHER_DELETE', 'Other reason to delete', true, 3, true
 WHERE NOT EXISTS (SELECT 1 FROM darts.object_hidden_reason WHERE ohr_reason = 'OTHER_DELETE');
-
-INSERT INTO darts.object_hidden_reason (ohr_id, ohr_reason, display_name, display_state, display_order, marked_for_deletion)
-SELECT 4, 'OTHER_HIDE', 'Other reason to hide only', true, 4, false
-WHERE NOT EXISTS (SELECT 1 FROM darts.object_hidden_reason WHERE ohr_reason = 'OTHER_HIDE');
