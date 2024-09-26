c96096a522bcdbacbff528f72ec8b5f8c0d3991d
benedwards <ben.edwards@cgi.com>
2024-09-26 14:51:42 +0100

    Removed redundant new lines

--- src/main/resources/db/migration/common/V1_384__additional_indexes_for_performance.sql
+++ src/main/resources/db/migration/common/V1_384__additional_indexes_for_performance.sql
@@ -1,5 +1,3 @@
 CREATE INDEX event_event_ts_idx ON darts."event" (event_ts);
 CREATE INDEX media_start_ts_idx ON darts.media (start_ts,end_ts);
 CREATE INDEX media_media_file_idx ON darts.media (media_file);
-
-
