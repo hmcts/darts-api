a8d36497eb6fc12050b357592f65eee0259e69de
mfesteba@yahoo.co.uk <63051024+mestebanez@users.noreply.github.com>
2024-09-26 15:42:51 +0100

    Fix PR flyway

--- src/main/resources/db/migration/common/V1_384__detsToArmTask.sql
+++ src/main/resources/db/migration/common/V1_384__detsToArmTask.sql
@@ -1,4 +1,4 @@
 INSERT INTO darts.automated_task (aut_id,task_name,task_description,cron_expression,cron_editable, batch_size,
 created_ts, created_by,last_modified_ts,last_modified_by)
-VALUES (nextval('aut_seq'),'DETSToArmDataStore','Move files from DETS to ARM data store','0 9 * * * *',true,100000,
+VALUES (nextval('aut_seq'),'DetsToArm','Move files from DETS to ARM data store','0 9 * * * *',true,100000,
 current_timestamp, 0 , current_timestamp,0);
