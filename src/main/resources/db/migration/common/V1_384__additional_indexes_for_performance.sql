6eccb0e5fcf611935c358b35a05738bc10cacf34
benedwards <ben.edwards@cgi.com>
2024-09-26 15:06:26 +0100

    Updated file name to prevent flyway from picking up the file

--- src/main/resources/db/migration/common/V1_384__additional_indexes_for_performance.sql
+++
@@ -1,3 +0,0 @@
-CREATE INDEX event_event_ts_idx ON darts."event" (event_ts);
-CREATE INDEX media_start_ts_idx ON darts.media (start_ts,end_ts);
-CREATE INDEX media_media_file_idx ON darts.media (media_file);
