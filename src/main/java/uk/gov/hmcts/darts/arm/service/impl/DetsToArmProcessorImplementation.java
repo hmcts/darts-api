7dced9a4b057cce36d3f9becb976e7b11c2325ad
karen-hedges <133129444+karen-hedges@users.noreply.github.com>
2024-10-01 11:12:13 +0100

    DMP-2630 Implement the DETS To ARM Push flow
    Committing changes made so far

--- src/main/java/uk/gov/hmcts/darts/arm/service/impl/DetsToArmProcessorImplementation.java
+++ src/main/java/uk/gov/hmcts/darts/arm/service/impl/DetsToArmProcessorImplementation.java
@@ -15,16 +15,27 @@ import uk.gov.hmcts.darts.arm.service.ArchiveRecordService;
 import uk.gov.hmcts.darts.authorisation.component.UserIdentity;
 import uk.gov.hmcts.darts.common.entity.ExternalLocationTypeEntity;
 import uk.gov.hmcts.darts.common.entity.ExternalObjectDirectoryEntity;
+import uk.gov.hmcts.darts.common.entity.ObjectRecordStatusEntity;
+import uk.gov.hmcts.darts.common.entity.ObjectStateRecordEntity;
 import uk.gov.hmcts.darts.common.entity.UserAccountEntity;
+import uk.gov.hmcts.darts.common.helper.CurrentTimeHelper;
 import uk.gov.hmcts.darts.common.repository.ExternalObjectDirectoryRepository;
+import uk.gov.hmcts.darts.common.repository.ObjectStateRecordRepository;
 import uk.gov.hmcts.darts.common.service.FileOperationService;
 import uk.gov.hmcts.darts.common.util.EodHelper;
 import uk.gov.hmcts.darts.log.api.LogApi;
 
 import java.io.File;
+import java.time.Duration;
+import java.time.Instant;
 import java.time.OffsetDateTime;
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
+import static java.util.Objects.nonNull;
+import static uk.gov.hmcts.darts.common.enums.ObjectRecordStatusEnum.ARM_INGESTION;
+import static uk.gov.hmcts.darts.common.enums.ObjectRecordStatusEnum.ARM_RAW_DATA_FAILED;
 import static uk.gov.hmcts.darts.common.util.EodHelper.isEqual;
 
 @Slf4j
@@ -42,6 +53,9 @@ public class DetsToArmProcessorImplementation {
     private final FileOperationService fileOperationService;
     private final ArmDataManagementApi armDataManagementApi;
     private final DetsToArmProcessorConfiguration detsToArmProcessorConfiguration;
+    private final ObjectStateRecordRepository objectStateRecordRepository;
+    private final CurrentTimeHelper currentTimeHelper;
+
 
     public void processDetsToArm(int taskBatchSize) {
         log.info("Started running DETS ARM Batch Push processing at: {}", OffsetDateTime.now());
@@ -73,7 +87,7 @@ public class DetsToArmProcessorImplementation {
         var batchItems = new ArmBatchItems();
 
         for (var currentEod : eodsForBatch) {
-
+            ObjectStateRecordEntity objectStateRecord = null;
             var batchItem = new ArmBatchItem();
             try {
                 ExternalObjectDirectoryEntity armEod;
@@ -83,16 +97,23 @@ public class DetsToArmProcessorImplementation {
                     batchItem.setArmEod(armEod);
                     dataStoreToArmHelper.updateArmEodToArmIngestionStatus(
                         currentEod, batchItem, batchItems, archiveRecordsFile, userAccount);
+                    objectStateRecord = getObjectStateRecordEntity(currentEod, armEod);
+
                 } else {
-                    
+
                     armEod = dataStoreToArmHelper.createArmEodWithArmIngestionStatus(
                         currentEod, batchItem, batchItems, archiveRecordsFile, userAccount);
+                    objectStateRecord = getObjectStateRecordEntity(currentEod, armEod);
+                    if (objectStateRecord != null) {
+                        objectStateRecord.setArmEodId(String.valueOf(armEod.getId()));
+                        objectStateRecordRepository.save(objectStateRecord);
+                    }
                 }
 
                 String rawFilename = dataStoreToArmHelper.generateRawFilename(armEod);
 
                 if (dataStoreToArmHelper.shouldPushRawDataToArm(batchItem)) {
-                    dataStoreToArmHelper.pushRawDataAndCreateArchiveRecordIfSuccess(batchItem, rawFilename, userAccount);
+                    pushRawDataAndCreateArchiveRecordIfSuccess(batchItem, rawFilename, userAccount, objectStateRecord);
                 } else if (dataStoreToArmHelper.shouldAddEntryToManifestFile(batchItem)) {
                     batchItem.setArchiveRecord(archiveRecordService.generateArchiveRecordInfo(batchItem.getArmEod().getId(), rawFilename));
                 }
@@ -117,5 +138,119 @@ public class DetsToArmProcessorImplementation {
             dataStoreToArmHelper.updateExternalObjectDirectoryStatus(batchItem.getArmEod(), EodHelper.armDropZoneStatus(), userAccount);
             logApi.armPushSuccessful(batchItem.getArmEod().getId());
         }
+    }
+
+    private ObjectStateRecordEntity getObjectStateRecordEntity(ExternalObjectDirectoryEntity currentEod,
+                                                               ExternalObjectDirectoryEntity armEod) {
+        if (nonNull(armEod.getOsrUuid())) {
+            return objectStateRecordRepository.findById(armEod.getOsrUuid())
+                .orElseGet(() -> findObjectStateRecordByEodId(currentEod, armEod));
+        } else {
+            log.warn("Unable to find ObjectStateRecordEntity for ARM EOD ID: {} for OSR UUID {}", armEod.getId(), armEod.getOsrUuid());
+            return findObjectStateRecordByEodId(currentEod, armEod);
+        }
+    }
+
+    private ObjectStateRecordEntity findObjectStateRecordByEodId(ExternalObjectDirectoryEntity currentEod,
+                                                                 ExternalObjectDirectoryEntity armEod) {
+        Optional<ObjectStateRecordEntity> objectStateRecordEntities = objectStateRecordRepository.findByEodId(String.valueOf(currentEod.getId()));
+        if (objectStateRecordEntities.isPresent()) {
+            return objectStateRecordEntities.get();
+        } else {
+            log.error("Unable to find ObjectStateRecordEntity for DETS EOD ID: {} ", currentEod.getId());
+            objectStateRecordEntities = objectStateRecordRepository.findByArmEodId(String.valueOf(armEod.getId()));
+            if (objectStateRecordEntities.isPresent()) {
+                return objectStateRecordEntities.get();
+            } else {
+                log.error("Unable to find ObjectStateRecordEntity for ARM EOD ID: {} ", armEod.getId());
+            }
+        }
+        return null;
+    }
+
+    private void pushRawDataAndCreateArchiveRecordIfSuccess(ArmBatchItem batchItem, String rawFilename, UserAccountEntity userAccount,
+                                                            ObjectStateRecordEntity objectStateRecord) {
+        log.info("Start of batch ARM Push processing for EOD {} running at: {}", batchItem.getArmEod().getId(), OffsetDateTime.now());
+        boolean copyRawDataToArmSuccessful = copyRawDataToArm(
+            batchItem.getSourceEod(),
+            batchItem.getArmEod(),
+            rawFilename,
+            batchItem.getPreviousStatus(),
+            userAccount,
+            objectStateRecord
+        );
+
+        if (copyRawDataToArmSuccessful) {
+            batchItem.setRawFilePushSuccessful(true);
+            var archiveRecord = archiveRecordService.generateArchiveRecordInfo(batchItem.getArmEod().getId(), rawFilename);
+            batchItem.setArchiveRecord(archiveRecord);
+        } else {
+            batchItem.setRawFilePushSuccessful(false);
+            batchItem.undoManifestFileChange();
+            dataStoreToArmHelper.updateExternalObjectDirectoryStatusToFailed(batchItem.getArmEod(), EodHelper.failedArmRawDataStatus(), userAccount);
+        }
+    }
+
+    private boolean copyRawDataToArm(ExternalObjectDirectoryEntity detsExternalObjectDirectory,
+                                     ExternalObjectDirectoryEntity armExternalObjectDirectory,
+                                     String filename,
+                                     ObjectRecordStatusEntity previousStatus, UserAccountEntity userAccount,
+                                     ObjectStateRecordEntity objectStateRecord) {
+        try {
+            if (previousStatus == null
+                || ARM_RAW_DATA_FAILED.getId().equals(previousStatus.getId())
+                || ARM_INGESTION.getId().equals(previousStatus.getId())) {
+                Instant start = Instant.now();
+                log.info("ARM PERFORMANCE PUSH START for EOD {} started at {}", armExternalObjectDirectory.getId(), start);
+
+                log.info("About to push raw data to ARM for EOD {}", armExternalObjectDirectory.getId());
+                armDataManagementApi.copyBlobDataToArm(detsExternalObjectDirectory.getExternalLocation().toString(), filename);
+                log.info("Pushed raw data to ARM for EOD {}", armExternalObjectDirectory.getId());
+
+                Instant finish = Instant.now();
+                long timeElapsed = Duration.between(start, finish).toMillis();
+                log.info("ARM PERFORMANCE PUSH END for EOD {} ended at {}", armExternalObjectDirectory.getId(), finish);
+                log.info("ARM PERFORMANCE PUSH ELAPSED TIME for EOD {} took {} ms", armExternalObjectDirectory.getId(), timeElapsed);
+
+                armExternalObjectDirectory.setChecksum(detsExternalObjectDirectory.getChecksum());
+                armExternalObjectDirectory.setExternalLocation(UUID.randomUUID());
+                armExternalObjectDirectory.setLastModifiedBy(userAccount);
+                externalObjectDirectoryRepository.saveAndFlush(armExternalObjectDirectory);
+
+                if (nonNull(objectStateRecord)) {
+                    objectStateRecord.setFlagFileTransferToDets(true);
+                    objectStateRecord.setDateFileTransferToDets(currentTimeHelper.currentOffsetDateTime());
+                    objectStateRecord.setMd5FileTransfArml(detsExternalObjectDirectory.getChecksum());
+                    setFileSize(detsExternalObjectDirectory, objectStateRecord);
+                    objectStateRecordRepository.save(objectStateRecord);
+                }
+            }
+        } catch (Exception e) {
+            log.error(
+                "Error copying BLOB data for file {}",
+                detsExternalObjectDirectory.getExternalLocation(),
+                e
+            );
+            return false;
+        }
+
+        return true;
+    }
+
+    private static void setFileSize(ExternalObjectDirectoryEntity detsExternalObjectDirectory, ObjectStateRecordEntity objectStateRecord) {
+        if (nonNull(detsExternalObjectDirectory.getMedia())
+            && nonNull(detsExternalObjectDirectory.getMedia().getFileSize())) {
+            objectStateRecord.setFileSizeBytesArml(detsExternalObjectDirectory.getMedia().getFileSize());
+        } else if (nonNull(detsExternalObjectDirectory.getAnnotationDocumentEntity())
+            && nonNull(detsExternalObjectDirectory.getAnnotationDocumentEntity().getFileSize())) {
+            objectStateRecord.setFileSizeBytesArml(Long.valueOf(detsExternalObjectDirectory.getAnnotationDocumentEntity().getFileSize()));
+        } else if (nonNull(detsExternalObjectDirectory.getTranscriptionDocumentEntity())
+            && nonNull(detsExternalObjectDirectory.getTranscriptionDocumentEntity().getFileSize())) {
+            objectStateRecord.setFileSizeBytesArml(Long.valueOf(detsExternalObjectDirectory.getTranscriptionDocumentEntity().getFileSize()));
+        } else if (nonNull(detsExternalObjectDirectory.getCaseDocument())
+            && nonNull(detsExternalObjectDirectory.getCaseDocument().getFileSize())) {
+            objectStateRecord.setFileSizeBytesArml(Long.valueOf(detsExternalObjectDirectory.getCaseDocument().getFileSize()));
+        }
+
     }
 }
