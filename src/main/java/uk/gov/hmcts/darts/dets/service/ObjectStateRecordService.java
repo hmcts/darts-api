dfe026677e48bee749e931a8959f0151990ddd02
karen-hedges <133129444+karen-hedges@users.noreply.github.com>
2024-10-01 12:41:01 +0100

    DMP-2630 DETS to ARM
    Removed service for ObjectStateRecordRepository and tests

--- src/main/java/uk/gov/hmcts/darts/dets/service/ObjectStateRecordService.java
+++
@@ -1,9 +0,0 @@
-package uk.gov.hmcts.darts.dets.service;
-
-import uk.gov.hmcts.darts.common.entity.ObjectStateRecordEntity;
-
-public interface ObjectStateRecordService {
-
-    ObjectStateRecordEntity getObjectStateRecordEntityById(Long objectStateRecordId);
-    
-}
