852a7422fc54d4602ba32e687b7199dffa106a69
Mr Michael Estebanez <63051024+mestebanez@users.noreply.github.com>
2024-10-02 15:48:03 +0100

    DMP-2652: Add Dets to Arm task (#2055)
    Co-authored-by: Ben Edwards <147524406+Ben-Edwards-cgi@users.noreply.github.com>
    Co-authored-by: benedwards <ben.edwards@cgi.com>

---
+++ src/main/java/uk/gov/hmcts/darts/arm/service/DetsToArmProcessor.java
@@ -0,0 +1,6 @@
+package uk.gov.hmcts.darts.arm.service;
+
+
+public interface DetsToArmProcessor {
+    void processDetsToArm(int batchSize);
+}
--- src/main/java/uk/gov/hmcts/darts/task/api/AutomatedTaskName.java
+++ src/main/java/uk/gov/hmcts/darts/task/api/AutomatedTaskName.java
@@ -33,10 +33,11 @@ public enum AutomatedTaskName {
     UNSTRUCTURED_TRANSCRIPTION_ANNOTATION_DELETER_TASK_NAME("UnstructuredTranscriptionAnnotationDeleter"),
     REMOVE_DUPLICATED_EVENTS_TASK_NAME("RemoveDuplicatedEvents"),
     GENERATE_CASE_DOCUMENT_FOR_RETENTION_DATE_TASK_NAME("GenerateCaseDocumentForRetentionDate"),
+    DETS_TO_ARM_TASK_NAME("DetsToArm"),
     CASE_EXPIRY_DELETION_TASK_NAME("CaseExpiryDeletion", "${darts.automated.task.expiry-deletion.enabled:false}"),
     ASSOCIATED_OBJECT_DATA_EXPIRY_DELETION_TASK_NAME("AssociatedObjectDataExpiryDeletion"),
-    MANUAL_DELETION("ManualDeletion"),
-    ;
+    MANUAL_DELETION("ManualDeletion");
+
     private final String taskName;
     private final String conditionalOnSpEL;
 
