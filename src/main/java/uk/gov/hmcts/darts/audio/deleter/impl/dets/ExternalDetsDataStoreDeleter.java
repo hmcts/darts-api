a959e3d898ba46ee5cb53ea8080f025e7a07922f
mariopaniccia <mario.paniccia@hmcts.net>
2024-10-07 12:38:24 +0100

    DMP-2626 added tests for deleter

--- src/main/java/uk/gov/hmcts/darts/audio/deleter/impl/dets/ExternalDetsDataStoreDeleter.java
+++ src/main/java/uk/gov/hmcts/darts/audio/deleter/impl/dets/ExternalDetsDataStoreDeleter.java
@@ -3,7 +3,6 @@ package uk.gov.hmcts.darts.audio.deleter.impl.dets;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Service;
 import uk.gov.hmcts.darts.audio.deleter.impl.ExternalDataStoreDeleterImpl;
-import uk.gov.hmcts.darts.audio.deleter.impl.unstructured.UnstructuredExternalObjectDirectoryDeletedFinder;
 import uk.gov.hmcts.darts.common.entity.ExternalObjectDirectoryEntity;
 import uk.gov.hmcts.darts.common.repository.TransformedMediaRepository;
 
@@ -11,7 +10,7 @@ import uk.gov.hmcts.darts.common.repository.TransformedMediaRepository;
 public class ExternalDetsDataStoreDeleter extends ExternalDataStoreDeleterImpl<ExternalObjectDirectoryEntity> {
 
     public ExternalDetsDataStoreDeleter(JpaRepository<ExternalObjectDirectoryEntity, Integer> repository,
-                                        UnstructuredExternalObjectDirectoryDeletedFinder finder,
+                                        DetsExternalObjectDirectoryDeletedFinder finder,
                                         DetsDataStoreDeleter deleter,
                                         TransformedMediaRepository transformedMediaRepository) {
         super(repository, finder, deleter, transformedMediaRepository);
