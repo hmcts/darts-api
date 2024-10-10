7b7af9a88a73d052acf57e1f85c6017fb3ac8a44
benedwards <ben.edwards@cgi.com>
2024-10-10 14:21:54 +0100

    Removed EventIdValidator.java in favor for EventService getEventById(...)

--- src/main/java/uk/gov/hmcts/darts/event/service/impl/EventServiceImpl.java
+++ src/main/java/uk/gov/hmcts/darts/event/service/impl/EventServiceImpl.java
@@ -10,23 +10,17 @@ import uk.gov.hmcts.darts.common.repository.EventRepository;
 import uk.gov.hmcts.darts.event.mapper.EventMapper;
 import uk.gov.hmcts.darts.event.model.AdminGetEventForIdResponseResult;
 import uk.gov.hmcts.darts.event.service.EventService;
-import uk.gov.hmcts.darts.event.validation.EventIdValidator;
-
-import java.util.Optional;
 
 @Service
 @Slf4j
 @RequiredArgsConstructor
 public class EventServiceImpl implements EventService {
     private final EventMapper eventMapper;
-    private final EventIdValidator eventIdValidator;
     private final EventRepository eventRepository;
 
     @Override
     public AdminGetEventForIdResponseResult adminGetEventById(Integer eventId) {
-        eventIdValidator.validate(eventId);
-        Optional<EventEntity> eventEntityOptional = eventRepository.findById(eventId);
-        return eventMapper.mapToAdminGetEventsResponseForId(eventEntityOptional);
+        return eventMapper.mapToAdminGetEventsResponseForId(getEventById(eventId));
     }
 
     @Override
