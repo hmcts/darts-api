1d2669323d05e68c8fc4376951d7fd9ac747cec9
karen-hedges <133129444+karen-hedges@users.noreply.github.com>
2024-10-10 11:13:51 +0100

    DMP-2630 Implement the DETS To ARM Push flow
    Committing changes made so far

---
+++ src/main/resources/db/migration/common/V1_388__disable_dets_automated_tasks.sql
@@ -0,0 +1,2 @@
+update darts.automated_task set task_enabled = false where task_name = 'DetsToArm';
+update darts.automated_task set task_enabled = false where task_name = 'ProcessDETSToArmResponse';
