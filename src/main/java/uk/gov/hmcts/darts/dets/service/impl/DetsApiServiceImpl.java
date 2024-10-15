4989aedeeb150ab91b1da56605e87fcb84461fb3
karen-hedges <133129444+karen-hedges@users.noreply.github.com>
2024-10-15 10:51:14 +0100

    DMP-2630 Implement the DETS To ARM Push flow
    Committing changes made so far

--- src/main/java/uk/gov/hmcts/darts/dets/service/impl/DetsApiServiceImpl.java
+++ src/main/java/uk/gov/hmcts/darts/dets/service/impl/DetsApiServiceImpl.java
@@ -1,17 +1,21 @@
 package uk.gov.hmcts.darts.dets.service.impl;
 
+import com.azure.core.http.rest.Response;
 import com.azure.core.util.BinaryData;
 import com.azure.storage.blob.BlobClient;
 import com.azure.storage.blob.BlobContainerClient;
 import com.azure.storage.blob.BlobServiceClient;
+import com.azure.storage.blob.models.DeleteSnapshotsOptionType;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import uk.gov.hmcts.darts.arm.config.ArmDataManagementConfiguration;
 import uk.gov.hmcts.darts.common.datamanagement.component.DataManagementAzureClientFactory;
 import uk.gov.hmcts.darts.common.datamanagement.component.impl.DownloadResponseMetaData;
 import uk.gov.hmcts.darts.common.datamanagement.component.impl.FileBasedDownloadResponseMetaData;
 import uk.gov.hmcts.darts.common.datamanagement.enums.DatastoreContainerType;
+import uk.gov.hmcts.darts.common.exception.AzureDeleteBlobException;
 import uk.gov.hmcts.darts.common.exception.DartsException;
 import uk.gov.hmcts.darts.datamanagement.exception.FileNotDownloadedException;
 import uk.gov.hmcts.darts.dets.config.DetsDataManagementConfiguration;
@@ -20,8 +24,13 @@ import uk.gov.hmcts.darts.util.AzureCopyUtil;
 
 import java.io.IOException;
 import java.io.OutputStream;
+import java.time.Duration;
+import java.time.temporal.ChronoUnit;
 import java.util.Date;
 import java.util.UUID;
+
+import static org.springframework.http.HttpStatus.NOT_FOUND;
+import static org.springframework.http.HttpStatus.valueOf;
 
 @Service
 @RequiredArgsConstructor
@@ -74,6 +83,35 @@ public class DetsApiServiceImpl implements DetsApiService {
     }
 
     @Override
+    public boolean deleteBlobDataFromContainer(UUID blobId) throws AzureDeleteBlobException {
+        try {
+            BlobServiceClient serviceClient = blobServiceFactory.getBlobServiceClientWithSasEndpoint(configuration.getSasEndpoint());
+            BlobContainerClient containerClient = blobServiceFactory.getBlobContainerClient(configuration.getContainerName(), serviceClient);
+            BlobClient client = blobServiceFactory.getBlobClient(containerClient, blobId);
+            Response<Boolean> response = client.deleteIfExistsWithResponse(DeleteSnapshotsOptionType.INCLUDE, null,
+                                                                           Duration.of(
+                                                                               configuration.getDeleteTimeout(),
+                                                                               ChronoUnit.SECONDS
+                                                                           ), null
+            );
+
+            HttpStatus httpStatus = valueOf(response.getStatusCode());
+            if (httpStatus.is2xxSuccessful() || NOT_FOUND.equals(httpStatus)) {
+                return response.getValue();
+            } else {
+                String message = String.format("Failed to delete from storage container=%s, blobId=%s, httpStatus=%s",
+                                               configuration.getContainerName(), blobId, httpStatus);
+                throw new AzureDeleteBlobException(message);
+            }
+
+        } catch (RuntimeException e) {
+            throw new AzureDeleteBlobException(
+                "Could not delete from storage container=" + configuration.getContainerName() + ", blobId=" + blobId, e
+            );
+        }
+    }
+
+    @Override
     public void copyDetsBlobDataToArm(String detsUuid, String blobPathAndName) {
         try {
             String sourceContainerSasUrl = configuration.getSasEndpoint();
@@ -99,5 +137,5 @@ public class DetsApiServiceImpl implements DetsApiService {
             return containerSasUrl.replace(containerName, containerName + "/" + location);
         }
     }
-    
+
 }
