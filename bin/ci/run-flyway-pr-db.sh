b6a06cc2cef8c0b41cf7357be465640beb22a1e5
Dave Thompson <davethompson21@gmail.com>
2024-10-16 10:00:07 +0100

    Testing flyway

--- bin/ci/run-flyway-pr-db.sh
+++ bin/ci/run-flyway-pr-db.sh
@@ -8,9 +8,12 @@ PR_USER="hmcts"
 PR_PASSWORD="$(kubectl -n darts-modernisation get secret postgres -o json | jq .data.PASSWORD -r | base64 -d)"
 PR_DATABASE="pr-${PR_NUMBER}-darts"
 
-FLYWAY_URL="jdbc:postgresql://${PR_HOST}:5432/${PR_DATABASE}"
-FLYWAY_USER=$PR_USER
-FLYWAY_PASSWORD=$PR_PASSWORD
+echo "Using Database password: ***${PR_PASSWORD: -3}"
+echo "Using PR_NUMBER: $PR_NUMBER"
+
+export FLYWAY_URL="jdbc:postgresql://${PR_HOST}:5432/${PR_DATABASE}"
+export FLYWAY_USER=$PR_USER
+export FLYWAY_PASSWORD=$PR_PASSWORD
 
 ./gradlew --no-daemon --init-script init.gradle assemble
 ./gradlew --no-daemon --init-script init.gradle migratePostgresDatabase
