abf431f8ab5607dd669556c4e29fdb7b070e0a26
Dave Thompson <davethompson21@gmail.com>
2024-10-18 16:28:32 +0100

    Trying github API

--- bin/ci/pr-labels.sh
+++ bin/ci/pr-labels.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
 set -e
 
-echo "env=${env}"
-echo "Using token: ***${GITHUB_API_TOKEN: -3}"
 echo "Using PR_NUMBER: $CHANGE_ID"
 
-curl -L -i \
+LABELS_ARRAY=$(curl -L \
   -H "Accept: application/vnd.github+json" \
   -H "Authorization: Bearer $GITHUB_API_TOKEN" \
   -H "X-GitHub-Api-Version: 2022-11-28" \
-  https://api.github.com/repos/hmcts/darts-api/pulls/$CHANGE_ID
+  https://api.github.com/repos/hmcts/darts-api/pulls/$CHANGE_ID | jq .labels)
 
-curl -L \
-  -H "Accept: application/vnd.github+json" \
-  -H "Authorization: Bearer $GITHUB_API_TOKEN" \
-  -H "X-GitHub-Api-Version: 2022-11-28" \
-  https://api.github.com/repos/hmcts/darts-api/pulls/$CHANGE_ID | jq .labels
+# if "enable_darts_portal" label is set on the PR, then set the DARTS_PORTAL_REPLICAS to 1, otherwise set it to 0
+# DARTS_PORTAL_REPLICAS is used in charts/darts-api/values.dev.template.yaml to set the number of replicas for the DARTS Portal
+export DARTS_PORTAL_REPLICAS=$(echo $LABELS_ARRAY | jq | grep '"name": "enable_darts_portal"' | wc -l | jq)
+echo "Required DARTS Portal replicas: $DARTS_PORTAL_REPLICAS"
