7c891ba0d4d2bd7866d49575a07005c7f2ff8915
Dave Thompson <davethompson21@gmail.com>
2024-10-18 14:31:57 +0100

    Trying github API

--- bin/ci/pr-labels.sh
+++ bin/ci/pr-labels.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
 set -e
 
-gh pr view $CHANGE_ID --json labels
+echo "Using token: ***${GITHUB_API_TOKEN: -3}"
+echo "Using PR_NUMBER: $CHANGE_ID"
+
+curl -L -i \
+  -H "Accept: application/vnd.github+json" \
+  -H "Authorization: Bearer $GITHUB_API_TOKEN" \
+  -H "X-GitHub-Api-Version: 2022-11-28" \
+  https://api.github.com/repos/hmcts/darts-api/pulls/$CHANGE_ID
+
+curl -L \
+  -H "Accept: application/vnd.github+json" \
+  -H "Authorization: Bearer $GITHUB_API_TOKEN" \
+  -H "X-GitHub-Api-Version: 2022-11-28" \
+  https://api.github.com/repos/hmcts/darts-api/pulls/$CHANGE_ID | jq .labels
