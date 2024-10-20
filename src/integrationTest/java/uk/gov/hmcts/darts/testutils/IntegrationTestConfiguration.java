6e9a840bc35fbad67aae2967b85293bd8b15d4c2
mfesteba@yahoo.co.uk <63051024+mestebanez@users.noreply.github.com>
2024-10-20 01:25:21 +0100

    conditionally override stubs

--- src/integrationTest/java/uk/gov/hmcts/darts/testutils/IntegrationTestConfiguration.java
+++ src/integrationTest/java/uk/gov/hmcts/darts/testutils/IntegrationTestConfiguration.java
@@ -1,10 +1,10 @@
 package uk.gov.hmcts.darts.testutils;
 
-import org.springframework.beans.BeansException;
+import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import uk.gov.hmcts.darts.arm.service.ArmService;
 import uk.gov.hmcts.darts.common.datamanagement.component.DataManagementAzureClientFactory;
 import uk.gov.hmcts.darts.datamanagement.config.DataManagementConfiguration;
@@ -14,24 +14,28 @@ import uk.gov.hmcts.darts.testutils.stubs.ArmServiceStubImpl;
 import uk.gov.hmcts.darts.testutils.stubs.DataManagementServiceStubImpl;
 import uk.gov.hmcts.darts.util.AzureCopyUtil;
 
-@Configuration
-public class TestConfiguration implements ApplicationContextAware {
+@TestConfiguration
+public class IntegrationTestConfiguration implements ApplicationContextAware {
 
     private ApplicationContext applicationContext;
 
+    private static final String BLOB_STORAGE_PROFILE = "blobTest";
+
     @Bean
+    @Primary
     public ArmService armService() {
         return new ArmServiceStubImpl();
     }
 
     @Bean
+    @Primary
     public DataManagementService getDartsDataManagementService(DataManagementConfiguration configuration,
                                                                DataManagementAzureClientFactory factory,
                                                                AzureCopyUtil azureCopyUtil) {
-        String profiles[] = applicationContext.getEnvironment().getActiveProfiles();
+        String[] profiles = applicationContext.getEnvironment().getActiveProfiles();
 
         for (String profile: profiles) {
-            if (profile.equals("blobTest")) {
+            if (BLOB_STORAGE_PROFILE.equals(profile)) {
                 return new DataManagementServiceImpl(configuration, factory, azureCopyUtil);
             }
         }
@@ -40,7 +44,7 @@ public class TestConfiguration implements ApplicationContextAware {
     }
 
     @Override
-    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+    public void setApplicationContext(ApplicationContext applicationContext) {
         this.applicationContext = applicationContext;
     }
 }
