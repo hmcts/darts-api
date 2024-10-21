f946933d00f45729a679c23f0943c9bfe9556101
benedwards <ben.edwards@cgi.com>
2024-10-21 10:04:53 +0100

    Added new index for event table

--- src/main/resources/db/migration/common/disabled.V1_384__additional_indexes_for_performance.sql
+++ src/main/resources/db/migration/common/disabled.V1_384__additional_indexes_for_performance.sql
@@ -1,3 +1,4 @@
 CREATE INDEX IF NOT EXISTS event_event_ts_idx ON darts."event" (event_ts);
-CREATE INDEX IF NOT EXISTS media_start_ts_idx ON darts.media (start_ts,end_ts);
+CREATE INDEX IF NOT EXISTS media_start_ts_idx ON darts.media (start_ts, end_ts);
 CREATE INDEX IF NOT EXISTS media_media_file_idx ON darts.media (media_file);
+CREATE INDEX IF NOT EXISTS event_event_id_idx ON darts."event" (event_id, is_current);
