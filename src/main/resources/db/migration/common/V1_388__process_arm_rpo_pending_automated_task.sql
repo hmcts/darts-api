e6aa05166a748e389045b3c827f339360a616d80
benedwards <ben.edwards@cgi.com>
2024-10-22 09:52:41 +0100

    Updated migration with hard coded id

--- src/main/resources/db/migration/common/V1_388__process_arm_rpo_pending_automated_task.sql
+++ src/main/resources/db/migration/common/V1_388__process_arm_rpo_pending_automated_task.sql
@@ -1,5 +1,6 @@
 INSERT INTO darts.automated_task (aut_id, task_name, task_description, cron_expression, cron_editable, batch_size,
                                   created_ts, created_by, last_modified_ts, last_modified_by)
-VALUES (nextval('aut_seq'), 'ProcessARMRPOPending', 'Update the record to STORED which has a status of ARM_RPO_PENDING and data_ingestion_ts is more than 24 hours',
+VALUES (321, 'ProcessARMRPOPending', 'Update the record to STORED which has a status of ARM_RPO_PENDING and data_ingestion_ts is more than 24 hours',
         '0 16 23 * * *', --(16 mins past 11pm every day)
         false, 100000, current_timestamp, 0, current_timestamp, 0);
+ALTER SEQUENCE aut_seq RESTART WITH 322;
