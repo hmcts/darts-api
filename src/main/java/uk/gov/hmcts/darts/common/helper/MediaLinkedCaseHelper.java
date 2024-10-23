e70aacee3210411ae8375cd61147381c96cc075a
Chris Bellingham <131763968+chrisbellingham-hmcts@users.noreply.github.com>
2024-10-23 13:46:33 +0100

    DMP-4076: Debug logging for investigation of duplicates in media_link… (#2168)

--- src/main/java/uk/gov/hmcts/darts/common/helper/MediaLinkedCaseHelper.java
+++ src/main/java/uk/gov/hmcts/darts/common/helper/MediaLinkedCaseHelper.java
@@ -1,6 +1,7 @@
 package uk.gov.hmcts.darts.common.helper;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.darts.common.entity.CourtCaseEntity;
 import uk.gov.hmcts.darts.common.entity.MediaEntity;
@@ -11,6 +12,7 @@ import java.util.List;
 
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class MediaLinkedCaseHelper {
 
     private final MediaLinkedCaseRepository mediaLinkedCaseRepository;
@@ -18,11 +20,24 @@ public class MediaLinkedCaseHelper {
     public void addCase(MediaEntity mediaEntity, CourtCaseEntity courtCase) {
         List<MediaLinkedCaseEntity> mediaLinkedCaseEntities = mediaLinkedCaseRepository.findByMedia(mediaEntity);
         List<CourtCaseEntity> linkedCases = mediaLinkedCaseEntities.stream().map(MediaLinkedCaseEntity::getCourtCase).toList();
+        if (log.isDebugEnabled()) {
+            log.debug("Handling med_id {} and cas_id {}. Currently the media has the following cas_ids linked: {}",
+                      mediaEntity.getId(),
+                      courtCase.getId(),
+                      linkedCases.stream()
+                          .map(CourtCaseEntity::getId)
+                          .toList());
+        }
         if (!linkedCases.contains(courtCase)) {
             MediaLinkedCaseEntity mediaLinkedCaseEntity = new MediaLinkedCaseEntity();
             mediaLinkedCaseEntity.setMedia(mediaEntity);
             mediaLinkedCaseEntity.setCourtCase(courtCase);
             mediaLinkedCaseRepository.saveAndFlush(mediaLinkedCaseEntity);
+            log.debug("cas_id {} and med_id {} were not linked, Created new link via mlc_id {}",
+                      courtCase.getId(), mediaEntity.getId(), mediaLinkedCaseEntity.getId());
+        } else {
+            log.debug("med_id {} and cas_id {} already linked, nothing to do",
+                      mediaEntity.getId(), courtCase.getId());
         }
     }
 }
