fb43bff9817e70be66bbfcb68f26ee49aeb7d69f
karen-hedges <133129444+karen-hedges@users.noreply.github.com>
2024-10-24 14:13:59 +0100

    DMP-3805 (Dev Only) ARM RPO Endpoint Integration Layer prerequisite
    Added code to get ArmRpoExecutionDetail

--- src/main/java/uk/gov/hmcts/darts/armrpo/service/ArmRpoService.java
+++ src/main/java/uk/gov/hmcts/darts/armrpo/service/ArmRpoService.java
@@ -2,16 +2,12 @@ package uk.gov.hmcts.darts.armrpo.service;
 
 import uk.gov.hmcts.darts.common.entity.ArmRpoExecutionDetailEntity;
 import uk.gov.hmcts.darts.common.entity.ArmRpoStatusEntity;
-import uk.gov.hmcts.darts.common.entity.UserAccountEntity;
-
-import java.time.OffsetDateTime;
 
 public interface ArmRpoService {
 
     ArmRpoExecutionDetailEntity getArmRpoExecutionDetailEntity(Integer executionId);
 
-    void updateArmRpoExecutionDetails(Integer executionId, ArmRpoStatusEntity armRpoStatus, OffsetDateTime lastModifiedDateTime,
-                                      UserAccountEntity lastModifiedBy);
+    void updateArmRpoExecutionDetails(Integer executionId, ArmRpoStatusEntity armRpoStatus);
 
 
 }
