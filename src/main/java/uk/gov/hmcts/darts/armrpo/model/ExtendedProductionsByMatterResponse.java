3b3de58584295a5cdcf42b99432429249acec97b
karen-hedges <133129444+karen-hedges@users.noreply.github.com>
2024-10-24 10:58:35 +0100

    DMP-3805 (Dev Only) ARM RPO Endpoint Integration Layer prerequisite
    Added simple tests

--- src/main/java/uk/gov/hmcts/darts/armrpo/model/ExtendedProductionsByMatterResponse.java
+++ src/main/java/uk/gov/hmcts/darts/armrpo/model/ExtendedProductionsByMatterResponse.java
@@ -2,11 +2,13 @@ package uk.gov.hmcts.darts.armrpo.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 @Data
 @NoArgsConstructor
+@EqualsAndHashCode(callSuper = true)
 public class ExtendedProductionsByMatterResponse extends AbstractMatterResponse {
 
 }
