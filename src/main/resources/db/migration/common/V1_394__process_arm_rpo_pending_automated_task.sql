a9c2a632dc794470e7477c7dcc9e0ae14991459f
benedwards <ben.edwards@cgi.com>
2024-10-25 13:33:09 +0100

    Updated migration

--- src/main/resources/db/migration/common/V1_394__process_arm_rpo_pending_automated_task.sql
+++ src/main/resources/db/migration/common/V1_394__process_arm_rpo_pending_automated_task.sql
@@ -1,3 +1,5 @@
+delete from darts.arm_automated_task where aat_id in (1, 2);
+
 update darts.automated_task a set aut_id = a.aut_id + 1000; --Required to avoid conflict with existing automated task ids
 update darts.automated_task set aut_id = 1 where task_name = 'ProcessDailyList';
 update darts.automated_task set aut_id = 2 where task_name = 'CloseOldUnfinishedTranscriptions';
@@ -27,9 +29,19 @@ update darts.automated_task set aut_id = 25 where task_name = 'ManualDeletion';
 update darts.automated_task set aut_id = 26 where task_name = 'DetsToArm';
 update darts.automated_task set aut_id = 27 where task_name = 'AudioLinking';
 
+
+update darts.automated_task set aut_id = 28 where task_name = 'ProcessE2EArmRpoPending';
+update darts.automated_task set aut_id = 29 where task_name = 'ArmRpoPolling';
+update darts.automated_task set aut_id = 30 where task_name = 'ArmRpoReplay';
+
+INSERT INTO arm_automated_task (aat_id, aut_id, rpo_csv_start_hour, rpo_csv_end_hour, arm_replay_start_ts, arm_replay_end_ts, arm_attribute_type)
+VALUES (1, 28, 25, 49, null, null, 'RPO'),
+       (2, 30, null, null, null, null, 'REPLAY');
+
+
 INSERT INTO darts.automated_task (aut_id, task_name, task_description, cron_expression, cron_editable, batch_size,
                                   created_ts, created_by, last_modified_ts, last_modified_by)
-VALUES (28, 'ProcessARMRPOPending', 'Update the record to STORED which has a status of ARM_RPO_PENDING and data_ingestion_ts is more than 24 hours',
+VALUES (31, 'ProcessARMRPOPending', 'Update the record to STORED which has a status of ARM_RPO_PENDING and data_ingestion_ts is more than 24 hours',
         '0 16 23 * * *', --(16 mins past 11pm every day)
         false, 100000, current_timestamp, 0, current_timestamp, 0);
 ALTER SEQUENCE aut_seq RESTART WITH 100;
