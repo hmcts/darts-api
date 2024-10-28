18c97094e2f3fc8b1c948f956b21c198a56445f8
benedwards <ben.edwards@cgi.com>
2024-10-28 11:28:02 +0000

    Main implementation of handling DEBTs XML download and mapping for daily lists that are missing the JSON content

---
+++ src/main/java/uk/gov/hmcts/darts/task/runner/dailylist/utilities/deserializer/OffsetDateTimeTypeDeserializer.java
@@ -0,0 +1,23 @@
+package uk.gov.hmcts.darts.task.runner.dailylist.utilities.deserializer;
+
+import com.fasterxml.jackson.core.JacksonException;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonDeserializer;
+
+import java.io.IOException;
+import java.time.OffsetDateTime;
+import java.time.format.DateTimeFormatter;
+
+@SuppressWarnings("PMD.LawOfDemeter")
+public class OffsetDateTimeTypeDeserializer extends JsonDeserializer<OffsetDateTime> {
+    public static OffsetDateTime getLOffsetDate(String offsetDate) {
+        return OffsetDateTime.parse(offsetDate, DateTimeFormatter.ISO_OFFSET_DATE_TIME);
+    }
+
+    @Override
+    public OffsetDateTime deserialize(JsonParser parser, DeserializationContext ctxt) throws IOException, JacksonException {
+        return getLOffsetDate(parser.getText());
+    }
+}
+
---
+++ src/test/java/uk/gov/hmcts/darts/dailylist/service/impl/mapper/DailyListRequestMapperTest.java
@@ -0,0 +1,77 @@
+package uk.gov.hmcts.darts.dailylist.service.impl.mapper;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import uk.gov.hmcts.darts.dailylist.model.DailyListJsonObject;
+import uk.gov.hmcts.darts.task.runner.dailylist.mapper.DailyListRequestMapper;
+import uk.gov.hmcts.darts.task.runner.dailylist.schemas.courtservice.DailyListStructure;
+import uk.gov.hmcts.darts.task.runner.dailylist.utilities.XmlParser;
+import uk.gov.hmcts.darts.task.runner.dailylist.utilities.deserializer.LocalDateTimeTypeDeserializer;
+import uk.gov.hmcts.darts.task.runner.dailylist.utilities.deserializer.LocalDateTypeDeserializer;
+import uk.gov.hmcts.darts.task.runner.dailylist.utilities.deserializer.OffsetDateTimeTypeDeserializer;
+import uk.gov.hmcts.darts.task.runner.dailylist.utilities.serializer.LocalDateTimeTypeSerializer;
+import uk.gov.hmcts.darts.task.runner.dailylist.utilities.serializer.LocalDateTypeSerializer;
+import uk.gov.hmcts.darts.task.runner.dailylist.utilities.serializer.OffsetDateTimeTypeSerializer;
+import uk.gov.hmcts.darts.test.common.TestUtils;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.OffsetDateTime;
+import java.util.List;
+
+@SpringBootTest
+class DailyListRequestMapperTest {
+
+    final XmlParser xmlParser = new XmlParser();
+
+    @Autowired
+    DailyListRequestMapper dailyListRequestMapper;
+
+
+    private ObjectMapper getObjMapper() {
+        JavaTimeModule module = new JavaTimeModule();
+        module.addSerializer(LocalDateTime.class, new LocalDateTimeTypeSerializer())
+            .addSerializer(LocalDate.class, new LocalDateTypeSerializer())
+            .addSerializer(OffsetDateTime.class, new OffsetDateTimeTypeSerializer())
+            .addDeserializer(LocalDateTime.class, new LocalDateTimeTypeDeserializer())
+            .addDeserializer(LocalDate.class, new LocalDateTypeDeserializer())
+            .addDeserializer(OffsetDateTime.class, new OffsetDateTimeTypeDeserializer());
+        return new ObjectMapper()
+            .setSerializationInclusion(JsonInclude.Include.NON_NULL)
+            .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
+            .registerModule(module);
+    }
+
+    @Test
+    void test1() throws Exception {
+        String requestXml = TestUtils.getContentsFromFile(
+            "tests/dailylist/DailyListRequestMapperTest/test1/request.xml");
+        DailyListStructure legacyDailyList = xmlParser.unmarshal(requestXml, DailyListStructure.class);
+        DailyListJsonObject modernisedDailyList = dailyListRequestMapper.mapToEntity(legacyDailyList);
+
+        String actualResponse = getObjMapper().writeValueAsString(modernisedDailyList);
+        String expectedResponse = TestUtils.getContentsFromFile(
+            "tests/dailylist/DailyListRequestMapperTest/test1/expectedResponse.json");
+
+        TestUtils.compareJson(expectedResponse, actualResponse, List.of());
+    }
+
+    @Test
+    void test2() throws Exception {
+        String requestXml = TestUtils.getContentsFromFile(
+            "tests/dailylist/DailyListRequestMapperTest/test2/request.xml");
+        DailyListStructure legacyDailyList = xmlParser.unmarshal(requestXml, DailyListStructure.class);
+        DailyListJsonObject modernisedDailyList = dailyListRequestMapper.mapToEntity(legacyDailyList);
+
+        String actualResponse = getObjMapper().writeValueAsString(modernisedDailyList);
+        String expectedResponse = TestUtils.getContentsFromFile(
+            "tests/dailylist/DailyListRequestMapperTest/test2/expectedResponse.json");
+
+        TestUtils.compareJson(expectedResponse, actualResponse, List.of());
+    }
+}
