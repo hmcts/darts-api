8bd8ba01e00e1ab08eab4fad85f3309f93969621
karen-hedges <133129444+karen-hedges@users.noreply.github.com>
2024-10-29 12:29:42 +0000

    DMP-4132 (Dev Only) ARM RPO - Integrate getRecordManagementMatter
    Merged master

--- src/main/java/uk/gov/hmcts/darts/arm/client/ArmRpoClient.java
+++ src/main/java/uk/gov/hmcts/darts/arm/client/ArmRpoClient.java
@@ -1,6 +1,12 @@
 package uk.gov.hmcts.darts.arm.client;
 
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import uk.gov.hmcts.darts.arm.model.rpo.RecordManagementMatterResponse;
+
+import static org.springframework.http.HttpHeaders.AUTHORIZATION;
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 @FeignClient(
     name = "arm-rpo-client",
@@ -8,5 +14,8 @@ import org.springframework.cloud.openfeign.FeignClient;
 )
 public interface ArmRpoClient {
 
-
+    @PostMapping(value = "${darts.storage.arm-api.rpo-url.get-record-management-matter}",
+        produces = APPLICATION_JSON_VALUE
+    )
+    RecordManagementMatterResponse getRecordManagementMatter(@RequestHeader(AUTHORIZATION) String bearerAuth);
 }
