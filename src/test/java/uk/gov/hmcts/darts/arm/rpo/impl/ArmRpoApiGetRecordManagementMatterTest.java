807dbc4261b83bbd506824a3681c5f1aca2a5465
karen-hedges <133129444+karen-hedges@users.noreply.github.com>
2024-10-29 14:26:57 +0000

    DMP-4132 (Dev Only) ARM RPO - Integrate getRecordManagementMatter
    Fixed mismerge

--- src/test/java/uk/gov/hmcts/darts/arm/rpo/impl/ArmRpoApiGetRecordManagementMatterTest.java
+++ src/test/java/uk/gov/hmcts/darts/arm/rpo/impl/ArmRpoApiGetRecordManagementMatterTest.java
@@ -1,6 +1,7 @@
 package uk.gov.hmcts.darts.arm.rpo.impl;
 
-import org.apache.commons.lang3.NotImplementedException;
+import feign.FeignException;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
