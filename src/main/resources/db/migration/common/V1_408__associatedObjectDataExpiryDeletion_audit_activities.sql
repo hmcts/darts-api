9cff6ea6fb58fae05b0293d45f492453b477f77d
benedwards <ben.edwards@cgi.com>
2024-11-08 12:44:21 +0000

    Fixed merge issues

--- src/main/resources/db/migration/common/V1_408__associatedObjectDataExpiryDeletion_audit_activities.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO audit_activity (aua_id, activity_name, activity_description, created_ts, created_by, last_modified_ts, last_modified_by)
-VALUES
-(36, 'Case document expired', 'Case document expired', CURRENT_TIMESTAMP, 0, CURRENT_TIMESTAMP, 0),
-(37, 'Transcription document expired', 'Transcription document expired', CURRENT_TIMESTAMP, 0, CURRENT_TIMESTAMP, 0),
-(38, 'Annotation document expired', 'Annotation document expired', CURRENT_TIMESTAMP, 0, CURRENT_TIMESTAMP, 0),
-(39, 'Audio file expired', 'Audio file expired', CURRENT_TIMESTAMP, 0, CURRENT_TIMESTAMP, 0);
