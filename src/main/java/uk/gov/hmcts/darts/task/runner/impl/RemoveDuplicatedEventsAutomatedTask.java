ce322b82627b949aea05d2a21317c0d7805cd63d
benedwards <ben.edwards@cgi.com>
2024-11-08 11:19:49 +0000

    Added batch size and updated lock limits for: RemoveDuplicatedEvents

--- src/main/java/uk/gov/hmcts/darts/task/runner/impl/RemoveDuplicatedEventsAutomatedTask.java
+++ src/main/java/uk/gov/hmcts/darts/task/runner/impl/RemoveDuplicatedEventsAutomatedTask.java
@@ -11,6 +11,8 @@ import uk.gov.hmcts.darts.task.config.AutomatedTaskConfigurationProperties;
 import uk.gov.hmcts.darts.task.runner.AutoloadingManualTask;
 import uk.gov.hmcts.darts.task.service.LockService;
 
+import java.time.Duration;
+
 import static uk.gov.hmcts.darts.task.api.AutomatedTaskName.REMOVE_DUPLICATED_EVENTS_TASK_NAME;
 
 @Slf4j
@@ -34,6 +36,11 @@ public class RemoveDuplicatedEventsAutomatedTask extends AbstractLockableAutomat
     @Override
     public AutomatedTaskName getAutomatedTaskName() {
         return REMOVE_DUPLICATED_EVENTS_TASK_NAME;
+    }
+
+    @Override
+    public Duration getLockAtMostFor() {
+        return Duration.ofMinutes(90);
     }
 
     @Override
