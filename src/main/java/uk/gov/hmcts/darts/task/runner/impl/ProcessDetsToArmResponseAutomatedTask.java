decc5b2ab4706ba5fe6accd5064b38b8dd6349a0
benedwards <ben.edwards@cgi.com>
2024-11-08 11:31:10 +0000

    Added batch size and updated lock limits for: ProcessDETSToArmResponse

--- src/main/java/uk/gov/hmcts/darts/task/runner/impl/ProcessDetsToArmResponseAutomatedTask.java
+++ src/main/java/uk/gov/hmcts/darts/task/runner/impl/ProcessDetsToArmResponseAutomatedTask.java
@@ -12,6 +12,8 @@ import uk.gov.hmcts.darts.task.config.AutomatedTaskConfigurationProperties;
 import uk.gov.hmcts.darts.task.runner.AutoloadingManualTask;
 import uk.gov.hmcts.darts.task.service.LockService;
 
+import java.time.Duration;
+
 import static uk.gov.hmcts.darts.task.api.AutomatedTaskName.PROCESS_DETS_TO_ARM_RESPONSE;
 
 @Slf4j
@@ -36,9 +38,14 @@ public class ProcessDetsToArmResponseAutomatedTask extends AbstractLockableAutom
     }
 
     @Override
+    public Duration getLockAtMostFor() {
+        return Duration.ofMinutes(90);
+    }
+
+    @Override
     protected void runTask() {
-        Integer batchSize = getAutomatedTaskBatchSize();
-        DetsToArmBatchProcessResponseFilesImpl detsToArmResponseFilesProcessor = automatedTaskProcessorFactory.createDetsToArmResponseFilesProcessor(batchSize);
+        DetsToArmBatchProcessResponseFilesImpl detsToArmResponseFilesProcessor = automatedTaskProcessorFactory.createDetsToArmResponseFilesProcessor(
+            getAutomatedTaskBatchSize());
         detsToArmResponseFilesProcessor.processResponseFiles();
     }
 }
