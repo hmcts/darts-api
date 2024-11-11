5a5a834c591fbe49bae811c6a9400d33b52b6fe4
karen-hedges <133129444+karen-hedges@users.noreply.github.com>
2024-11-11 10:43:26 +0000

    DMP-3806 (Dev Only) ARM RPO - Integrate getExtendedSearchesByMatter
    Merged master

---
+++ src/test/java/uk/gov/hmcts/darts/arm/util/ArmRpoJsonUtilTest.java
@@ -0,0 +1,57 @@
+package uk.gov.hmcts.darts.arm.util;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.Assert.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class ArmRpoJsonUtilTest {
+
+    @Test
+    void sanitiseValidJson() {
+        // given
+        String validJson = """
+            {
+              "filter": "1",
+              "filterBy": {},
+              "matterId": "12345",
+              "usePaging": true,
+              "rowsNumber": 10,
+              "pageIndex": 0,
+              "orderBy": "createdDate",
+              "orderByAsc": false,
+              "search": ""
+            }
+            """;
+
+        // when
+        String sanitisedJson = ArmRpoJsonUtil.sanitise(validJson);
+
+        // then
+        String expectedResult = "{\"filter\":\"1\",\"filterBy\":{},\"matterId\":\"12345\",\"usePaging\":true,\"rowsNumber\":10,\"pageIndex\":0,\"orderBy\":\"createdDate\",\"orderByAsc\":false,\"search\":\"\"}";
+        assertEquals(expectedResult, sanitisedJson);
+    }
+
+    @Test
+    void sanitiseInvalidJson() {
+        // given
+        String invalidJson = """
+            {
+              "filter": "1",
+              "filterBy": {},
+              "matterId": "12345",
+              "usePaging": true,
+              "rowsNumber": 10,
+              "pageIndex": 0,
+              "orderBy": "createdDate",
+              "orderByAsc": false,
+              "search": ""
+            """;
+
+        // when
+        var exception = assertThrows(IllegalArgumentException.class, () -> ArmRpoJsonUtil.sanitise(invalidJson));
+
+        // then
+        assertEquals("Failed to serialise the templated json", exception.getMessage());
+    }
+}
