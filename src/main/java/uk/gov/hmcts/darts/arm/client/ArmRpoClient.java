5fa60cedb781ed4ef7b054a1bcd137f15c4768db
karen-hedges <133129444+karen-hedges@users.noreply.github.com>
2024-11-11 17:03:57 +0000

    DMP-4138 (Dev Only) ARM RPO - Integrate createExportBasedOnSearchResultsTable (#2249)

--- src/main/java/uk/gov/hmcts/darts/arm/client/ArmRpoClient.java
+++ src/main/java/uk/gov/hmcts/darts/arm/client/ArmRpoClient.java
@@ -5,6 +5,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import uk.gov.hmcts.darts.arm.client.model.rpo.ArmAsyncSearchResponse;
+import uk.gov.hmcts.darts.arm.client.model.rpo.CreateExportBasedOnSearchResultsTableRequest;
+import uk.gov.hmcts.darts.arm.client.model.rpo.CreateExportBasedOnSearchResultsTableResponse;
 import uk.gov.hmcts.darts.arm.client.model.rpo.ExtendedSearchesByMatterResponse;
 import uk.gov.hmcts.darts.arm.client.model.rpo.IndexesByMatterIdRequest;
 import uk.gov.hmcts.darts.arm.client.model.rpo.IndexesByMatterIdResponse;
@@ -41,6 +43,7 @@ public interface ArmRpoClient {
     )
     StorageAccountResponse getStorageAccounts(@RequestHeader(AUTHORIZATION) String bearerToken,
                                               @RequestBody StorageAccountRequest storageAccountRequest);
+
 
     @PostMapping(value = "${darts.storage.arm-api.rpo-url.get-master-index-field-by-record-class-schema-path}",
         consumes = APPLICATION_JSON_VALUE,
@@ -88,5 +91,13 @@ public interface ArmRpoClient {
     )
     ProductionOutputFilesResponse getProductionOutputFiles(@RequestHeader(AUTHORIZATION) String bearerToken,
                                                            @RequestBody ProductionOutputFilesRequest productionOutputFilesRequest);
+
+    @PostMapping(value = "${darts.storage.arm-api.rpo-url.create-export-based-on-search-results-table-path}",
+        consumes = APPLICATION_JSON_VALUE,
+        produces = APPLICATION_JSON_VALUE
+    )
+    CreateExportBasedOnSearchResultsTableResponse createExportBasedOnSearchResultsTable(
+        @RequestHeader(AUTHORIZATION) String bearerToken, @RequestBody CreateExportBasedOnSearchResultsTableRequest request);
+
 
 }
--- src/main/java/uk/gov/hmcts/darts/arm/client/model/rpo/BaseRpoResponse.java
+++ src/main/java/uk/gov/hmcts/darts/arm/client/model/rpo/BaseRpoResponse.java
@@ -3,12 +3,14 @@ package uk.gov.hmcts.darts.arm.client.model.rpo;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 import java.util.List;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 @Data
 @NoArgsConstructor
+@ToString()
 public abstract class BaseRpoResponse {
 
     Integer itemsCount;
