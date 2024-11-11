86f6fa8564e1045e10fb7fb0a75ce6e34de01e7d
Dave Thompson <davethompson21@gmail.com>
2024-11-11 10:33:13 +0000

    Fixing flyway script number

--- src/main/resources/db/migration/common/V1_410__manual_obfuscation_audit_activity.sql
+++ src/main/resources/db/migration/common/V1_410__manual_obfuscation_audit_activity.sql
@@ -1,2 +1,2 @@
 INSERT INTO audit_activity (aua_id, activity_name, activity_description, created_ts, created_by, last_modified_ts, last_modified_by)
-VALUES (36, 'Manual Obfuscation', 'Manual Obfuscation', CURRENT_TIMESTAMP, 0, CURRENT_TIMESTAMP, 0);
+VALUES (41, 'Manual Obfuscation', 'Manual Obfuscation', CURRENT_TIMESTAMP, 0, CURRENT_TIMESTAMP, 0);
