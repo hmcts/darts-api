3fecda793a6294732b9f24fbd9f3a5502c6fbb83
karen-hedges <133129444+karen-hedges@users.noreply.github.com>
2024-11-11 13:55:17 +0000

    DMP-4141 (Dev Only) ARM RPO - Integrate downloadProduction
    Created functionality to call ARM RPO downloadProduction API

---
+++ src/integrationTest/java/uk/gov/hmcts/darts/arm/rpo/ArmRpoApiDownloadProductionIntTest.java
@@ -0,0 +1,87 @@
+package uk.gov.hmcts.darts.arm.rpo;
+
+import feign.FeignException;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import uk.gov.hmcts.darts.arm.client.ArmRpoClient;
+import uk.gov.hmcts.darts.arm.exception.ArmRpoException;
+import uk.gov.hmcts.darts.common.entity.ArmRpoExecutionDetailEntity;
+import uk.gov.hmcts.darts.common.entity.UserAccountEntity;
+import uk.gov.hmcts.darts.common.enums.ArmRpoStateEnum;
+import uk.gov.hmcts.darts.common.enums.ArmRpoStatusEnum;
+import uk.gov.hmcts.darts.testutils.IntegrationBase;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsString;
+import static org.junit.Assert.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+@SuppressWarnings("PMD.CloseResource")
+class ArmRpoApiDownloadProductionIntTest extends IntegrationBase {
+
+    @MockBean
+    private ArmRpoClient armRpoClient;
+
+    @Autowired
+    private ArmRpoApi armRpoApi;
+
+
+    @Test
+    void downloadProductionSuccess() {
+        // given
+        feign.Response response = mock(feign.Response.class);
+        when(response.status()).thenReturn(200);
+        when(armRpoClient.downloadProduction(anyString(), anyString())).thenReturn(response);
+
+        UserAccountEntity userAccount = dartsDatabase.getUserAccountStub().getIntegrationTestUserAccountEntity();
+        ArmRpoExecutionDetailEntity armRpoExecutionDetailEntity = new ArmRpoExecutionDetailEntity();
+        armRpoExecutionDetailEntity.setCreatedBy(userAccount);
+        armRpoExecutionDetailEntity.setLastModifiedBy(userAccount);
+        var armRpoExecutionDetail = dartsPersistence.save(armRpoExecutionDetailEntity);
+
+
+        // when
+        try (feign.Response result =
+                 armRpoApi.downloadProduction("token", armRpoExecutionDetailEntity.getId(), "productionExportId", userAccount)) {
+            // then
+            assertNotNull(result);
+        }
+
+        // then
+        var armRpoExecutionDetailEntityUpdated = dartsPersistence.getArmRpoExecutionDetailRepository().findById(armRpoExecutionDetail.getId()).get();
+        assertEquals(ArmRpoStateEnum.DOWNLOAD_PRODUCTION.getId(), armRpoExecutionDetailEntityUpdated.getArmRpoState().getId());
+        assertEquals(ArmRpoStatusEnum.COMPLETED.getId(), armRpoExecutionDetailEntityUpdated.getArmRpoStatus().getId());
+
+    }
+
+    @Test
+    void downloadProductionThrowsFeignException() {
+        // given
+        when(armRpoClient.downloadProduction(anyString(), anyString())).thenThrow(FeignException.class);
+
+        UserAccountEntity userAccount = dartsDatabase.getUserAccountStub().getIntegrationTestUserAccountEntity();
+        ArmRpoExecutionDetailEntity armRpoExecutionDetailEntity = new ArmRpoExecutionDetailEntity();
+        armRpoExecutionDetailEntity.setCreatedBy(userAccount);
+        armRpoExecutionDetailEntity.setLastModifiedBy(userAccount);
+        var armRpoExecutionDetail = dartsPersistence.save(armRpoExecutionDetailEntity);
+
+        // when
+        ArmRpoException armRpoException = assertThrows(ArmRpoException.class, () ->
+            armRpoApi.downloadProduction("token", armRpoExecutionDetailEntity.getId(), "productionExportId", userAccount));
+
+
+        // then
+        assertThat(armRpoException.getMessage(), containsString(
+            "Failure during ARM get storage accounts: Unable to get indexes from storage account response"));
+
+        var armRpoExecutionDetailEntityUpdated = dartsPersistence.getArmRpoExecutionDetailRepository().findById(armRpoExecutionDetail.getId()).get();
+        assertEquals(ArmRpoStateEnum.DOWNLOAD_PRODUCTION.getId(), armRpoExecutionDetailEntityUpdated.getArmRpoState().getId());
+        assertEquals(ArmRpoStatusEnum.COMPLETED.getId(), armRpoExecutionDetailEntityUpdated.getArmRpoStatus().getId());
+
+    }
+}
