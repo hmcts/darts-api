6dd022641205d61a7650c1396cc1e063f1fadee1
Chris Bellingham <131763968+chrisbellingham-hmcts@users.noreply.github.com>
2024-11-11 15:47:33 +0000

    DMP-4140: Implement getProductionOutputFiles for ARM RPO (#2256)

---
+++ src/main/java/uk/gov/hmcts/darts/arm/client/model/rpo/ProductionOutputFilesResponse.java
@@ -0,0 +1,39 @@
+package uk.gov.hmcts.darts.arm.client.model.rpo;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+@Data
+@NoArgsConstructor
+@EqualsAndHashCode(callSuper = true)
+public class ProductionOutputFilesResponse extends BaseRpoResponse {
+
+    @JsonProperty("productionID")
+    private String productionId;
+
+    @JsonProperty("productionExportFile")
+    private List<ProductionExportFile> productionExportFiles;
+
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    @Data
+    @NoArgsConstructor
+    public static class ProductionExportFile {
+        @JsonProperty("productionExportFile")
+        private ProductionExportFileDetail productionExportFileDetails;
+    }
+
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    @Data
+    @NoArgsConstructor
+    public static class ProductionExportFileDetail {
+        @JsonProperty("productionExportFileID")
+        private String productionExportFileId;
+    }
+
+}
