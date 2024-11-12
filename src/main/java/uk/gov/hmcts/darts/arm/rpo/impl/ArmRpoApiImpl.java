461c02521f791ba6fc883a7c4e9aa5909dd7c424
karen-hedges <133129444+karen-hedges@users.noreply.github.com>
2024-11-12 13:41:02 +0000

    DMP-4141 (Dev Only) ARM RPO - Integrate downloadProduction
    Created functionality to call ARM RPO downloadProduction API

--- src/main/java/uk/gov/hmcts/darts/arm/rpo/impl/ArmRpoApiImpl.java
+++ src/main/java/uk/gov/hmcts/darts/arm/rpo/impl/ArmRpoApiImpl.java
@@ -4,7 +4,6 @@ import feign.FeignException;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.collections4.CollectionUtils;
-import org.apache.commons.lang3.NotImplementedException;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
