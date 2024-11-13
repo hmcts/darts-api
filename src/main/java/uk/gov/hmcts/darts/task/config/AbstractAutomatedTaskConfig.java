5ffa1f8362cb532afacc3a36502bcb6a2cd52809
benedwards <ben.edwards@cgi.com>
2024-11-13 10:26:26 +0000

    Added validation

--- src/main/java/uk/gov/hmcts/darts/task/config/AbstractAutomatedTaskConfig.java
+++ src/main/java/uk/gov/hmcts/darts/task/config/AbstractAutomatedTaskConfig.java
@@ -1,15 +1,21 @@
 package uk.gov.hmcts.darts.task.config;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.validation.annotation.Validated;
 
 import java.time.Duration;
 
 @Getter
 @Setter
+@Validated
 public class AbstractAutomatedTaskConfig {
-
+    @NotBlank
     private String systemUserEmail;
+    @NotNull
     private Duration lockAtMostFor = Duration.ofMinutes(300);
+    @NotNull
     private Duration lockAtLeastFor = Duration.ofMinutes(1);
 }
