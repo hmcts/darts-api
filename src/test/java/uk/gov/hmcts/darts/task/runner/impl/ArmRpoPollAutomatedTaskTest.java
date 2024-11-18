ea2f87611858023cfd20ae0011eb5e63e8fded7d
karen-hedges <133129444+karen-hedges@users.noreply.github.com>
2024-11-18 09:08:44 +0000

    DMP-4144 ARM RPO - Integrate all Endpoints for ARMRPOPollAutomatedTask
    Merged master

---
+++ src/test/java/uk/gov/hmcts/darts/task/runner/impl/ArmRpoPollAutomatedTaskTest.java
@@ -0,0 +1,43 @@
+package uk.gov.hmcts.darts.task.runner.impl;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import uk.gov.hmcts.darts.arm.service.ArmRpoPollService;
+import uk.gov.hmcts.darts.common.repository.AutomatedTaskRepository;
+import uk.gov.hmcts.darts.log.api.LogApi;
+import uk.gov.hmcts.darts.task.service.LockService;
+
+@ExtendWith(MockitoExtension.class)
+class ArmRpoPollAutomatedTaskTest {
+
+    @Mock
+    private AutomatedTaskRepository automatedTaskRepository;
+    @Mock
+    private ArmRpoPollService armRpoPollService;
+    @Mock
+    private LogApi logApi;
+    @Mock
+    private LockService lockService;
+
+    @Test
+    void runTask() {
+
+        // given
+        ArmRpoPollAutomatedTask armRpoPollAutomatedTask = new ArmRpoPollAutomatedTask(
+            null,
+            null,
+            armRpoPollService,
+            logApi,
+            lockService
+        );
+
+        // when
+        armRpoPollAutomatedTask.runTask();
+
+        // then
+        Mockito.verify(armRpoPollService, Mockito.times(1)).pollArmRpo(false);
+    }
+}
