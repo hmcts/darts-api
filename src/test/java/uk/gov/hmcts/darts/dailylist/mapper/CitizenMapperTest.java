3e8f1c9e747484f1f02cec422ddf744bd98edb6a
Ben Edwards <147524406+Ben-Edwards-cgi@users.noreply.github.com>
2024-11-19 12:17:16 +0000

    DMP-4119: Trim Defendant, Prosecutor and Defender names (#2269)

--- src/test/java/uk/gov/hmcts/darts/dailylist/mapper/CitizenMapperTest.java
+++ src/test/java/uk/gov/hmcts/darts/dailylist/mapper/CitizenMapperTest.java
@@ -16,7 +16,7 @@ class CitizenMapperTest {
 
     @Test
     void testCreateCitizenMapperFromStringWithBreaks() {
-        String name = "Jow Bloggs Test";
+        String name = " Jow Bloggs Test ";
         CitizenNameMapper mapper = new CitizenNameMapper();
         CitizenName citizenName = mapper.getCitizenName(name);
         Assertions.assertEquals("Jow", citizenName.getCitizenNameForename());
@@ -25,17 +25,60 @@ class CitizenMapperTest {
 
     @Test
     void testGetStringFromCitizenMapperWithSurnameAndForename() {
-        String name = "Jow Bloggs Test";
+        String name = " Jow Bloggs Test ";
         CitizenNameMapper mapper = new CitizenNameMapper();
         CitizenName citizenName = mapper.getCitizenName(name);
-        Assertions.assertEquals(name, mapper.getCitizenName(citizenName));
+        Assertions.assertEquals("Jow Bloggs Test", mapper.getCitizenName(citizenName));
     }
 
     @Test
     void testGetStringFromCitizenMapperWithForename() {
-        String name = "DMP-723-AC1-D001";
+        String name = " DMP-723-AC1-D001 ";
         CitizenNameMapper mapper = new CitizenNameMapper();
         CitizenName citizenName = mapper.getCitizenName(name);
-        Assertions.assertEquals(name, mapper.getCitizenName(citizenName));
+        Assertions.assertEquals("DMP-723-AC1-D001", mapper.getCitizenName(citizenName));
+    }
+
+    @Test
+    void positiveGetCitizenNameFromCitizenNameObjectTypical() {
+        CitizenNameMapper mapper = new CitizenNameMapper();
+        CitizenName citizenName = new CitizenName();
+        citizenName.setCitizenNameForename("John");
+        citizenName.setCitizenNameSurname("Smith");
+        Assertions.assertEquals("John Smith", mapper.getCitizenName(citizenName));
+    }
+
+    @Test
+    void positiveGetCitizenNameFromCitizenNameObjectOnlyForename() {
+        CitizenNameMapper mapper = new CitizenNameMapper();
+        CitizenName citizenName = new CitizenName();
+        citizenName.setCitizenNameForename("John");
+        Assertions.assertEquals("John", mapper.getCitizenName(citizenName));
+    }
+
+    @Test
+    void positiveGetCitizenNameFromCitizenNameObjectOnlyForenameWithSpace() {
+        CitizenNameMapper mapper = new CitizenNameMapper();
+        CitizenName citizenName = new CitizenName();
+        citizenName.setCitizenNameForename(" John  ");
+        Assertions.assertEquals("John", mapper.getCitizenName(citizenName));
+    }
+
+    @Test
+    void positiveGetCitizenNameFromCitizenNameObjectBlankSurname() {
+        CitizenNameMapper mapper = new CitizenNameMapper();
+        CitizenName citizenName = new CitizenName();
+        citizenName.setCitizenNameForename("John");
+        citizenName.setCitizenNameSurname("");
+        Assertions.assertEquals("John", mapper.getCitizenName(citizenName));
+    }
+
+    @Test
+    void positiveGetCitizenNameFromCitizenNameObjectExtraSpace() {
+        CitizenNameMapper mapper = new CitizenNameMapper();
+        CitizenName citizenName = new CitizenName();
+        citizenName.setCitizenNameForename("  John  ");
+        citizenName.setCitizenNameSurname("  Smith  ");
+        Assertions.assertEquals("John Smith", mapper.getCitizenName(citizenName));
     }
 }
