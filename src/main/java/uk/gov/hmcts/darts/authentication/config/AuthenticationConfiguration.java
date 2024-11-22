840aa7b51891f1f4480068761b0c31a4b977a4e5
benedwards <ben.edwards@cgi.com>
2024-11-22 14:02:56 +0000

    Updated lastModifiedBy and createdBy to auto update based on entity lifecycle events

--- src/main/java/uk/gov/hmcts/darts/authentication/config/AuthenticationConfiguration.java
+++ src/main/java/uk/gov/hmcts/darts/authentication/config/AuthenticationConfiguration.java
@@ -2,8 +2,13 @@ package uk.gov.hmcts.darts.authentication.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
 import uk.gov.hmcts.darts.authentication.exception.AuthenticationError;
+import uk.gov.hmcts.darts.authorisation.component.UserIdentity;
+import uk.gov.hmcts.darts.common.entity.UserAccountEntity;
 import uk.gov.hmcts.darts.common.exception.DartsApiException;
+
+import java.util.Optional;
 
 @Configuration
 public class AuthenticationConfiguration {
@@ -12,6 +17,17 @@ public class AuthenticationConfiguration {
     public AuthConfigFallback getNoAuthConfigurationFallback(DefaultAuthConfigurationPropertiesStrategy strategy) {
         return (req) -> {
             throw new DartsApiException(AuthenticationError.FAILED_TO_OBTAIN_AUTHENTICATION_CONFIG);
+        };
+    }
+
+    @Bean
+    public AuditorAware<UserAccountEntity> auditorAware(UserIdentity userIdentity) {
+        return () -> {
+            try {
+                return Optional.ofNullable(userIdentity.getUserAccount());
+            } catch (Exception e) {
+                return Optional.empty();
+            }
         };
     }
 }
