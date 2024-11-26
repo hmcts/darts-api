0d09b7ff5ca49052db7cc637838e5b4601f64e57
karen-hedges <133129444+karen-hedges@users.noreply.github.com>
2024-11-26 09:10:21 +0000

    DMP-4065 UnstructuredToArmDataStore leaves record stuck in 'Arm Ingestion'
    Added a shutdown hook to reset states before closing down

--- src/integrationTest/java/uk/gov/hmcts/darts/common/repository/ExternalObjectDirectoryRepositoryTest.java
+++ src/integrationTest/java/uk/gov/hmcts/darts/common/repository/ExternalObjectDirectoryRepositoryTest.java
@@ -22,6 +22,7 @@ import java.util.Optional;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static uk.gov.hmcts.darts.common.enums.ObjectRecordStatusEnum.ARM_PROCESSING_RESPONSE_FILES;
 import static uk.gov.hmcts.darts.common.enums.ObjectRecordStatusEnum.ARM_RPO_PENDING;
 import static uk.gov.hmcts.darts.common.enums.ObjectRecordStatusEnum.STORED;
 
@@ -465,4 +466,27 @@ class ExternalObjectDirectoryRepositoryTest extends PostgresIntegrationBase {
         });
 
     }
+
+    @Test
+    void updateEodByIdAndStatus() throws InvocationTargetException, NoSuchMethodException, IllegalAccessException {
+        // given
+        OffsetDateTime now = currentTimeHelper.currentOffsetDateTime();
+        var user = externalObjectDirectoryStub.getUserAccountStub().getIntegrationTestUserAccountEntity();
+
+        List<ExternalObjectDirectoryEntity> externalObjectDirectoryEntities
+            = externalObjectDirectoryStub.generateWithStatusAndMediaLocation(
+            ExternalLocationTypeEnum.ARM, ARM_PROCESSING_RESPONSE_FILES, 20, Optional.of(now));
+        List<Integer> eodsIds = externalObjectDirectoryEntities.stream().map(ExternalObjectDirectoryEntity::getId).toList();
+
+        // when
+        externalObjectDirectoryRepository.updateEodByIdAndStatus(eodsIds, EodHelper.armDropZoneStatus(), EodHelper.armProcessingResponseFilesStatus(), user);
+
+        // then
+        List<ExternalObjectDirectoryEntity> updatedEods = externalObjectDirectoryRepository.findAllById(eodsIds);
+        updatedEods.forEach(eod -> {
+            assertEquals(EodHelper.armDropZoneStatus(), eod.getStatus());
+            assertEquals(user.getId(), eod.getLastModifiedBy().getId());
+        });
+    }
+
 }
