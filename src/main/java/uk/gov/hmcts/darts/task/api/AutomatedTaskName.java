149ecb683c4ebaea0f9a3859578597a2a51e22da
karen-hedges <133129444+karen-hedges@users.noreply.github.com>
2024-11-27 17:41:46 +0000

    DMP-4413 ARM RPO Automated Tasks issues with feature flag PROCESS_E2E_ARM_RPO
    Added code so that the automated tasks ArmRpoPolling and ArmRpoReplay are hidden behind the feature flag PROCESS_E2E_ARM_RPO

--- src/main/java/uk/gov/hmcts/darts/task/api/AutomatedTaskName.java
+++ src/main/java/uk/gov/hmcts/darts/task/api/AutomatedTaskName.java
@@ -39,8 +39,8 @@ public enum AutomatedTaskName {
     PROCESS_ARM_RPO_PENDING("ProcessARMRPOPending", "!${darts.automated.task.process-e2e-arm-rpo-pending.process-e2e-arm-rpo:false}"),
     AUDIO_LINKING_TASK_NAME("AudioLinking"),
     PROCESS_E2E_ARM_PENDING_TASK_NAME("ProcessE2EArmRpoPending", "${darts.automated.task.process-e2e-arm-rpo-pending.process-e2e-arm-rpo:false}"),
-    ARM_RPO_POLLING_TASK_NAME("ArmRpoPolling", "!${darts.automated.task.process-e2e-arm-rpo-pending.process-e2e-arm-rpo:false}"),
-    ARM_RPO_REPLAY_TASK_NAME("ArmRpoReplay", "!${darts.automated.task.process-e2e-arm-rpo-pending.process-e2e-arm-rpo:false}"),
+    ARM_RPO_POLLING_TASK_NAME("ArmRpoPolling", "${darts.automated.task.process-e2e-arm-rpo-pending.process-e2e-arm-rpo:false}"),
+    ARM_RPO_REPLAY_TASK_NAME("ArmRpoReplay", "${darts.automated.task.process-e2e-arm-rpo-pending.process-e2e-arm-rpo:false}"),
     ARM_MISSING_RESPONSE_REPLY_TASK_NAME("ArmMissingResponseReplay"),
     DETS_CLEANUP_ARM_RESPONSE_FILES("DETSCleanupArmResponseFiles");
 
