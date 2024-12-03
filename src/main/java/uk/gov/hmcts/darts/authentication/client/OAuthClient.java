fbea9c962ac50a9b3cedbefe232db7c53031b4cf
Dave Thompson <davethompson21@gmail.com>
2024-12-03 15:53:47 +0000

    PoC refresh token implementation

--- src/main/java/uk/gov/hmcts/darts/authentication/client/OAuthClient.java
+++ src/main/java/uk/gov/hmcts/darts/authentication/client/OAuthClient.java
@@ -3,10 +3,17 @@ package uk.gov.hmcts.darts.authentication.client;
 import com.nimbusds.oauth2.sdk.http.HTTPResponse;
 import uk.gov.hmcts.darts.authentication.config.AuthProviderConfigurationProperties;
 
+import java.io.IOException;
+import java.net.URISyntaxException;
+
 public interface OAuthClient {
     @SuppressWarnings({"PMD.UseObjectForClearerAPI"})
     HTTPResponse fetchAccessToken(AuthProviderConfigurationProperties providerConfigurationProperties,
                                   String redirectType, String authCode,
                                   String clientId,
                                   String authClientSecret, String scope);
+
+    HTTPResponse fetchAccessToken(AuthProviderConfigurationProperties providerConfig,
+                                  String refreshToken, String clientId,
+                                  String clientSecret, String scope);
 }
