45baf4bb4ab7dfdcfea5a643b9d4a91677d807e3
Jack Maloney <jack.maloney1@hmcts.net>
2024-12-12 09:51:19 +0000

    DMP-4430 Auditing for unhiding audio (#2365)

---
+++ src/main/resources/db/migration/common/V1_421__insert_hiding_unhiding_audit_activities.sql
@@ -0,0 +1,8 @@
+INSERT INTO audit_activity (aua_id, activity_name, activity_description, created_ts, created_by, last_modified_ts, last_modified_by)
+VALUES (42, 'Unhiding Audio', 'Unhiding audio', CURRENT_TIMESTAMP, 0, CURRENT_TIMESTAMP, 0);
+
+INSERT INTO audit_activity (aua_id, activity_name, activity_description, created_ts, created_by, last_modified_ts, last_modified_by)
+VALUES (43, 'Hiding Transcription', 'Hiding transcription', CURRENT_TIMESTAMP, 0, CURRENT_TIMESTAMP, 0);
+
+INSERT INTO audit_activity (aua_id, activity_name, activity_description, created_ts, created_by, last_modified_ts, last_modified_by)
+VALUES (44, 'Unhiding Transcription', 'Unhiding transcription', CURRENT_TIMESTAMP, 0, CURRENT_TIMESTAMP, 0);
