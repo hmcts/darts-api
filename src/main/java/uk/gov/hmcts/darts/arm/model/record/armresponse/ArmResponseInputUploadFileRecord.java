880f06ff675d37a7a90b434dad495be1b90eba83
benedwards <ben.edwards@cgi.com>
2024-12-13 14:12:26 +0000

    Fixed tests

--- src/main/java/uk/gov/hmcts/darts/arm/model/record/armresponse/ArmResponseInputUploadFileRecord.java
+++ src/main/java/uk/gov/hmcts/darts/arm/model/record/armresponse/ArmResponseInputUploadFileRecord.java
@@ -1,6 +1,5 @@
 package uk.gov.hmcts.darts.arm.model.record.armresponse;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
@@ -17,7 +16,6 @@ public class ArmResponseInputUploadFileRecord {
     private String operation;
 
     @JsonProperty("timestamp")
-    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss.SSSSSS")
     private LocalDateTime timestamp;
 
     @JsonProperty("status")
