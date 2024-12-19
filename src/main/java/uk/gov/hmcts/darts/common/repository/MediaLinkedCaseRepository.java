647c1f3230a857bdf095cfe28e7b4bdb1c19cc87
Jack Maloney <jack.maloney1@hmcts.net>
2024-12-19 15:59:42 +0000

    DMP-2542 Unlink expired cases from media (#2391)

--- src/main/java/uk/gov/hmcts/darts/common/repository/MediaLinkedCaseRepository.java
+++ src/main/java/uk/gov/hmcts/darts/common/repository/MediaLinkedCaseRepository.java
@@ -1,6 +1,7 @@
 package uk.gov.hmcts.darts.common.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import uk.gov.hmcts.darts.common.entity.CourtCaseEntity;
 import uk.gov.hmcts.darts.common.entity.MediaEntity;
@@ -12,10 +13,23 @@ import java.util.List;
 @Repository
 public interface MediaLinkedCaseRepository extends JpaRepository<MediaLinkedCaseEntity, Integer> {
 
+    List<MediaLinkedCaseEntity> findAllByCourtCase(CourtCaseEntity courtCase);
+
     List<MediaLinkedCaseEntity> findByMedia(MediaEntity media);
 
     List<MediaLinkedCaseEntity> findByMediaAndSource(MediaEntity mediaEntity, MediaLinkedCaseSourceType source);
 
     boolean existsByMediaAndCourtCase(MediaEntity media, CourtCaseEntity courtCase);
 
+    @Query("""
+        SELECT COUNT(DISTINCT cc) = (COUNT(cc.isDataAnonymised) filter (where cc.isDataAnonymised = true))
+                    FROM MediaLinkedCaseEntity mlc
+                    LEFT JOIN CourtCaseEntity cc
+                    ON (mlc.courtCase = cc
+                    or (cc.courthouse.courthouseName = mlc.courthouseName and cc.caseNumber = mlc.caseNumber))
+                    WHERE mlc.media = :mediaEntity
+                    group by mlc.media
+        """
+    )
+    boolean areAllAssociatedCasesAnonymised(MediaEntity mediaEntity);
 }
