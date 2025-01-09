66404df23d19ad465858da6aeab1a550a388e8ee
benedwards <ben.edwards@cgi.com>
2025-01-09 15:28:36 +0000

    Fixed styles

--- src/test/java/uk/gov/hmcts/darts/util/LogUtil.java
+++ src/test/java/uk/gov/hmcts/darts/util/LogUtil.java
@@ -12,6 +12,7 @@ public final class LogUtil {
     }
 
     @SneakyThrows
+    @SuppressWarnings("PMD.DoNotUseThreads")//Required to prevent busy waiting
     public static void waitUntilMessag(CapturedOutput capturedOutput, String message,
                                        int timeoutInSeconds) {
         long startTime = System.currentTimeMillis();
