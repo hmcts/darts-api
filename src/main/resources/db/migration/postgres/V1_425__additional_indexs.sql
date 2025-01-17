66088ee322b170de85e70a0b00871e0bdc8d6960
benedwards <ben.edwards@cgi.com>
2025-01-17 13:32:09 +0000

    Review comments

--- src/main/resources/db/migration/postgres/V1_425__additional_indexs.sql
+++ src/main/resources/db/migration/postgres/V1_425__additional_indexs.sql
@@ -1,11 +1,11 @@
-drop index concurrently event_event_id_is_current_idx;
-create index concurrently request_status_mer_id_idx on media_request (request_status, mer_id);
-create index concurrently created_ts_med_id_idx on media (created_ts, med_id);
-create index concurrently event_status_idx on event (event_status);
-create index concurrently hearing_date_ctr_id_idx on hearing (hearing_date, ctr_id);
-create index concurrently cas_id_mlc_id_idx on media_linked_case (cas_id, mlc_id);
-create index concurrently med_id_idx on media_linked_case (med_id);
-create index concurrently source_idx on media_linked_case (source);
+drop index event_event_id_is_current_idx;
+create index request_status_mer_id_idx on media_request (request_status, mer_id);
+create index created_ts_med_id_idx on media (created_ts, med_id);
+create index event_status_idx on event (event_status);
+create index hearing_date_ctr_id_idx on hearing (hearing_date, ctr_id);
+create index cas_id_mlc_id_idx on media_linked_case (cas_id, mlc_id);
+create index med_id_idx on media_linked_case (med_id);
+create index source_idx on media_linked_case (source);
 CREATE INDEX court_case_created_ts_idx ON court_case (created_ts, case_closed);
 
 update user_account
