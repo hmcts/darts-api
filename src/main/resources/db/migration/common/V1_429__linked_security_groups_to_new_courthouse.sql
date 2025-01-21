45d6e722742e387c25d6505c94ee8f69a361bd51
benedwards <ben.edwards@cgi.com>
2025-01-21 10:52:41 +0000

    Added security groups + linked court houses to security groups

---
+++ src/main/resources/db/migration/common/V1_429__linked_security_groups_to_new_courthouse.sql
@@ -0,0 +1,15 @@
+insert into security_group (grp_id, rol_id, global_access, display_state, display_name, dm_group_s_object_id, group_name, use_interpreter,
+                            created_ts, created_by, last_modified_ts, last_modified_by)
+VALUES (nextval('grp_seq'), 3, false, true, 'Burnley Mags Two Approver', '121707588000cd01', 'moj_ch_burnley_mags_two_appr', false,
+        current_timestamp, 0, current_timestamp,0),
+       (nextval('grp_seq'), 2, false, true, 'Burnley Mags Two Requester', '121707588000cd00', 'moj_ch_burnley_mags_two_staff', false,
+        current_timestamp, 0, current_timestamp,0);
+
+
+insert into security_group_courthouse_ae(grp_id, cth_id)
+values ((select grp_id from security_group where group_name = 'moj_ch_burnley_mags_two_appr'),
+        (select cth_id from courthouse where courthouse_name = 'BURNLEY MAGS TWO')),
+       ((select grp_id from security_group where group_name = 'moj_ch_burnley_mags_two_staff'),
+        (select cth_id from courthouse where courthouse_name = 'BURNLEY MAGS TWO')),
+       ((select grp_id from security_group where group_name = 'tc_martinwalshcherer'),
+        (select cth_id from courthouse where courthouse_name = 'BURNLEY MAGS TWO'));
