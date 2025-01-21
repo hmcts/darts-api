fd1e27252812fdda4d6bec967ee468dd9c408ec0
benedwards <ben.edwards@cgi.com>
2025-01-21 16:44:01 +0000

    Review comments

--- src/main/resources/db/migration/common/V1_429__added_role_MEDIA_IN_PERPETUITY.sql
+++ src/main/resources/db/migration/common/V1_429__added_role_MEDIA_IN_PERPETUITY.sql
@@ -4,7 +4,7 @@ values (nextval('rol_seq'), 'MEDIA_IN_PERPETUITY', 'Media in Perpetuity', TRUE);
 
 update security_group
 set rol_id = (select rol_id from security_role where role_name = 'MEDIA_IN_PERPETUITY')
-where grp_id = 7;
+where grp_id = (select grp_id from security_group where group_name = 'MEDIA_IN_PERPETUITY');
 
 update security_role
 set display_state = FALSE
