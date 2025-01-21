3e0a2c05c4fd103256f1a9325efcd03149af801b
benedwards <ben.edwards@cgi.com>
2025-01-21 10:58:05 +0000

    Added security groups + linked court houses to security groups

--- src/main/resources/db/migration/common/V1_429__linked_security_groups_to_new_courthouse.sql
+++ src/main/resources/db/migration/common/V1_429__linked_security_groups_to_new_courthouse.sql
@@ -1,9 +1,9 @@
-insert into security_group (grp_id, rol_id, global_access, display_state, display_name, dm_group_s_object_id, group_name, use_interpreter,
+insert into security_group (grp_id, rol_id, global_access, display_state, display_name, dm_group_s_object_id, group_name, use_interpreter, group_display_name,
                             created_ts, created_by, last_modified_ts, last_modified_by)
-VALUES (nextval('grp_seq'), 3, false, true, 'Burnley Mags Two Approver', '121707588000cd01', 'moj_ch_burnley_mags_two_appr', false,
-        current_timestamp, 0, current_timestamp,0),
-       (nextval('grp_seq'), 2, false, true, 'Burnley Mags Two Requester', '121707588000cd00', 'moj_ch_burnley_mags_two_staff', false,
-        current_timestamp, 0, current_timestamp,0);
+VALUES (nextval('grp_seq'), 3, false, true, 'Burnley Mags Two Approver', '121707588000cd01', 'moj_ch_burnley_mags_two_appr', false, '0b1707589a44e76a_appr',
+        current_timestamp, 0, current_timestamp, 0),
+       (nextval('grp_seq'), 2, false, true, 'Burnley Mags Two Requester', '121707588000cd00', 'moj_ch_burnley_mags_two_staff', false, '0b1707589a44e76a_staff',
+        current_timestamp, 0, current_timestamp, 0);
 
 
 insert into security_group_courthouse_ae(grp_id, cth_id)
