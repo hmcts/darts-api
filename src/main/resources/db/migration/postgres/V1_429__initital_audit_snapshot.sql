d6cf45e4c2227e3543185f66cc7e7cdb7153b748
benedwards <ben.edwards@cgi.com>
2025-01-22 10:42:14 +0000

    Create an initial snapshot into the envers tables for the tables that modernised darts (not legacy) is responsible for

---
+++ src/main/resources/db/migration/postgres/V1_429__initital_audit_snapshot.sql
@@ -0,0 +1,102 @@
+-- Create one revinfo as all tables will be created at the same time, and there is no additional value in giving each table its own revinfo
+insert into revinfo (rev, revtstmp)
+values (nextval('revinfo_seq'), current_timestamp);
+
+
+insert into courthouse_aud
+select cth_id,
+       courthouse_code,
+       courthouse_name,
+       display_name,
+       currval('revinfo_seq'),
+       0 as revtype
+from courthouse;
+
+
+insert into courthouse_region_ae_aud
+select cth_id,
+       reg_id,
+       currval('revinfo_seq'),
+       0 as revtype
+from courthouse_region_ae;
+
+
+insert into security_group_aud
+select grp_id,
+       group_name,
+       display_name,
+       description,
+       currval('revinfo_seq'),
+       0 as revtype
+from security_group;
+
+
+insert into security_group_courthouse_ae_aud
+select cth_id,
+       grp_id,
+       currval('revinfo_seq'),
+       0 as revtype
+from security_group_courthouse_ae;
+
+insert into node_register_aud
+select node_id,
+       hostname,
+       ip_address,
+       mac_address,
+       node_type,
+       currval('revinfo_seq'),
+       0 as revtype
+from node_register;
+
+
+insert into event_handler_aud
+select evh_id,
+       event_type,
+       event_sub_type,
+       event_name,
+       handler,
+       active,
+       is_reporting_restriction,
+       currval('revinfo_seq'),
+       0 as revtype
+from event_handler;
+
+insert into retention_policy_type_aud
+select rtp_id,
+       fixed_policy_key,
+       policy_name,
+       duration,
+       display_name,
+       description,
+       policy_start_ts,
+       policy_end_ts,
+       currval('revinfo_seq'),
+       0 as revtype
+from retention_policy_type;
+
+
+
+insert into automated_task_aud
+select aut_id,
+       task_name,
+       task_description,
+       cron_expression,
+       cron_editable,
+       batch_size,
+       task_enabled,
+       currval('revinfo_seq'),
+       0 as revtype
+from automated_task;
+
+
+insert into arm_automated_task_aud
+select aat_id,
+       aut_id,
+       rpo_csv_start_hour,
+       rpo_csv_end_hour,
+       arm_replay_start_ts,
+       arm_replay_end_ts,
+       arm_attribute_type,
+       currval('revinfo_seq'),
+       0 as revtype
+from arm_automated_task;
