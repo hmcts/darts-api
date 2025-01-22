1b7a5a0f41f76ff30a94654f42d76eeb07f0fba8
benedwards <ben.edwards@cgi.com>
2025-01-22 10:46:55 +0000

    Updated migration

--- src/main/resources/db/migration/postgres/V1_429__initital_audit_snapshot.sql
+++ src/main/resources/db/migration/postgres/V1_429__initital_audit_snapshot.sql
@@ -1,6 +1,6 @@
 -- Create one revinfo as all tables will be created at the same time, and there is no additional value in giving each table its own revinfo
 insert into revinfo (rev, revtstmp)
-values (nextval('revinfo_seq'), current_timestamp);
+values (nextval('revinfo_seq'), EXTRACT(EPOCH FROM NOW()));
 
 
 insert into courthouse_aud
@@ -12,14 +12,12 @@ select cth_id,
        0 as revtype
 from courthouse;
 
-
 insert into courthouse_region_ae_aud
 select cth_id,
        reg_id,
        currval('revinfo_seq'),
        0 as revtype
 from courthouse_region_ae;
-
 
 insert into security_group_aud
 select grp_id,
@@ -29,7 +27,6 @@ select grp_id,
        currval('revinfo_seq'),
        0 as revtype
 from security_group;
-
 
 insert into security_group_courthouse_ae_aud
 select cth_id,
@@ -48,7 +45,6 @@ select node_id,
        0 as revtype
 from node_register;
 
-
 insert into event_handler_aud
 select evh_id,
        event_type,
@@ -56,13 +52,13 @@ select evh_id,
        event_name,
        handler,
        active,
-       is_reporting_restriction,
        currval('revinfo_seq'),
+       is_reporting_restriction,
        0 as revtype
 from event_handler;
 
 insert into retention_policy_type_aud
-select rtp_id,
+select rpt_id,
        fixed_policy_key,
        policy_name,
        duration,
@@ -73,8 +69,6 @@ select rtp_id,
        currval('revinfo_seq'),
        0 as revtype
 from retention_policy_type;
-
-
 
 insert into automated_task_aud
 select aut_id,
@@ -87,7 +81,6 @@ select aut_id,
        currval('revinfo_seq'),
        0 as revtype
 from automated_task;
-
 
 insert into arm_automated_task_aud
 select aat_id,
