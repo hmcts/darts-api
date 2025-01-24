ea2f28dacc5ee1f62b3b1781164540e97986b13b
karen-hedges <133129444+karen-hedges@users.noreply.github.com>
2025-01-24 12:43:50 +0000

    DMP-4655 ARM RPO Amendments
    Fixed broken test

--- src/main/java/uk/gov/hmcts/darts/arm/util/ArmRpoUtil.java
+++ src/main/java/uk/gov/hmcts/darts/arm/util/ArmRpoUtil.java
@@ -2,12 +2,14 @@ package uk.gov.hmcts.darts.arm.util;
 
 import org.springframework.stereotype.Component;
 
+import java.util.UUID;
+
 @Component
 public class ArmRpoUtil {
 
     private static final String CREATE_EXPORT_CSV_EXTENSION = "_CSV";
 
     public String generateUniqueProductionName(String productionName) {
-        return productionName + "_" + CREATE_EXPORT_CSV_EXTENSION;
+        return productionName + "_" + UUID.randomUUID().toString() + CREATE_EXPORT_CSV_EXTENSION;
     }
 }
