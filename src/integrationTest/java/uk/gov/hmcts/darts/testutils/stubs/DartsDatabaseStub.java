db578c37a83e486ef8dec36a29d251cd3496a956
benedwards <ben.edwards@cgi.com>
2025-01-28 16:15:02 +0000

    Updated database clear down to take into account revinfo users

--- src/integrationTest/java/uk/gov/hmcts/darts/testutils/stubs/DartsDatabaseStub.java
+++ src/integrationTest/java/uk/gov/hmcts/darts/testutils/stubs/DartsDatabaseStub.java
@@ -272,7 +272,7 @@ public class DartsDatabaseStub {
         );
 
 
-        entityManager.createNativeQuery("UPDATE darts.revinfo set audit_user = null where audit_user > " + SEQUENCE_START_VALUE).executeUpdate();
+        entityManager.createNativeQuery("UPDATE darts.revinfo set audit_user = null where audit_user >= " + SEQUENCE_START_VALUE).executeUpdate();
         userAccountRepository.deleteAll(
             userAccountRepository.findByIdGreaterThanEqual(SEQUENCE_START_VALUE)
         );
