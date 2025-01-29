c802e444eeef6a452eb6804abcc56473b1949bfd
Ben Edwards <147524406+Ben-Edwards-cgi@users.noreply.github.com>
2025-01-29 14:11:05 +0000

    Update V1_434__corrections_to_courthouse_data.sql

--- src/main/resources/db/migration/common/V1_434__corrections_to_courthouse_data.sql
+++ src/main/resources/db/migration/common/V1_434__corrections_to_courthouse_data.sql
@@ -10,14 +10,6 @@ set cth_id = (select cth_id from courthouse where courthouse_name = 'RCJROLLSCH3
 where grp_id in (select grp_id from security_group where group_name = 'RCJ Rolls All Buildings Approver')
   and cth_id = (select cth_id from courthouse where courthouse_name = 'PRESTON [THE SESSIONS HOUSE]');
 update security_group_courthouse_ae
-set cth_id = (select cth_id from courthouse where courthouse_name = 'RCJROLLSCH2')
-where grp_id in (select grp_id from security_group where group_name = 'RCJ Rolls All Buildings Approver')
-  and cth_id = (select cth_id from courthouse where courthouse_name = 'RCJROLLSCH2');
-update security_group_courthouse_ae
-set cth_id = (select cth_id from courthouse where courthouse_name = 'RCJROLLSCH1')
-where grp_id in (select grp_id from security_group where group_name = 'RCJ Rolls All Buildings Approver')
-  and cth_id = (select cth_id from courthouse where courthouse_name = 'RCJROLLSCH1');
-update security_group_courthouse_ae
 set cth_id = (select cth_id from courthouse where courthouse_name = 'RCJROLLSCH4')
 where grp_id in (select grp_id from security_group where group_name = 'RCJ Rolls All Buildings Approver')
   and cth_id = (select cth_id from courthouse where courthouse_name = 'PROSPERO HOUSE');
@@ -29,11 +21,3 @@ update security_group_courthouse_ae
 set cth_id = (select cth_id from courthouse where courthouse_name = 'RCJROLLSCH4')
 where grp_id in (select grp_id from security_group where group_name = 'RCJ Rolls All Buildings Requester')
   and cth_id = (select cth_id from courthouse where courthouse_name = 'PROSPERO HOUSE');
-update security_group_courthouse_ae
-set cth_id = (select cth_id from courthouse where courthouse_name = 'RCJROLLSCH1')
-where grp_id in (select grp_id from security_group where group_name = 'RCJ Rolls All Buildings Requester')
-  and cth_id = (select cth_id from courthouse where courthouse_name = 'RCJROLLSCH1');
-update security_group_courthouse_ae
-set cth_id = (select cth_id from courthouse where courthouse_name = 'RCJROLLSCH2')
-where grp_id in (select grp_id from security_group where group_name = 'RCJ Rolls All Buildings Requester')
-  and cth_id = (select cth_id from courthouse where courthouse_name = 'RCJROLLSCH2');
