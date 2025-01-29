cc7960875957b1448005b3767a82e48de3a1de66
benedwards <ben.edwards@cgi.com>
2025-01-29 12:47:05 +0000

    Corrections to courthouse data

---
+++ src/main/resources/db/migration/common/V1_434__corrections_to_courthouse_data.sql
@@ -0,0 +1,39 @@
+update security_group_courthouse_ae
+set cth_id = (select cth_id from courthouse where courthouse_name = 'ST ALBANS MAGISTRATES COURT')
+where grp_id in (select grp_id from security_group where group_name in ('moj_ch_crown_court_si1_appr', 'moj_ch_crown_court_si1_staff'))
+  and cth_id = (select cth_id from courthouse where courthouse_name = 'ST ALBANS');
+
+
+
+update security_group_courthouse_ae
+set cth_id = (select cth_id from courthouse where courthouse_name = 'RCJROLLSCH3')
+where grp_id in (select grp_id from security_group where group_name = 'RCJ Rolls All Buildings Approver')
+  and cth_id = (select cth_id from courthouse where courthouse_name = 'PRESTON [THE SESSIONS HOUSE]');
+update security_group_courthouse_ae
+set cth_id = (select cth_id from courthouse where courthouse_name = 'RCJROLLSCH2')
+where grp_id in (select grp_id from security_group where group_name = 'RCJ Rolls All Buildings Approver')
+  and cth_id = (select cth_id from courthouse where courthouse_name = 'RCJROLLSCH2');
+update security_group_courthouse_ae
+set cth_id = (select cth_id from courthouse where courthouse_name = 'RCJROLLSCH1')
+where grp_id in (select grp_id from security_group where group_name = 'RCJ Rolls All Buildings Approver')
+  and cth_id = (select cth_id from courthouse where courthouse_name = 'RCJROLLSCH1');
+update security_group_courthouse_ae
+set cth_id = (select cth_id from courthouse where courthouse_name = 'RCJROLLSCH4')
+where grp_id in (select grp_id from security_group where group_name = 'RCJ Rolls All Buildings Approver')
+  and cth_id = (select cth_id from courthouse where courthouse_name = 'PROSPERO HOUSE');
+update security_group_courthouse_ae
+set cth_id = (select cth_id from courthouse where courthouse_name = 'RCJROLLSCH3')
+where grp_id in (select grp_id from security_group where group_name = 'RCJ Rolls All Buildings Requester')
+  and cth_id = (select cth_id from courthouse where courthouse_name = 'PRESTON [THE SESSIONS HOUSE]');
+update security_group_courthouse_ae
+set cth_id = (select cth_id from courthouse where courthouse_name = 'RCJROLLSCH4')
+where grp_id in (select grp_id from security_group where group_name = 'RCJ Rolls All Buildings Requester')
+  and cth_id = (select cth_id from courthouse where courthouse_name = 'PROSPERO HOUSE');
+update security_group_courthouse_ae
+set cth_id = (select cth_id from courthouse where courthouse_name = 'RCJROLLSCH1')
+where grp_id in (select grp_id from security_group where group_name = 'RCJ Rolls All Buildings Requester')
+  and cth_id = (select cth_id from courthouse where courthouse_name = 'RCJROLLSCH1');
+update security_group_courthouse_ae
+set cth_id = (select cth_id from courthouse where courthouse_name = 'RCJROLLSCH2')
+where grp_id in (select grp_id from security_group where group_name = 'RCJ Rolls All Buildings Requester')
+  and cth_id = (select cth_id from courthouse where courthouse_name = 'RCJROLLSCH2');
