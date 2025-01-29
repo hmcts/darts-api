8c7922dedda0c1bca81414e7f75f4cf83a2c492f
benedwards <ben.edwards@cgi.com>
2025-01-29 12:19:59 +0000

    Updated name mapper

--- src/main/java/uk/gov/hmcts/darts/dailylist/mapper/CitizenNameMapper.java
+++ src/main/java/uk/gov/hmcts/darts/dailylist/mapper/CitizenNameMapper.java
@@ -2,9 +2,14 @@ package uk.gov.hmcts.darts.dailylist.mapper;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.darts.dailylist.model.CitizenName;
 import uk.gov.hmcts.darts.util.DataUtil;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Component
 @RequiredArgsConstructor
@@ -14,13 +19,14 @@ public class CitizenNameMapper {
     private static final String NAME_DELIMITER = " ";
 
     public String getCitizenName(CitizenName citizenName) {
-        String returnName = "";
-        if (citizenName.getCitizenNameForename() != null) {
-            returnName = DataUtil.trim(citizenName.getCitizenNameForename());
-            if (citizenName.getCitizenNameSurname() != null && !citizenName.getCitizenNameSurname().isEmpty()) {
-                returnName = returnName + NAME_DELIMITER + DataUtil.trim(citizenName.getCitizenNameSurname());
-            }
-        }
-        return DataUtil.trim(returnName);
+        List<String> nameSegments = new ArrayList<>();
+        nameSegments.add(citizenName.getCitizenNameForename());
+        nameSegments.add(citizenName.getCitizenNameSurname());
+
+        return DataUtil.trim(
+            nameSegments.stream()
+                .map(s -> DataUtil.trim(s))
+                .filter(s -> StringUtils.isNotBlank(s))
+                .collect(Collectors.joining(NAME_DELIMITER)));
     }
 }
--- src/test/java/uk/gov/hmcts/darts/dailylist/mapper/CitizenMapperTest.java
+++ src/test/java/uk/gov/hmcts/darts/dailylist/mapper/CitizenMapperTest.java
@@ -2,6 +2,8 @@ package uk.gov.hmcts.darts.dailylist.mapper;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
 import uk.gov.hmcts.darts.dailylist.model.CitizenName;
 
 class CitizenMapperTest {
@@ -47,5 +49,25 @@ class CitizenMapperTest {
         citizenName.setCitizenNameForename("  John  ");
         citizenName.setCitizenNameSurname("  Smith  ");
         Assertions.assertEquals("John Smith", mapper.getCitizenName(citizenName));
+    }
+
+    @ParameterizedTest
+    @NullAndEmptySource
+    void getCitizenName_missingSurename(String surename) {
+        CitizenNameMapper mapper = new CitizenNameMapper();
+        CitizenName citizenName = new CitizenName();
+        citizenName.setCitizenNameForename("  John  ");
+        citizenName.setCitizenNameSurname(surename);
+        Assertions.assertEquals("John", mapper.getCitizenName(citizenName));
+    }
+
+    @ParameterizedTest
+    @NullAndEmptySource
+    void getCitizenName_missingForename(String forename) {
+        CitizenNameMapper mapper = new CitizenNameMapper();
+        CitizenName citizenName = new CitizenName();
+        citizenName.setCitizenNameForename(forename);
+        citizenName.setCitizenNameSurname("  Smith  ");
+        Assertions.assertEquals("Smith", mapper.getCitizenName(citizenName));
     }
 }
