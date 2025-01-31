d703ad21eeff9e0ce127431ae656019d2011fceb
benedwards <ben.edwards@cgi.com>
2025-01-31 18:17:07 +0000

    Updated test

--- src/integrationTest/java/uk/gov/hmcts/darts/audit/service/AuditorRevisionListenerIntTest.java
+++ src/integrationTest/java/uk/gov/hmcts/darts/audit/service/AuditorRevisionListenerIntTest.java
@@ -3,6 +3,7 @@ package uk.gov.hmcts.darts.audit.service;
 import org.hibernate.SessionFactory;
 import org.hibernate.envers.AuditReader;
 import org.hibernate.envers.AuditReaderFactory;
+import org.hibernate.envers.query.AuditEntity;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import uk.gov.hmcts.darts.audit.model.RevisionInfo;
@@ -10,6 +11,7 @@ import uk.gov.hmcts.darts.common.entity.UserAccountEntity;
 import uk.gov.hmcts.darts.testutils.GivenBuilder;
 import uk.gov.hmcts.darts.testutils.PostgresIntegrationBase;
 import uk.gov.hmcts.darts.usermanagement.model.User;
+import uk.gov.hmcts.darts.usermanagement.model.UserPatch;
 import uk.gov.hmcts.darts.usermanagement.model.UserWithId;
 import uk.gov.hmcts.darts.usermanagement.service.UserManagementService;
 
@@ -17,7 +19,7 @@ import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-class AuditorRevisionListenerITest extends PostgresIntegrationBase {
+class AuditorRevisionListenerIntTest extends PostgresIntegrationBase {
 
     @Autowired
     private UserManagementService userManagementService;
@@ -36,19 +38,34 @@ class AuditorRevisionListenerITest extends PostgresIntegrationBase {
 
         UserWithId createdUser = userManagementService.createUser(user);
 
-
         assertThat(createdUser).isNotNull();
         assertThat(createdUser.getEmailAddress()).isEqualTo("someemail@email.com");
         assertThat(createdUser.getFullName()).isEqualTo("Some Name");
         assertThat(createdUser.getDescription()).isEqualTo("Some Description");
 
+
+        UserAccountEntity modifiedUser = dartsDatabase.getUserAccountStub().createSuperAdminUser();
+        GivenBuilder.anAuthenticatedUserFor(modifiedUser);
+        UserPatch userPatch = new UserPatch();
+        userPatch.setFullName("Some Other Name");
+        userManagementService.modifyUser(createdUser.getId(), userPatch);
+
         AuditReader reader = AuditReaderFactory.get(sessionFactory.createEntityManager());
 
-        List<RevisionInfo> audits = reader.createQuery()
-            .forRevisionsOfEntity(UserAccountEntity.class, true)
+        List<Object[]> audits = reader.createQuery()
+            .forRevisionsOfEntity(UserAccountEntity.class, false, true)
+            .add(AuditEntity.property("id").eq(createdUser.getId()))
             .getResultList();
 
-        assertThat(audits.get(0).getAuditUser()).isNull();//Created without user loggedin so should be null
-        assertThat(audits.get(audits.size() - 1).getAuditUser()).isEqualTo(currentUser.getId());//Updated with user loggedin so should be current user
+        assertThat(audits).hasSize(2);
+        Object[] createAudit = audits.get(0);
+        Object[] editAudit = audits.get(1);
+
+        RevisionInfo createAuditRevisionInfo = (RevisionInfo) createAudit[1];
+        assertThat(createAuditRevisionInfo.getAuditUser()).isEqualTo(currentUser.getId());
+
+        RevisionInfo editAuditRevisionInfo = (RevisionInfo) editAudit[1];
+        assertThat(editAuditRevisionInfo.getAuditUser()).isEqualTo(modifiedUser.getId());
+
     }
 }
