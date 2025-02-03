def37dcefefe83619a0348555117282687111166
Ben Edwards <147524406+Ben-Edwards-cgi@users.noreply.github.com>
2025-02-03 18:21:59 +0000

    DMP-4671: Add audio file size log entry for Dynatrace (#2513)

--- src/test/java/uk/gov/hmcts/darts/util/DurationUtilTest.java
+++ src/test/java/uk/gov/hmcts/darts/util/DurationUtilTest.java
@@ -1,6 +1,7 @@
 package uk.gov.hmcts.darts.util;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -9,6 +10,8 @@ import java.time.Duration;
 import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @DisplayName("DurationUtil Tests")
 class DurationUtilTest {
@@ -31,5 +34,29 @@ class DurationUtilTest {
         builder.add(Arguments.of(Duration.ofHours(24), "1 day"));
         builder.add(Arguments.of(Duration.ofHours(25), "1 day 1 hour"));
         return builder.build();
+    }
+
+    @Test
+    void greaterThan_shouldReturnFalse_whenDuration1IsLessThanDuration2() {
+        Duration duration1 = Duration.ofSeconds(1);
+        Duration duration2 = Duration.ofSeconds(2);
+        boolean result = DurationUtil.greaterThan(duration1, duration2);
+        assertFalse(result);
+    }
+
+    @Test
+    void greaterThan_shouldReturnTrue_whenDuration1IsGreaterThanDuration2() {
+        Duration duration1 = Duration.ofSeconds(2);
+        Duration duration2 = Duration.ofSeconds(1);
+        boolean result = DurationUtil.greaterThan(duration1, duration2);
+        assertTrue(result);
+    }
+
+    @Test
+    void greaterThan_shouldReturnFalse_whenDuration1IsEqualToDuration2() {
+        Duration duration1 = Duration.ofSeconds(1);
+        Duration duration2 = Duration.ofSeconds(1);
+        boolean result = DurationUtil.greaterThan(duration1, duration2);
+        assertFalse(result);
     }
 }
