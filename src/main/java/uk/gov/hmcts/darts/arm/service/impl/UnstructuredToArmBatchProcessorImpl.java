dbbf6ac6b3f95cc78f059b7b6693e2f49b438956
karen-hedges <133129444+karen-hedges@users.noreply.github.com>
2025-02-04 12:19:33 +0000

    DMP-4647 Implement batching in ArmRpoReplay automated task
    Added batching to ArmRpoReplay automated task. Put code in seperate service class so transactional does not effect the auditing of automated tasks

--- src/main/java/uk/gov/hmcts/darts/arm/service/impl/UnstructuredToArmBatchProcessorImpl.java
+++ src/main/java/uk/gov/hmcts/darts/arm/service/impl/UnstructuredToArmBatchProcessorImpl.java
@@ -136,8 +136,6 @@ public class UnstructuredToArmBatchProcessorImpl implements UnstructuredToArmBat
             if (batchItem.isRawFilePushNotNeededOrSuccessfulWhenNeeded() && batchItem.getArchiveRecord() != null) {
                 unstructuredToArmHelper.updateExternalObjectDirectoryStatus(batchItem.getArmEod(), EodHelper.armDropZoneStatus(), userAccount);
                 logApi.armPushSuccessful(batchItem.getArmEod().getId());
-            } else {
-                //set manifest file back to null if it was set
             }
         }
     }
