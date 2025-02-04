5b636c88718f356312af5557756e884b8be0b11b
karen-hedges <133129444+karen-hedges@users.noreply.github.com>
2025-02-04 11:56:23 +0000

    DMP-4705 Fix issue with manifest file name being saved when raw data push fails
    Added code to set the eod manifest file to null if the push fails

--- src/main/java/uk/gov/hmcts/darts/arm/service/impl/UnstructuredToArmBatchProcessorImpl.java
+++ src/main/java/uk/gov/hmcts/darts/arm/service/impl/UnstructuredToArmBatchProcessorImpl.java
@@ -137,7 +137,6 @@ public class UnstructuredToArmBatchProcessorImpl implements UnstructuredToArmBat
                 unstructuredToArmHelper.updateExternalObjectDirectoryStatus(batchItem.getArmEod(), EodHelper.armDropZoneStatus(), userAccount);
                 logApi.armPushSuccessful(batchItem.getArmEod().getId());
             } else {
-                batchItem.getArmEod().setManifestFile(null);
                 recoverByUpdatingEodToFailedArmStatus(batchItem, userAccount);
             }
         }
