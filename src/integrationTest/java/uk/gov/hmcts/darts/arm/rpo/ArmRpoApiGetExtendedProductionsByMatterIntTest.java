eb52b4b968a4f15e79f03d86988ac0d120adfab5
karen-hedges <133129444+karen-hedges@users.noreply.github.com>
2025-02-06 15:45:24 +0000

    DMP-4613-IntegrationTests
    Integration tests for DMP-4613

--- src/integrationTest/java/uk/gov/hmcts/darts/arm/rpo/ArmRpoApiGetExtendedProductionsByMatterIntTest.java
+++ src/integrationTest/java/uk/gov/hmcts/darts/arm/rpo/ArmRpoApiGetExtendedProductionsByMatterIntTest.java
@@ -1,11 +1,12 @@
 package uk.gov.hmcts.darts.arm.rpo;
 
+import org.jetbrains.annotations.NotNull;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import uk.gov.hmcts.darts.arm.client.ArmRpoClient;
 import uk.gov.hmcts.darts.arm.client.model.rpo.ExtendedProductionsByMatterResponse;
-import uk.gov.hmcts.darts.arm.config.ArmApiConfigurationProperties;
 import uk.gov.hmcts.darts.common.entity.ArmRpoExecutionDetailEntity;
 import uk.gov.hmcts.darts.common.entity.UserAccountEntity;
 import uk.gov.hmcts.darts.common.enums.ArmRpoStateEnum;
@@ -24,35 +25,36 @@ class ArmRpoApiGetExtendedProductionsByMatterIntTest extends IntegrationBase {
 
     private static final String PRODUCTION_NAME = "DARTS_RPO_2024-08-13";
     public static final String END_PRODUCTION_TIME = "2025-01-16T12:30:09.9129726+00:00";
-    @MockitoBean
-    private ArmRpoClient armRpoClient;
 
     @MockitoBean
-    private ArmApiConfigurationProperties armApiConfigurationProperties;
+    private ArmRpoClient armRpoClient;
 
     @Autowired
     private ArmRpoApi armRpoApi;
 
+    private UserAccountEntity userAccount;
+    private ArmRpoExecutionDetailEntity armRpoExecutionDetail;
+
+    @BeforeEach
+    void setup() {
+        userAccount = dartsDatabase.getUserAccountStub().getIntegrationTestUserAccountEntity();
+        ArmRpoExecutionDetailEntity armRpoExecutionDetailEntity = new ArmRpoExecutionDetailEntity();
+        armRpoExecutionDetailEntity.setMatterId("1");
+        armRpoExecutionDetailEntity.setCreatedBy(userAccount);
+        armRpoExecutionDetailEntity.setLastModifiedBy(userAccount);
+        armRpoExecutionDetail = dartsPersistence.save(armRpoExecutionDetailEntity);
+    }
+
     @Test
-    void getExtendedSearchesByMatter_ReturnsTrue() {
+    void getExtendedProductionsByMatter_ReturnsTrue() {
         // given
-        ExtendedProductionsByMatterResponse extendedProductionsByMatterResponse = new ExtendedProductionsByMatterResponse();
-        extendedProductionsByMatterResponse.setStatus(200);
-        extendedProductionsByMatterResponse.setIsError(false);
-        ExtendedProductionsByMatterResponse.Productions productions = new ExtendedProductionsByMatterResponse.Productions();
-        productions.setProductionId("1234");
-        productions.setName(PRODUCTION_NAME);
+        ExtendedProductionsByMatterResponse extendedProductionsByMatterResponse = getExtendedProductionsByMatterResponse();
+        ExtendedProductionsByMatterResponse.Productions productions = getProductions();
         productions.setEndProductionTime(END_PRODUCTION_TIME);
         extendedProductionsByMatterResponse.setProductions(List.of(productions));
 
         when(armRpoClient.getExtendedProductionsByMatter(any(), any())).thenReturn(extendedProductionsByMatterResponse);
 
-        UserAccountEntity userAccount = dartsDatabase.getUserAccountStub().getIntegrationTestUserAccountEntity();
-        ArmRpoExecutionDetailEntity armRpoExecutionDetailEntity = new ArmRpoExecutionDetailEntity();
-        armRpoExecutionDetailEntity.setMatterId("1");
-        armRpoExecutionDetailEntity.setCreatedBy(userAccount);
-        armRpoExecutionDetailEntity.setLastModifiedBy(userAccount);
-        var armRpoExecutionDetail = dartsPersistence.save(armRpoExecutionDetailEntity);
         var bearerAuth = "Bearer some-token";
 
         // when
@@ -68,24 +70,14 @@ class ArmRpoApiGetExtendedProductionsByMatterIntTest extends IntegrationBase {
     }
 
     @Test
-    void getExtendedSearchesByMatter_ReturnsFalse_WhenEndProductionTimeIsNullInProgress() {
+    void getExtendedProductionsByMatter_ReturnsFalseForInProgress_WhenEndProductionTimeIsNull() {
         // given
-        ExtendedProductionsByMatterResponse extendedProductionsByMatterResponse = new ExtendedProductionsByMatterResponse();
-        extendedProductionsByMatterResponse.setStatus(200);
-        extendedProductionsByMatterResponse.setIsError(false);
-        ExtendedProductionsByMatterResponse.Productions productions = new ExtendedProductionsByMatterResponse.Productions();
-        productions.setProductionId("1234");
-        productions.setName(PRODUCTION_NAME);
+        ExtendedProductionsByMatterResponse extendedProductionsByMatterResponse = getExtendedProductionsByMatterResponse();
+        ExtendedProductionsByMatterResponse.Productions productions = getProductions();
         extendedProductionsByMatterResponse.setProductions(List.of(productions));
 
         when(armRpoClient.getExtendedProductionsByMatter(any(), any())).thenReturn(extendedProductionsByMatterResponse);
 
-        UserAccountEntity userAccount = dartsDatabase.getUserAccountStub().getIntegrationTestUserAccountEntity();
-        ArmRpoExecutionDetailEntity armRpoExecutionDetailEntity = new ArmRpoExecutionDetailEntity();
-        armRpoExecutionDetailEntity.setMatterId("1");
-        armRpoExecutionDetailEntity.setCreatedBy(userAccount);
-        armRpoExecutionDetailEntity.setLastModifiedBy(userAccount);
-        var armRpoExecutionDetail = dartsPersistence.save(armRpoExecutionDetailEntity);
         var bearerAuth = "Bearer some-token";
 
         // when
@@ -97,5 +89,19 @@ class ArmRpoApiGetExtendedProductionsByMatterIntTest extends IntegrationBase {
         assertEquals(ArmRpoStateEnum.GET_EXTENDED_PRODUCTIONS_BY_MATTER.getId(), armRpoExecutionDetailEntityUpdated.getArmRpoState().getId());
         assertEquals(ArmRpoStatusEnum.IN_PROGRESS.getId(), armRpoExecutionDetailEntityUpdated.getArmRpoStatus().getId());
 
+    }
+
+    private static @NotNull ExtendedProductionsByMatterResponse getExtendedProductionsByMatterResponse() {
+        ExtendedProductionsByMatterResponse extendedProductionsByMatterResponse = new ExtendedProductionsByMatterResponse();
+        extendedProductionsByMatterResponse.setStatus(200);
+        extendedProductionsByMatterResponse.setIsError(false);
+        return extendedProductionsByMatterResponse;
+    }
+
+    private static ExtendedProductionsByMatterResponse.@NotNull Productions getProductions() {
+        ExtendedProductionsByMatterResponse.Productions productions = new ExtendedProductionsByMatterResponse.Productions();
+        productions.setProductionId("1234");
+        productions.setName(PRODUCTION_NAME);
+        return productions;
     }
 }
