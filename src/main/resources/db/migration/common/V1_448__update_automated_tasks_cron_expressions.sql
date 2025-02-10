02a52adf6908b74f53b19cd7bdac68fe02c6f26f
karen-hedges <133129444+karen-hedges@users.noreply.github.com>
2025-02-10 11:15:26 +0000

    DMP-4735 Correct cron expression for tasks that run multiple times per second
    Updated cron expressions that result in jobs attempting to run multiple times every second

--- src/main/resources/db/migration/common/V1_448__update_automated_tasks_cron_expressions.sql
+++ src/main/resources/db/migration/common/V1_448__update_automated_tasks_cron_expressions.sql
@@ -1,2 +1,2 @@
-update darts.automated_task set cron_expression = '0 0/5 * ? * *' where task_name = 'InboundToUnstructuredDataStore';
-update darts.automated_task set cron_expression = '0 44 * ? * *' where task_name = 'ProcessArmResponseFiles';
+update automated_task set cron_expression = '0 0/5 * ? * *' where task_name = 'InboundToUnstructuredDataStore';
+update automated_task set cron_expression = '0 44 * ? * *' where task_name = 'ProcessArmResponseFiles';
