94100d133acf0baa87ef574631fc53249b39ebe2
Jack Maloney <jack.maloney1@hmcts.net>
2025-02-14 13:45:19 +0000

    Test fix

--- src/integrationTest/java/uk/gov/hmcts/darts/event/controller/EventsControllerAdminGetVersionsByEventIdTest.java
+++ src/integrationTest/java/uk/gov/hmcts/darts/event/controller/EventsControllerAdminGetVersionsByEventIdTest.java
@@ -23,6 +23,7 @@ import java.time.ZoneOffset;
 import java.util.List;
 import java.util.Map;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -136,7 +137,7 @@ class EventsControllerAdminGetVersionsByEventIdTest extends IntegrationBase {
                                                                                          AdminGetVersionsByEventIdResponseResult.class);
 
         // Then
-        Assertions.assertNull(responseResult.getPreviousVersions());
+        assertThat(responseResult.getPreviousVersions()).hasSize(0);
 
         assertEquals(currentEventEntity.getId(), responseResult.getCurrentVersion().getId());
         assertEquals(currentEventEntity.getLegacyObjectId(), responseResult.getCurrentVersion().getDocumentumId());
