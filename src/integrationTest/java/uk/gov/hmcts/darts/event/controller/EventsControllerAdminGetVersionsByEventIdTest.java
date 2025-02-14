d7689caefb8cf18542015f69402c471d74a03d37
Jack Maloney <jack.maloney1@hmcts.net>
2025-02-14 13:58:41 +0000

    Remove import

--- src/integrationTest/java/uk/gov/hmcts/darts/event/controller/EventsControllerAdminGetVersionsByEventIdTest.java
+++ src/integrationTest/java/uk/gov/hmcts/darts/event/controller/EventsControllerAdminGetVersionsByEventIdTest.java
@@ -1,6 +1,5 @@
 package uk.gov.hmcts.darts.event.controller;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
