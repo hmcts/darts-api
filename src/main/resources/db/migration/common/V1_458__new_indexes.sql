c93641adba52063ab2d331e4bb9cb86a203deb20
benedwards <ben.edwards@cgi.com>
2025-02-18 09:47:41 +0000

    Updated index

--- src/main/resources/db/migration/common/V1_458__new_indexes.sql
+++ src/main/resources/db/migration/common/V1_458__new_indexes.sql
@@ -1,5 +1,5 @@
-CREATE INDEX dfd_dn_trgm_idx ON defendant USING gin (defendant_name gin_trgm_ops);
-CREATE INDEX eve_evt_trgm_idx ON event USING gin (event_text gin_trgm_ops);
-CREATE INDEX jud_jn_trgm_idx ON judge USING gin (judge_name gin_trgm_ops);
-CREATE INDEX cas_cn_trgm_idx ON court_case USING gin (case_number gin_trgm_ops);
-CREATE INDEX ctr_cn_trgm_idx ON courtroom USING gin (courtroom_name gin_trgm_ops);
+CREATE INDEX dfd_dn_trgm_idx ON defendant USING gin (defendant_name, gin_trgm_ops);
+CREATE INDEX eve_evt_trgm_idx ON event USING gin (event_text, gin_trgm_ops);
+CREATE INDEX jud_jn_trgm_idx ON judge USING gin (judge_name, gin_trgm_ops);
+CREATE INDEX cas_cn_trgm_idx ON court_case USING gin (case_number, gin_trgm_ops);
+CREATE INDEX ctr_cn_trgm_idx ON courtroom USING gin (courtroom_name, gin_trgm_ops);
