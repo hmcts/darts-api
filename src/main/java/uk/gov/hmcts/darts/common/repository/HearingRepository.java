2867fc123725e2c4f3773dbf1f1bc603e649328e
Ben Edwards <147524406+Ben-Edwards-cgi@users.noreply.github.com>
2025-02-18 11:04:17 +0000

    DMP-4754: When I search for cases I should get the results returned in a reasonable time (#2607)

--- src/main/java/uk/gov/hmcts/darts/common/repository/HearingRepository.java
+++ src/main/java/uk/gov/hmcts/darts/common/repository/HearingRepository.java
@@ -24,12 +24,20 @@ public interface HearingRepository extends JpaRepository<HearingEntity, Integer>
     List<HearingEntity> findByCourthouseCourtroomAndDate(String courthouse, String courtroom, LocalDate date);
 
     @Query("""
-        SELECT h FROM HearingEntity h, CourtCaseEntity case
-        WHERE case.id in :caseIds
-        AND h.courtCase = case
+        SELECT h FROM HearingEntity h
+        WHERE h.courtCase.id in :caseIds
         """
     )
     List<HearingEntity> findByCaseIds(List<Integer> caseIds);
+
+    @Query("""
+        SELECT h FROM HearingEntity h
+        WHERE h.courtCase.id in :caseIds
+        AND h.hearingIsActual = true
+        ORDER BY h.courtCase.caseNumber desc
+        """
+    )
+    List<HearingEntity> findByIsActualCaseIds(List<Integer> caseIds);
 
     @Query("""
         SELECT h.id FROM HearingEntity h
@@ -80,7 +88,7 @@ public interface HearingRepository extends JpaRepository<HearingEntity, Integer>
             AND (cast(:endDate as LocalDate) IS NULL OR hearing.hearingDate <= :endDate)
             ORDER BY hearing.hearingDate DESC
             LIMIT :numberOfRecords
-          """)
+        """)
     List<HearingEntity> findHearingDetails(List<Integer> courthouseIds, String caseNumber,
                                            String courtroomName,
                                            LocalDate startDate, LocalDate endDate, Integer numberOfRecords);
