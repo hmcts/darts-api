2b0e1b6cac423af63a77b2c133ea244ede16c10e
benedwards <ben.edwards@cgi.com>
2025-02-20 09:49:08 +0000

    Migration update

--- src/main/resources/db/migration/common/V1_459__eod_uuid_to_varchar.sql
+++ src/main/resources/db/migration/common/V1_459__eod_uuid_to_varchar.sql
@@ -3,7 +3,8 @@ alter table external_object_directory
 alter table transient_object_directory
     alter column external_location type CHARACTER VARYING;
 alter table daily_list
-    alter column external_location type CHARACTER VARYING,
+    alter column external_location type CHARACTER VARYING;
+alter table daily_list
     add column osr_uuid BIGINT;
 alter table object_state_record
     add column dal_id int;
