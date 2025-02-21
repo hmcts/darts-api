85333e9cc2632ddf7c7a16b094d814c21af7e3e1
Chris Bellingham <131763968+chrisbellingham-hmcts@users.noreply.github.com>
2025-02-21 16:26:47 +0000

    Safety improvements to migrate-dets.sh (#2581)

--- bin/migrate-dets.sh
+++ bin/migrate-dets.sh
@@ -22,14 +22,28 @@ if [ -z "$DATABASE_NAME" ]; then
   exit 1
 fi
 
+if [ "$(git symbolic-ref --short -q HEAD)" != "master" ]; then
+  echo "This script is only permitted to run on the master branch."
+  exit 1
+fi
+
+cd ..
+
+MIGRATION_SRC_DIR="src/main/resources/db/migration/"
+if [ -n "$(git status --porcelain ${MIGRATION_SRC_DIR})" ]; then
+  echo "Uncommitted changes exist in ${MIGRATION_SRC_DIR}, which is probably not intentional. Please check the state of your branch."
+  exit 1
+fi
+
+./gradlew clean assemble
+
 # Port 5433 is the local port that is mapped to the DB on the production bastion, per
 # https://tools.hmcts.net/confluence/display/DMP/Applying+Flyway+changes+to+Migration
 export FLYWAY_URL="jdbc:postgresql://localhost:5433/$DATABASE_NAME"
 
-cd ..
 ./gradlew flywayInfo
 
-read -p 'Execute flywayMigrate? (y/n): ' continue
+read -p "Execute flywayMigrate as user ${FLYWAY_USER}? (y/n): " continue
 if [ "$continue" != "y" ]
 then
   exit 1
