7a555a29783548af0eb0e4cdeecb6a19691ed155
karen-hedges <133129444+karen-hedges@users.noreply.github.com>
2025-02-27 09:03:58 +0000

    DMP-4215 Trim courthouse and courtroom on entry into DARTS
    Added trim to courthouse and courtroom name before attempting to find the entry in DARTS

--- src/main/java/uk/gov/hmcts/darts/common/service/impl/CaseCommonServiceImpl.java
+++ src/main/java/uk/gov/hmcts/darts/common/service/impl/CaseCommonServiceImpl.java
@@ -23,8 +23,8 @@ public class CaseCommonServiceImpl implements CaseCommonService {
     @Override
     @Transactional
     public CourtCaseEntity retrieveOrCreateCase(String courthouseName, String caseNumber, UserAccountEntity userAccount) {
-        String courthouseNameUC = StringUtils.toRootUpperCase(courthouseName);
-        Optional<CourtCaseEntity> foundCase = caseRepository.findByCaseNumberAndCourthouse_CourthouseName(caseNumber, courthouseNameUC);
+        String courthouseNameUpperTrimmed = StringUtils.toRootUpperCase(StringUtils.trimToNull(courthouseName));
+        Optional<CourtCaseEntity> foundCase = caseRepository.findByCaseNumberAndCourthouse_CourthouseName(caseNumber, courthouseNameUpperTrimmed);
         return foundCase
             .map(entity -> setCourtCaseLastDateModifiedBy(entity, userAccount))
             .orElseGet(() -> createCase(courthouseName, caseNumber, userAccount));
