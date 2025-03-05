22f915f0c71ac3345829e8f3733b42c7040d85d0
karen-hedges <133129444+karen-hedges@users.noreply.github.com>
2025-03-05 22:09:11 +0000

    DMP-4633 Review and resolve sonar issues with the master branch
    Fixed more sonar issues

--- src/test/java/uk/gov/hmcts/darts/arm/mapper/impl/MediaArchiveRecordMapperImplTest.java
+++ src/test/java/uk/gov/hmcts/darts/arm/mapper/impl/MediaArchiveRecordMapperImplTest.java
@@ -34,7 +34,7 @@ class MediaArchiveRecordMapperImplTest {
     @Mock
     private CurrentTimeHelper currentTimeHelper;
 
-    MockedStatic<PropertyFileLoader> propertyFileLoader;
+    private MockedStatic<PropertyFileLoader> propertyFileLoader;
 
     @BeforeEach
     void setUp() {
