fedb29d390aa36e21513af3b9c7b7612ccf33fe7
benedwards <ben.edwards@cgi.com>
2025-03-05 12:39:43 +0000

    Removed cache for ORS and ELT

--- src/main/java/uk/gov/hmcts/darts/common/repository/ExternalLocationTypeRepository.java
+++ src/main/java/uk/gov/hmcts/darts/common/repository/ExternalLocationTypeRepository.java
@@ -1,20 +1,11 @@
 package uk.gov.hmcts.darts.common.repository;
 
-import org.springframework.cache.annotation.Cacheable;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import uk.gov.hmcts.darts.common.entity.ExternalLocationTypeEntity;
 
-import java.util.Optional;
-
 @Repository
 public interface ExternalLocationTypeRepository extends JpaRepository<ExternalLocationTypeEntity, Integer> {
 
-    @Cacheable("externalLocationTypeEntity")
-    @Override
-    ExternalLocationTypeEntity getReferenceById(Integer id);
-
-    @Cacheable("externalLocationTypeEntityOptional")
-    @Override
-    Optional<ExternalLocationTypeEntity> findById(Integer id);
 }
