78d22d41551848a65430dbc72e2c61e577c1a71b
Ben Edwards <147524406+Ben-Edwards-cgi@users.noreply.github.com>
2025-03-05 12:44:43 +0000

    Update ExternalLocationTypeRepository.java

--- src/main/java/uk/gov/hmcts/darts/common/repository/ExternalLocationTypeRepository.java
+++ src/main/java/uk/gov/hmcts/darts/common/repository/ExternalLocationTypeRepository.java
@@ -1,6 +1,5 @@
 package uk.gov.hmcts.darts.common.repository;
 
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import uk.gov.hmcts.darts.common.entity.ExternalLocationTypeEntity;
