c52f8fb06e225627139224499c007c9996841e81
benedwards <ben.edwards@cgi.com>
2025-03-10 13:52:09 +0000

    Added new test

--- src/test/java/uk/gov/hmcts/darts/task/runner/impl/AbstractLockableAutomatedTaskTest.java
+++ src/test/java/uk/gov/hmcts/darts/task/runner/impl/AbstractLockableAutomatedTaskTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Timeout;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
+import org.mockito.MockedStatic;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.system.CapturedOutput;
 import org.springframework.boot.test.system.OutputCaptureExtension;
@@ -18,13 +19,20 @@ import uk.gov.hmcts.darts.util.LogUtil;
 import uk.gov.hmcts.darts.util.WaitUtil;
 
 import java.time.Duration;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.atLeastOnce;
 import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.mockStatic;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.verify;
@@ -149,6 +157,47 @@ public class AbstractLockableAutomatedTaskTest {
             verify(abstractLockableAutomatedTask, atLeastOnce()).setAutomatedTaskStatus(AutomatedTaskStatus.FAILED);
             verify(abstractLockableAutomatedTask).handleException(exception);
             verify(lockedTask).assertLocked();
+        }
+
+        @Test
+        //Timeout after 10 seconds to ensure the task is killed before completion
+        @Timeout(value = 10, unit = TimeUnit.SECONDS)
+        @SuppressWarnings({
+            "PMD.DoNotUseThreads", //Required to test timeout
+            "PMD.CloseResource"//ExecutorService does not need closing as it is a mock
+        })
+        void lockedTaskRun_shouldFail_whenTaskInterupted(CapturedOutput output) throws Exception {
+            try (MockedStatic<Executors> executorsmock = mockStatic(Executors.class)) {
+                ExecutorService executorService = mock(ExecutorService.class);
+                executorsmock.when(() -> Executors.newSingleThreadExecutor()).thenReturn(executorService);
+
+                Future<?> future = mock(Future.class);
+                doReturn(future).when(executorService).submit(any(Runnable.class));
+
+                doThrow(new InterruptedException()).when(future).get(anyLong(), any());
+
+                AbstractAutomatedTaskConfig.Lock lock = new AbstractAutomatedTaskConfig.Lock();
+                lock.setAtLeastFor(Duration.ofMillis(1));
+                lock.setAtMostFor(Duration.ofSeconds(10));
+                when(abstractAutomatedTaskConfig.getLock()).thenReturn(lock);
+
+
+                AbstractLockableAutomatedTask<AbstractAutomatedTaskConfig> abstractLockableAutomatedTask = spy(createAbstractLockableAutomatedTask(() -> {
+                }));
+
+                AbstractLockableAutomatedTask.LockedTask lockedTask = spy(abstractLockableAutomatedTask.createLockableTask());
+
+                doNothing().when(lockedTask).assertLocked();
+
+                lockedTask.run();
+
+                //Ensure task errors are logged correctly
+                LogUtil.assertOutputHasMessage(output, "Task: TEST_TASK interrupted", 10);
+                WaitUtil.waitFor(() -> abstractLockableAutomatedTask.getAutomatedTaskStatus().equals(AutomatedTaskStatus.FAILED),
+                                 "Timeout waiting for task status to be FAILED", 10);
+                verify(abstractLockableAutomatedTask, atLeastOnce()).setAutomatedTaskStatus(AutomatedTaskStatus.FAILED);
+                verify(lockedTask).assertLocked();
+            }
         }
 
 
