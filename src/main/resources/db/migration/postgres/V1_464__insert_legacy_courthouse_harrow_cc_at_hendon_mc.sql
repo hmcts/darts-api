13ba56176b869bc84bae4202bda2ed81650cac5d
karen-hedges <133129444+karen-hedges@users.noreply.github.com>
2025-03-13 15:25:37 +0000

    New Court (Harrow CC at Hendon MC) to be added to Modernised DARTS
    Added legacy courthouse Harrow CC at Hendon MC and associated security groups

--- src/main/resources/db/migration/postgres/V1_464__insert_legacy_courthouse_harrow_cc_at_hendon_mc.sql
+++ src/main/resources/db/migration/postgres/V1_464__insert_legacy_courthouse_harrow_cc_at_hendon_mc.sql
@@ -65,3 +65,31 @@ INSERT INTO security_group_courthouse_ae_aud
     SELECT grp_id, cth_id, currval('revinfo_seq'), 0 as revtype
     FROM security_group_courthouse_ae
     WHERE grp_id = (SELECT grp_id FROM darts.security_group WHERE group_name = 'moj_ch_harrow_cc_at_he_appr');
+
+
+INSERT INTO security_group_courthouse_ae (grp_id, cth_id)
+VALUES (
+    (SELECT grp_id FROM darts.security_group WHERE group_name = 'rasso_users'),
+    (SELECT cth_id FROM courthouse WHERE courthouse_name = 'HARROW CC AT HENDON MC'));
+
+INSERT INTO revinfo (rev, revtstmp, audit_user)
+    VALUES (nextval('revinfo_seq'), EXTRACT(EPOCH FROM NOW()), 0);
+
+INSERT INTO security_group_courthouse_ae_aud
+    SELECT grp_id, cth_id, currval('revinfo_seq'), 0 as revtype
+    FROM security_group_courthouse_ae
+    WHERE grp_id = (SELECT grp_id FROM darts.security_group WHERE group_name = 'rasso_users');
+
+
+INSERT INTO security_group_courthouse_ae (grp_id, cth_id)
+VALUES (
+    (SELECT grp_id FROM darts.security_group WHERE group_name = 'tc_ubiqus'),
+    (SELECT cth_id FROM courthouse WHERE courthouse_name = 'HARROW CC AT HENDON MC'));
+
+INSERT INTO revinfo (rev, revtstmp, audit_user)
+    VALUES (nextval('revinfo_seq'), EXTRACT(EPOCH FROM NOW()), 0);
+
+INSERT INTO security_group_courthouse_ae_aud
+    SELECT grp_id, cth_id, currval('revinfo_seq'), 0 as revtype
+    FROM security_group_courthouse_ae
+    WHERE grp_id = (SELECT grp_id FROM darts.security_group WHERE group_name = 'tc_ubiqus');
