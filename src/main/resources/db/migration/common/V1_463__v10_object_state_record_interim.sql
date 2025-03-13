8c33c97cbfd51f73f8e88ae46560aa2ecdaadcde
chris-bellingham <chris.bellingham@hmcts.net>
2025-03-13 08:55:47 +0000

    DMP-4868: Updates per formal release of v72.6

--- src/main/resources/db/migration/common/V1_463__v10_object_state_record_interim.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE object_state_record ALTER COLUMN eod_id TYPE INTEGER USING (eod_id::integer);
-ALTER TABLE object_state_record ALTER COLUMN arm_eod_id TYPE INTEGER USING (arm_eod_id::integer);
---
+++ src/main/resources/db/migration/common/V1_464__v20_retention.sql
@@ -0,0 +1,8 @@
+ALTER TABLE case_overflow ADD COLUMN c_case_id CHARACTER VARYING(32);
+ALTER TABLE case_overflow ADD COLUMN c_courthouse CHARACTER VARYING(64);
+ALTER TABLE case_overflow ADD COLUMN c_type CHARACTER VARYING(32);
+ALTER TABLE case_overflow ADD COLUMN c_upload_priority INTEGER;
+ALTER TABLE case_overflow ADD COLUMN c_reporting_restrictions CHARACTER VARYING(128);
+ALTER TABLE case_overflow ADD COLUMN case_object_name CHARACTER VARYING(255);
+ALTER TABLE case_overflow ADD COLUMN r_folder_path CHARACTER VARYING(740);
+ALTER TABLE case_overflow ADD COLUMN c_interpreter_used INTEGER;
