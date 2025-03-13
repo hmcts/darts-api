2458fe4330115e2f9011e60669a7f8c46334746b
karen-hedges <133129444+karen-hedges@users.noreply.github.com>
2025-03-13 12:24:06 +0000

    Dmp 4873 performance testing investigate azcopy command line parameter options (#2685)
    Co-authored-by: hmcts-jenkins-cnp <60659747+hmcts-jenkins-cnp[bot]@users.noreply.github.com>

--- src/main/java/uk/gov/hmcts/darts/util/AzureCopyUtil.java
+++ src/main/java/uk/gov/hmcts/darts/util/AzureCopyUtil.java
@@ -1,26 +1,32 @@
 package uk.gov.hmcts.darts.util;
 
+import io.micrometer.common.util.StringUtils;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.darts.common.exception.DartsException;
 import uk.gov.hmcts.darts.datamanagement.config.DataManagementConfiguration;
 
+import java.io.IOException;
 import java.time.Duration;
 import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
 
 @Slf4j
 @Component
 @RequiredArgsConstructor
 public class AzureCopyUtil {
 
+    private static final int SUCCESS_EXIT_CODE = 0;
+    private static final String COPY_COMMAND = "copy";
     private final DataManagementConfiguration config;
 
     public void copy(String source, String destination) {
         try {
+            List<String> command = buildCommand(source, destination);
             ProcessBuilder builder = new ProcessBuilder();
-            builder.command(config.getAzCopyExecutable(), "copy", source, destination, config.getAzCopyPreserveAccessTier());
-
+            builder.command(command);
             var startTime = Instant.now();
             log.info("Copy of blob started at {}", startTime);
             builder.redirectErrorStream(true);
@@ -29,22 +35,44 @@ public class AzureCopyUtil {
             var endTime = Instant.now();
             log.info("Copy of blob completed at {}. Total duration in seconds: {}. Exit value: {}",
                      endTime, Duration.between(startTime, endTime).getSeconds(), exitValue);
-            if (exitValue != 0) {
-                String result = new String(process.getInputStream().readAllBytes());
-                String errorMessage = String.format(
-                    "Failed to execute azcopy from source: '%s' to destination '%s'- error exit value. Command: '%s'. Result: %s",
-                    source,
-                    destination,
-                    builder.command(),
-                    result);
-                log.error(errorMessage);
-                throw new DartsException(errorMessage);
+            if (exitValue != SUCCESS_EXIT_CODE) {
+                handleError(source, destination, process, builder);
             }
         } catch (InterruptedException ie) {
             Thread.currentThread().interrupt();
-            throw new DartsException("Failed to execute azure copy - interrupted", ie);
+            throw new DartsException("Failed to execute azure copy - interrupted ", ie);
         } catch (Exception e) {
-            throw new DartsException("Failed to execute azure copy", e);
+            throw new DartsException("Failed to execute azure copy ", e);
         }
+    }
+
+    private void handleError(String source, String destination, Process process, ProcessBuilder builder) throws IOException {
+        String result = new String(process.getInputStream().readAllBytes());
+        String errorMessage = String.format(
+            "Failed to execute azcopy from source: '%s' to destination '%s'- error exit value. Command: '%s'. Result: %s",
+            source,
+            destination,
+            builder.command(),
+            result);
+        log.error(errorMessage);
+        throw new DartsException(errorMessage);
+    }
+
+    List<String> buildCommand(String source, String destination) {
+        List<String> command = new ArrayList<>();
+        command.add(config.getAzCopyExecutable());
+        command.add(COPY_COMMAND);
+        command.add(source);
+        command.add(destination);
+        if (StringUtils.isNotEmpty(config.getAzCopyPreserveAccessTier())) {
+            command.add(config.getAzCopyPreserveAccessTier());
+        }
+        if (StringUtils.isNotEmpty(config.getAzCopyLogLevel())) {
+            command.add(config.getAzCopyLogLevel());
+        }
+        if (StringUtils.isNotEmpty(config.getAzCopyOutputLevel())) {
+            command.add(config.getAzCopyOutputLevel());
+        }
+        return command;
     }
 }
--- src/test/java/uk/gov/hmcts/darts/util/AzureCopyUtilTest.java
+++ src/test/java/uk/gov/hmcts/darts/util/AzureCopyUtilTest.java
@@ -2,25 +2,31 @@ package uk.gov.hmcts.darts.util;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import uk.gov.hmcts.darts.common.exception.DartsException;
 import uk.gov.hmcts.darts.datamanagement.config.DataManagementConfiguration;
 
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class AzureCopyUtilTest {
 
+    private static final String NOT_EXISTING_PATH_AZCOPY = "not/existing/path/azcopy";
     @Mock
-    DataManagementConfiguration configuration;
+    private DataManagementConfiguration configuration;
 
     @Test
-    void testExceptionMessageDoesNotIncludeSourceOrDestinationInfoToAvoidSecretsLeak() {
+    void copy_withExceptionMessageDoesNotIncludeSourceOrDestinationInfoToAvoidSecretsLeak() {
 
         AzureCopyUtil azureCopyUtil = new AzureCopyUtil(configuration);
-        when(configuration.getAzCopyExecutable()).thenReturn("not/existing/path/azcopy");
+        when(configuration.getAzCopyExecutable()).thenReturn(NOT_EXISTING_PATH_AZCOPY);
         String sourceSasUrl = "someSasUrl";
         String destinationSasUrl = "someOtherSasUrl";
 
@@ -32,7 +38,7 @@ class AzureCopyUtilTest {
     }
 
     @Test
-    void testInterruptedExceptionMessageDoesNotIncludeSourceOrDestinationInfoToAvoidSecretsLeak() {
+    void copy_withInterruptedExceptionMessageDoesNotIncludeSourceOrDestinationInfoToAvoidSecretsLeak() {
 
         AzureCopyUtil azureCopyUtil = new AzureCopyUtil(configuration);
         when(configuration.getAzCopyExecutable()).thenAnswer(invocation -> {
@@ -48,4 +54,63 @@ class AzureCopyUtilTest {
             .hasMessageNotContaining(destinationSasUrl);
     }
 
+    @Test
+    void copy_withPreserveAccessTier() {
+        // given
+        AzureCopyUtil azureCopyUtil = new AzureCopyUtil(configuration);
+
+        when(configuration.getAzCopyExecutable()).thenReturn(NOT_EXISTING_PATH_AZCOPY);
+        String preserveAccessTier = "--s2s-preserve-access-tier=false";
+        when(configuration.getAzCopyPreserveAccessTier()).thenReturn(preserveAccessTier);
+
+        String sourceSasUrl = "someSasUrl";
+        String destinationSasUrl = "someOtherSasUrl";
+
+        // when
+        List<String> results = azureCopyUtil.buildCommand(sourceSasUrl, destinationSasUrl);
+
+        // then
+        assertThat(results).contains(preserveAccessTier);
+    }
+
+    @Test
+    void copy_withLogLevel() {
+        AzureCopyUtil azureCopyUtil = new AzureCopyUtil(configuration);
+
+        when(configuration.getAzCopyExecutable()).thenReturn(NOT_EXISTING_PATH_AZCOPY);
+        String logLevel = "--log-level=ERROR";
+        when(configuration.getAzCopyLogLevel()).thenReturn(logLevel);
+
+        String sourceSasUrl = "someSasUrl";
+        String destinationSasUrl = "someOtherSasUrl";
+
+        // when
+        List<String> results = azureCopyUtil.buildCommand(sourceSasUrl, destinationSasUrl);
+
+        // then
+        assertThat(results).contains(logLevel);
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+        "--output-level=quiet",
+        "--output-level=essential",
+        "--output-level=default"
+    })
+    void copy_withOutputLevel(String outputLevel) {
+        // given
+        AzureCopyUtil azureCopyUtil = new AzureCopyUtil(configuration);
+
+        when(configuration.getAzCopyExecutable()).thenReturn(NOT_EXISTING_PATH_AZCOPY);
+        when(configuration.getAzCopyOutputLevel()).thenReturn(outputLevel);
+
+        String sourceSasUrl = "someSasUrl";
+        String destinationSasUrl = "someOtherSasUrl";
+
+        // when
+        List<String> results = azureCopyUtil.buildCommand(sourceSasUrl, destinationSasUrl);
+
+        // then
+        assertThat(results).contains(outputLevel);
+    }
 }
