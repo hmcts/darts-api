4afd9e21d95cb54c49c576092dc49f445fd28c1c
karen-hedges <133129444+karen-hedges@users.noreply.github.com>
2025-03-17 14:49:47 +0000

    DMP-4906 New Courthouse (Burnley CC Sitting at Burnley MC) to be added to modernised DARTS
    Added legacy courthouse Burnley CC Sitting at Burnley MC and associated security groups

--- src/main/resources/db/migration/postgres/V1_466__insert_legacy_courthouse_burnley_cc_sitting_at_burnley_mc.sql
+++ src/main/resources/db/migration/postgres/V1_466__insert_legacy_courthouse_burnley_cc_sitting_at_burnley_mc.sql
@@ -1,5 +1,5 @@
 INSERT INTO courthouse (cth_id, courthouse_name, display_name, folder_path, courthouse_object_id, created_by, created_ts, last_modified_by, last_modified_ts)
-VALUES (nextVal('cth_seq'), 'BURNLEY CC SITTING AT BURNLEY MC', 'Burnley CC Sitting at Burnley MC', '/Area 2/Burnley CC Sitting at Burnley MC', '0b1707589ae8caf3', 0, current_timestamp, 0, current_timestamp);
+VALUES (nextVal('cth_seq'), 'BURNLEY CC SITTING AT BURNLEY MC', 'Burnley CC Sitting at Burnley MC', '/Area 2/Burnley CC sitting at Burnley MC', '0b1707589afa4018', 0, current_timestamp, 0, current_timestamp);
 
 INSERT INTO revinfo (rev, revtstmp, audit_user)
     VALUES (nextval('revinfo_seq'), EXTRACT(EPOCH FROM NOW()), 0);
@@ -25,7 +25,7 @@ INSERT INTO courthouse_region_ae_aud
 
 
 INSERT INTO security_group (grp_id, rol_id, group_name, display_name, global_access, display_state, use_interpreter, dm_group_s_object_id, group_display_name, created_by, created_ts, last_modified_by, last_modified_ts)
-VALUES (nextval('grp_seq'), 2, 'moj_ch_burnley_cc_at_bu_staff', 'Burnley CC Sitting at Burnley MC Requester', false, true, false, '121707588000d100', '0b1707589ae8caf3_staff', 0, current_timestamp, 0, current_timestamp);
+VALUES (nextval('grp_seq'), 2, 'moj_ch_burnley_cc_sitt_staff', 'Burnley CC sitting at Burnley MC Requestor', false, true, false, '121707588000d500', '0b1707589afa4018_staff', 0, current_timestamp, 0, current_timestamp);
 
 INSERT INTO revinfo (rev, revtstmp, audit_user)
     VALUES (nextval('revinfo_seq'), EXTRACT(EPOCH FROM NOW()), 0);
@@ -33,11 +33,11 @@ INSERT INTO revinfo (rev, revtstmp, audit_user)
 INSERT INTO security_group_aud (grp_id, group_name, display_name, description, rev, revtype)
     SELECT grp_id, group_name, display_name, null, currval('revinfo_seq'), 0
     FROM security_group
-    WHERE group_name = 'moj_ch_burnley_cc_at_bu_staff';
+    WHERE group_name = 'moj_ch_burnley_cc_sitt_staff';
 
 
 INSERT INTO security_group (grp_id, rol_id, group_name, display_name, global_access, display_state, use_interpreter, dm_group_s_object_id, group_display_name, created_by, created_ts, last_modified_by, last_modified_ts)
-VALUES (nextval('grp_seq'), 3, 'moj_ch_burnley_cc_at_bu_appr', 'Burnley CC Sitting at Burnley MC Approver', false, true, false, '121707588000d101', '0b1707589ae8caf3_appr', 0, current_timestamp, 0, current_timestamp);
+VALUES (nextval('grp_seq'), 3, 'moj_ch_burnley_cc_sitt_appr', 'Burnley CC sitting at Burnley MC Approver', false, true, false, '121707588000d501', '0b1707589afa4018_appr', 0, current_timestamp, 0, current_timestamp);
 
 INSERT INTO revinfo (rev, revtstmp, audit_user)
     VALUES (nextval('revinfo_seq'), EXTRACT(EPOCH FROM NOW()), 0);
@@ -45,12 +45,12 @@ INSERT INTO revinfo (rev, revtstmp, audit_user)
 INSERT INTO security_group_aud (grp_id, group_name, display_name, description, rev, revtype)
     SELECT grp_id, group_name, display_name, null, currval('revinfo_seq'), 0
     FROM security_group
-    WHERE group_name = 'moj_ch_burnley_cc_at_he_appr';
+    WHERE group_name = 'moj_ch_burnley_cc_sitt_appr';
 
 
 INSERT INTO security_group_courthouse_ae (grp_id, cth_id)
 VALUES (
-    (SELECT grp_id FROM security_group WHERE group_name = 'moj_ch_burnley_cc_at_he_staff'),
+    (SELECT grp_id FROM security_group WHERE group_name = 'moj_ch_burnley_cc_sitt_staff'),
     (SELECT cth_id FROM courthouse WHERE courthouse_name = 'BURNLEY CC SITTING AT BURNLEY MC'));
 
 INSERT INTO revinfo (rev, revtstmp, audit_user)
@@ -59,12 +59,12 @@ INSERT INTO revinfo (rev, revtstmp, audit_user)
 INSERT INTO security_group_courthouse_ae_aud
     SELECT grp_id, cth_id, currval('revinfo_seq'), 0 as revtype
     FROM security_group_courthouse_ae
-    WHERE grp_id = (SELECT grp_id FROM security_group WHERE group_name = 'moj_ch_burnley_cc_at_he_staff');
+    WHERE grp_id = (SELECT grp_id FROM security_group WHERE group_name = 'moj_ch_burnley_cc_sitt_staff');
 
 
 INSERT INTO security_group_courthouse_ae (grp_id, cth_id)
 VALUES (
-    (SELECT grp_id FROM security_group WHERE group_name = 'moj_ch_burnley_cc_at_he_appr'),
+    (SELECT grp_id FROM security_group WHERE group_name = 'moj_ch_burnley_cc_sitt_appr'),
     (SELECT cth_id FROM courthouse WHERE courthouse_name = 'BURNLEY CC SITTING AT BURNLEY MC'));
 
 INSERT INTO revinfo (rev, revtstmp, audit_user)
@@ -73,7 +73,7 @@ INSERT INTO revinfo (rev, revtstmp, audit_user)
 INSERT INTO security_group_courthouse_ae_aud
     SELECT grp_id, cth_id, currval('revinfo_seq'), 0 as revtype
     FROM security_group_courthouse_ae
-    WHERE grp_id = (SELECT grp_id FROM security_group WHERE group_name = 'moj_ch_burnley_cc_at_he_appr');
+    WHERE grp_id = (SELECT grp_id FROM security_group WHERE group_name = 'moj_ch_burnley_cc_sitt_appr');
 
 
 INSERT INTO security_group_courthouse_ae (grp_id, cth_id)
