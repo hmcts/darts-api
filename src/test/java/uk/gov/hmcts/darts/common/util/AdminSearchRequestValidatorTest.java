35b80bb27d2852376c041353a3c09eef77c4ba3c
karen-hedges <133129444+karen-hedges@users.noreply.github.com>
2025-03-21 14:54:58 +0000

    DMP-4833 Restrict the search in admin portal to improve performance
    Added restriction so admin search must have defined case id or courthouse ids and hearing date start and end

--- src/test/java/uk/gov/hmcts/darts/common/util/AdminSearchRequestValidatorTest.java
+++ src/test/java/uk/gov/hmcts/darts/common/util/AdminSearchRequestValidatorTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.junit.jupiter.MockitoExtension;
+import uk.gov.hmcts.darts.audio.exception.AudioApiError;
 import uk.gov.hmcts.darts.common.exception.DartsApiError;
 import uk.gov.hmcts.darts.common.exception.DartsApiException;
 import uk.gov.hmcts.darts.common.model.PostAdminSearchRequest;
@@ -13,8 +14,8 @@ import java.time.LocalDate;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.mockito.Mockito.mock;
 
 
 @ExtendWith(MockitoExtension.class)
@@ -32,9 +33,8 @@ class AdminSearchRequestValidatorTest {
         validator.hearingDatesMaxSearchPeriod = "P1Y";
 
         request = PostAdminSearchRequest.builder().build();
-        criteriaTooBroad = mock(DartsApiError.class);
-        invalidRequest = mock(DartsApiError.class);
-
+        criteriaTooBroad = AudioApiError.CRITERIA_TOO_BROAD;
+        invalidRequest = AudioApiError.INVALID_REQUEST;
     }
 
     @Test
@@ -44,9 +44,9 @@ class AdminSearchRequestValidatorTest {
     }
 
     @Test
-    void validate_shouldThrowException_WhenCourthouseIdsAreEmpty() {
+    void validate_shouldThrowException_WhenCourthouseIdsAndCaseNumberAreEmpty() {
         request.setCaseNumber("");
-        request.setCourthouseIds(null);
+        request.setCourthouseIds(List.of());
 
         assertThrows(DartsApiException.class, () -> validator.validate(request, criteriaTooBroad, invalidRequest));
     }
@@ -56,7 +56,9 @@ class AdminSearchRequestValidatorTest {
         request.setHearingStartAt(LocalDate.of(2023, 1, 2));
         request.setHearingEndAt(LocalDate.of(2023, 1, 1));
 
-        assertThrows(DartsApiException.class, () -> validator.validate(request, criteriaTooBroad, invalidRequest));
+        var exception = assertThrows(DartsApiException.class, () -> validator.validate(request, criteriaTooBroad, invalidRequest));
+
+        assertEquals("The request is not valid. The hearing start date cannot be after the end date.", exception.getMessage());
     }
 
     @Test
@@ -65,14 +67,15 @@ class AdminSearchRequestValidatorTest {
         request.setHearingStartAt(LocalDate.of(2022, 1, 1));
         request.setHearingEndAt(LocalDate.of(2023, 2, 1));
 
-        assertThrows(DartsApiException.class, () -> validator.validate(request, criteriaTooBroad, invalidRequest));
+        var exception = assertThrows(DartsApiException.class, () -> validator.validate(request, criteriaTooBroad, invalidRequest));
+        assertEquals("The request is not valid. The time between the start and end date cannot be more than 12 months", exception.getMessage());
     }
 
     @Test
     void validate_shouldNotThrowException_WhenHearingPeriodIsWithinMaxSearchPeriod() {
         request.setCourthouseIds(List.of(1));
         request.setHearingStartAt(LocalDate.of(2022, 1, 2));
-        request.setHearingEndAt(LocalDate.of(2023, 1, 1));
+        request.setHearingEndAt(LocalDate.of(2023, 1, 2));
 
         assertDoesNotThrow(() -> validator.validate(request, criteriaTooBroad, invalidRequest));
     }
