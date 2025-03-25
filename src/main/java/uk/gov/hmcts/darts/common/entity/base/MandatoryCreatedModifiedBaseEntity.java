0adfd13eaa3d318ea7aaf05408818e6b74fef5f4
benedwards <ben.edwards@cgi.com>
2025-03-25 14:40:41 +0000

    Updated lastModifiedBy to hide an incorrect log message indicating last modified was not updated

--- src/main/java/uk/gov/hmcts/darts/common/entity/base/MandatoryCreatedModifiedBaseEntity.java
+++ src/main/java/uk/gov/hmcts/darts/common/entity/base/MandatoryCreatedModifiedBaseEntity.java
@@ -1,11 +1,17 @@
 package uk.gov.hmcts.darts.common.entity.base;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.PostPersist;
+import jakarta.persistence.PostUpdate;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+import jakarta.persistence.Transient;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.Setter;
@@ -43,4 +49,25 @@ public class MandatoryCreatedModifiedBaseEntity extends MandatoryCreatedBaseEnti
         this.skipUserAudit = true;
     }
 
+    @Transient
+    @JsonIgnore
+    @Getter(AccessLevel.NONE)
+    private transient UserAccountEntity tempLastModifiedBy;
+
+    @PrePersist
+    @PreUpdate
+    public void prePersist() {
+        // Store the lastModifiedBy in a temporary variable to be used in postPersist
+        // This is required to prevent the incorrect log messages of
+        // entity was modified but it wont be updated in the database becuase it is marked as updatable = false
+        tempLastModifiedBy = lastModifiedBy;
+        lastModifiedBy = null;
+    }
+
+    @PostPersist
+    @PostUpdate
+    public void postPersist() {
+        lastModifiedBy = tempLastModifiedBy;
+        tempLastModifiedBy = null;
+    }
 }
