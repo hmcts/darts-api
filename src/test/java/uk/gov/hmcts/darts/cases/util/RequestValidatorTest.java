bbf11070e1ed5213cd28ecb0f48f0d8720c6d93f
Ben Edwards <147524406+Ben-Edwards-cgi@users.noreply.github.com>
2025-03-25 11:04:24 +0000

    DMP-4826: Return error when search does not meet minimum characters to prevent performance impacting queries from running (#2732)
    Co-authored-by: Jack Maloney <jack.maloney1@hmcts.net>

--- src/test/java/uk/gov/hmcts/darts/cases/util/RequestValidatorTest.java
+++ src/test/java/uk/gov/hmcts/darts/cases/util/RequestValidatorTest.java
@@ -1,11 +1,18 @@
 package uk.gov.hmcts.darts.cases.util;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import uk.gov.hmcts.darts.cases.exception.CaseApiError;
 import uk.gov.hmcts.darts.cases.model.GetCasesSearchRequest;
 import uk.gov.hmcts.darts.common.exception.DartsApiException;
 
 import java.time.LocalDate;
+import java.util.function.Consumer;
+import java.util.stream.Stream;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
@@ -113,5 +120,48 @@ class RequestValidatorTest {
 
         RequestValidator.validate(request);
     }
+
+    static Stream<Arguments> shortFieldTestProvider() {
+        return Stream.of(
+            Arguments.of("caseNumber", (Consumer<GetCasesSearchRequest>) (request) -> request.setCaseNumber("1")),
+            Arguments.of("courtHouse", (Consumer<GetCasesSearchRequest>) (request) -> request.setCourthouse("1")),
+            Arguments.of("courtRoom", (Consumer<GetCasesSearchRequest>) (request) -> request.setCourtroom("1")),
+            Arguments.of("judgeName", (Consumer<GetCasesSearchRequest>) (request) -> request.setJudgeName("1")),
+            Arguments.of("defendantName", (Consumer<GetCasesSearchRequest>) (request) -> request.setDefendantName("1"))
+        );
+    }
+
+    @ParameterizedTest(name = "All fields with but has value for {0} should not error")
+    @MethodSource("shortFieldTestProvider")
+    void allFields_withShortField_shouldNotError(String field, Consumer<GetCasesSearchRequest> consumer) {
+        GetCasesSearchRequest request = getPopulatedGetCasesSearchRequest();
+        consumer.accept(request);
+        RequestValidator.validate(request);
+    }
+
+    @Test
+    void allFields_withShortEventText_shouldError() {
+        GetCasesSearchRequest request = getPopulatedGetCasesSearchRequest();
+        request.setEventTextContains("1");
+        DartsApiException exception = assertThrows(DartsApiException.class, () -> RequestValidator.validate(request));
+        assertThat(exception.getError()).isEqualTo(CaseApiError.CRITERIA_TOO_BROAD);
+        assertThat(exception.getMessage())
+            .isEqualTo("Search criteria is too broad, please include at least 3 characters.");
+
+    }
+
+    private GetCasesSearchRequest getPopulatedGetCasesSearchRequest() {
+        return GetCasesSearchRequest.builder()
+            .caseNumber("1234")
+            .courthouse("1234")
+            .courtroom("1234")
+            .judgeName("1234")
+            .defendantName("1234")
+            .eventTextContains("1234")
+            .dateFrom(LocalDate.of(2023, 6, 20))
+            .dateTo(LocalDate.of(2023, 6, 20))
+            .build();
+    }
+
 
 }
