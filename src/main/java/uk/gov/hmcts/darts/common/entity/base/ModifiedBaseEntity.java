9363dd5f8ae1b1b70031c1f65938b91b735178c0
benedwards <ben.edwards@cgi.com>
2025-03-28 12:15:17 +0000

    Style

--- src/main/java/uk/gov/hmcts/darts/common/entity/base/ModifiedBaseEntity.java
+++ src/main/java/uk/gov/hmcts/darts/common/entity/base/ModifiedBaseEntity.java
@@ -5,7 +5,6 @@ import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import jakarta.persistence.Transient;
-import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.Setter;
 import org.hibernate.annotations.UpdateTimestamp;
