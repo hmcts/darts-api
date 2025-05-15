f08f3e207b69b457605ca263cd0fa72d3ba8c52c
Ben Edwards <147524406+Ben-Edwards-cgi@users.noreply.github.com>
2025-05-15 09:16:31 +0100

    DMP-5086: ApplyRetentionCaseAssociatedObjects -> Fails if media_linked_case has a cas_id that is null (#2907)

--- src/test/java/uk/gov/hmcts/darts/retention/service/impl/ApplyRetentionCaseAssociatedObjectsSingleCaseProcessorImplTest.java
+++ src/test/java/uk/gov/hmcts/darts/retention/service/impl/ApplyRetentionCaseAssociatedObjectsSingleCaseProcessorImplTest.java
@@ -282,8 +282,10 @@ class ApplyRetentionCaseAssociatedObjectsSingleCaseProcessorImplTest {
         when(mediaRepository.findAllLinkedByMediaLinkedCaseByCaseId(case1PerfectlyClosed.getId())).thenReturn(List.of(mediaC1));
 
         var mediaC1LinkedToCase1 = createMediaLinkedCase(mediaC1, case5PerfectlyClosed);
-
-        when(mediaLinkedCaseRepository.findByMedia(mediaA1)).thenReturn(List.of(mediaC1LinkedToCase1));
+        //Adding mediaLinkedWithNull case (refering legacy data that has courtroom/casenumber but could not be linked to a case correctly)
+        //Previously this would cause a nullpointer this should not happen anymore
+        var mediaLinkedToNullCase = createMediaLinkedCase(mediaC1, null);
+        when(mediaLinkedCaseRepository.findByMedia(mediaA1)).thenReturn(List.of(mediaC1LinkedToCase1, mediaLinkedToNullCase));
 
         when(caseRetentionRepository.findTopByCourtCaseOrderByRetainUntilAppliedOnDesc(case1PerfectlyClosed)).thenReturn(Optional.of(caseRetentionA1));
         when(caseRetentionRepository.findTopByCourtCaseOrderByRetainUntilAppliedOnDesc(case2PerfectlyClosed)).thenReturn(Optional.of(caseRetentionB1));
