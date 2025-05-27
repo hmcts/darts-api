7602bb43296de7fc26a3353f89e4d15b1aaa7471
Jamie McConnell <jamie.mcconnell@justice.gov.uk>
2025-05-27 15:55:18 +0100

    Added import block for 'darts-api-test' PostgreSQL server

---
+++ infrastructure/import.tf
@@ -0,0 +1,4 @@
+import {
+  to = module.postgresql_flexible.azurerm_postgresql_flexible_server.pgsql_server
+  id = "/subscriptions/3eec5bde-7feb-4566-bfb6-805df6e10b90/resourceGroups/darts-test-rg/providers/Microsoft.DBforPostgreSQL/flexibleServers/darts-api-test"
+}
