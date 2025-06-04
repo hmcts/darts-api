518436921f169c4f114035b03660e02cd4d9bdd8
Jamie McConnell <jamie.mcconnell@justice.gov.uk>
2025-06-04 11:41:15 +0100

    Removed import block for pgsql_principal_admin

--- infrastructure/import.tf
+++
@@ -1,4 +0,0 @@
-import {
-  to = module.postgresql_flexible.azurerm_postgresql_flexible_server_active_directory_administrator.pgsql_principal_admin[0]
-  id = "/subscriptions/3eec5bde-7feb-4566-bfb6-805df6e10b90/resourceGroups/darts-test-rg/providers/Microsoft.DBforPostgreSQL/flexibleServers/darts-api-test/administrators/7ef3b6ce-3974-41ab-8512-c3ef4bb8ae01"
-}
