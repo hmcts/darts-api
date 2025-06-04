5c562eaff80981faeae137f0e8879df217d3f916
karen-hedges <133129444+karen-hedges@users.noreply.github.com>
2025-06-04 19:05:45 +0100

    DMP-5138 ARM RPO search missing EODIds
    Added fix so that when the RPO job runs it always runs at the same time each day and uses the created date time as the basis for the date time

--- src/test/java/uk/gov/hmcts/darts/util/DateTimeHelperTest.java
+++ src/test/java/uk/gov/hmcts/darts/util/DateTimeHelperTest.java
@@ -1,8 +1,12 @@
 package uk.gov.hmcts.darts.util;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.time.OffsetDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
@@ -22,15 +26,24 @@ class DateTimeHelperTest {
         assertNull(DateTimeHelper.getDateTimeIsoFormatted(null));
     }
 
-    @Test
-    void floorToMinutes_ReturnsOffsetDateTimeWithSecondsAndNanosZeroed() {
-        var dateTime = OffsetDateTime.parse("2024-07-31T11:29:56.101701Z");
+    @ParameterizedTest
+    @MethodSource("provideDateTimesForFloorToMinutes")
+    void floorToMinutes_ReturnsOffsetDateTimeWithSecondsAndNanosZeroed(OffsetDateTime dateTime) {
         OffsetDateTime result = DateTimeHelper.floorToMinutes(dateTime);
-        assertEquals("2024-07-31T11:29Z", result.toString());
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSSX");
+        assertEquals("2024-07-31T11:29:00.000Z", result.format(dateTimeFormatter));
     }
 
     @Test
     void floorToMinutes_ReturnsNullDateTimeIsHandled() {
         assertNull(DateTimeHelper.floorToMinutes(null));
     }
+
+    static Stream<OffsetDateTime> provideDateTimesForFloorToMinutes() {
+        return Stream.of(
+            OffsetDateTime.parse("2024-07-31T11:29:56.101701Z"),
+            OffsetDateTime.parse("2024-07-31T11:29:00.00000Z")
+        );
+    }
+
 }
