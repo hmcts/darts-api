d6364399fe4cb86d8e1d3fddbda4d808c4f70213
hemantasharma <hemanta.sharma@hmcts.net>
2025-06-05 14:11:50 +0100

    DMP-5133 : script to reset the status of Failed ARM objects

---
+++ src/main/resources/db/reference/manual-data-fixes/002_failed_arm_jobs_reset_status/reset_status_for_failed_arm_objects.sql
@@ -0,0 +1,20 @@
+begin;
+savepoint dmp5130;
+
+--
+-- pre-check list of EOD records to be updated to set status to 14 (ARM_RAW_DATA_FAILED) and trasnfer_attempts to 1
+--
+SELECT count(*) FROM darts.external_object_directory
+WHERE ors_id IN (14, 17);
+
+--
+--
+--
+UPDATE darts.external_object_directory
+SET ors_id = 14,
+transfer_attempts=1
+WHERE ors_id IN (14, 17);
+
+-- script is currently configured to rollback, if run end-to-end
+rollback to dmp5130;
+--commit;
