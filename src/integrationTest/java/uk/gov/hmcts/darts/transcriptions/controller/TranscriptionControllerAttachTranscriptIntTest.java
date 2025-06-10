19d4b0a977901335fa6925421900def40f998912
Dave Thompson <davethompson21@gmail.com>
2025-06-10 14:15:00 +0100

    Adding more transcript document allowed file/content types
    - to match the file/content types used on legacy

--- src/integrationTest/java/uk/gov/hmcts/darts/transcriptions/controller/TranscriptionControllerAttachTranscriptIntTest.java
+++ src/integrationTest/java/uk/gov/hmcts/darts/transcriptions/controller/TranscriptionControllerAttachTranscriptIntTest.java
@@ -4,6 +4,8 @@ import com.jayway.jsonpath.DocumentContext;
 import com.jayway.jsonpath.JsonPath;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.skyscreamer.jsonassert.JSONAssert;
 import org.skyscreamer.jsonassert.JSONCompareMode;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -122,7 +124,7 @@ class TranscriptionControllerAttachTranscriptIntTest extends IntegrationBase {
     }
 
     @Test
-    void attachTranscriptShouldReturnForbiddenError() throws Exception {
+    void attachTranscript_shouldReturn403Error_whenUserDoesNotHavePermission() throws Exception {
 
         when(mockUserIdentity.getUserAccount()).thenReturn(null);
 
@@ -152,12 +154,12 @@ class TranscriptionControllerAttachTranscriptIntTest extends IntegrationBase {
     }
 
     @Test
-    void attachTranscriptShouldReturnUnprocessableEntityErrorWithFileExtensionTypeBlocked() throws Exception {
+    void attachTranscript_shouldReturn422Error_whenFileExtensionTypeIsNotAllowed() throws Exception {
         setPermissions(authorisationStub.getSeparateIntegrationUser());
 
         MockMultipartFile transcript = new MockMultipartFile(
             "transcript",
-            "Test Document.txt",
+            "Test Document.exe",
             "text/plain",
             "Test Document (txt)".getBytes()
         );
@@ -181,7 +183,36 @@ class TranscriptionControllerAttachTranscriptIntTest extends IntegrationBase {
     }
 
     @Test
-    void attachTranscriptShouldReturnUnprocessableEntityErrorWithFileSizeLimitExceeded() throws Exception {
+    void attachTranscript_shouldReturn422Error_whenContentTypeIsNotAllowed() throws Exception {
+        setPermissions(authorisationStub.getSeparateIntegrationUser());
+
+        MockMultipartFile transcript = new MockMultipartFile(
+            "transcript",
+            "Test Document.txt",
+            "application/octet-stream",
+            "Test Document (txt)".getBytes()
+        );
+
+        final MvcResult mvcResult = mockMvc.perform(
+                multipart(
+                    URL_TEMPLATE,
+                    transcriptionId
+                ).file(transcript))
+            .andExpect(status().isUnprocessableEntity())
+            .andReturn();
+
+        String actualResponse = mvcResult.getResponse().getContentAsString();
+
+        String expectedResponse = """
+            {"type":"TRANSCRIPTION_108","title":"Failed to attach transcript","status":422}
+            """;
+        JSONAssert.assertEquals(expectedResponse, actualResponse, JSONCompareMode.NON_EXTENSIBLE);
+
+        verifyNoInteractions(mockAuditApi);
+    }
+
+    @Test
+    void attachTranscript_shouldReturn422Error_whenFileSizeLimitExceeded() throws Exception {
 
         setPermissions(authorisationStub.getSeparateIntegrationUser());
 
@@ -216,15 +247,30 @@ class TranscriptionControllerAttachTranscriptIntTest extends IntegrationBase {
         dartsDatabase.getUserAccountRepository().save(user);
     }
 
-    @Test
-    void attachTranscriptShouldReturnOkWithMicrosoftWordNew() throws Exception {
+    @ParameterizedTest
+    @CsvSource({
+        "docx,application/vnd.openxmlformats-officedocument.wordprocessingml.document",
+        "doc,application/msword",
+        "dot,application/vnd.openxmlformats-officedocument.wordprocessingml.template",
+        "dotx,application/vnd.openxmlformats-officedocument.wordprocessingml.template",
+        "txt,text/plain",
+        "pdf,application/pdf",
+        "rtf,application/rtf",
+        "zip,application/zip",
+        "zip,application/x-zip-compressed",
+        "odt,application/vnd.oasis.opendocument.text",
+    })
+    void attachTranscript_shouldReturnOk_whenFileTypeAndContentTypeAreAllowed(String fileType, String contentType) throws Exception {
+        var fileName = "Test Document." + fileType;
+        var fileContent = "Test Document (" + fileType + ")";
+
         setPermissions(authorisationStub.getSeparateIntegrationUser());
 
         MockMultipartFile transcript = new MockMultipartFile(
             "transcript",
-            "Test Document.docx",
-            "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
-            "Test Document (docx)".getBytes()
+            fileName,
+            contentType,
+            fileContent.getBytes()
         );
 
         final MvcResult mvcResult = mockMvc.perform(
@@ -260,9 +306,9 @@ class TranscriptionControllerAttachTranscriptIntTest extends IntegrationBase {
         final List<TranscriptionDocumentEntity> transcriptionDocumentEntities = completeTranscriptionEntity.getTranscriptionDocumentEntities();
         assertEquals(1, transcriptionDocumentEntities.size());
         TranscriptionDocumentEntity transcriptionDocumentEntity = transcriptionDocumentEntities.getFirst();
-        assertEquals("Test Document.docx", transcriptionDocumentEntity.getFileName());
+        assertEquals(fileName, transcriptionDocumentEntity.getFileName());
         assertEquals(
-            "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
+            contentType,
             transcriptionDocumentEntity.getFileType()
         );
         assertTrue(transcriptionDocumentEntity.getFileSize() > 0);
@@ -283,77 +329,6 @@ class TranscriptionControllerAttachTranscriptIntTest extends IntegrationBase {
         assertEquals(UNSTRUCTURED.getId(), externalObjectDirectoryUnstructuredEntity.getExternalLocationType().getId());
         assertNotNull(externalObjectDirectoryUnstructuredEntity.getExternalLocation());
         assertEquals(transcriptionDocumentEntity.getChecksum(), externalObjectDirectoryUnstructuredEntity.getChecksum());
-
-        List<NotificationEntity> notificationEntities = dartsDatabase.getNotificationRepository().findAll();
-        List<String> templateList = notificationEntities.stream().map(NotificationEntity::getEventId).toList();
-        assertTrue(templateList.contains(TRANSCRIPTION_AVAILABLE.toString()));
-
-        verify(mockAuditApi).record(COMPLETE_TRANSCRIPTION, authorisationStub.getSeparateIntegrationUser(), transcriptionEntity.getCourtCase());
-        verify(mockAuditApi).record(IMPORT_TRANSCRIPTION, authorisationStub.getSeparateIntegrationUser(), transcriptionEntity.getCourtCase());
-    }
-
-    @Test
-    void attachTranscriptShouldReturnOkWithMicrosoftWordOld() throws Exception {
-        setPermissions(authorisationStub.getSeparateIntegrationUser());
-
-        MockMultipartFile transcript = new MockMultipartFile(
-            "transcript",
-            "Test Document.doc",
-            "application/msword",
-            "Test Document (doc)".getBytes()
-        );
-
-        final MvcResult mvcResult = mockMvc.perform(
-                multipart(
-                    URL_TEMPLATE,
-                    transcriptionId
-                ).file(transcript))
-            .andExpect(status().isOk())
-            .andReturn();
-
-        String actualResponse = mvcResult.getResponse().getContentAsString();
-
-        DocumentContext documentContext = JsonPath.parse(actualResponse);
-        Integer transcriptionDocumentId = documentContext.read("$.transcription_document_id");
-        assertNotNull(transcriptionDocumentId);
-        Integer transcriptionWorkflowId = documentContext.read("$.transcription_workflow_id");
-        assertNotNull(transcriptionWorkflowId);
-
-        final TranscriptionEntity completeTranscriptionEntity = dartsDatabase.getTranscriptionRepository()
-            .findById(transcriptionId).orElseThrow();
-        assertEquals(COMPLETE.getId(), completeTranscriptionEntity.getTranscriptionStatus().getId());
-        final List<TranscriptionWorkflowEntity> transcriptionWorkflowEntities = completeTranscriptionEntity.getTranscriptionWorkflowEntities();
-        final TranscriptionWorkflowEntity transcriptionWorkflowEntity = transcriptionWorkflowEntities
-            .get(transcriptionWorkflowEntities.size() - 1);
-        assertEquals(transcriptionWorkflowId, transcriptionWorkflowEntity.getId());
-        assertEquals(
-            COMPLETE.getId(),
-            transcriptionWorkflowEntity.getTranscriptionStatus().getId()
-        );
-        assertEquals(0, dartsDatabase.getTranscriptionCommentRepository().findAll().size());
-        assertEquals(testUserId, transcriptionWorkflowEntity.getWorkflowActor().getId());
-
-        final List<TranscriptionDocumentEntity> transcriptionDocumentEntities = completeTranscriptionEntity.getTranscriptionDocumentEntities();
-        assertEquals(1, transcriptionDocumentEntities.size());
-        TranscriptionDocumentEntity transcriptionDocumentEntity = transcriptionDocumentEntities.getFirst();
-        assertEquals("Test Document.doc", transcriptionDocumentEntity.getFileName());
-        assertEquals("application/msword", transcriptionDocumentEntity.getFileType());
-        assertTrue(transcriptionDocumentEntity.getFileSize() > 0);
-
-        final List<ExternalObjectDirectoryEntity> externalObjectDirectoryEntities = transcriptionDocumentEntity
-            .getExternalObjectDirectoryEntities();
-        assertEquals(2, externalObjectDirectoryEntities.size());
-        ExternalObjectDirectoryEntity externalObjectDirectoryInboundEntity = externalObjectDirectoryEntities.getFirst();
-        assertEquals(STORED.getId(), externalObjectDirectoryInboundEntity.getStatus().getId());
-        assertEquals(INBOUND.getId(), externalObjectDirectoryInboundEntity.getExternalLocationType().getId());
-        assertNotNull(externalObjectDirectoryInboundEntity.getExternalLocation());
-        assertEquals(transcriptionDocumentEntity.getChecksum(), externalObjectDirectoryInboundEntity.getChecksum());
-
-        ExternalObjectDirectoryEntity externalObjectDirectoryUnstructuredEntity = externalObjectDirectoryEntities.get(1);
-        assertEquals(STORED.getId(), externalObjectDirectoryUnstructuredEntity.getStatus().getId());
-        assertEquals(UNSTRUCTURED.getId(), externalObjectDirectoryUnstructuredEntity.getExternalLocationType().getId());
-        assertNotNull(externalObjectDirectoryUnstructuredEntity.getExternalLocation());
-        assertEquals(externalObjectDirectoryUnstructuredEntity.getChecksum(), externalObjectDirectoryUnstructuredEntity.getChecksum());
 
         List<NotificationEntity> notificationEntities = dartsDatabase.getNotificationRepository().findAll();
         List<String> templateList = notificationEntities.stream().map(NotificationEntity::getEventId).toList();
