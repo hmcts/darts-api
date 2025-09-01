f7890dc72f38129d0befc02d7f3ae143459d2f25
karen-hedges <133129444+karen-hedges@users.noreply.github.com>
2025-09-01 18:11:34 +0100

    DMP-5241 Amend database schema to accommodate Retention validation tables
    Added flyway scripts for retention setup and validation

--- src/main/resources/db/migration/postgres/V1_483_2__add_retention_validation_unique_indexes.sql
+++
@@ -1,8 +0,0 @@
-CREATE UNIQUE INDEX IF NOT EXISTS wk_case_best_values_p1_pk ON wk_case_best_values_p1(cas_id) TABLESPACE pg_default;
-ALTER TABLE IF EXISTS wk_case_best_values_p1 ADD PRIMARY KEY USING INDEX wk_case_best_values_p1_pk;
-
-CREATE UNIQUE INDEX IF NOT EXISTS wk_case_best_values_post_p1_pk ON wk_case_best_values_post_p1(cas_id) TABLESPACE pg_default;
-ALTER TABLE IF EXISTS wk_case_best_values_post_p1 ADD PRIMARY KEY USING INDEX wk_case_best_values_post_p1_pk;
-
-CREATE UNIQUE INDEX IF NOT EXISTS wk_case_activity_data_pk ON wk_case_activity_data(cas_id,closed_date_type) TABLESPACE pg_default;
-ALTER TABLE IF EXISTS wk_case_activity_data ADD PRIMARY KEY USING INDEX wk_case_activity_data_pk;
