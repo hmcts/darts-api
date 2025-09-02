fcf653026f58923398a91ebb54eec12df80eab78
karen-hedges <133129444+karen-hedges@users.noreply.github.com>
2025-09-02 11:34:26 +0100

    DMP-3819 Update POST /admin/users to disallow duplicate email address
    Fixed PMD issue

--- src/main/java/uk/gov/hmcts/darts/common/repository/UserAccountRepository.java
+++ src/main/java/uk/gov/hmcts/darts/common/repository/UserAccountRepository.java
@@ -14,7 +14,6 @@ import java.util.List;
 import java.util.Optional;
 import java.util.Set;
 
-@SuppressWarnings({"PMD.TooManyMethods"})
 @Repository
 public interface UserAccountRepository extends
     RevisionRepository<UserAccountEntity, Integer, Long>,
