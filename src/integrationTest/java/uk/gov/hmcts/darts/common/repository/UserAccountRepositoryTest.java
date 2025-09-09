196a994742d766999d7e902b559b1d1520e87c33
stephenokeefe <Stephen.OKeefe@HMCTS.NET>
2025-09-09 13:30:56 +0100

    Address peer review comments

--- src/integrationTest/java/uk/gov/hmcts/darts/common/repository/UserAccountRepositoryTest.java
+++ src/integrationTest/java/uk/gov/hmcts/darts/common/repository/UserAccountRepositoryTest.java
@@ -1,20 +1,27 @@
 package uk.gov.hmcts.darts.common.repository;
 
 import org.hamcrest.Matchers;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import uk.gov.hmcts.darts.common.entity.UserAccountEntity;
 import uk.gov.hmcts.darts.test.common.data.PersistableFactory;
 import uk.gov.hmcts.darts.testutils.PostgresIntegrationBase;
 import uk.gov.hmcts.darts.testutils.stubs.DartsPersistence;
 
 import java.util.List;
+import java.util.stream.Stream;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.containsInAnyOrder;
 import static org.hamcrest.Matchers.equalTo;
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class UserAccountRepositoryTest extends PostgresIntegrationBase {
 
     @Autowired
@@ -29,10 +36,10 @@ class UserAccountRepositoryTest extends PostgresIntegrationBase {
 
     private UserAccountEntity userAccountEntity3;
 
-    @BeforeEach
+    @BeforeAll
     void setUpData() {
         UserAccountEntity user1 = PersistableFactory.getUserAccountTestData().someMinimalBuilder()
-            .emailAddress("some-user-email")
+            .emailAddress("some.user.email@example.net")
             .build()
             .getEntity();
         dartsPersistence.save(user1);
@@ -40,7 +47,7 @@ class UserAccountRepositoryTest extends PostgresIntegrationBase {
         userAccountEntity1 = user1;
 
         UserAccountEntity user2 = PersistableFactory.getUserAccountTestData().someMinimalBuilder()
-            .emailAddress("some-user-email-2")
+            .emailAddress("some.user.email-2@example.net")
             .build()
             .getEntity();
         dartsPersistence.save(user2);
@@ -48,7 +55,7 @@ class UserAccountRepositoryTest extends PostgresIntegrationBase {
         userAccountEntity2 = user2;
 
         UserAccountEntity user3 = PersistableFactory.getUserAccountTestData().someMinimalBuilder()
-            .emailAddress("some-user-email-3")
+            .emailAddress("some.user.email-3@example.net")
             .isSystemUser(true)
             .build()
             .getEntity();
@@ -59,7 +66,7 @@ class UserAccountRepositoryTest extends PostgresIntegrationBase {
     }
 
     @Test
-    void findUsers_shouldReturnUsers_WhenEmailProvided() {
+    void findUsers_shouldReturnUsers_WhenOnlyEmailProvided() {
         List<UserAccountEntity> users = userAccountRepository.findUsers(
             false, userAccountEntity1.getEmailAddress(), null, null
         );
@@ -69,7 +76,7 @@ class UserAccountRepositoryTest extends PostgresIntegrationBase {
     }
 
     @Test
-    void findUsers_shouldReturnUser_WhenIdProvided() {
+    void findUsers_shouldReturnUser_WhenOnlyIdProvided() {
         List<UserAccountEntity> users = userAccountRepository.findUsers(
             false, null, List.of(userAccountEntity1.getId()), null
         );
@@ -79,17 +86,7 @@ class UserAccountRepositoryTest extends PostgresIntegrationBase {
     }
 
     @Test
-    void findUsers_shouldReturnUser_WhenEmailProvided() {
-        List<UserAccountEntity> users = userAccountRepository.findUsers(
-            false, userAccountEntity1.getEmailAddress(), null, null
-        );
-
-        assertThat(users, Matchers.hasSize(1));
-        assertThat(users.get(0).getEmailAddress(), equalTo(userAccountEntity1.getEmailAddress()));
-    }
-
-    @Test
-    void findUsers_shouldReturnMultipleUsers_WhenIDsProvided() {
+    void findUsers_shouldReturnMultipleUsers_WhenOnlyIDsProvided() {
         List<UserAccountEntity> users = userAccountRepository.findUsers(
             false, null, List.of(userAccountEntity1.getId(), userAccountEntity2.getId()), null
         );
@@ -101,16 +98,28 @@ class UserAccountRepositoryTest extends PostgresIntegrationBase {
     }
 
     @Test
-    void findUsers_shouldNotReturnUsers_WhenNonMatchingEmailProvided() {
+    void findUsers_shouldSortUsersByIdDesc_WhenSortSetToIdDesc() {
         List<UserAccountEntity> users = userAccountRepository.findUsers(
-            false, "non-matching-email", null, null
+            false, null, List.of(userAccountEntity1.getId(), userAccountEntity2.getId()), Sort.by(Sort.Direction.DESC, "id")
+        );
+        assertThat(users, Matchers.hasSize(2));
+        UserAccountEntity firstFoundUser = users.get(0);
+        UserAccountEntity secondFoundUser = users.get(1);
+        assertThat(firstFoundUser.getId(), equalTo(userAccountEntity2.getId()));
+        assertThat(secondFoundUser.getId(), equalTo(userAccountEntity1.getId()));
+    }
+
+    @Test
+    void findUsers_shouldNotReturnUsers_WhenOnlyNonMatchingEmailProvided() {
+        List<UserAccountEntity> users = userAccountRepository.findUsers(
+            false, "non-matching-email@example.net", null, null
         );
 
         assertThat(users, Matchers.hasSize(0));
     }
 
     @Test
-    void findUsers_shouldNotReturnUsers_WhenNonMatchingIdProvided() {
+    void findUsers_shouldNotReturnUsers_WhenOnlyNonMatchingIdProvided() {
         List<UserAccountEntity> users = userAccountRepository.findUsers(
             false, null, List.of(123), null
         );
@@ -136,7 +145,26 @@ class UserAccountRepositoryTest extends PostgresIntegrationBase {
     }
 
     @Test
-    void findUsers_shouldReturnSystemUser_WhenIdProvided() {
+    void findUsers_shouldReturnUser_WhenAllOptionalFieldsProvided() {
+        List<UserAccountEntity> users = userAccountRepository.findUsers(
+            false, userAccountEntity1.getEmailAddress(), List.of(userAccountEntity1.getId()), Sort.by(Sort.Direction.DESC, "id")
+        );
+
+        assertThat(users, Matchers.hasSize(1));
+        assertThat(users.get(0).getId(), equalTo(userAccountEntity1.getId()));
+    }
+
+    @Test
+    void findUsers_shouldNotReturnUser_WhenAllOptionalFieldsIncorrect() {
+        List<UserAccountEntity> users = userAccountRepository.findUsers(
+            false, "non-matching-email@example.net", List.of(123), Sort.by(Sort.Direction.DESC, "id")
+        );
+
+        assertThat(users, Matchers.hasSize(0));
+    }
+
+    @Test
+    void findUsers_shouldReturnSystemUser_WhenOnlyIdProvided() {
         List<UserAccountEntity> users = userAccountRepository.findUsers(
             true, null, List.of(userAccountEntity3.getId()), null
         );
@@ -144,4 +172,34 @@ class UserAccountRepositoryTest extends PostgresIntegrationBase {
         assertThat(users, Matchers.hasSize(1));
         assertThat(users.get(0).getId(), equalTo(userAccountEntity3.getId()));
     }
+
+    @Test
+    void findUsers_shouldReturnSystemUser_WhenOnlyEmailProvided() {
+        List<UserAccountEntity> users = userAccountRepository.findUsers(
+            true, userAccountEntity3.getEmailAddress(), null, null
+        );
+
+        assertThat(users, Matchers.hasSize(1));
+        assertThat(users.get(0).getId(), equalTo(userAccountEntity3.getId()));
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideTestCombinations")
+    void findUsers_shouldFailWithOneNonMatchingField_WhenOtherFieldsMatch(boolean includeSystemUsers, String emailAddress, List<Integer> userIds) {
+        List<UserAccountEntity> users = userAccountRepository.findUsers(
+            includeSystemUsers, emailAddress, userIds, null
+        );
+
+        assertThat(users, Matchers.hasSize(0));
+    }
+
+    private Stream<Arguments> provideTestCombinations() {
+        return Stream.of(
+            Arguments.of(false, userAccountEntity1.getEmailAddress(), List.of(123)), // Non-matching ID
+            Arguments.of(false, "non-matching-email@example.net", List.of(userAccountEntity1.getId())), // Non-matching Email
+            Arguments.of(false, userAccountEntity3.getEmailAddress(), List.of(userAccountEntity3.getId())), // Include system user false
+            Arguments.of(true, userAccountEntity3.getEmailAddress(), List.of(123)) // Include system user true, non-matching ID
+        );
+    }
+
 }
