382e96a9aea530b3a1aa5c2d61d84ffd255e1a59
karen-hedges <133129444+karen-hedges@users.noreply.github.com>
2025-09-18 15:48:51 +0100

    DMP-5257 Create new endpoint GET /admin/reports
    Added new endpoint to get admin reports that currently only returns not yet implemented

---
+++ src/main/java/uk/gov/hmcts/darts/reports/service/ReportsAdminService.java
@@ -0,0 +1,9 @@
+package uk.gov.hmcts.darts.reports.service;
+
+import uk.gov.hmcts.darts.reports.model.AdminReportsResponseItem;
+
+public interface ReportsAdminService {
+
+    AdminReportsResponseItem getAdminReports();
+
+}
---
+++ src/main/java/uk/gov/hmcts/darts/reports/service/impl/ReportsAdminServiceImpl.java
@@ -0,0 +1,18 @@
+package uk.gov.hmcts.darts.reports.service.impl;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import uk.gov.hmcts.darts.reports.model.AdminReportsResponseItem;
+import uk.gov.hmcts.darts.reports.service.ReportsAdminService;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class ReportsAdminServiceImpl implements ReportsAdminService {
+
+    public AdminReportsResponseItem getAdminReports() {
+        return null;
+    }
+    
+}
