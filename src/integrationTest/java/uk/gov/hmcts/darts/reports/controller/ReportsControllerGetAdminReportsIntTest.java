9adfe2bb74c014837141ec0bef8498590f770ad3
karen-hedges <133129444+karen-hedges@users.noreply.github.com>
2025-09-18 15:48:51 +0100

    DMP-5257 Create new endpoint GET /admin/reports
    Added new endpoint to get admin reports that currently only returns not yet implemented

---
+++ src/integrationTest/java/uk/gov/hmcts/darts/reports/controller/ReportsControllerGetAdminReportsIntTest.java
@@ -0,0 +1,60 @@
+package uk.gov.hmcts.darts.reports.controller;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
+import org.skyscreamer.jsonassert.JSONAssert;
+import org.skyscreamer.jsonassert.JSONCompareMode;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import uk.gov.hmcts.darts.common.enums.SecurityRoleEnum;
+import uk.gov.hmcts.darts.testutils.GivenBuilder;
+import uk.gov.hmcts.darts.testutils.IntegrationBase;
+
+import static org.junit.jupiter.params.provider.EnumSource.Mode.EXCLUDE;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@AutoConfigureMockMvc
+class ReportsControllerGetAdminReportsIntTest extends IntegrationBase {
+
+    private static final String ENDPOINT_URL = "/admin/reports";
+
+    @Autowired
+    private transient MockMvc mockMvc;
+    @Autowired
+    private GivenBuilder given;
+
+    @ParameterizedTest
+    @EnumSource(value = SecurityRoleEnum.class, names = {"SUPER_ADMIN", "SUPER_USER"}, mode = EnumSource.Mode.INCLUDE)
+    void getReportsAdmin_shouldReturn501Error_NotYetImplemented(SecurityRoleEnum role) throws Exception {
+
+        given.anAuthenticatedUserWithGlobalAccessAndRole(role);
+
+        MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.get(ENDPOINT_URL))
+            .andExpect(status().isNotImplemented())
+            .andReturn();
+        
+    }
+
+    @ParameterizedTest
+    @EnumSource(value = SecurityRoleEnum.class, names = {"SUPER_ADMIN", "SUPER_USER"}, mode = EXCLUDE)
+    void getReportsAdmin_shouldReturnForbidden_ForNotAuthorisedUsers(SecurityRoleEnum role) throws Exception {
+        given.anAuthenticatedUserWithGlobalAccessAndRole(role);
+
+        MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.get(ENDPOINT_URL))
+            .andExpect(status().isForbidden())
+            .andReturn();
+
+        var jsonString = mvcResult.getResponse().getContentAsString();
+        JSONAssert.assertEquals(
+            """
+                {
+                  "type": "AUTHORISATION_109",
+                  "title": "User is not authorised for this endpoint",
+                  "status": 403
+                }
+                """, jsonString, JSONCompareMode.STRICT);
+    }
+}
