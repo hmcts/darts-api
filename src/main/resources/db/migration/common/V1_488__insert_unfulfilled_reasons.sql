16074baac290f2fd2ce62564f70af5bb4ad2e4ae
karen-hedges <133129444+karen-hedges@users.noreply.github.com>
2025-09-19 16:44:10 +0100

    DMP-5255 Flyway - Add new 'Unfulfilled' transcription status
    Added unfulfilled transcription status and reason

---
+++ src/main/resources/db/migration/common/V1_488__insert_unfulfilled_reasons.sql
@@ -0,0 +1,4 @@
+INSERT INTO unfulfilled_transcription_reason (utr_id, utr_display_name, is_active) VALUES (1, 'Inaudible', TRUE);
+INSERT INTO unfulfilled_transcription_reason (utr_id, utr_display_name, is_active) VALUES (2, 'No audio / white noise', TRUE);
+INSERT INTO unfulfilled_transcription_reason (utr_id, utr_display_name, is_active) VALUES (3, '1 second audio', TRUE);
+INSERT INTO unfulfilled_transcription_reason (utr_id, utr_display_name, is_active) VALUES (4, 'Other', TRUE);
