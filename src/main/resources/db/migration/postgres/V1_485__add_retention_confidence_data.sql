735a40eb2954aacbc84f54baba5d4035e2f6ea2f
karen-hedges <133129444+karen-hedges@users.noreply.github.com>
2025-09-19 14:42:57 +0100

    DMP-5251 Further changes to the DARTS database schema for Retention Migration
    Added extra retention migration data

--- src/main/resources/db/migration/postgres/V1_485__add_retention_confidence_data.sql
+++ src/main/resources/db/migration/postgres/V1_485__add_retention_confidence_data.sql
@@ -11,24 +11,24 @@ DROP TABLE IF EXISTS case_retention_extra;
 ALTER TABLE IF EXISTS cr_dets ALTER COLUMN retention_object_id TYPE CHARACTER VARYING(16);
 
 -- WK_CASE_ACTIVITY_DATA
-ALTER TABLE wk_case_activity_data DROP COLUMN rownum;
-ALTER TABLE wk_case_activity_data ALTER COLUMN closed_date_type CHARACTER VARYING(100);
+ALTER TABLE IF EXISTS wk_case_activity_data DROP COLUMN rownum;
+ALTER TABLE IF EXISTS wk_case_activity_data ALTER COLUMN closed_date_type TYPE CHARACTER VARYING(100);
 
 -- WK_CASE_CONFIDENCE_LEVEL
-CREATE TABLE wk_case_confidence_level
+CREATE TABLE IF NOT EXISTS wk_case_confidence_level
 (
 	cas_id                         INTEGER         NOT NULL,
 	confidence_level               INTEGER
 );
 
-CREATE UNIQUE INDEX wk_case_confidence_level_pk ON wk_case_confidence_level(cas_id);
-ALTER TABLE wk_case_confidence_level ADD PRIMARY KEY USING INDEX  wk_case_confidence_level_pk;
+CREATE UNIQUE INDEX IF NOT EXISTS wk_case_confidence_level_pk ON wk_case_confidence_level(cas_id);
+ALTER TABLE IF EXISTS wk_case_confidence_level ADD PRIMARY KEY USING INDEX  wk_case_confidence_level_pk;
 
 -- WK_CASE_CORRECTION
-ALTER TABLE wk_case_correction ADD COLUMN cmr_eve_id BIGINT;
+ALTER TABLE IF EXISTS wk_case_correction ADD COLUMN cmr_eve_id BIGINT;
 
 --WK_CRAH_IS_CURRENT_BY_CREATION
-CREATE TABLE wk_crah_is_current_by_creation
+CREATE TABLE IF NOT EXISTS wk_crah_is_current_by_creation
 (
 cas_id                         INTEGER
 ,rah_id                         INTEGER
@@ -45,11 +45,11 @@ cas_id                         INTEGER
 ,is_current                     BOOLEAN
 );
 
-CREATE UNIQUE INDEX wk_crah_is_current_by_creation_pk ON wk_crah_is_current_by_creation(cas_id);
-ALTER TABLE wk_crah_is_current_by_creation ADD PRIMARY KEY USING INDEX wk_crah_is_current_by_creation_pk;
+CREATE UNIQUE INDEX IF NOT EXISTS wk_crah_is_current_by_creation_pk ON wk_crah_is_current_by_creation(cas_id);
+ALTER TABLE IF EXISTS wk_crah_is_current_by_creation ADD PRIMARY KEY USING INDEX wk_crah_is_current_by_creation_pk;
 
 -- WK_CRAH_IS_CURRENT_BY_LOGIC
-CREATE TABLE wk_crah_is_current_by_logic
+CREATE TABLE IF NOT EXISTS wk_crah_is_current_by_logic
 (
 cas_id                         INTEGER
 ,rah_id                         INTEGER
@@ -65,12 +65,13 @@ cas_id                         INTEGER
 ,is_current                     BOOLEAN
 );
 
-CREATE UNIQUE INDEX wk_crah_is_current_by_logic_pk ON wk_crah_is_current_by_logic(cas_id);
-ALTER TABLE wk_crah_is_current_by_logic ADD PRIMARY KEY USING INDEX wk_crah_is_current_by_logic_pk;
+CREATE UNIQUE INDEX IF NOT EXISTS wk_crah_is_current_by_logic_pk ON wk_crah_is_current_by_logic(cas_id);
+ALTER TABLE IF EXISTS wk_crah_is_current_by_logic ADD PRIMARY KEY USING INDEX wk_crah_is_current_by_logic_pk;
 
 -- WK_RR_IS_CURRENT_BY_CREATION
-CREATE TABLE wk_rr_is_current_by_creation
-(cas_id                         INTEGER
+CREATE TABLE IF NOT EXISTS wk_rr_is_current_by_creation
+(
+cas_id                         INTEGER
 ,rpr_id                         INTEGER
 ,rpt_id                         INTEGER
 ,case_object_id                 CHARACTER VARYING(16)
@@ -83,11 +84,11 @@ CREATE TABLE wk_rr_is_current_by_creation
 ,is_current                     BOOLEAN
 );
 
-CREATE UNIQUE INDEX wk_rr_is_current_by_creation_pk ON wk_rr_is_current_by_creation(cas_id);
-ALTER TABLE wk_rr_is_current_by_creation ADD PRIMARY KEY USING INDEX wk_rr_is_current_by_creation_pk;
+CREATE UNIQUE INDEX IF NOT EXISTS wk_rr_is_current_by_creation_pk ON wk_rr_is_current_by_creation(cas_id);
+ALTER TABLE IF EXISTS wk_rr_is_current_by_creation ADD PRIMARY KEY USING INDEX wk_rr_is_current_by_creation_pk;
 
 -- WK_RR_IS_CURRENT_BY_LOGIC
-CREATE TABLE wk_rr_is_current_by_logic
+CREATE TABLE IF NOT EXISTS wk_rr_is_current_by_logic
 (cas_id                         INTEGER
 ,rpr_id                         INTEGER
 ,rpt_id                         INTEGER
@@ -100,5 +101,5 @@ CREATE TABLE wk_rr_is_current_by_logic
 ,is_current                     BOOLEAN
 );
 
-CREATE UNIQUE INDEX wk_rr_is_current_by_logic_pk ON wk_rr_is_current_by_logic(cas_id);
-ALTER TABLE wk_rr_is_current_by_logic ADD PRIMARY KEY USING INDEX wk_rr_is_current_by_logic_pk;
+CREATE UNIQUE INDEX IF NOT EXISTS wk_rr_is_current_by_logic_pk ON wk_rr_is_current_by_logic(cas_id);
+ALTER TABLE IF EXISTS wk_rr_is_current_by_logic ADD PRIMARY KEY USING INDEX wk_rr_is_current_by_logic_pk;
