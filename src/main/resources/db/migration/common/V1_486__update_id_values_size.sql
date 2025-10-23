5caf05e4740a50af852e8af567a0df5128e99c49
karen-hedges <133129444+karen-hedges@users.noreply.github.com>
2025-10-23 10:45:55 +0100

    DMP-5282 Database - Data type consistency check
    Fixed bug that was introduced when certain ids where converted to bigints in the db but where they were used as foreign keys in other tables, they were still integers, which caused a mismatch between longs in java and integers in the DB

--- src/main/resources/db/migration/common/V1_486__update_id_values_size.sql
+++ src/main/resources/db/migration/common/V1_486__update_id_values_size.sql
@@ -1,13 +1,6 @@
 ALTER TABLE annotation_document ALTER COLUMN ado_id type bigint;
-ALTER TABLE external_object_directory ALTER COLUMN ado_id type bigint;
-ALTER TABLE object_admin_action ALTER COLUMN ado_id type bigint;
-
 ALTER TABLE case_document ALTER COLUMN cad_id type bigint;
-ALTER TABLE external_object_directory ALTER COLUMN cad_id type bigint;
-ALTER TABLE object_admin_action ALTER COLUMN cad_id type bigint;
-
 ALTER TABLE case_management_retention ALTER COLUMN eve_id type bigint;
-
 ALTER TABLE transcription ALTER COLUMN tra_id type bigint;
 ALTER TABLE transcription_document ALTER COLUMN tra_id type bigint;
 ALTER TABLE transcription_aud ALTER COLUMN tra_id type bigint;
@@ -18,10 +11,16 @@ ALTER TABLE transcription_workflow ALTER COLUMN tra_id type bigint;
 ALTER TABLE transcription_workflow_aud ALTER COLUMN tra_id type bigint;
 ALTER TABLE case_transcription_ae ALTER COLUMN tra_id type bigint;
 ALTER TABLE hearing_transcription_ae ALTER COLUMN tra_id type bigint;
-
 ALTER TABLE transcription_document ALTER COLUMN trd_id type bigint;
 ALTER TABLE object_retrieval_queue ALTER COLUMN trd_id type bigint;
-ALTER TABLE object_admin_action ALTER COLUMN trd_id type bigint;
-ALTER TABLE external_object_directory ALTER COLUMN trd_id type bigint;
-
 ALTER TABLE transient_object_directory ALTER COLUMN tod_id type bigint;
+
+ALTER TABLE object_admin_action
+    ALTER COLUMN ado_id TYPE bigint,
+    ALTER COLUMN cad_id TYPE bigint,
+    ALTER COLUMN trd_id TYPE bigint;
+
+ALTER TABLE external_object_directory
+    ALTER COLUMN ado_id TYPE bigint,
+    ALTER COLUMN cad_id TYPE bigint,
+    ALTER COLUMN trd_id TYPE bigint;
