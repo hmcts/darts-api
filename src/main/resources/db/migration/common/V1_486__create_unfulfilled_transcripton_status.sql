3bd5e1557b5967a6582c9c638943537c6f6101c2
karen-hedges <133129444+karen-hedges@users.noreply.github.com>
2025-10-28 16:39:59 +0000

    DMP-5255 Flyway - Add new 'Unfulfilled' transcription status (#3082)

---
+++ src/main/resources/db/migration/common/V1_486__create_unfulfilled_transcripton_status.sql
@@ -0,0 +1,1 @@
+INSERT INTO transcription_status (trs_id, status_type, display_name) VALUES (8, 'Unfulfilled', 'Unfulfilled');
---
+++ src/main/resources/db/migration/common/V1_487__create_unfulfilled_transcripton_audit_activity.sql
@@ -0,0 +1,2 @@
+INSERT INTO audit_activity (aua_id, activity_name, activity_description, created_ts, created_by, last_modified_ts, last_modified_by)
+VALUES (48, 'Unfulfilled Transcription', 'Unfulfilled Transcription', CURRENT_TIMESTAMP, 0, CURRENT_TIMESTAMP, 0);
