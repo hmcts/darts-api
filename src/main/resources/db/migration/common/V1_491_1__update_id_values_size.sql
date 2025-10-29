9c204e3b3a3f13fac67e76c02e33ce019da687ee
karen-hedges <133129444+karen-hedges@users.noreply.github.com>
2025-10-29 12:24:49 +0000

    DMP-5282 Database - Data type consistency check
    Fixed bug that was introduced when certain ids where converted to bigints in the db but where they were used as foreign keys in other tables, they were still integers, which caused a mismatch between longs in java and integers in the DB

--- src/main/resources/db/migration/common/V1_491_1__update_id_values_size.sql
+++ src/main/resources/db/migration/common/V1_491_1__update_id_values_size.sql
@@ -19,8 +19,3 @@ ALTER TABLE object_admin_action
     ALTER COLUMN ado_id TYPE bigint,
     ALTER COLUMN cad_id TYPE bigint,
     ALTER COLUMN trd_id TYPE bigint;
-
-ALTER TABLE external_object_directory
-    ALTER COLUMN ado_id TYPE bigint,
-    ALTER COLUMN cad_id TYPE bigint,
-    ALTER COLUMN trd_id TYPE bigint;
