49da7abbff25d6cdd2c2897bad89189c3391e041
karen-hedges <133129444+karen-hedges@users.noreply.github.com>
2025-11-07 17:33:41 +0000

    DMP-5305 Retention related schema changes to 3 assurance tables
    Made suggestion retention changes

--- src/main/resources/db/migration/postgres/V1_491__add_retention_assurance_changes.sql
+++ src/main/resources/db/migration/postgres/V1_491__add_retention_assurance_changes.sql
@@ -2,5 +2,5 @@ ALTER TABLE IF EXISTS wk_case_best_values_p1 ALTER COLUMN subtype varchar(30) NU
 ALTER TABLE IF EXISTS wk_case_best_values_post_p1 ALTER COLUMN subtype varchar(30) NULL;
 ALTER TABLE IF EXISTS wk_case_correction ALTER COLUMN closed_date_subtype varchar(30) NULL;
 
-ALTER TABLE IF EXISTS wk_case_best_values_p1 DROP COLUMN rownum;
-ALTER TABLE IF EXISTS wk_case_best_values_post_p1 DROP COLUMN rownum;
+ALTER TABLE IF EXISTS wk_case_best_values_p1 DROP COLUMN 'rownum';
+ALTER TABLE IF EXISTS wk_case_best_values_post_p1 DROP COLUMN 'rownum';
