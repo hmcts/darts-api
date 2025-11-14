aebab546e134f9603df33d3c467b2eb4df7f367b
stephenokeefe <Stephen.OKeefe@HMCTS.NET>
2025-11-14 12:28:19 +0000

    Change batch size and duration

--- src/main/resources/db/migration/postgres/V1_492__removeOldArmRpoProductionsAutomatedTask.sql
+++ src/main/resources/db/migration/postgres/V1_492__removeOldArmRpoProductionsAutomatedTask.sql
@@ -1,6 +1,6 @@
 INSERT INTO automated_task (aut_id, task_name, task_description, cron_expression, cron_editable, batch_size,
                             created_ts, created_by, last_modified_ts, last_modified_by, task_enabled)
-VALUES (nextval('aut_seq'), 'RemoveOldArmRpoProductions', 'Remove old ARM RPO productions', '0 0 22 * * ?', true, 100,
+VALUES (nextval('aut_seq'), 'RemoveOldArmRpoProductions', 'Remove old ARM RPO productions', '0 22 22 * * ?', true, 50,
         current_timestamp, 0, current_timestamp, 0, false);
 
 
