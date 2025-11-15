a0ddfe290721ee7fc4edf3437c8e4abd7635a314
karen-hedges <133129444+karen-hedges@users.noreply.github.com>
2025-11-15 15:21:51 +0000

    DMP-4312-AutomatedJobCleanupDETS
    Added new automated task to clean up the DETS storage and eod record

--- src/main/java/uk/gov/hmcts/darts/task/config/DetsBatchCleanupArmResponseFilesAutomatedTaskConfig.java
+++ src/main/java/uk/gov/hmcts/darts/task/config/DetsBatchCleanupArmResponseFilesAutomatedTaskConfig.java
@@ -10,4 +10,5 @@ import org.springframework.context.annotation.Configuration;
 @Setter
 @Configuration
 public class DetsBatchCleanupArmResponseFilesAutomatedTaskConfig extends AbstractAutomatedTaskConfig {
+    
 }
