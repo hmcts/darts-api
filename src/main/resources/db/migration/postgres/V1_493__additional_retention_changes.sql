bb69266d737b8d62ad31534f409952b8c727c481
karen-hedges <133129444+karen-hedges@users.noreply.github.com>
2025-12-12 12:39:31 +0000

    DMP-5331 Additional DDL changes required for retention preparation tables
    Made additional retention db changes

--- src/main/resources/db/migration/postgres/V1_493__additional_retention_changes.sql
+++ src/main/resources/db/migration/postgres/V1_493__additional_retention_changes.sql
@@ -1,9 +1,23 @@
-ALTER TABLE IF EXISTS wk_case_correction ADD COLUMN ret_conf_score INTEGER NULL;
-ALTER TABLE IF EXISTS wk_case_correction ADD COLUMN ret_conf_reason CHARACTER VARYING NULL;
-ALTER TABLE IF EXISTS wk_case_correction ADD COLUMN merge_action_group INTEGER NULL;
-ALTER TABLE IF EXISTS wk_case_correction ADD COLUMN merge_action_subgroup INTEGER NULL;
+ALTER TABLE IF EXISTS wk_case_correction
+ADD COLUMN IF NOT EXISTS ret_conf_score INTEGER;
 
-ALTER TABLE IF EXISTS retention_process_log DROP COLUMN process_ts;
-ALTER TABLE IF EXISTS retention_process_log ADD COLUMN process_start_ts TIMESTAMP WITH TIME ZONE NULL;
-ALTER TABLE IF EXISTS retention_process_log ADD COLUMN process_end_ts TIMESTAMP WITH TIME ZONE NULL;
-ALTER TABLE IF EXISTS retention_process_log ADD COLUMN is_court_case_updated BOOLEAN;
+ALTER TABLE IF EXISTS wk_case_correction
+ADD COLUMN IF NOT EXISTS ret_conf_reason CHARACTER VARYING;
+
+ALTER TABLE IF EXISTS wk_case_correction
+ADD COLUMN IF NOT EXISTS merge_action_group INTEGER;
+
+ALTER TABLE IF EXISTS wk_case_correction
+ADD COLUMN IF NOT EXISTS merge_action_subgroup INTEGER;
+
+ALTER TABLE IF EXISTS retention_process_log
+DROP COLUMN IF EXISTS processed_ts;
+
+ALTER TABLE IF EXISTS retention_process_log
+ADD COLUMN IF NOT EXISTS process_start_ts TIMESTAMP WITH TIME ZONE;
+
+ALTER TABLE IF EXISTS retention_process_log
+ADD COLUMN IF NOT EXISTS process_end_ts TIMESTAMP WITH TIME ZONE;
+
+ALTER TABLE IF EXISTS retention_process_log
+ADD COLUMN IF NOT EXISTS is_court_case_updated BOOLEAN;
