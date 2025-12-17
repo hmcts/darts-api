384dc48f91d531e6ae346b549600f6f28ed90d06
karen-hedges <133129444+karen-hedges@users.noreply.github.com>
2025-12-17 13:25:46 +0000

    DMP-5342 Set Cron Expressions
    Set the cron expressions for DETS jobs

--- src/main/resources/db/migration/common/V1_496__set_dets_automated_tasks_cron_expressions.sql
+++ src/main/resources/db/migration/common/V1_496__set_dets_automated_tasks_cron_expressions.sql
@@ -1,7 +1,7 @@
--- Run every hour at 9 mins past the hour
+-- Run every hour at 15 mins past the hour
 update automated_task set cron_expression = '0 15 * * * *'
 where task_name = 'DetsToArm';
 
--- Run every hour at 4 mins and 34 mins past the hour
+-- Run every hour at 11 mins and 44 mins past the hour
 update automated_task set cron_expression = '0 11/30 * * * *'
 where task_name = 'ProcessDETSToArmResponse';
