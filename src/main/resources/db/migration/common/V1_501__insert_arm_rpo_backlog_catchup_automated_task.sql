13d98e4e2e2d7a519f22bbc75cc06525861123bb
karen-hedges <133129444+karen-hedges@users.noreply.github.com>
2026-02-06 11:22:44 +0000

    DMP-5381 Add RPO backlog job
    Added automated task to run the ARM RPO backlog

--- src/main/resources/db/migration/common/V1_501__insert_arm_rpo_backlog_catchup_automated_task.sql
+++ src/main/resources/db/migration/common/V1_501__insert_arm_rpo_backlog_catchup_automated_task.sql
@@ -1,6 +1,6 @@
 INSERT INTO automated_task
 (aut_id, task_name, task_description, cron_expression, cron_editable, batch_size, created_ts, created_by, last_modified_ts, last_modified_by, task_enabled)
-VALUES (nextval('aut_seq'), 'ArmRpoBacklogCatchup', 'ARM RPO backlog catchup job', '0 21,51 7-18 * * *', true, 1000, current_timestamp, 0, current_timestamp, 0, false);
+VALUES (nextval('aut_seq'), 'ArmRpoBacklogCatchup', 'ARM RPO backlog catchup job', '0 21,51 5-18 * * *', true, 1000, current_timestamp, 0, current_timestamp, 0, false);
 
 INSERT INTO user_account
 (usr_id, user_name, user_email_address, description, created_ts, last_modified_ts, last_modified_by, created_by,
