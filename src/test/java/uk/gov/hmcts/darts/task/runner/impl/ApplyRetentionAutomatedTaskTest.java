34bba96e9ca1dce9259edb12321f5f4195cc2126
karen-hedges <133129444+karen-hedges@users.noreply.github.com>
2026-02-22 16:02:29 +0000

    DMP-5358 Enhancement - Updating confidence category in case retention table within apply retention automated task
    Updated apply retention to use stage 3 process for calculating retention confidence

--- src/test/java/uk/gov/hmcts/darts/task/runner/impl/ApplyRetentionAutomatedTaskTest.java
+++ src/test/java/uk/gov/hmcts/darts/task/runner/impl/ApplyRetentionAutomatedTaskTest.java
@@ -7,30 +7,36 @@ import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import uk.gov.hmcts.darts.log.api.LogApi;
 import uk.gov.hmcts.darts.retention.service.ApplyRetentionProcessor;
+import uk.gov.hmcts.darts.task.config.ApplyRetentionAutomatedTaskConfig;
 import uk.gov.hmcts.darts.task.service.LockService;
 
 import java.time.Duration;
 
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class ApplyRetentionAutomatedTaskTest {
+    
     @Mock
     private ApplyRetentionProcessor applyRetentionProcessor;
     @Mock
     private LogApi logApi;
     @Mock
     private LockService lockService;
+    @Mock
+    private ApplyRetentionAutomatedTaskConfig applyRetentionAutomatedTaskConfig;
 
     private final Duration daysBetweenEvents = Duration.ofDays(10);
 
     @Test
     void runTask() {
+        when(applyRetentionAutomatedTaskConfig.getDaysBetweenEvents()).thenReturn(daysBetweenEvents);
         ApplyRetentionAutomatedTask applyRetentionAutomatedTaskTest =
             spy(new ApplyRetentionAutomatedTask(
                 null,
-                null,
+                applyRetentionAutomatedTaskConfig,
                 applyRetentionProcessor,
                 logApi,
                 lockService
