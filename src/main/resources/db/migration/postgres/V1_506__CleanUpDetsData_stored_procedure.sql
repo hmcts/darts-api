b2a9196bfc7116d9e479346b7526c97aaf576350
benedwards <ben.edwards@cgi.com>
2026-03-02 09:55:55 +0000

    Migration update

--- src/main/resources/db/migration/postgres/V1_506__CleanUpDetsData_stored_procedure.sql
+++ src/main/resources/db/migration/postgres/V1_506__CleanUpDetsData_stored_procedure.sql
@@ -1,14 +1,3 @@
-INSERT INTO darts.automated_task (aut_id, task_name, task_description, cron_expression, cron_editable, batch_size,
-                                  created_ts, created_by, last_modified_ts, last_modified_by, task_enabled)
-VALUES (36, 'CleanUpDetsData', 'Cleans up Dets files that have successfully been stored in ARM', '0 24 0-6,19-23 ? * *', true, 100_000,
-        current_timestamp, 0, current_timestamp, 0, false);
-
-INSERT INTO user_account (usr_id, user_name, user_email_address, description, created_ts, last_modified_ts, last_modified_by, created_by, is_system_user,
-                          is_active, user_full_name)
-
-VALUES (-40, 'systemCleanUpDetsDataAutomatedTask', 'systemCleanUpDetsDataAutomatedTask@hmcts.net', 'systemCleanUpDetsDataAutomatedTask',
-        '2024-01-01 00:00:00+00', '2024-01-01 00:00:00+00', 0, 0, true, true, 'systemCleanUpDetsDataAutomatedTask');
-
 SET SEARCH_PATH TO darts;
 
 DROP FUNCTION IF EXISTS dets_cleanup_eod_osr_rows;
