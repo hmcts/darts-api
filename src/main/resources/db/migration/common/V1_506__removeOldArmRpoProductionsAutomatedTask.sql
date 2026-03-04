a25ac12b4a071a1936e7d66d327a8e6d30e27126
stephenokeefe <Stephen.OKeefe@HMCTS.NET>
2026-03-04 16:12:55 +0000

    Update user account ID for automated task

--- src/main/resources/db/migration/common/V1_506__removeOldArmRpoProductionsAutomatedTask.sql
+++ src/main/resources/db/migration/common/V1_506__removeOldArmRpoProductionsAutomatedTask.sql
@@ -6,5 +6,5 @@ VALUES (nextval('aut_seq'), 'RemoveOldArmRpoProductions', 'Remove old ARM RPO pr
 
 INSERT INTO user_account (usr_id, user_name, user_email_address, description, created_ts, last_modified_ts, last_modified_by, created_by, is_system_user,
                           is_active, user_full_name)
-VALUES (-44, 'system_RemoveOldArmRpoProductions', 'system_RemoveOldArmRpoProductions@hmcts.net', 'system_RemoveOldArmRpoProductions',
+VALUES (-50, 'system_RemoveOldArmRpoProductions', 'system_RemoveOldArmRpoProductions@hmcts.net', 'system_RemoveOldArmRpoProductions',
         '2024-01-01 00:00:00+00', '2024-01-01 00:00:00+00', 0, 0, true, true, 'system_RemoveOldArmRpoProductions');
