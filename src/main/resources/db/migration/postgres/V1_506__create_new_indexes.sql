493a3247225606c7a3af07baa89435466aed743e
karen-hedges <133129444+karen-hedges@users.noreply.github.com>
2026-03-11 15:08:23 +0000

    DMP-5287 Create new index to improve retention performance
    Added if not exists

--- src/main/resources/db/migration/postgres/V1_506__create_new_indexes.sql
+++ src/main/resources/db/migration/postgres/V1_506__create_new_indexes.sql
@@ -1,1 +1,1 @@
-CREATE INDEX cc_rr_idx ON court_case (retention_retries) WHERE is_retention_updated = true;
+CREATE INDEX IF NOT EXISTS cc_rr_idx ON court_case (retention_retries) WHERE is_retention_updated = true;
