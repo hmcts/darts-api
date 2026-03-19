5b5c48e60db6e9727e7a3f7a18d762964e52f739
karen-hedges <133129444+karen-hedges@users.noreply.github.com>
2026-03-19 14:47:04 +0000

    DMP-5445 Media request expiry state needs to be saved
    Added code to save the media request once set to expired and also fixed an issue with deleting media from hearing

--- src/main/java/uk/gov/hmcts/darts/audio/service/impl/AudioUploadServiceImpl.java
+++ src/main/java/uk/gov/hmcts/darts/audio/service/impl/AudioUploadServiceImpl.java
@@ -35,6 +35,7 @@ import java.time.OffsetDateTime;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
@@ -228,7 +229,8 @@ public class AudioUploadServiceImpl implements AudioUploadService {
 
     @Override
     public void deleteMediaLinkingAndSetCurrentFalse(MediaEntity mediaEntity) {
-        Set<HearingEntity> hearingList = mediaEntity.getHearings();
+        // removeHearing mutates mediaEntity.getHearings(); iterate over a copy to avoid ConcurrentModificationException
+        Set<HearingEntity> hearingList = new HashSet<>(mediaEntity.getHearings());
         for (HearingEntity hearing : hearingList) {
             mediaEntity.removeHearing(hearing);
         }
