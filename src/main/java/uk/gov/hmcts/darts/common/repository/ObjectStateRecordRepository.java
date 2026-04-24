6997abbedd525dcfb4207aecfd552fb7d76295a4
karen-hedges <133129444+karen-hedges@users.noreply.github.com>
2026-04-24 16:50:01 +0100

    DMP-4312 Automated Job to cleanup DETS objects
    Corrected the call to the stored procedure

--- src/main/java/uk/gov/hmcts/darts/common/repository/ObjectStateRecordRepository.java
+++ src/main/java/uk/gov/hmcts/darts/common/repository/ObjectStateRecordRepository.java
@@ -21,7 +21,7 @@ public interface ObjectStateRecordRepository extends JpaRepository<ObjectStateRe
     @Query(value =
         """
             SELECT osr_uuid AS osrUuid, dets_location AS detsLocation
-            FROM dets_cleanup_eod_osr_rows(:limit, CAST(:last_modified_before_ts AS timestamptz))
+            FROM darts.dets_cleanup_eod_osr_rows(:limit, CAST(:last_modified_before_ts AS timestamptz))
             """, nativeQuery = true)
     List<CleanUpDetsDataProcessorImpl.CleanUpDetsProcedureResponse> cleanUpDetsDataProcedure(
         @Param("limit") Integer limit, @Param("last_modified_before_ts") OffsetDateTime lastModifiedBefore);
