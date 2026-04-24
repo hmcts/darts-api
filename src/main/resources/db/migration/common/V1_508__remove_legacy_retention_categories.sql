5eef5b4b445d4d57d32977e1c876bb1caf3b9fca
karen-hedges <133129444+karen-hedges@users.noreply.github.com>
2026-04-24 18:14:37 +0100

    DMP-5531 Update retention confidence categories
    Updated ret_conf_score to 0 for categories, 4, 9 and 10. Deleted 2 digit categories greater than 10.

--- src/main/resources/db/migration/common/V1_508__remove_legacy_retention_categories.sql
+++ src/main/resources/db/migration/common/V1_508__remove_legacy_retention_categories.sql
@@ -1,6 +1,6 @@
 DELETE
 FROM retention_confidence_category_mapper
-where confidence_category between 11 and 100;
+where confidence_category between 11 and 99;
 
 UPDATE retention_confidence_category_mapper
 SET ret_conf_score = 0
