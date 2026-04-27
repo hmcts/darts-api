118fb31a1f27388f437702868c8ec0b07ae3b364
karen-hedges <133129444+karen-hedges@users.noreply.github.com>
2026-04-27 09:56:12 +0100

    DMP-4312 Automated Job to cleanup DETS objects
    Corrected the call to the stored procedure

--- src/main/resources/db/migration/common/V1_509__CleanUpDetsData_automated_task.sql
+++ src/main/resources/db/migration/common/V1_509__CleanUpDetsData_automated_task.sql
@@ -1,6 +1,6 @@
-INSERT INTO darts.automated_task (aut_id, task_name, task_description, cron_expression, cron_editable, batch_size,
-                                  created_ts, created_by, last_modified_ts, last_modified_by, task_enabled)
-VALUES ( nextval('aut_seq'), 'CleanUpDetsData', 'Cleans up Dets files that have successfully been stored in ARM', '0 24 0-6,19-23 ? * *', true, 100_000,
+INSERT INTO automated_task (aut_id, task_name, task_description, cron_expression, cron_editable, batch_size,
+                            created_ts, created_by, last_modified_ts, last_modified_by, task_enabled)
+VALUES (nextval('aut_seq'), 'CleanUpDetsData', 'Cleans up Dets files that have successfully been stored in ARM', '0 24 0-6,19-23 ? * *', true, 100_000,
         current_timestamp, 0, current_timestamp, 0, false);
 
 INSERT INTO user_account (usr_id, user_name, user_email_address, description, created_ts, last_modified_ts, last_modified_by, created_by, is_system_user,
