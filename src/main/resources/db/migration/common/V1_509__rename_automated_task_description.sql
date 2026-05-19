8c0d84118b607a27953c729529c275a24de617dc
karen-hedges <133129444+karen-hedges@users.noreply.github.com>
2026-05-19 09:34:46 +0100

    DMP-5556 SIT Cycle 7 - Automated Task Description for Job Id 4 is incorrect
    Updated task description

--- src/main/resources/db/migration/common/V1_509__rename_automated_task_description.sql
+++ src/main/resources/db/migration/common/V1_509__rename_automated_task_description.sql
@@ -1,3 +1,3 @@
 UPDATE automated_task
-SET set task_description = 'Marks for deletion audio that is stored in inbound that has been successfully been stored in unstructured.'
+SET task_description = 'Marks for deletion audio that is stored in inbound that has been successfully been stored in unstructured.'
 WHERE task_name = 'InboundAudioDeleter';
