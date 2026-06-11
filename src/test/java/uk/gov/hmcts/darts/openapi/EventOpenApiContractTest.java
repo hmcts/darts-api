b2bea97e3d4eeffb4fe5dfb0b4b7a185b4b03047
cgi-jhjsimpson <jon.simpson1@hmcts.net>
2026-06-11 16:00:46 +0100

    Updated string maxLength to 512 for several fields, updated tests and addressed linting

--- src/test/java/uk/gov/hmcts/darts/openapi/EventOpenApiContractTest.java
+++ src/test/java/uk/gov/hmcts/darts/openapi/EventOpenApiContractTest.java
@@ -16,6 +16,107 @@ class EventOpenApiContractTest {
                 .getResource("/openapi/event.yaml")
                 .toExternalForm()
         ).build();
+    private static final String STRING_EXCEEDING_512_CHARS = "a".repeat(513);
+
+    @Test
+    void openApi_ShouldReturnError_WhenMessageIdExceedsMaxLength() {
+        ValidationReport report = VALIDATOR.validateRequest(postEventRequest("""
+            {
+              "message_id": "%s",
+              "type": "10100",
+              "sub_type": "10100",
+              "event_id": "1",
+              "courthouse": "SNARESBROOK",
+              "courtroom": "1",
+              "case_numbers": ["A20230049"],
+              "date_time": "2023-06-14T08:37:30.945Z"
+            }
+            """.formatted(STRING_EXCEEDING_512_CHARS)));
+
+        assertHasMessageContaining(report, "maximum allowed: 512");
+    }
+
+    @Test
+    void openApi_ShouldReturnError_WhenTypeExceedsMaxLength() {
+        ValidationReport report = VALIDATOR.validateRequest(postEventRequest("""
+            {
+              "message_id": "18422",
+              "type": "%s",
+              "sub_type": "10100",
+              "event_id": "1",
+              "courthouse": "SNARESBROOK",
+              "courtroom": "1",
+              "case_numbers": ["A20230049"],
+              "date_time": "2023-06-14T08:37:30.945Z"
+            }
+            """.formatted(STRING_EXCEEDING_512_CHARS)));
+
+        assertHasMessageContaining(report, "maximum allowed: 512");
+    }
+
+    @Test
+    void openApi_ShouldReturnError_WhenSubTypeExceedsMaxLength() {
+        ValidationReport report = VALIDATOR.validateRequest(postEventRequest("""
+            {
+              "message_id": "18422",
+              "type": "10100",
+              "sub_type": "%s",
+              "event_id": "1",
+              "courthouse": "SNARESBROOK",
+              "courtroom": "1",
+              "case_numbers": ["A20230049"],
+              "date_time": "2023-06-14T08:37:30.945Z"
+            }
+            """.formatted(STRING_EXCEEDING_512_CHARS)));
+
+        assertHasMessageContaining(report, "maximum allowed: 512");
+    }
+
+    @Test
+    void openApi_ShouldReturnError_WhenCaseRetentionFixedPolicyExceedsMaxLength() {
+        ValidationReport report = VALIDATOR.validateRequest(postEventRequest("""
+            {
+              "message_id": "18422",
+              "type": "40750",
+              "sub_type": "11504",
+              "event_id": "3",
+              "courthouse": "SNARESBROOK",
+              "courtroom": "1",
+              "case_numbers": ["T20190441"],
+              "event_text": "[Defendant: DEFENDANT ONE]",
+              "date_time": "2023-06-14T08:37:30.945Z",
+              "retention_policy": {
+                "case_retention_fixed_policy": "%s",
+                "case_total_sentence": "26Y0M0D"
+              }
+            }
+            """.formatted(STRING_EXCEEDING_512_CHARS)));
+
+        assertHasMessageContaining(report, "maximum allowed: 512");
+    }
+
+    @Test
+    void openApi_ShouldReturnError_WhenCaseTotalSentenceExceedsMaxLength() {
+        ValidationReport report = VALIDATOR.validateRequest(postEventRequest("""
+            {
+              "message_id": "18422",
+              "type": "40750",
+              "sub_type": "11504",
+              "event_id": "3",
+              "courthouse": "SNARESBROOK",
+              "courtroom": "1",
+              "case_numbers": ["T20190441"],
+              "event_text": "[Defendant: DEFENDANT ONE]",
+              "date_time": "2023-06-14T08:37:30.945Z",
+              "retention_policy": {
+                "case_retention_fixed_policy": "4",
+                "case_total_sentence": "%s"
+              }
+            }
+            """.formatted(STRING_EXCEEDING_512_CHARS)));
+
+        assertHasMessageContaining(report, "maximum allowed: 512");
+    }
 
     @Test
     void openApi_ShouldReturnError_WhenEventTextExceedsMaxLength() {
@@ -31,14 +132,14 @@ class EventOpenApiContractTest {
               "event_text": "%s",
               "date_time": "2023-06-14T08:37:30.945Z"
             }
-            """.formatted("a".repeat(2001))));
+            """.formatted("a".repeat(2049))));
 
-        assertHasMessageContaining(report, "maximum allowed: 2000");
+        assertHasMessageContaining(report, "maximum allowed: 2048");
     }
 
     @Test
     void openApi_ShouldReturnError_WhenTooManyCaseNumbersProvided() {
-        String caseNumbers = ("\"T20190441\",").repeat(100) + "\"T20190441\"";
+        String caseNumbers = "\"T20190441\",".repeat(100) + "\"T20190441\"";
 
         ValidationReport report = VALIDATOR.validateRequest(postEventRequest("""
             {
