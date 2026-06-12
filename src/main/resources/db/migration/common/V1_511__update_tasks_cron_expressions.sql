adc831efc290a489367b4825af028c7efc529dc1
karen-hedges <133129444+karen-hedges@users.noreply.github.com>
2026-06-12 14:41:44 +0100

    DMP-5558 Cron job settings needing added for CaseExpiryDeletion & AssociatedObjectDataExpiryDeletion
    Updated cron expressions so they run at 3am and 4am respectively

--- src/main/resources/db/migration/common/V1_511__update_tasks_cron_expressions.sql
+++ src/main/resources/db/migration/common/V1_511__update_tasks_cron_expressions.sql
@@ -1,9 +1,9 @@
 -- Run every day at 3am
 update automated_task
-set cron_expression = '0 3 * * * *'
+set cron_expression = '0 0 3 * * *'
 where task_name = 'CaseExpiryDeletion';
 
 -- Run every day at 4am
 update automated_task
-set cron_expression = '0 4 * * * *'
+set cron_expression = '0 0 4 * * *'
 where task_name = 'AssociatedObjectDataExpiryDeletion';
