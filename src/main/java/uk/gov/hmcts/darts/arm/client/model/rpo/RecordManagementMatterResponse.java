b7514a34687166667641e8b93bff66f66bca693f
cgi-jhjsimpson <jon.simpson1@hmcts.net>
2026-07-10 11:07:08 +0100

    Fix isUsedForRM duplication.

--- src/main/java/uk/gov/hmcts/darts/arm/client/model/rpo/RecordManagementMatterResponse.java
+++ src/main/java/uk/gov/hmcts/darts/arm/client/model/rpo/RecordManagementMatterResponse.java
@@ -4,7 +4,9 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 import lombok.ToString;
 
 import java.time.OffsetDateTime;
@@ -28,8 +30,12 @@ public class RecordManagementMatterResponse extends BaseRpoResponse {
         private String matterId;
         private String name;
         private boolean isQuickSearch;
+        // These tests read our OpenAPI YAML directly and do not need springdoc.
+        // Exclude springdoc's Swagger jars from the test runtime to avoid duplicate Swagger classes.
+        @Getter(onMethod_ = @JsonProperty("isUsedForRM"))
+        @Setter(onMethod_ = @JsonProperty("isUsedForRM"))
         @JsonProperty("isUsedForRM")
-        private boolean usedForRm;
+        private boolean isUsedForRm;
         private String description;
         private OffsetDateTime createdDate;
         private Integer type;
