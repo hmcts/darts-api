0a89f43207d415bf8ce2c07d9c1f8103d6a5cd86
Neil Perry <142518199+npv0@users.noreply.github.com>
2026-08-18 10:05:14 +0100

    DMP-5573: update postgres module (#3384)

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -74,7 +74,7 @@ module "postgresql_flexible" {
     azurerm.postgres_network = azurerm.postgres_network
   }
 
-  source               = "git@github.com:hmcts/terraform-module-postgresql-flexible?ref=master"
+  source               = "git@github.com:hmcts/terraform-module-postgresql-flexible?ref=DTSPO-30107-additional-postgres-admins"
   env                  = var.env
   product              = var.product
   resource_group_name  = local.rg_name
