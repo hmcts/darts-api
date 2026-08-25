70e5bca984c6049fd2dc7abd7e068ebd24cae24f
cgi-jhjsimpson <jon.simpson1@hmcts.net>
2026-08-25 13:27:38 +0100

    Updated incorrect cron expression.

--- src/main/resources/db/migration/common/V1_517__DisableInactiveUserAccounts_automated_task.sql
+++ src/main/resources/db/migration/common/V1_517__DisableInactiveUserAccounts_automated_task.sql
@@ -2,7 +2,7 @@ INSERT INTO automated_task (aut_id, task_name, task_description, cron_expression
                             created_ts, created_by, last_modified_ts, last_modified_by, task_enabled)
 VALUES (nextval('aut_seq'), 'DisableInactiveUserAccounts',
         'Disable user accounts that have not been active for six months or more and remove the user from any security groups',
-        '0 5 10 * * *', true, 1000, current_timestamp, 0, current_timestamp, 0, false);
+        '0 10 5 * * *', true, 1000, current_timestamp, 0, current_timestamp, 0, false);
 
 INSERT INTO user_account (usr_id, user_name, user_email_address, description, created_ts, last_modified_ts, last_modified_by, created_by, is_system_user,
                           is_active, user_full_name)
