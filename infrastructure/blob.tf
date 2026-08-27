943e6cb7adef4cbae0201e2dd3360c4722d7249f
Neil Perry <142518199+npv0@users.noreply.github.com>
2026-08-27 14:07:18 +0100

    DMP-5678: update to create empty text file (#3390)

--- infrastructure/blob.tf
+++ infrastructure/blob.tf
@@ -33,7 +33,7 @@ module "armsa" {
 
 resource "azurerm_storage_blob" "blob_folder" {
   for_each               = toset(local.blob_folders)
-  name                   = "DARTS/${each.value}/"
+  name                   = "DARTS/${each.value}/.keep"
   storage_account_name   = module.armsa.storageaccount_name
   storage_container_name = "dropzone"
   type                   = "Block"
