64dbd0806bd1cec43a7da3e81b563543426a5ceb
stephenokeefe <Stephen.OKeefe@HMCTS.NET>
2026-09-09 11:58:33 +0100

    Remove unused method, update flyway name

--- src/main/java/uk/gov/hmcts/darts/common/repository/ObjectStateRecordRepository.java
+++ src/main/java/uk/gov/hmcts/darts/common/repository/ObjectStateRecordRepository.java
@@ -1,7 +1,6 @@
 package uk.gov.hmcts.darts.common.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import uk.gov.hmcts.darts.arm.service.impl.CleanUpDetsDataProcessorImpl;
@@ -14,11 +13,7 @@ import java.util.Optional;
 public interface ObjectStateRecordRepository extends JpaRepository<ObjectStateRecordEntity, Long> {
     Optional<ObjectStateRecordEntity> findByArmEodId(long armEodId);
 
-    @Modifying
-    @Query("UPDATE ObjectStateRecordEntity o SET o.flagFileDetsCleanupStatus = true where o.uuid in :uuids")
-    void markDetsCleanupStatusAsComplete(List<Long> uuids);
-
-
+    
     @Query(value = "SELECT osr_uuid AS osrUuid, dets_location AS detsLocation  " +
         "FROM darts.dets_cleanup_eod_osr_rows(:limit, :last_modified_before_ts)", nativeQuery = true)
     List<CleanUpDetsDataProcessorImpl.CleanUpDetsProcedureResponse> cleanUpDetsDataProcedure(
